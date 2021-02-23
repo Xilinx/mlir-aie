b15f7f37950d1c7f6261eeaf8c96335a08ec2df1
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2021-02-23 15:07:44 -0800

    [aie] Refactor lib/Targets/AIETargets.cpp
    This file was getting huge and we'll need to duplicate alot of it for
    XAIEv1 and XAIEv2

---
+++ lib/Targets/AIETargets.h
@@ -0,0 +1,11 @@
+// (c) Copyright 2019 Xilinx Inc. All Rights Reserved.
+
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace xilinx {
+namespace AIE {
+mlir::LogicalResult AIETranslateToXAIEV1(mlir::ModuleOp module, llvm::raw_ostream &output);
+}
+}
