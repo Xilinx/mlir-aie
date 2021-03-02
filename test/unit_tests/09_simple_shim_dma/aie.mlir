526c23f71c3e53dfbc62c6971a67c1788606e759
Jack Lo <jackl@xilinx.com>
2021-03-02 11:52:18 -0800

    Added adapted simple shim dma.

---
+++ test/unit_tests/09_simple_shim_dma/aie.mlir
@@ -0,0 +1,61 @@
+
+// aie-opt --aie-create-flows --aie-find-flows --aie-assign-buffer_addresses %s | aie-translate --aie-generate-xaie 
+
+module {
+  %t70 = AIE.tile(7, 0)
+  %t71 = AIE.tile(7, 1)
+  %t72 = AIE.tile(7, 2)
+
+//  %buffer = AIE.external_buffer 0x20000 : memref<512 x i32>
+  %buffer = AIE.external_buffer 0x020100004000 : memref<512 x i32>
+
+  // Fixup
+  %sw = AIE.switchbox(%t70) {
+    AIE.connect<"South" : 3, "North" : 3>
+  }
+  %mux = AIE.shimmux(%t70) {
+    AIE.connect<"DMA" : 0, "South": 3>
+  }
+
+  %dma = AIE.shimDMA(%t70) {
+    %lock1 = AIE.lock(%t70, 1)
+
+      AIE.dmaStart(MM2S0, ^bd0, ^end)
+
+    ^bd0:
+      AIE.useLock(%lock1, Acquire, 1, 0)
+      AIE.dmaBd(<%buffer : memref<512 x i32>, 0, 512>, 0)
+      AIE.useLock(%lock1, Release, 0, 0)
+      br ^bd0
+    ^end:
+      AIE.end
+  }
+
+  AIE.flow(%t71, "South" : 3, %t72, "DMA" : 0)
+
+  %buf72_0 = AIE.buffer(%t72) : memref<256xi32>
+  %buf72_1 = AIE.buffer(%t72) : memref<256xi32>
+
+  %l72_0 = AIE.lock(%t72, 0)
+  %l72_1 = AIE.lock(%t72, 1)
+
+  %m72 = AIE.mem(%t72) {
+      %srcDma = AIE.dmaStart("S2MM0", ^bd0, ^end)
+    ^bd0:
+      AIE.useLock(%l72_0, "Acquire", 0, 0)
+      AIE.dmaBd(<%buf72_0 : memref<256xi32>, 0, 256>, 0)
+      AIE.useLock(%l72_0, "Release", 1, 0)
+      br ^bd1
+    ^bd1:
+      AIE.useLock(%l72_1, "Acquire", 0, 0)
+      AIE.dmaBd(<%buf72_1 : memref<256xi32>, 0, 256>, 0)
+      AIE.useLock(%l72_1, "Release", 1, 0)
+      br ^bd0
+    ^end:
+      AIE.end
+  }
+
+  
+
+
+}
---
+++ test/unit_tests/09_simple_shim_dma/test.cpp
@@ -0,0 +1,117 @@
+// (c) Copyright 2020 Xilinx Inc. All Rights Reserved.
+
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <thread>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <xaiengine.h>
+
+#define XAIE_NUM_ROWS            8
+#define XAIE_NUM_COLS           50
+#define XAIE_ADDR_ARRAY_OFF     0x800
+
+#define HIGH_ADDR(addr)	((addr & 0xffffffff00000000) >> 32)
+#define LOW_ADDR(addr)	(addr & 0x00000000ffffffff)
+
+namespace {
+
+XAieGbl_Config *AieConfigPtr;	                          /**< AIE configuration pointer */
+XAieGbl AieInst;	                                      /**< AIE global instance */
+XAieGbl_HwCfg AieConfig;                                /**< AIE HW configuration instance */
+XAieGbl_Tile TileInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];  /**< Instantiates AIE array of [XAIE_NUM_COLS] x [XAIE_NUM_ROWS] */
+XAieDma_Tile TileDMAInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];
+
+#include "acdc_project/aie_inc.cpp"
+
+}
+
+void printCoreStatus(int col, int row) {
+
+	
+	u32 status, coreTimerLow, locks;
+	status = XAieGbl_Read32(TileInst[col][row].TileAddr + 0x032004);
+	coreTimerLow = XAieGbl_Read32(TileInst[col][row].TileAddr + 0x0340F8);
+	locks = XAieGbl_Read32(TileInst[col][row].TileAddr + 0x0001EF00);
+	printf("Core [%d, %d] status is %08X, timer is %u, locks are %08X\n",col, row, status, coreTimerLow, locks);
+	for (int lock=0;lock<16;lock++) {
+		u32 two_bits = (locks >> (lock*2)) & 0x3;
+		if (two_bits) {
+			printf("Lock %d: ", lock);
+			u32 acquired = two_bits & 0x1;
+			u32 value = two_bits & 0x2;
+			if (acquired)
+				printf("Acquired ");
+			printf(value?"1":"0");
+			printf("\n");
+		}
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+  auto col = 7;
+
+  size_t aie_base = XAIE_ADDR_ARRAY_OFF << 14;
+  XAIEGBL_HWCFG_SET_CONFIG((&AieConfig), XAIE_NUM_ROWS, XAIE_NUM_COLS, XAIE_ADDR_ARRAY_OFF);
+  XAieGbl_HwInit(&AieConfig);
+  AieConfigPtr = XAieGbl_LookupConfig(XPAR_AIE_DEVICE_ID);
+  XAieGbl_CfgInitialize(&AieInst, &TileInst[0][0], AieConfigPtr);
+
+  printCoreStatus(col, 2);
+
+  // Run auto generated config functions
+
+  mlir_configure_cores();
+  mlir_configure_switchboxes();
+  mlir_initialize_locks();
+  mlir_configure_dmas();
+
+  uint32_t *bram_ptr;
+
+  #define BRAM_ADDR (0x4000+0x020100000000LL)
+  #define DMA_COUNT 512
+
+  int fd = open("/dev/mem", O_RDWR | O_SYNC);
+  if (fd != -1) {
+    bram_ptr = (uint32_t *)mmap(NULL, 0x8000, PROT_READ|PROT_WRITE, MAP_SHARED, fd, BRAM_ADDR);
+    for (int i=0; i<DMA_COUNT; i++) {
+      bram_ptr[i] = i+1;
+      //printf("%p %llx\n", &bram_ptr[i], bram_ptr[i]);
+    }
+  }
+
+  // We're going to stamp over the memory
+  for (int i=0; i<DMA_COUNT; i++) {
+    XAieTile_DmWriteWord(&(TileInst[col][2]), 0x1000+i*4, 0xdeadbeef);
+  }
+
+// Release lock for reading from DDR
+  XAieTile_LockRelease(&(TileInst[7][0]), 1, 1, 0); 
+
+  XAieLib_usleep(1000);
+
+  printCoreStatus(col, 2);
+  
+  int errors = 0;
+  for (int i=0; i<DMA_COUNT; i++) {
+    uint32_t d = XAieTile_DmReadWord(&(TileInst[col][2]), 0x1000+i*4);
+    if (d != (i+1)) {
+      errors++;
+      printf("mismatch %x != 1 + %x\n", d, i);
+    }
+  }
+
+  if (!errors) {
+    printf("PASS!\n");
+  }
+  else {
+    printf("fail %d/%d.\n", (DMA_COUNT-errors), DMA_COUNT);
+  }
+
+}
