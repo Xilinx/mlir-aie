0ca914fe2268b18ad494cc68d0f0407205ad26c6
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2021-08-06 20:33:14 +0000

    Add shim test with chess

---
+++ test/unit_tests/chess_compiler_tests/07_shim_dma_with_core/kernel.cc
@@ -0,0 +1,19 @@
+//===- kernel.cc ------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "kernel.h"
+
+void func(int *a, int *b, int size)
+{
+    int new_size = 64;
+    for (int i = 0; i < 64; i ++){
+        b[i] = a[i];
+    }
+}
