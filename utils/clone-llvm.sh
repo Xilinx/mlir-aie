7a2ac3331c4548b10d5e24532656bad88923a13c
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2021-08-17 15:05:40 -0700

    Add checkout script

---
+++ utils/clone-llvm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+##===- utils/clone-llvm.sh - Build LLVM for github workflow --*- Script -*-===##
+# 
+# This file licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# 
+##===----------------------------------------------------------------------===##
+#
+# This script checks out LLVM.  We use this instead of a git submodule to avoid
+# excessive copies of the LLVM tree.
+#
+##===----------------------------------------------------------------------===##
+
+git clone --depth 1 https://github.com/llvm/llvm-project.git llvm --branch ebe408ad8003
