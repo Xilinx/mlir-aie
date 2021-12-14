3fdd581077f652e1f817fef7305d4470ff87c817
Nishant Mysore <nmysore@xilinx.com>
2021-12-14 10:09:44 -0800

    updated git clang format

--- runtime_lib/test_library.cpp
+++ runtime_lib/test_library.cpp
@@ -11,8 +11,8 @@
 // This file contains common libraries used for testing.
 
 #include "test_library.h"
-#include <stdio.h>
 #include <cmath>
+#include <stdio.h>
 
 extern "C" {
 extern aie_libxaie_ctx_t *ctx /* = nullptr*/;
@@ -481,18 +481,21 @@ void mlir_aie_print_dma_status(aie_libxaie_ctx_t *ctx, int col, int row) {
   u32 mm2s_ch0_running = dma_mm2s_status & 0x3;
   u32 mm2s_ch1_running = (dma_mm2s_status >> 2) & 0x3;
 
-  printf("DMA [%d, %d] mm2s_status/ctrl is %08X %08X, s2mm_status is %08X %08X, BD0_Addr_A is %08X, BD0_control is %08X\n",col, row, dma_mm2s_status, dma_mm2s_control, dma_s2mm_status, dma_s2mm_control, dma_bd0_a, dma_bd0_control);
-  for (int bd=0;bd<8;bd++) {
+  printf("DMA [%d, %d] mm2s_status/ctrl is %08X %08X, s2mm_status is %08X "
+         "%08X, BD0_Addr_A is %08X, BD0_control is %08X\n",
+         col, row, dma_mm2s_status, dma_mm2s_control, dma_s2mm_status,
+         dma_s2mm_control, dma_bd0_a, dma_bd0_control);
+  for (int bd = 0; bd < 8; bd++) {
     u32 dma_bd_addr_a =
         XAieGbl_Read32(tile->TileAddr + 0x0001D000 + (0x20 * bd));
     u32 dma_bd_control =
         XAieGbl_Read32(tile->TileAddr + 0x0001D018 + (0x20 * bd));
     if (dma_bd_control & 0x80000000) {
-      printf("BD %d valid\n",bd);
-      int current_s2mm_ch0 = (dma_s2mm_status >> 16) & 0xf;  
-      int current_s2mm_ch1 = (dma_s2mm_status >> 20) & 0xf;  
-      int current_mm2s_ch0 = (dma_mm2s_status >> 16) & 0xf;  
-      int current_mm2s_ch1 = (dma_mm2s_status >> 20) & 0xf;  
+      printf("BD %d valid\n", bd);
+      int current_s2mm_ch0 = (dma_s2mm_status >> 16) & 0xf;
+      int current_s2mm_ch1 = (dma_s2mm_status >> 20) & 0xf;
+      int current_mm2s_ch0 = (dma_mm2s_status >> 16) & 0xf;
+      int current_mm2s_ch1 = (dma_mm2s_status >> 20) & 0xf;
 
       if (s2mm_ch0_running && bd == current_s2mm_ch0) {
         printf(" * Current BD for s2mm channel 0\n");
@@ -510,47 +513,48 @@ void mlir_aie_print_dma_status(aie_libxaie_ctx_t *ctx, int col, int row) {
       if (dma_bd_control & 0x08000000) {
         u32 dma_packet =
             XAieGbl_Read32(tile->TileAddr + 0x0001D010 + (0x20 * bd));
-        printf("   Packet mode: %02X\n",dma_packet & 0x1F);
+        printf("   Packet mode: %02X\n", dma_packet & 0x1F);
       }
-      int words_to_transfer = 1+(dma_bd_control & 0x1FFF);
-      int base_address = dma_bd_addr_a  & 0x1FFF;
-      printf("   Transfering %d 32 bit words to/from %06X\n",words_to_transfer, base_address);
+      int words_to_transfer = 1 + (dma_bd_control & 0x1FFF);
+      int base_address = dma_bd_addr_a & 0x1FFF;
+      printf("   Transfering %d 32 bit words to/from %06X\n", words_to_transfer,
+             base_address);
 
       printf("   ");
-      for (int w=0;w<7; w++) {
+      for (int w = 0; w < 7; w++) {
         printf("%08X ", XAieTile_DmReadWord(tile, (base_address + w) * 4));
       }
       printf("\n");
       if (dma_bd_addr_a & 0x40000) {
         u32 lock_id = (dma_bd_addr_a >> 22) & 0xf;
-        printf("   Acquires lock %d ",lock_id);
-        if (dma_bd_addr_a & 0x10000) 
-          printf("with value %d ",(dma_bd_addr_a >> 17) & 0x1);
+        printf("   Acquires lock %d ", lock_id);
+        if (dma_bd_addr_a & 0x10000)
+          printf("with value %d ", (dma_bd_addr_a >> 17) & 0x1);
 
         printf("currently ");
         u32 locks = XAieGbl_Read32(tile->TileAddr + 0x0001EF00);
-        u32 two_bits = (locks >> (lock_id*2)) & 0x3;
+        u32 two_bits = (locks >> (lock_id * 2)) & 0x3;
         if (two_bits) {
           u32 acquired = two_bits & 0x1;
           u32 value = two_bits & 0x2;
           if (acquired)
             printf("Acquired ");
-          printf(value?"1":"0");
-        }
-        else printf("0");
+          printf(value ? "1" : "0");
+        } else
+          printf("0");
         printf("\n");
-
       }
       if (dma_bd_control & 0x30000000) { // FIFO MODE
         int FIFO = (dma_bd_control >> 28) & 0x3;
         u32 dma_fifo_counter = XAieGbl_Read32(tile->TileAddr + 0x0001DF20);
-        printf("   Using FIFO Cnt%d : %08X\n",FIFO, dma_fifo_counter);
+        printf("   Using FIFO Cnt%d : %08X\n", FIFO, dma_fifo_counter);
       }
     }
   }
 }
 
-/// Print the status of a core represented by the given tile, at the given coordinates.
+/// Print the status of a core represented by the given tile, at the given
+/// coordinates.
 void mlir_aie_print_tile_status(aie_libxaie_ctx_t *ctx, int col, int row) {
   // int col = tile.ColId;
   // int row = tile.RowId;
@@ -615,8 +619,8 @@ void mlir_aie_print_tile_status(aie_libxaie_ctx_t *ctx, int col, int row) {
 }
 
 static void clear_range(u64 TileAddr, u64 low, u64 high) {
-  for (int i=low; i<=high; i+=4) {
-    XAieGbl_Write32(TileAddr+i, 0);
+  for (int i = low; i <= high; i += 4) {
+    XAieGbl_Write32(TileAddr + i, 0);
     // int x = XAieGbl_Read32(TileAddr+i);
     // if(x != 0) {
     //   printf("@%x = %x\n", i, x);
@@ -670,11 +674,10 @@ void mlir_aie_clear_shim_config(aie_libxaie_ctx_t *ctx, int col, int row) {
   clear_range(TileAddr, 0x3F200, 0x3F37C);
 }
 
-void computeStats(u32 performance_counter[], int n){
+void computeStats(u32 performance_counter[], int n) {
   u32 total_0 = 0;
 
-  for (int i = 0; i < n; i ++)
-  {
+  for (int i = 0; i < n; i++) {
     total_0 += performance_counter[i];
   }
 
@@ -682,15 +685,13 @@ void computeStats(u32 performance_counter[], int n){
 
   float sdev_0 = 0;
 
-  for (int i = 0; i < n; i ++)
-  {
+  for (int i = 0; i < n; i++) {
     sdev_0 += std::pow(((float)performance_counter[i] - mean_0), 2);
   }
 
   sdev_0 = std::sqrt(sdev_0 / n);
 
   printf("Mean and Standard Devation: %f, %f \n", mean_0, sdev_0);
-  
 }
 
 #endif
--- runtime_lib/test_library.h
+++ runtime_lib/test_library.h
@@ -9,10 +9,10 @@
 //===----------------------------------------------------------------------===//
 #ifndef AIE_TEST_LIBRARY_H
 #define AIE_TEST_LIBRARY_H
+#include <cmath>
 #include <stdio.h>
 #include <stdlib.h>
 #include <xaiengine.h>
-#include <cmath>
 
 extern "C" {
 
@@ -87,7 +87,6 @@ struct aie_libxaie_ctx_t {
 #define MODE_PL 1
 #define MODE_MEM 2
 
-
 //#define HIGH_ADDR(addr)	((addr & 0xffffffff00000000) >> 32)
 //#define LOW_ADDR(addr)	(addr & 0x00000000ffffffff)
 
@@ -139,57 +138,61 @@ void mlir_aie_clear_config(aie_libxaie_ctx_t *ctx, int col, int row);
 void mlir_aie_clear_shim_config(aie_libxaie_ctx_t *ctx, int col, int row);
 
 // class for using events and PF cpounters
-class EventMon{
-    public:
-        EventMon(struct XAieGbl_Tile *_tilePtr, u32 _pfc, u32 _startE, u32 _endE, u32 _resetE, u8 _mode){
-            tilePtr = _tilePtr;
-            pfc = _pfc;
-            mode = _mode; // 0: Core, 1: PL, 2, Mem
-            if(mode == MODE_CORE){
-                XAieTileCore_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);}
-            else if(mode == MODE_PL){
-                XAieTilePl_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);}
-            else{
-                XAieTileMem_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);}
-            
-        }
-        void set(){
-            if(mode == MODE_CORE){
-                start = XAieTileCore_PerfCounterGet(tilePtr, pfc);}
-            else if(mode == MODE_PL){
-                start = XAieTilePl_PerfCounterGet(tilePtr, pfc);}
-            else{
-                start = XAieTileMem_PerfCounterGet(tilePtr, pfc);}
-        }
-        u32 read(){
-            if(mode == MODE_CORE){
-                return XAieTileCore_PerfCounterGet(tilePtr, pfc);}
-            else if(mode == MODE_PL){
-                return XAieTilePl_PerfCounterGet(tilePtr, pfc);}
-            else{
-                return XAieTileMem_PerfCounterGet(tilePtr, pfc);}
-        }
-        u32 diff(){
-            u32 end;
-            if(mode == MODE_CORE){
-                end = XAieTileCore_PerfCounterGet(tilePtr, pfc);}
-            else if(mode == MODE_PL){
-                end = XAieTilePl_PerfCounterGet(tilePtr, pfc);}
-            else{
-                end = XAieTileMem_PerfCounterGet(tilePtr, pfc);}
-            if(end < start){
-                printf("WARNING: EventMon: performance counter wrapped!\n");
-                return 0; // TODO: fix this
-            }
-            else{
-                return end - start;
-            }
-        }
-    private:
-        u32 start;
-        u32 pfc;
-        u8 mode;
-        struct XAieGbl_Tile *tilePtr;
+class EventMon {
+public:
+  EventMon(struct XAieGbl_Tile *_tilePtr, u32 _pfc, u32 _startE, u32 _endE,
+           u32 _resetE, u8 _mode) {
+    tilePtr = _tilePtr;
+    pfc = _pfc;
+    mode = _mode; // 0: Core, 1: PL, 2, Mem
+    if (mode == MODE_CORE) {
+      XAieTileCore_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);
+    } else if (mode == MODE_PL) {
+      XAieTilePl_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);
+    } else {
+      XAieTileMem_PerfCounterControl(tilePtr, pfc, _startE, _endE, _resetE);
+    }
+  }
+  void set() {
+    if (mode == MODE_CORE) {
+      start = XAieTileCore_PerfCounterGet(tilePtr, pfc);
+    } else if (mode == MODE_PL) {
+      start = XAieTilePl_PerfCounterGet(tilePtr, pfc);
+    } else {
+      start = XAieTileMem_PerfCounterGet(tilePtr, pfc);
+    }
+  }
+  u32 read() {
+    if (mode == MODE_CORE) {
+      return XAieTileCore_PerfCounterGet(tilePtr, pfc);
+    } else if (mode == MODE_PL) {
+      return XAieTilePl_PerfCounterGet(tilePtr, pfc);
+    } else {
+      return XAieTileMem_PerfCounterGet(tilePtr, pfc);
+    }
+  }
+  u32 diff() {
+    u32 end;
+    if (mode == MODE_CORE) {
+      end = XAieTileCore_PerfCounterGet(tilePtr, pfc);
+    } else if (mode == MODE_PL) {
+      end = XAieTilePl_PerfCounterGet(tilePtr, pfc);
+    } else {
+      end = XAieTileMem_PerfCounterGet(tilePtr, pfc);
+    }
+    if (end < start) {
+      printf("WARNING: EventMon: performance counter wrapped!\n");
+      return 0; // TODO: fix this
+    } else {
+      return end - start;
+    }
+  }
+
+private:
+  u32 start;
+  u32 pfc;
+  u8 mode;
+  struct XAieGbl_Tile *tilePtr;
 };
 
 void computeStats(u32 performance_counter[], int n);
