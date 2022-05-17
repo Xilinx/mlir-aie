c40bc6f9ac1a52c91a511f2bafc2d9e7993d65c3
Andra Bisca <andra.bisca@gmail.com>
2022-05-17 09:49:42 -0600

    Fixing dialect includes.

--- include/aie/AIEDialect.h
+++ include/aie/AIEDialect.h
@@ -16,6 +16,7 @@
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
--- lib/AIEObjectFifoRegisterProcess.cpp
+++ lib/AIEObjectFifoRegisterProcess.cpp
@@ -24,7 +24,7 @@
 #include "aie/AIEDialect.h"
 #include "aie/AIETokenAnalysis.h"
 #include "mlir/Dialect/SCF/SCF.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
 
 using namespace mlir;
 using namespace xilinx;
