061b79d36546d4f90cf9c53eee63ad3b5fc290b4
Andra Bisca <andra.bisca@gmail.com>
2022-05-18 07:21:45 -0600

    Trying to solve issues with CF dialect.

--- lib/AIEObjectFifoStatefulTransform.cpp
+++ lib/AIEObjectFifoStatefulTransform.cpp
@@ -10,22 +10,21 @@
 // 
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Pass/Pass.h"
+#include "aie/AIEDialect.h"
+#include "aie/AIETokenAnalysis.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BlockAndValueMapping.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/Location.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Tools/mlir-translate/MlirTranslateMain.h"
 #include "llvm/Support/Debug.h"
-#include "aie/AIEDialect.h"
-#include "aie/AIETokenAnalysis.h"
-#include "mlir/Dialect/SCF/SCF.h"
-#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
-#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 
 using namespace mlir;
 using namespace xilinx;
