59b029f66f844fa5777604306bd9925cf6fd6ec4
Jinming Zhuang <m13688033757@163.com>
2022-07-18 17:33:52 -0600

    Fix the co-existence with flow pass

--- lib/AIECreatePacketFlows.cpp
+++ lib/AIECreatePacketFlows.cpp
@@ -660,7 +660,7 @@ struct AIERoutePacketFlowsPass
       auto retVal = switchbox->getOperand(0);
       auto tileOp = retVal.getDefiningOp<TileOp>();
 
-      if (tileOp.isShimNOCorPLTile()) {
+      if (tileOp.isShimNOCTile()) {
 
         Region &r = switchbox.connections();
         Block &b = r.front();
@@ -729,14 +729,6 @@ struct AIERoutePacketFlowsPass
           builder.setInsertionPointToEnd(b0);
           builder.create<xilinx::AIE::EndOp>(builder.getUnknownLoc());
         }
-      }
-    }
-
-    for (auto shimMux : llvm::make_early_inc_range(m.getOps<ShimMuxOp>())) {
-      // Remove empty shim muxes.
-      if (&shimMux.getBody()->front() == shimMux.getBody()->getTerminator()) {
-        shimMux->dropAllUses();
-        shimMux->erase();
       }
     }
 
