172dd3512385d31e38ff662366402ff7d9eed048
nbaron <nbaron@amd.com>
2022-08-03 08:57:21 -0700

    Cleanup includes

--- lib/Targets/AIETargetAirbin.cpp
+++ lib/Targets/AIETargetAirbin.cpp
@@ -8,37 +8,19 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/IR/Attributes.h"
-#include "mlir/IR/Block.h"
-#include "mlir/IR/BlockAndValueMapping.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/Location.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Target/LLVMIR/Import.h"
-#include "mlir/Tools/mlir-translate/MlirTranslateMain.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/Passes.h"
-
 #include "llvm/ADT/None.h"
-#include "llvm/ADT/StringExtras.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Support/Debug.h"
 #include "llvm/Support/Format.h"
-#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <array>
 #include <cstdint>
 #include <elf.h>
-#include <fcntl.h>
+#include <fcntl.h> // open
 #include <sstream>
-#include <unistd.h>
-#include <utility>
+#include <unistd.h> // read
+#include <utility>  // pair
 #include <vector>
 
-#include "aie/AIEDialect.h"
 #include "aie/AIENetlistAnalysis.h"
 
 #include "AIETargets.h"
