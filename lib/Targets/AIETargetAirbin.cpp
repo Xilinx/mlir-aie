e82798b4c0d2f7f4bf133e66af07d0c187f28b63
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-08-05 09:12:23 -0700

    The default Y value is 0xFFFF0100

--- lib/Targets/AIETargetAirbin.cpp
+++ lib/Targets/AIETargetAirbin.cpp
@@ -533,7 +533,7 @@ static void configure_dmas(mlir::ModuleOp module, NetlistAnalysis &NL) {
         uint32_t x{0x00ff0001u};
         // The Y register has some fields
         // which need to be nonzero in the default state.
-        uint32_t y{0xffff0001u};
+        uint32_t y{0xffff0100u};
         uint32_t packet{0};
         uint32_t interleave{0};
         uint32_t control{0};
