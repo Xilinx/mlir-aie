fe1bc637685785dce6f7cca7c94954d84e1f7fd1
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2022-10-20 14:54:11 -0700

    [aiecc] Add profiling and progress bar

--- tools/aiecc/aiecc/cl_arguments.py
+++ tools/aiecc/aiecc/cl_arguments.py
@@ -114,6 +114,11 @@ def parse_args():
             default=1,
             action='store',
             help='Compile with max n-threads in the machine (default is 1).  An argument of zero corresponds to the maximum number of threads on the machine.')
+    parser.add_argument('--profile',
+            dest="profiling",
+            default=False,
+            action='store_true',
+            help='Profile commands to find the most expensive executions.')
 
 
     opts = parser.parse_args(sys.argv[1:])
--- tools/aiecc/aiecc/main.py
+++ tools/aiecc/aiecc/main.py
@@ -18,16 +18,42 @@ from subprocess import PIPE, run, call
 from multiprocessing.pool import ThreadPool
 import tempfile
 import shutil
+import timeit
 
 import aiecc.cl_arguments
 
-def do_call(command):
+from rich.progress import *
+
+runtimes = dict()
+progress_bar = None
+maxtasks = 5
+stopall = False
+def do_call(task, command):
     global opts
+    global progress_bar
+    global runtimes
+    global maxtasks
+    global stopall
+    if(stopall):
+      return
+
     if(opts.verbose):
         print(" ".join(command))
+    commandstr = " ".join(command)
+    if(task):
+      progress_bar.update(task, advance=0, command=commandstr[0:30])
+    start = time.time()
     ret = call(command)
+    end = time.time()
+    runtimes[commandstr] = end-start
+    if(task):
+      progress_bar.update(task, advance=1, command="")
+      maxtasks = max(progress_bar._tasks[task].completed, maxtasks)
+      progress_bar._tasks[task].total = maxtasks
+
     if(ret != 0):
-        print("Error encountered while running: " + " ".join(command))
+        progress_bar._tasks[task].description = "[red] Error"
+        print("Error encountered while running: " + commandstr)
         sys.exit(1)
 
 def do_run(command):
@@ -45,17 +71,17 @@ def run_flow(opts, tmpdirname):
     chess_intrinsic_wrapper_cpp = os.path.join(thispath, '..','..','runtime_lib', 'chess_intrinsic_wrapper.cpp')
 
     file_with_addresses = os.path.join(tmpdirname, 'input_with_addresses.mlir')
-    do_call(['aie-opt', '--lower-affine', '--aie-register-objectFifos', '--aie-objectFifo-stateful-transform', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', '--aie-assign-buffer-addresses', '-convert-scf-to-cf', opts.filename, '-o', file_with_addresses])
+    do_call(None, ['aie-opt', '--lower-affine', '--aie-register-objectFifos', '--aie-objectFifo-stateful-transform', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', '--aie-assign-buffer-addresses', '-convert-scf-to-cf', opts.filename, '-o', file_with_addresses])
     t = do_run(['aie-translate', '--aie-generate-corelist', file_with_addresses])
     cores = eval(t.stdout)
 
     if(opts.xchesscc == True):
       chess_intrinsic_wrapper = os.path.join(tmpdirname, 'chess_intrinsic_wrapper.ll')
-      do_call(['xchesscc_wrapper', '-c', '-d', '-f', '+f', '+P', '4', chess_intrinsic_wrapper_cpp, '-o', chess_intrinsic_wrapper])      
-      do_call(['sed', '-i', 's/^target.*//', chess_intrinsic_wrapper])     
+      do_call(None, ['xchesscc_wrapper', '-c', '-d', '-f', '+f', '+P', '4', chess_intrinsic_wrapper_cpp, '-o', chess_intrinsic_wrapper])
+      do_call(None, ['sed', '-i', 's/^target.*//', chess_intrinsic_wrapper])
 
-      do_call(['sed', '-i', 's/noalias_sidechannel[^,]*,//', chess_intrinsic_wrapper])
-      do_call(['sed', '-i', 's/nocallback[^,]*,//', chess_intrinsic_wrapper])
+      do_call(None, ['sed', '-i', 's/noalias_sidechannel[^,]*,//', chess_intrinsic_wrapper])
+      do_call(None, ['sed', '-i', 's/nocallback[^,]*,//', chess_intrinsic_wrapper])
 
     def corefile(dirname, core, ext):
         (corecol, corerow, _) = core
@@ -72,13 +98,21 @@ def run_flow(opts, tmpdirname):
         return ' '.join(t.stdout.split())
 
     def process_core(core):
+        global progress_bar
+        global maxtasks
+        global stopall
+        if(stopall):
+          return
+        task = progress_bar.add_task("[yellow] Core (%d, %d)" % core[0:2], total=maxtasks, command="starting")
+
         (corecol, corerow, elf_file) = core
         file_core = tmpcorefile(core, "mlir")
-        do_call(['aie-opt', '--aie-localize-locks',
+        do_call(task, ['aie-opt', '--aie-localize-locks',
                             '--aie-standard-lowering=tilecol=%d tilerow=%d' % core[0:2],
                             file_with_addresses, '-o', file_core])
+
         file_opt_core = tmpcorefile(core, "opt.mlir")
-        do_call(['aie-opt', '--aie-normalize-address-spaces',
+        do_call(task, ['aie-opt', '--aie-normalize-address-spaces',
                             '--canonicalize',
                             '--cse',
                             '--convert-vector-to-llvm',
@@ -87,69 +121,65 @@ def run_flow(opts, tmpdirname):
                             '--convert-cf-to-llvm',
                             '--canonicalize', '--cse', file_core, '-o', file_opt_core])
         file_core_bcf = tmpcorefile(core, "bcf")
-        do_call(['aie-translate', file_with_addresses, '--aie-generate-bcf', '--tilecol=%d' % corecol, '--tilerow=%d' % corerow, '-o', file_core_bcf])
+        do_call(task, ['aie-translate', file_with_addresses, '--aie-generate-bcf', '--tilecol=%d' % corecol, '--tilerow=%d' % corerow, '-o', file_core_bcf])
         file_core_ldscript = tmpcorefile(core, "ld.script")
-        do_call(['aie-translate', file_with_addresses, '--aie-generate-ldscript', '--tilecol=%d' % corecol, '--tilerow=%d' % corerow, '-o', file_core_ldscript])
+        do_call(task, ['aie-translate', file_with_addresses, '--aie-generate-ldscript', '--tilecol=%d' % corecol, '--tilerow=%d' % corerow, '-o', file_core_ldscript])
         file_core_llvmir = tmpcorefile(core, "ll")
-        do_call(['aie-translate', '--mlir-to-llvmir', '--opaque-pointers=0', file_opt_core, '-o', file_core_llvmir])
+        do_call(task, ['aie-translate', '--mlir-to-llvmir', '--opaque-pointers=0', file_opt_core, '-o', file_core_llvmir])
         file_core_elf = elf_file if elf_file else corefile(".", core, "elf")
         file_core_obj = tmpcorefile(core, "o")
-        if not opts.compile:
-          return
-        if(opts.xchesscc):
+
+        if(opts.compile and opts.xchesscc):
           file_core_llvmir_chesshack = tmpcorefile(core, "chesshack.ll")
-          do_call(['cp', file_core_llvmir, file_core_llvmir_chesshack])
-          do_call(['sed', '-i', 's/noundef//', file_core_llvmir_chesshack])
-          do_call(['sed', '-i', 's/noalias_sidechannel[^,],//', file_core_llvmir_chesshack])
+          do_call(task, ['cp', file_core_llvmir, file_core_llvmir_chesshack])
+          do_call(task, ['sed', '-i', 's/noundef//', file_core_llvmir_chesshack])
+          do_call(task, ['sed', '-i', 's/noalias_sidechannel[^,],//', file_core_llvmir_chesshack])
           file_core_llvmir_chesslinked = tmpcorefile(core, "chesslinked.ll")
-          do_call(['llvm-link', file_core_llvmir_chesshack, chess_intrinsic_wrapper, '-S', '-o', file_core_llvmir_chesslinked])
-          do_call(['sed', '-i', 's/noundef//', file_core_llvmir_chesslinked])
+          do_call(task, ['llvm-link', file_core_llvmir_chesshack, chess_intrinsic_wrapper, '-S', '-o', file_core_llvmir_chesslinked])
+          do_call(task, ['sed', '-i', 's/noundef//', file_core_llvmir_chesslinked])
           # Formal function argument names not used in older LLVM
-          do_call(['sed', '-i', '-E', '/define .*@/ s/%[0-9]*//g', file_core_llvmir_chesslinked])
-          do_call(['sed', '-i', '-E', 's/mustprogress//g', file_core_llvmir_chesslinked])
-          do_call(['sed', '-i', '-E', 's/poison/undef/g', file_core_llvmir_chesslinked])
-          do_call(['sed', '-i', '-E', 's/nocallback//g', file_core_llvmir_chesslinked])
-          if not opts.link:
-            return
-          if(opts.xbridge):
+          do_call(task, ['sed', '-i', '-E', '/define .*@/ s/%[0-9]*//g', file_core_llvmir_chesslinked])
+          do_call(task, ['sed', '-i', '-E', 's/mustprogress//g', file_core_llvmir_chesslinked])
+          do_call(task, ['sed', '-i', '-E', 's/poison/undef/g', file_core_llvmir_chesslinked])
+          do_call(task, ['sed', '-i', '-E', 's/nocallback//g', file_core_llvmir_chesslinked])
+          if(opts.link and opts.xbridge):
             link_with_obj = extract_input_files(file_core_bcf)
-            do_call(['xchesscc_wrapper', '-d', '-f', '+P', '4', file_core_llvmir_chesslinked, link_with_obj, '+l', file_core_bcf, '-o', file_core_elf])
-          else:
-            do_call(['xchesscc_wrapper', '-c', '-d', '-f', '+P', '4', file_core_llvmir_chesslinked, '-o', file_core_obj])
-            do_call(['clang', '-O2', '--target=aie', file_core_obj, me_basic_o, libm,
+            do_call(task, ['xchesscc_wrapper', '-d', '-f', '+P', '4', file_core_llvmir_chesslinked, link_with_obj, '+l', file_core_bcf, '-o', file_core_elf])
+          elif(opts.link):
+            do_call(task, ['xchesscc_wrapper', '-c', '-d', '-f', '+P', '4', file_core_llvmir_chesslinked, '-o', file_core_obj])
+            do_call(task, ['clang', '-O2', '--target=aie', file_core_obj, me_basic_o, libm,
             '-Wl,-T,'+file_core_ldscript, '-o', file_core_elf])
-        else:
+        elif(opts.compile):
           file_core_llvmir_stripped = tmpcorefile(core, "stripped.ll")
-          do_call(['opt', '--passes=default<O2>,strip', '-S', file_core_llvmir, '-o', file_core_llvmir_stripped])
-          do_call(['llc', file_core_llvmir_stripped, '-O2', '--march=aie', '--filetype=obj', '-o', file_core_obj])
-          if not opts.link:
-            return
-          if(opts.xbridge):
+          do_call(task, ['opt', '--passes=default<O2>,strip', '-S', file_core_llvmir, '-o', file_core_llvmir_stripped])
+          do_call(task, ['llc', file_core_llvmir_stripped, '-O2', '--march=aie', '--filetype=obj', '-o', file_core_obj])
+          if(opts.link and opts.xbridge):
             link_with_obj = extract_input_files(file_core_bcf)
-            do_call(['xchesscc_wrapper', '-d', '-f', file_core_obj, link_with_obj, '+l', file_core_bcf, '-o', file_core_elf])
-          else:
-            do_call(['clang', '-O2', '--target=aie', file_core_obj, me_basic_o, libm,
+            do_call(task, ['xchesscc_wrapper', '-d', '-f', file_core_obj, link_with_obj, '+l', file_core_bcf, '-o', file_core_elf])
+          elif(opts.link):
+            do_call(task, ['clang', '-O2', '--target=aie', file_core_obj, me_basic_o, libm,
             '-Wl,-T,'+file_core_ldscript, '-o', file_core_elf])
 
-
+        progress_bar.update(progress_bar.task_completed,advance=1)
+        progress_bar.update(task,advance=0,visible=False)
 
     def process_arm_cgen():
+      global progress_bar
+      global stopall
+      if(stopall):
+        return
+      task = progress_bar.add_task("[yellow] ARM Core ", total=10, command="starting")
       # Generate the included host interface
       file_physical = os.path.join(tmpdirname, 'input_physical.mlir')
       if(opts.pathfinder):
-        do_call(['aie-opt', '--aie-create-pathfinder-flows', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', file_with_addresses, '-o', file_physical]);
+        do_call(task, ['aie-opt', '--aie-create-pathfinder-flows', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', file_with_addresses, '-o', file_physical]);
       else:
-        do_call(['aie-opt', '--aie-create-flows', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', file_with_addresses, '-o', file_physical]);
+        do_call(task, ['aie-opt', '--aie-create-flows', '--aie-lower-broadcast-packet', '--aie-create-packet-flows', file_with_addresses, '-o', file_physical]);
       file_inc_cpp = os.path.join(tmpdirname, 'aie_inc.cpp')
       if(opts.xaie == 1):
-          do_call(['aie-translate', '--aie-generate-xaie', '--xaie-target=v1', file_physical, '-o', file_inc_cpp])
+          do_call(task, ['aie-translate', '--aie-generate-xaie', '--xaie-target=v1', file_physical, '-o', file_inc_cpp])
       else:
-          do_call(['aie-translate', '--aie-generate-xaie', '--xaie-target=v2', file_physical, '-o', file_inc_cpp])
-
-
-      # Lastly, compile the generated host interface with any ARM code.
-      if not opts.compile_host:
-        return
+          do_call(task, ['aie-translate', '--aie-generate-xaie', '--xaie-target=v2', file_physical, '-o', file_inc_cpp])
 
       if(opts.host_target):
         cmd = ['clang','--target=%s' % opts.host_target, '-std=c++11']
@@ -177,23 +207,38 @@ def run_flow(opts, tmpdirname):
     
 
       if(len(opts.arm_args) > 0):
-        do_call(cmd + opts.arm_args)
+        do_call(task, cmd + opts.arm_args)
 
+      progress_bar.update(progress_bar.task_completed,advance=1)
+      progress_bar.update(task,advance=0,visible=False)
 
     nthreads = int(opts.nthreads)
-    if (nthreads == 0 or nthreads > 1):
-      if(nthreads == 0):
-        nthreads = None
-      with ThreadPool(nthreads) as thdpool:
-          # prefer to dispatch and process_arm_cgen() first, it typically takes longer than process_core()
-          thdpool.apply_async(process_arm_cgen)
-          thdpool.map(process_core, (cores))
-          thdpool.close()
-          thdpool.join()
-    else:
-      for core in cores:
-        process_core(core)
-      process_arm_cgen()
+    nworkers = nthreads
+    if(nthreads == 0):
+      nworkers = os.cpu_count()
+
+    with Progress(
+      *Progress.get_default_columns(),
+      TimeElapsedColumn(),
+      MofNCompleteColumn(),
+      TextColumn("{task.fields[command]}")) as progress:
+      progress.task_completed = progress.add_task("[green] AIE Compilation:", total=len(cores)+1, command="")
+
+      global progress_bar
+      progress_bar = progress
+      if (nthreads == 0 or nthreads > 1):
+        if(nthreads == 0):
+          nthreads = None
+        with ThreadPool(nthreads) as thdpool:
+            # prefer to dispatch and process_arm_cgen() first, it typically takes longer than process_core()
+            thdpool.apply_async(process_arm_cgen)
+            thdpool.map(process_core, (cores))
+            thdpool.close()
+            thdpool.join()
+      else:
+        for core in cores:
+          process_core(core)
+        process_arm_cgen()
 
 
 def main(builtin_params={}):
@@ -253,3 +298,10 @@ def main(builtin_params={}):
     else:
       with tempfile.TemporaryDirectory() as tmpdirname:
         run_flow(opts, tmpdirname)
+
+    if(opts.profiling):
+      global runtimes
+      sortedruntimes = sorted(runtimes.items(), key=lambda item: item[1], reverse=True)
+      for i in range(50):
+        if(i < len(sortedruntimes)):
+          print("%.4f sec: %s" % (sortedruntimes[i][1], sortedruntimes[i][0]))
