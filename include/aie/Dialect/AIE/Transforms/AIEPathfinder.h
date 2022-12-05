b99b36786df92ae5ea7199ba10a0605ad41d8e8d
Stephen Neuendorffer <stephen.neuendorffer@amd.com>
2022-12-04 23:41:37 -0800

    Refactor AIE Dialect into subdirectories. (#207)
    Now that we have multiple dialects, this makes sense.
    
    We also update clang tidy to version 12, since version 9
    seems no longer to be available.

---
+++ include/aie/Dialect/AIE/Transforms/AIEPathfinder.h
@@ -0,0 +1,104 @@
+//===- AIEPathfinder.h ------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef AIE_PATHFINDER_H
+#define AIE_PATHFINDER_H
+
+#include <algorithm>
+#include <limits>
+#include <utility> //for std::pair
+#include <vector>
+
+// builds against at least boost graph 1.7.1
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-local-typedef"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/graph_traits.hpp>
+#pragma GCC diagnostic pop
+
+#include "aie/Dialect/AIE/IR/AIEDialect.h" // for WireBundle and Port
+
+namespace xilinx {
+namespace AIE {
+
+using namespace boost;
+
+struct Switchbox { // acts as a vertex
+  unsigned short col, row;
+  // int dist;
+  unsigned int pred; // predecessor for dijkstra's
+  bool processed;    // denotes this switchbox has already been processed
+};
+
+struct Channel { // acts as an edge
+  float demand;  // indicates how many flows want to use this Channel
+  unsigned short
+      used_capacity;           // how many flows are actually using this Channel
+  unsigned short max_capacity; // maximum number of routing resources
+  std::set<short> fixed_capacity;     // channels not available to the algorithm
+  unsigned short over_capacity_count; // history of Channel being over capacity
+  WireBundle bundle;
+};
+
+// create a graph type that uses Switchboxes as vertices and Channels as edges
+typedef adjacency_list<vecS, vecS, bidirectionalS, Switchbox, Channel>
+    SwitchboxGraph;
+
+typedef graph_traits<SwitchboxGraph>::vertex_descriptor vertex_descriptor;
+typedef graph_traits<SwitchboxGraph>::edge_descriptor edge_descriptor;
+typedef graph_traits<SwitchboxGraph>::vertex_iterator vertex_iterator;
+typedef graph_traits<SwitchboxGraph>::edge_iterator edge_iterator;
+typedef graph_traits<SwitchboxGraph>::in_edge_iterator in_edge_iterator;
+
+typedef std::pair<int, int> Coord;
+// A SwitchSetting defines the required settings for a Switchbox for a flow
+// SwitchSetting.first is the incoming signal
+// SwitchSetting.second is the fanout
+typedef std::pair<Port, std::set<Port>> SwitchSetting;
+typedef std::map<Switchbox *, SwitchSetting> SwitchSettings;
+
+// A Flow defines source and destination vertices
+// Only one source, but any number of destinations (fanout)
+typedef std::pair<Switchbox *, Port> PathEndPoint;
+typedef std::pair<PathEndPoint, std::vector<PathEndPoint>> Flow;
+
+class Pathfinder {
+private:
+  SwitchboxGraph graph;
+  std::vector<Flow> flows;
+  bool maxIterReached;
+
+public:
+  Pathfinder();
+  Pathfinder(int maxcol, int maxrow);
+  void initializeGraph(int maxcol, int maxrow);
+  void addFlow(Coord srcCoords, Port srcPort, Coord dstCoords, Port dstPort);
+  void addFixedConnection(Coord coord, Port port);
+  bool isLegal();
+  std::map<PathEndPoint, SwitchSettings>
+  findPaths(const int MAX_ITERATIONS = 1000);
+
+  Switchbox *getSwitchbox(TileID coords) {
+    auto vpair = vertices(graph);
+    Switchbox *sb;
+    for (vertex_iterator v = vpair.first; v != vpair.second; v++) {
+      sb = &graph[*v];
+      if (sb->col == coords.first && sb->row == coords.second)
+        return sb;
+    }
+    return nullptr;
+  }
+};
+
+} // namespace AIE
+} // namespace xilinx
+#endif
---
+++ lib/Dialect/AIE/Transforms/AIEVectorOpt.cpp
@@ -0,0 +1,58 @@
+//===- AIEVectorOpt.cpp -----------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/Dialect/Vector/Transforms/VectorTransforms.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BlockAndValueMapping.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+#include "aie/Dialect/AIE/IR/AIEDialect.h"
+
+#define DEBUG_TYPE "aie-vector-opt"
+
+using namespace mlir;
+using namespace xilinx;
+using namespace xilinx::AIE;
+
+struct AIEVectorOptPass : public AIEVectorOptBase<AIEVectorOptPass> {
+  void getDependentDialects(::mlir::DialectRegistry &registry) const override {
+    registry.insert<func::FuncDialect>();
+    registry.insert<memref::MemRefDialect>();
+  }
+  void runOnOperation() override {
+    func::FuncOp f = getOperation();
+
+    // Initial store->load forwarding
+    vector::transferOpflowOpt(f);
+
+    ConversionTarget target(getContext());
+    target.addLegalDialect<memref::MemRefDialect>();
+    target.addLegalOp<vector::BroadcastOp>();
+    // To start with, we're mainly interested in eliminating TransferRead ops
+    // that can be converted to load + broadcast
+    target.addDynamicallyLegalOp<vector::TransferReadOp>(
+        [](vector::TransferReadOp op) { return false; });
+    RewritePatternSet patterns(&getContext());
+    vector::populateVectorTransferLoweringPatterns(patterns);
+    vector::populateVectorMaskMaterializationPatterns(patterns, true);
+
+    if (failed(applyPartialConversion(f, target, std::move(patterns))))
+      signalPassFailure();
+  }
+};
+
+std::unique_ptr<OperationPass<func::FuncOp>>
+xilinx::AIE::createAIEVectorOptPass() {
+  return std::make_unique<AIEVectorOptPass>();
+}
