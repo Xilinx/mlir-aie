369ba0a9c84a978b73b4460e7bcca8c31902c2ba
Jeff Fifield <jeff.fifield@amd.com>
2022-12-08 08:55:45 -0700

    Update llvm to 798fa4b4

--- utils/clone-llvm.sh
+++ utils/clone-llvm.sh
@@ -12,7 +12,7 @@
 #
 ##===----------------------------------------------------------------------===##
 
-export commithash=74fb770de9399d7258a8eda974c93610cfde698e
+export commithash=798fa4b415eea55c868ae42b874083cb9886991e
 
 git clone --depth 1 https://github.com/Xilinx/cmakeModules cmakeModules/cmakeModulesXilinx
 export CMAKE_MODULE_PATH=`pwd`/cmakeModules/cmakeModulesXilinx
