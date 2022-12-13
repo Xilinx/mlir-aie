d627ccf8f8ec4202cb089bcd478b120f86499c01
Jeff Fifield <jeff.fifield@xilinx.com>
2022-12-13 11:45:19 -0700

    Add ObjectFifo type to python bindings

--- test/python/aie_ops.py
+++ test/python/aie_ops.py
@@ -56,3 +56,16 @@ def memOp():
     bb = Block.create_at_start(m.body)
     with InsertionPoint(bb):
         EndOp()
+
+# CHECK-LABEL: objFifo
+# CHECK: AIE.objectFifo.createObjectFifo(%{{.*}}, {%{{.*}}}, 2) : !AIE.objectFifo<memref<10xf32>>
+@constructAndPrintInModule
+def objFifo():
+    iTy = IntegerType.get_signless(32)
+    two = IntegerAttr.get(iTy, 2)
+    six = IntegerAttr.get(iTy, 6)
+    tile0 = TileOp(IndexType.get(), six, six)
+    tile1 = TileOp(IndexType.get(), two, two)
+    dtype = F32Type.get()
+    memTy = MemRefType.get((10,), dtype)
+    fifo = ObjectFifoCreateOp(ObjectFifoType.get(memTy), tile0, tile1, two)
