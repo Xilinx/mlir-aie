f31038dee3f6acadfe1b88cd33758c1307e8e4b8
abisca <andra.bisca@gmail.com>
2022-12-14 15:11:43 -0700

    Clang patch.

--- test/reference_designs/autocorrelation/objectFifo_version/test.cpp
+++ test/reference_designs/autocorrelation/objectFifo_version/test.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
   mlir_aie_configure_dmas(_xaie);
 
   int errors = 0;
-  
+
   mlir_aie_init_mems(_xaie, 2);
   int *input = mlir_aie_mem_alloc(_xaie, 0, 1024);
   int *output = mlir_aie_mem_alloc(_xaie, 1, 1024);
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
   mlir_aie_configure_shimdma_70(_xaie);
   mlir_aie_start_cores(_xaie);
 
-  for(int i = 0; i < 64; i++) {
+  for (int i = 0; i < 64; i++) {
     input[i] = rand() & 0xFF;
   }
 
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
   mlir_aie_sync_mem_dev(_xaie, 1); // only used in libaiev2
 
   mlir_aie_release_of_0_lock_0(_xaie, 1, 10000);
-  
+
   printf("Waiting for the result ...\n");
   if (mlir_aie_acquire_of_6_lock_0(_xaie, 1, 100000)) {
     printf("ERROR: timeout hit!\n");
@@ -64,12 +64,11 @@ int main(int argc, char *argv[]) {
   mlir_aie_sync_mem_dev(_xaie, 0); // only used in libaiev2
   mlir_aie_sync_mem_dev(_xaie, 1); // only used in libaiev2
 
-  for(int i = 0; i < 64; i++) {
+  for (int i = 0; i < 64; i++) {
     printf("output[%d] = %d\n", i, output[i]);
   }
 
-  mlir_aie_check("output[63]", output[63], 13905,
-                 errors);
+  mlir_aie_check("output[63]", output[63], 13905, errors);
 
   int res = 0;
   if (!errors) {
