ebf68a0e84cfd568e21827d4d305cd242e4d70e9
Andra Bisca <andra.bisca@gmail.com>
2022-12-14 16:56:50 -0700

    Object FIFO version of autocorrelation reference design (#232)
    * Added objFifo version of autocorrelation ref design. Updated copyright.
    
    * Clang patch.

--- test/reference_designs/autocorrelation/objectFifo_version/test.cpp
+++ test/reference_designs/autocorrelation/objectFifo_version/test.cpp
@@ -54,10 +54,10 @@ int main(int argc, char *argv[]) {
   mlir_aie_sync_mem_dev(_xaie, 0); // only used in libaiev2
   mlir_aie_sync_mem_dev(_xaie, 1); // only used in libaiev2
 
-  mlir_aie_release_input_lock(_xaie, 1, 10000);
+  mlir_aie_release_of_0_lock_0(_xaie, 1, 10000);
 
   printf("Waiting for the result ...\n");
-  if (mlir_aie_acquire_output_lock(_xaie, 1, 100000)) {
+  if (mlir_aie_acquire_of_6_lock_0(_xaie, 1, 100000)) {
     printf("ERROR: timeout hit!\n");
   }
 
