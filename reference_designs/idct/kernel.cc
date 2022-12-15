6270db705dbc4507f9b6271500f80cc9ecdd4498
abisca <andra.bisca@gmail.com>
2022-12-15 13:53:25 -0700

    Clang patch.

--- reference_designs/idct/kernel.cc
+++ reference_designs/idct/kernel.cc
@@ -11,17 +11,16 @@
 
 #include "kernel.h"
 
-void func1(int *a, int *b)
-{
-    // int new_size = 64;
-    // for (int i = 0; i < 64; i ++){
-    //     b[i] = a[i] + 1;
-    // }
+void func1(int *a, int *b) {
+  // int new_size = 64;
+  // for (int i = 0; i < 64; i ++){
+  //     b[i] = a[i] + 1;
+  // }
 
-    int val=a[3];
-    int val2=val+val;
-    val2 += val;
-    val2 += val;
-    val2 += val;
-    b[5] = val2;
+  int val = a[3];
+  int val2 = val + val;
+  val2 += val;
+  val2 += val;
+  val2 += val;
+  b[5] = val2;
 }
--- reference_designs/idct/pass.cc
+++ reference_designs/idct/pass.cc
@@ -10,10 +10,8 @@
 
 #include "kernel.h"
 
-    
-void pass(int *a, int *b)
-{
-    for (int i = 0; i < 64; i ++){
-        b[i] = a[i];
-    }
+void pass(int *a, int *b) {
+  for (int i = 0; i < 64; i++) {
+    b[i] = a[i];
+  }
 }
