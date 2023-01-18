4cd11e01a343219377186297245e7df42aaad502
Andra Bisca <andra.bisca@gmail.com>
2023-01-18 18:12:27 +0100

    Updated build prerequisites and added script to install python packages. (#269)
    * Updated prerequisites and added script to install python packages.
    
    * Reverted name of requirements.txt.
    
    * Removed joblib requirement.

---
+++ utils/setup_python_packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+##===- utils/setup_python_packages.sh - Setup python packages for mlir-aie build --*- Script -*-===##
+# 
+# This file licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# 
+##===----------------------------------------------------------------------===##
+#
+# This script sets up and installs the required python packages to build mlir-aie.
+#
+# source ./setup_python_packages.sh
+#
+##===----------------------------------------------------------------------===##
+
+python3 -m virtualenv sandbox
+source sandbox/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r utils/requirements.txt
