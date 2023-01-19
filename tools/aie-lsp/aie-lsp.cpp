c751b26dd02d76c078a78c538d7f9ed4673f0dbc
Samuel Bayliss <samuelb@xilinx.com>
2023-01-18 22:13:35 -0700

    Added AIE and AIEVec dialects to LSP

--- tools/aie-lsp/aie-lsp.cpp
+++ tools/aie-lsp/aie-lsp.cpp
@@ -4,10 +4,18 @@
 #include "mlir/InitAllDialects.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
 
-
+#include "aie/Dialect/AIE/IR/AIEDialect.h"
+#include "aie/Dialect/AIEVec/IR/AIEVecDialect.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
+  registerAllDialects(registry);
+
+  registry.insert<scf::SCFDialect>();
+  registry.insert<memref::MemRefDialect>();
+  registry.insert<xilinx::AIE::AIEDialect>();
+  registry.insert<xilinx::aievec::AIEVecDialect>();
+  registry.insert<mlir::LLVM::LLVMDialect>();
   // registerMyDialects(registry);
   //registerMyPasses();
   return mlir::failed(mlir::MlirLspServerMain(argc, argv, registry));
