aacfae8e99c37b299ce4606ec55c1f1dcc6f7504
abisca <andra.bisca@gmail.com>
2023-01-23 04:51:45 -0700

    Remove unused address macros in host side tests.

--- test/unit_tests/18_simple_shim_dma_routed/test.cpp
+++ test/unit_tests/18_simple_shim_dma_routed/test.cpp
@@ -20,9 +20,6 @@
 #include <unistd.h>
 #include <xaiengine.h>
 
-#define HIGH_ADDR(addr) ((addr & 0xffffffff00000000) >> 32)
-#define LOW_ADDR(addr) (addr & 0x00000000ffffffff)
-
 #include "aie_inc.cpp"
 
 int main(int argc, char *argv[]) {
