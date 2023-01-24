5f087212b795fab089f5cf6ff76832f8302dea73
Stephen Neuendorffer <stephen.neuendorffer@amd.com>
2023-01-24 10:32:44 -0800

    format

--- test/aiecc/test.cpp
+++ test/aiecc/test.cpp
@@ -8,48 +8,46 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "test_library.h"
 #include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <cstring>
-#include <thread>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <sys/mman.h>
+#include <thread>
+#include <unistd.h>
 #include <xaiengine.h>
-#include "test_library.h"
 
 #include "aie_inc.cpp"
 
-int
-main(int argc, char *argv[])
-{
-    printf("test start.\n");
+int main(int argc, char *argv[]) {
+  printf("test start.\n");
 
-    aie_libxaie_ctx_t *_xaie = mlir_aie_init_libxaie();
-    mlir_aie_init_device(_xaie);
+  aie_libxaie_ctx_t *_xaie = mlir_aie_init_libxaie();
+  mlir_aie_init_device(_xaie);
 
-    mlir_aie_clear_tile_memory(_xaie, 1, 3);
+  mlir_aie_clear_tile_memory(_xaie, 1, 3);
 
-    mlir_aie_configure_cores(_xaie);
-    mlir_aie_configure_switchboxes(_xaie);
-    mlir_aie_initialize_locks(_xaie);
-    mlir_aie_configure_dmas(_xaie);
-    mlir_aie_start_cores(_xaie);
+  mlir_aie_configure_cores(_xaie);
+  mlir_aie_configure_switchboxes(_xaie);
+  mlir_aie_initialize_locks(_xaie);
+  mlir_aie_configure_dmas(_xaie);
+  mlir_aie_start_cores(_xaie);
 
-    int errors = 0;
+  int errors = 0;
 
-    int res = 0;
-    if (!errors) {
-      printf("PASS!\n");
-      res = 0;
-    } else {
-      printf("Fail!\n");
-      res = -1;
-    }
-    mlir_aie_deinit_libxaie(_xaie);
+  int res = 0;
+  if (!errors) {
+    printf("PASS!\n");
+    res = 0;
+  } else {
+    printf("Fail!\n");
+    res = -1;
+  }
+  mlir_aie_deinit_libxaie(_xaie);
 
-    printf("test done.\n");
-    return res;
+  printf("test done.\n");
+  return res;
 }
