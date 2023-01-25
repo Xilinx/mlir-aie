60d4356645007f948bd48406d774247a8bc105db
singagan <gagan.gagandeepsingh@safari.ethz.ch>
2023-01-25 21:36:45 +0100

    horizontal diffussion reference design for AIE

---
+++ reference_designs/horizontal_diffussion/HDIFF_single_AIE_objectFIFO_ping_pong/hdiff.h
@@ -0,0 +1,10 @@
+// (c) 2023 SAFARI Research Group at ETH Zurich, Gagandeep Singh, D-ITET     
+//
+//===----------------------------------------------------------------------===//
+
+// #include <stdint.h>
+
+extern "C" {
+void vec_hdiff(int32_t* restrict in0,int32_t* restrict in1,int32_t* restrict in2,int32_t* restrict in3,int32_t* restrict in4, int32_t* restrict out);
+}
+
