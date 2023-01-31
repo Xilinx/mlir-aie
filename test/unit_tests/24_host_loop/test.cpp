13f5f89a8fbde16dc9713f437ddcc195a2b706bf
abisca <andra.bisca@gmail.com>
2023-01-31 10:30:53 -0700

    Removed redundant shimTile configuration.

--- test/unit_tests/24_host_loop/test.cpp
+++ test/unit_tests/24_host_loop/test.cpp
@@ -40,20 +40,16 @@ int main(int argc, char *argv[]) {
     mlir_aie_external_set_addr_ddr_test_buffer_in((u64)mem_ptr_in);
     mlir_aie_external_set_addr_ddr_test_buffer_out((u64)mem_ptr_out);
     mlir_aie_configure_shimdma_70(_xaie);
-    mem_ptr_in[3] = 14;
-
-    mlir_aie_sync_mem_dev(_xaie, 0);
-    mlir_aie_configure_shimdma_70(_xaie);
 
     int errors = 0;
 
     mlir_aie_clear_tile_memory(_xaie, 3, 4);
-    mlir_aie_check("Before start cores:",
-                    mlir_aie_read_buffer_of_1_buff_0(_xaie, 5), 0, errors);
 
     // Helper function to enable all AIE cores
     printf("Start cores\n");
     mlir_aie_release_of_3_lock_0(_xaie, 0, 0);
+    printf("Status of shim (7,0) before core start:\n");
+    mlir_aie_print_shimdma_status(_xaie, 7, 0);
     mlir_aie_start_cores(_xaie);
 
     for (int i = 11; i < 13; i++) {
@@ -61,6 +57,9 @@ int main(int argc, char *argv[]) {
         mem_ptr_in[3] = i;
         mlir_aie_sync_mem_dev(_xaie, 0);
         mlir_aie_release_of_0_lock_0(_xaie, 1, 0);
+
+        printf("Status of shim (7,0) after core start, after host release for read:\n");
+        mlir_aie_print_shimdma_status(_xaie, 7, 0);
 
         if (mlir_aie_acquire_of_3_lock_0(_xaie, 1, 10000) == XAIE_OK)
             printf("Acquired objFifo 3 lock 0 for read\n");
@@ -79,6 +78,9 @@ int main(int argc, char *argv[]) {
             printf("Released objFifo 3 lock 0 for write\n");
         else
             printf("ERROR: timed out on objFifo 3 lock 0 for write\n");
+
+        printf("Status of shim (7,0) after core start, after host release for write:\n");
+        mlir_aie_print_shimdma_status(_xaie, 7, 0);
     }
 
     int res = 0;
