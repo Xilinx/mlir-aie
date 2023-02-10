0bf477556b9903d55db09ca5c9be979b094b1768
Alireza Khodamoradi <ar.khodamoradi@gmail.com>
2023-02-10 11:42:35 -0700

    corrected code format (#350)
    * corrected code format
    
    * format corrected
    
    ---------
    
    Co-authored-by: Alireza Khodamoradi <alirezak@xilinx.com>

--- reference_designs/horizontal_diffusion/HDIFF_single_AIE_objectFIFO_scaled/hdiff.h
+++
@@ -1,7 +0,0 @@
-/*  (c) 2023 SAFARI Research Group at ETH Zurich, Gagandeep Singh, D-ITET             */
-
-
-extern "C" {
-void vec_hdiff(int32_t* restrict in0,int32_t* restrict in1,int32_t* restrict in2,int32_t* restrict in3,int32_t* restrict in4, int32_t* restrict out);
-}
-
