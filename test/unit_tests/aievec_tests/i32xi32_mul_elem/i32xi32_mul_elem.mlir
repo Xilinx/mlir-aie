5be9671f5779d0581e6a555e25e9dd3def0d043c
David Van Campenhout <dvancamp@xilinx.com>
2023-04-11 16:22:28 -0700

    Refactor testbench and fix test failure.

--- test/unit_tests/aievec_tests/i32xi32_mul_elem/i32xi32_mul_elem.mlir
+++ test/unit_tests/aievec_tests/i32xi32_mul_elem/i32xi32_mul_elem.mlir
@@ -1,12 +1,12 @@
-// RUN: aie-opt %s -affine-super-vectorize="virtual-vector-size=16" --convert-vector-to-aievec="aie-target=aieml" -lower-affine | aie-translate -aieml=true --aievec-to-cpp -o convert_aie-ml.cc
-// RUN: xchesscc %s -f -g +s -p me -P %aietools/data/aie_ml/lib/ +w work +o work -I%S -I. %S/testbench.cc %S/kernel.cc
+// RUN: aie-opt %s -affine-super-vectorize="virtual-vector-size=16" --convert-vector-to-aievec="aie-target=aieml" -lower-affine | aie-translate -aieml=true --aievec-to-cpp -o dut.cc
+// RUN: xchesscc -f -g +s -p me -P %aietools/data/aie_ml/lib/ +w work +o work -I%S  -I %S/../common -I. %S/testbench.cc dut.cc
+// RUN: mkdir -p data
 // RUN: xme_ca_udm_dbg -qf -T -P %aietools/data/aie_ml/lib/ -t "%S/../profiling.tcl ./work/a.out" >& xme_ca_udm_dbg.stdout
 // RUN: FileCheck --input-file=./xme_ca_udm_dbg.stdout %s
-
-// CHECK: PASSED
+// CHECK: TEST PASSED
 
 module {
-  func.func @mul_elem(%arg0: memref<1024xi32>, %arg1: memref<1024xi32>, %arg2: memref<1024xi32>) {
+  func.func @dut(%arg0: memref<1024xi32>, %arg1: memref<1024xi32>, %arg2: memref<1024xi32>) {
     affine.for %arg3 = 0 to 1024 {
       %0 = affine.load %arg0[%arg3] : memref<1024xi32>
       %1 = affine.load %arg1[%arg3] : memref<1024xi32>
--- test/unit_tests/aievec_tests/i32xi32_mul_elem/testbench.cc
+++ test/unit_tests/aievec_tests/i32xi32_mul_elem/testbench.cc
@@ -1,58 +1,59 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include "defines.h"
 #include "testbench.h"
-#include <math.h>
+#include <algorithm>
+void dut(int32_t * restrict in0, int32_t * restrict in1, int32_t * restrict out0);
+void dut_ref(int32_t *in0, int32_t *in1, int32_t *out0);
 
-using namespace std;
+alignas(32) int32_t g_in0[IN0_SIZE];
+alignas(32) int32_t g_in1[IN1_SIZE];
+alignas(32) int32_t g_out0[OUT0_SIZE];
+alignas(32) int32_t g_out0Ref[OUT0_SIZE];
 
-#define MAT_SIZE 1024
+int main(int argc, char *argv[])
+{
+  //XXX Figure out how to use argv with xme_ca_udm_dbg -A
+  std::string dataDir(TO_STR(DATA_DIR));
+  srand(10);
+  std::generate(g_in0, g_in0 + IN0_SIZE,
+                [&]() mutable {return random_integer<int32_t>();});
+  std::generate(g_in1, g_in1 + IN1_SIZE,
+                [&]() mutable {return random_integer<int32_t>();});
 
-alignas(32) int32_t mat_a_data[MAT_SIZE];
-alignas(32) int32_t mat_b_data[MAT_SIZE];
-alignas(32) int32_t mat_c_data[MAT_SIZE];
-alignas(32) int32_t ref_c_data[MAT_SIZE];
+  writeData(g_in0, IN0_SIZE, dataDir + "/in0.txt");
+  writeData(g_in1, IN1_SIZE, dataDir + "/in1.txt");
 
-#ifndef __chess__
-int chess_cycle_count() { return 0; }
-#endif
-
-int main() {
-  int i;
-  // Read in matrix_a to local memory
-  for (i = 0; i < MAT_SIZE; i++) {
-    mat_a_data[i] = i % 100;
-  }
-
-  // Read in matrix_b to local memory
-  for (i = 0; i < MAT_SIZE; i++) {
-    mat_b_data[i] = (i + 11) % 100;
-  }
-
-  // Compute the reference result
-  for (i = 0; i < MAT_SIZE; i++) {
-    ref_c_data[i] = mat_a_data[i] * mat_b_data[i];
-  }
-
-  printf("Running MUL_ELEM...\n\n");
+  chess_memory_fence();
   auto cyclesBegin = chess_cycle_count();
-  mul_elem(mat_a_data, mat_b_data, mat_c_data);
+  dut(g_in0, g_in1, g_out0);
   auto cyclesEnd = chess_cycle_count();
-  printf("Cycle count: %d\n", (int)(cyclesEnd - cyclesBegin));
+  chess_memory_fence();
+  
+  auto cycleCount = (int)(cyclesEnd - cyclesBegin);
+  reportCycleCount(cycleCount, dataDir + "/cycle_count.txt");
 
-  printf("Finish MUL_ELEM!\n\n");
+  writeData(g_out0, OUT0_SIZE, dataDir + "/out0.txt");
 
-  int errors = 0;
-  // Compare results with reference result
-  printf("Compare the results\n\n");
-  for (int i = 0; i < MAT_SIZE; i++) {
-    if (mat_c_data[i] != ref_c_data[i]) {
-      errors++;
-    }
+  dut_ref(g_in0, g_in1, g_out0Ref);
+  writeData(g_out0Ref, OUT0_SIZE, dataDir + "/out0_ref.txt");
+
+  bool ok = true;
+  ok &= checkData(g_out0, g_out0Ref, OUT0_SIZE, 1);
+
+  if (ok)
+    printf("TEST PASSED\n");
+  else
+    printf("TEST FAILED\n");
+  
+  return ok ? 0 : 1;
+}
+
+// in0, in1, out0 are in C4 layout.
+void dut_ref(int32_t *in0, int32_t *in1, int32_t *out0)
+{
+  for (unsigned k = 0; k < OUT0_SIZE; k += 1) {
+      out0[k] = in0[k] * in1[k];
   }
-
-  if (errors == 0) {
-    printf("PASSED.\n\n");
-  } else {
-    printf("FAIL. Number of errors = %d\n\n", errors);
-  }
-
-  return 0;
 }
