7ae12b5f72c5d42b5a6aedaf8240fe582f264feb
David Van Campenhout <dvancamp@xilinx.com>
2023-04-12 07:25:42 -0700

    Formatting

--- test/unit_tests/aievec_tests/i32xi32_mul_elem/testbench.cc
+++ test/unit_tests/aievec_tests/i32xi32_mul_elem/testbench.cc
@@ -1,10 +1,10 @@
+#include "../common/testbench.h"
+#include "defines.h"
+#include <algorithm>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
-#include "defines.h"
-#include "../common/testbench.h"
-#include <algorithm>
-void dut(int32_t * restrict in0, int32_t * restrict in1, int32_t * restrict out0);
+void dut(int32_t *restrict in0, int32_t *restrict in1, int32_t *restrict out0);
 void dut_ref(int32_t *in0, int32_t *in1, int32_t *out0);
 
 alignas(32) int32_t g_in0[IN0_SIZE];
@@ -12,15 +12,14 @@ alignas(32) int32_t g_in1[IN1_SIZE];
 alignas(32) int32_t g_out0[OUT0_SIZE];
 alignas(32) int32_t g_out0Ref[OUT0_SIZE];
 
-int main(int argc, char *argv[])
-{
-  //XXX Figure out how to use argv with xme_ca_udm_dbg -A
+int main(int argc, char *argv[]) {
+  // XXX Figure out how to use argv with xme_ca_udm_dbg -A
   std::string dataDir(TO_STR(DATA_DIR));
   srand(10);
   std::generate(g_in0, g_in0 + IN0_SIZE,
-                [&]() mutable {return random_integer<int32_t>();});
+                [&]() mutable { return random_integer<int32_t>(); });
   std::generate(g_in1, g_in1 + IN1_SIZE,
-                [&]() mutable {return random_integer<int32_t>();});
+                [&]() mutable { return random_integer<int32_t>(); });
 
   writeData(g_in0, IN0_SIZE, dataDir + "/in0.txt");
   writeData(g_in1, IN1_SIZE, dataDir + "/in1.txt");
@@ -30,7 +29,7 @@ int main(int argc, char *argv[])
   dut(g_in0, g_in1, g_out0);
   auto cyclesEnd = chess_cycle_count();
   chess_memory_fence();
-  
+
   auto cycleCount = (int)(cyclesEnd - cyclesBegin);
   reportCycleCount(cycleCount, dataDir + "/cycle_count.txt");
 
@@ -46,14 +45,13 @@ int main(int argc, char *argv[])
     printf("TEST PASSED\n");
   else
     printf("TEST FAILED\n");
-  
+
   return ok ? 0 : 1;
 }
 
 // in0, in1, out0 are in C4 layout.
-void dut_ref(int32_t *in0, int32_t *in1, int32_t *out0)
-{
+void dut_ref(int32_t *in0, int32_t *in1, int32_t *out0) {
   for (unsigned k = 0; k < OUT0_SIZE; k += 1) {
-      out0[k] = in0[k] * in1[k];
+    out0[k] = in0[k] * in1[k];
   }
 }
