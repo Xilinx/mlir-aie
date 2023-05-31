c9f52bf0624c5441f85c393ae4e154f2eff65fbe
David Van Campenhout <99038895+david-vc@users.noreply.github.com>
2023-05-31 13:58:24 -0700

    test case clean up (#507)
    Test case clean up per Ronan's suggestions:
    - omit inappropriate use of mutable
    - use constexpr over macros in defines.h
    - use common test bench infra for i16xi16_mul_elem test case.

--- test/unit_tests/aievec_tests/i16xi16_mul_elem/testbench.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-void mul_elem(int16_t *__restrict mat_a_data, int16_t *__restrict mat_b_data,
-              int16_t *__restrict mat_c_data);
--- test/unit_tests/aievec_tests/i8xi8_add_elem_2/testbench.cc
+++ test/unit_tests/aievec_tests/i8xi8_add_elem_2/testbench.cc
@@ -16,9 +16,9 @@ int main(int argc, char *argv[]) {
   std::string dataDir(TO_STR(DATA_DIR));
   srand(10);
   std::generate(g_in0, g_in0 + IN0_SIZE,
-                [&]() mutable { return random_integer<int8_t>(); });
+                [&]() { return random_integer<int8_t>(); });
   std::generate(g_in1, g_in1 + IN1_SIZE,
-                [&]() mutable { return random_integer<int8_t>(); });
+                [&]() { return random_integer<int8_t>(); });
 
   writeData(g_in0, IN0_SIZE, dataDir + "/in0.txt");
   writeData(g_in1, IN1_SIZE, dataDir + "/in1.txt");
