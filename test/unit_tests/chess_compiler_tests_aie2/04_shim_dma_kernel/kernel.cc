712c4463ff5548436add8e876bba14497b53b84a
Stephen Neuendorffer <stephen.neuendorffer@amd.com>
2023-06-15 13:14:35 -0700

    fix chess_compiler_tests_aie2/04_shim_dma_kernel

--- test/unit_tests/chess_compiler_tests_aie2/04_shim_dma_kernel/kernel.cc
+++ test/unit_tests/chess_compiler_tests_aie2/04_shim_dma_kernel/kernel.cc
@@ -5,12 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 // (c) Copyright 2021 Xilinx Inc.
+// (c) Copyright 2023 Advanced Micro Devices, Inc.
 //
 //===----------------------------------------------------------------------===//
 
 #include <stdio.h>
-
-// int32_t iter;
 
 extern int32_t a_ping[256];
 extern int32_t a_pong[256];
@@ -18,12 +17,10 @@ extern int32_t b_ping[256];
 extern int32_t b_pong[256];
 
 #define LOCK_OFFSET 48
-#define A_PING (LOCK_OFFSET + 3)
-#define A_PONG (LOCK_OFFSET + 4)
-#define B_PING (LOCK_OFFSET + 5)
-#define B_PONG (LOCK_OFFSET + 6)
-#define LOCK_READ 1
-#define LOCK_WRITE 0
+#define A_WRITE (LOCK_OFFSET + 3)
+#define A_READ (LOCK_OFFSET + 4)
+#define B_WRITE (LOCK_OFFSET + 5)
+#define B_READ (LOCK_OFFSET + 6)
 
 inline void func(int32_t *a, int32_t *b) {
   int val = a[3];
@@ -37,23 +34,16 @@ inline void func(int32_t *a, int32_t *b) {
 extern "C" void core_7_3() {
   int bounds = 2; // iter;
 
-  // NOTE: odd iterations need locks reset externally when core is run again
-  while (
-      bounds >
-      0) { // TODO: need to change this to start count at 0 so we do ping first
+  while (bounds > 0) {
+    acquire_greater_equal(A_READ, 1);
+    acquire_greater_equal(B_WRITE, 1);
     if ((bounds & 0x1) == 0) {
-      acquire(A_PING, LOCK_READ);
-      acquire(B_PING, LOCK_WRITE);
       func(a_ping, b_ping);
-      release(A_PING, LOCK_WRITE);
-      release(B_PING, LOCK_READ);
     } else {
-      acquire(A_PONG, LOCK_READ);
-      acquire(B_PONG, LOCK_WRITE);
       func(a_pong, b_pong);
-      release(A_PONG, LOCK_WRITE);
-      release(B_PONG, LOCK_READ);
     }
+    release(A_WRITE, 1);
+    release(B_READ, 1);
     bounds--;
   }
 }
