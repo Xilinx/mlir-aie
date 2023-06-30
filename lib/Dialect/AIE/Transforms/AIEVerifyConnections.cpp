950edce33dae5e2044da58188075a9da765cb9ac
andrej <an.roesti@gmail.com>
2023-06-30 10:35:35 -0700

    fix license comment in new --aie-verify-connections pass

--- lib/Dialect/AIE/Transforms/AIEVerifyConnections.cpp
+++ lib/Dialect/AIE/Transforms/AIEVerifyConnections.cpp
@@ -1,12 +1,10 @@
-//===- AIEObjectFifoStatefulTransform.cpp ----------------------*- MLIR -*-===//
+//===- AIEVerifyConnections.cpp ---------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2021 Xilinx Inc.
-//
-// Date: October 18th 2021
+// (c) Copyright 2023 Xilinx Inc.
 //
 //===----------------------------------------------------------------------===//
 
