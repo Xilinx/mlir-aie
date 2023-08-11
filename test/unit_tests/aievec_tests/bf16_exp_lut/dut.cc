270402e0fc870eb1354b0c52860406b5a8a313ea
Lina Yu <linay@xsjlinay40x.xlnx.xilinx.com>
2023-08-11 16:59:23 -0700

    clean up

--- test/unit_tests/aievec_tests/bf16_exp_lut/dut.cc
+++ test/unit_tests/aievec_tests/bf16_exp_lut/dut.cc
@@ -1,18 +1,14 @@
 #include "exp_lut.h"
-void dut(bfloat16 * restrict v1, bfloat16 * restrict v2) {
+void dut(bfloat16 *restrict v1, bfloat16 *restrict v2) {
   size_t v3 = 0;
   size_t v4 = 1024;
   size_t v5 = 16;
   for (size_t v6 = v3; v6 < v4; v6 += v5)
-  chess_prepare_for_pipelining
-  chess_loop_range(64, 64)
-  {
-    v16bfloat16 v7 = *(v16bfloat16 *)(v1 + v6);
-    v16accfloat v8 = getExpBf16(v7);
-    v16bfloat16 v9 = to_v16bfloat16(v8);
-    *(v16bfloat16 *)(v2 + v6) = v9;
-  }
+    chess_prepare_for_pipelining chess_loop_range(64, 64) {
+      v16bfloat16 v7 = *(v16bfloat16 *)(v1 + v6);
+      v16accfloat v8 = getExpBf16(v7);
+      v16bfloat16 v9 = to_v16bfloat16(v8);
+      *(v16bfloat16 *)(v2 + v6) = v9;
+    }
   return;
 }
-
-
