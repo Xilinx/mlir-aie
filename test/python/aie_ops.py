1462deb5d2c49262a95d11f1d16cd61ec3d988ab
Jeff Fifield <jeff.fifield@amd.com>
2023-08-18 14:14:45 -0600

    Repair and enable python test (#588)
    * Fix python binding test
    
    * Enable python tests when python bindings enabled

--- test/python/aie_ops.py
+++ test/python/aie_ops.py
@@ -57,15 +57,34 @@ def memOp():
     with InsertionPoint(bb):
         EndOp()
 
+# CHECK-LABEL: deviceOp
+# CHECK: AIE.device
+@constructAndPrintInModule
+def deviceOp():
+    iTy = IntegerType.get_signless(32)
+    i = IntegerAttr.get(iTy, 1)
+    dev = DeviceOp(IntegerAttr.get(iTy, 1))
+    bb = Block.create_at_start(dev.bodyRegion)
+    with InsertionPoint(bb):
+        EndOp()
+
 # CHECK-LABEL: objFifo
-# CHECK: AIE.objectFifo.createObjectFifo(%{{.*}}, {%{{.*}}}, 2) : !AIE.objectFifo<memref<10xf32>>
+# CHECK: %[[VAL_0:.*]] = AIE.tile(6, 6)
+# CHECK: %[[VAL_1:.*]] = AIE.tile(2, 2)
+# CHECK: AIE.objectFifo @of0(%[[VAL_0]], {%[[VAL_1]]}, 2 : i32) : !AIE.objectFifo<memref<12xf16>>
 @constructAndPrintInModule
 def objFifo():
     iTy = IntegerType.get_signless(32)
-    two = IntegerAttr.get(iTy, 2)
-    six = IntegerAttr.get(iTy, 6)
-    tile0 = TileOp(IndexType.get(), six, six)
-    tile1 = TileOp(IndexType.get(), two, two)
-    dtype = F32Type.get()
-    memTy = MemRefType.get((10,), dtype)
-    fifo = ObjectFifoCreateOp(ObjectFifoType.get(memTy), tile0, tile1, two)
+    one = IntegerAttr.get(iTy, 1)
+    dev = DeviceOp(one)
+    bb = Block.create_at_start(dev.bodyRegion)
+    with InsertionPoint(bb):
+        two = IntegerAttr.get(iTy, 2)
+        six = IntegerAttr.get(iTy, 6)
+        tile0 = TileOp(IndexType.get(), six, six)
+        tile1 = TileOp(IndexType.get(), two, two)
+        dtype = F16Type.get()
+        memTy = MemRefType.get((12,), dtype)
+        ofTy = ObjectFifoType.get(memTy)
+        ObjectFifoCreateOp("of0", tile0, tile1, two, TypeAttr.get(ofTy))
+        EndOp()
