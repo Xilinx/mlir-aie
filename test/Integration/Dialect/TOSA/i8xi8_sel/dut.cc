15667c77e296783cf286681602b92b1ab0afc271
jamestcl-amd <linjames@xilinx.com>
2023-08-29 17:34:59 -0500

    Add i8 1-D add/mul/sub/sel TOSA tests

---
+++ test/Integration/Dialect/TOSA/i8xi8_sel/dut.cc
@@ -0,0 +1,18 @@
+void dut(int8_t * restrict v1, int8_t * restrict v2, int8_t * restrict v3) {
+  size_t v4 = 0;
+  size_t v5 = 1024;
+  size_t v6 = 64;
+  for (size_t v7 = v4; v7 < v5; v7 += v6)
+  chess_prepare_for_pipelining
+  chess_loop_range(16, 16)
+  {
+    v64int8 v8 = *(v64int8 *)(v1 + v7);
+    v64int8 v9 = *(v64int8 *)(v2 + v7);
+    uint64_t v10 = gt(v8, v9);
+    v64int8 v11 = sel(v9, v8, v10);
+    *(v64int8 *)(v3 + v7) = v11;
+  }
+  return;
+}
+
+
