8e5413911763a1cc3db4cdc0c91e43a40e372d81
jamestcl-amd <linjames@xilinx.com>
2023-09-05 16:36:18 -0700

    Add i16xi16_sub_elem_2d_broadcast_scalar passing test

---
+++ test/Integration/Dialect/TOSA/i16xi16_sub_elem_2d_broadcast_scalar/dut.cc
@@ -0,0 +1,28 @@
+void dut(int16_t * restrict v1, int16_t * restrict v2, int16_t * restrict v3) {
+  size_t v4 = 0;
+  int16_t * restrict v5 = v2;
+  v32int16 v6 = *(v32int16 *)(v5 + v4);
+  v32int16 v7 = broadcast_elem(v6, 0);
+  size_t v8 = 0;
+  size_t v9 = 16;
+  size_t v10 = 1;
+  for (size_t v11 = v8; v11 < v9; v11 += v10)
+  chess_prepare_for_pipelining
+  chess_loop_range(16, 16)
+  {
+    size_t v12 = 0;
+    size_t v13 = 1024;
+    size_t v14 = 32;
+    for (size_t v15 = v12; v15 < v13; v15 += v14)
+    chess_prepare_for_pipelining
+    chess_loop_range(32, 32)
+    {
+      v32int16 v16 = *(v32int16 *)(v1 + 1024*v11+v15);
+      v32int16 v17 = sub(v16, v7);
+      *(v32int16 *)(v3 + 1024*v11+v15) = v17;
+    }
+  }
+  return;
+}
+
+
