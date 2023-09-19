679c3ce4df923b54d5f55adc0fe9cea1cc47064c
James Lin <linjames@xilinx.com>
2023-09-19 17:43:27 -0500

    Refactor conversion of aievec.mul_elem to support combined precision (#643)
    * Refactor AIE-ML acc datatype emission
    * Refactor arith.muli/mulf to aievec.mul_elem conversion pattern to make it extensible and clean
      - Reorganize the existing case-by-case patterns and decouple the pattern that requires two inputs to be the same type
      - Make it a cleaner pattern considering lhs/rhs/out datatype
      - Verified that all the dut.cc are identical before/after the refactor
    * Add convertValueToTargetTypeAieML() which can be helpful for handling the vector lane mismatch issue later on.
    * Add CPP emission for aievec.unpack op
    * Add VectorToAIEVec lit tests to cover the lowering patterns
    * Add new combined precision tosa tests for element-wise multiply:
      - i8xi16_mul_elem_v32 (out=i32, lane=32) (cycle count=144, PM=272), PASS
      - i8xi16_mul_elem_v16 (out=i32, lane=16) (cycle count=792, PM=368), XFAIL
        - No intent to work on this at the moment, but keep a record there
      - i16xi32_mul_elem (out=i32, lane=16) (cycle count=408, PM=384), PASS
      - i8xi32_mul_elem (out=i32, lane=16) (cycle count=728, PM=368), PASS

--- test/unit_tests/aievec_tests/bf16xbf16_mul_elem/dut.cc
+++ test/unit_tests/aievec_tests/bf16xbf16_mul_elem/dut.cc
@@ -1,19 +1,23 @@
-void dut(bfloat16 *restrict v1, bfloat16 *restrict v2, bfloat16 *restrict v3) {
-  size_t v4 = 16;
-  size_t v5 = 1024;
-  size_t v6 = 0;
-  bfloat16 v7 = 0.0e+00;
-  v32bfloat16 v8 = broadcast_to_v32bfloat16(v7);
-  v16bfloat16 v9 = extract_v16bfloat16(v8, 0);
-  for (size_t v10 = v6; v10 < v5; v10 += v4)
-    chess_prepare_for_pipelining chess_loop_range(64, 64) {
-      v16bfloat16 v11 = *(v16bfloat16 *)(v1 + v10);
-      v16bfloat16 v12 = *(v16bfloat16 *)(v2 + v10);
-      v32bfloat16 v13 = concat(v11, v9);
-      v32bfloat16 v14 = concat(v12, v9);
-      v16accfloat v15 = mul_elem_16_2(v14, v13);
-      v16bfloat16 v16 = to_v16bfloat16(v15);
-      *(v16bfloat16 *)(v3 + v10) = v16;
-    }
+// clang-format off
+void dut(bfloat16 * restrict v1, bfloat16 * restrict v2, bfloat16 * restrict v3) {
+  bfloat16 v4 = 0.0e+00;
+  v32bfloat16 v5 = broadcast_to_v32bfloat16(v4);
+  v16bfloat16 v6 = extract_v16bfloat16(v5, 0);
+  size_t v7 = 0;
+  size_t v8 = 1024;
+  size_t v9 = 16;
+  for (size_t v10 = v7; v10 < v8; v10 += v9)
+  chess_prepare_for_pipelining
+  chess_loop_range(64, 64)
+  {
+    v16bfloat16 v11 = *(v16bfloat16 *)(v1 + v10);
+    v16bfloat16 v12 = *(v16bfloat16 *)(v2 + v10);
+    v32bfloat16 v13 = concat(v11, v6);
+    v32bfloat16 v14 = concat(v12, v6);
+    v16accfloat v15 = mul_elem_16_2(v14, v13);
+    v16bfloat16 v16 = to_v16bfloat16(v15);
+    *(v16bfloat16 *)(v3 + v10) = v16;
+  }
   return;
 }
+// clang-format on
