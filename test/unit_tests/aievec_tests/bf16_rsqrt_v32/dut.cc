f4eba9cb1b5e8cd66466f2ca1578f970a4e6a930
Lina Yu <108146828+linay-xsj@users.noreply.github.com>
2023-09-26 13:29:27 -0700

    Convert math.rsqrt to a function call getRsqrtBf16() for v16bfloat16 and v32bfloat16 types and reorganize files in aie_runtime_lib (#655)

--- test/unit_tests/aievec_tests/bf16_rsqrt_v32/dut.cc
+++ test/unit_tests/aievec_tests/bf16_rsqrt_v32/dut.cc
@@ -6,7 +6,7 @@ void dut(bfloat16 *restrict v1, bfloat16 *restrict v2) {
   for (size_t v6 = v3; v6 < v4; v6 += v5)
     chess_prepare_for_pipelining chess_loop_range(32, 32) {
       v32bfloat16 v7 = *(v32bfloat16 *)(v1 + v6);
-      v32bfloat16 v8 = getSqrtBf16(v7);
+      v32bfloat16 v8 = getRsqrtBf16(v7);
       *(v32bfloat16 *)(v2 + v6) = v8;
     }
   return;
