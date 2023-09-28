7adeff91db5d9099fe9db675051eca49cc608314
Lina Yu <linay@xsjlinay40x.xlnx.xilinx.com>
2023-09-28 10:20:55 -0700

    Lower tosa.abs, math.absi and math.absf patterns to aievec dialects for i8, i16, i32, float and bfloat16 types

---
+++ test/unit_tests/aievec_tests/bf16_abs_v32/dut.cc
@@ -0,0 +1,13 @@
+#include "vec_math.h"
+void dut(bfloat16 *restrict v1, bfloat16 *restrict v2) {
+  size_t v3 = 0;
+  size_t v4 = 1024;
+  size_t v5 = 32;
+  for (size_t v6 = v3; v6 < v4; v6 += v5)
+    chess_prepare_for_pipelining chess_loop_range(32, 32) {
+      v32bfloat16 v7 = *(v32bfloat16 *)(v1 + v6);
+      v32bfloat16 v8 = getAbs(v7);
+      *(v32bfloat16 *)(v2 + v6) = v8;
+    }
+  return;
+}
