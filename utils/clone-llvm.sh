d6c1c9fb7721905888034b566ced777f94036d59
Jeff Fifield <jeff.fifield@amd.com>
2023-10-09 14:19:28 -0600

    Update llvm to d13da15

--- utils/clone-llvm.sh
+++ utils/clone-llvm.sh
@@ -15,7 +15,7 @@
 # The LLVM commit to use.
 # TODO: create a branch or a tag instead, to avoid fetching main and
 # this commit later.
-commithash=11c3b979e6512b00a5bd9c3e0d4ed986cf500630
+commithash=d13da154a7c7eff77df8686b2de1cfdfa7cc7029
 
 here=$PWD
 
