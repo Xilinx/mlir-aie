eb7b6e9a37f05489d4502d7fb9a419c3f93083d0
STFleming <sfleming@amd.com>
2023-11-14 17:33:01 +0000

    Added small README for simple printf example reference design

--- reference_designs/log_hello_world/elfStringParser.py
+++ reference_designs/log_hello_world/elfStringParser.py
@@ -54,6 +54,7 @@ def main():
     parser.add_argument("--output", default="elfstrings.csv")
     args = parser.parse_args()
     
+<<<<<<< HEAD
     # Collect all the elfs
     ofiles = []
     for filename in os.listdir(args.input):
@@ -69,6 +70,13 @@ def main():
         ro_offset = _get_ro_offset(ofile)
         d = _gen_string_dict(object_strings_str, ro_offset) 
         res = {**res, **d}
+=======
+    ofile = args.input+"/core_0_2.elf"
+    strings_cmd = f"strings --radix x -a {ofile}"
+    object_strings_str = call_unix_proc(strings_cmd)
+    ro_offset = _get_ro_offset(ofile)
+    res = _gen_string_dict(object_strings_str, ro_offset)
+>>>>>>> 7dfe8e2d (Added small README for simple printf example reference design)
     with open(args.output, "w") as fp:
         for addr,s in res.items():
             fp.write(f"{addr},{s}\n")
