588a69a40e5ec391b2de8f7a9d4dc2f971459491
STFleming <sfleming@amd.com>
2023-11-14 14:02:51 +0000

    Initial hello world PoC example up, some issues with more than 1 parameter in the decoder that I need to investigate.

---
+++ reference_designs/hello_world/kernel.cc
@@ -0,0 +1,37 @@
+#define NOCPP
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define REL_WRITE 0
+#define REL_READ 1
+
+#include <aie_api/aie.hpp>
+
+#include "ipulog.h"
+
+extern "C" {
+
+void kernel(uint32_t* in_buffer, uint32_t* out_buffer, uint8_t* logbuffer){
+
+	IPULogger log(logbuffer, 2048);
+	log.write("Starting kernel execution!\n");
+
+	uint32_t col = (get_coreid() >> 16) & 0x0000FFFF;
+	uint32_t row = get_coreid() & 0x0000FFFF;
+
+	aie::tile tile=aie::tile::current();
+        uint64_t Tstart = tile.cycles(); 
+	log.write("Core col=%u\n", col); 
+	log.write("Core row=%u\n", row); 
+
+	memcpy(out_buffer, in_buffer, 2048);
+
+        uint64_t Tend = tile.cycles(); 
+        uint64_t cycles = Tend - Tstart;
+        log.write("Completed executing. cycles=%u\n", cycles);
+
+    }	
+
+}
