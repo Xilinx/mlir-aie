acf177b47ec513e48c0a527800c48d53d1aa51e1
STFleming <sfleming@amd.com>
2023-11-15 10:32:39 +0000

    Adding licenses

--- reference_designs/log_hello_world/decodelog.hpp
+++ reference_designs/log_hello_world/decodelog.hpp
@@ -1,3 +1,13 @@
+//===- decodelog.hpp ---------------------------------------000---*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2023, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
 #include "xrt/xrt_bo.h"
 #include <boost/filesystem.hpp>
 #include <boost/tokenizer.hpp>
