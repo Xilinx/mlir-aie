045f2d37927df8028dc8b821d35c1bba6f00dc50
Jack Lo <jack.lo@amd.com>
2023-11-16 00:23:42 -0800

    Changed xchesscc calls to xchesscc_wrapper

--- reference_designs/ipu-xrt/vision_pipelines/edge_detect/aie2_lineBased_8b_tiny.py
+++ reference_designs/ipu-xrt/vision_pipelines/edge_detect/aie2_lineBased_8b_tiny.py
@@ -53,8 +53,8 @@ def edge_detect():
         def coreBody():
             @forLoop(lowerBound = 0, upperBound = 36, step = 1)
             def loopBody():
-                elemIn = Acquire("inOF_L2L1", ObjectFifoPort.Consume, 1, memRef_256_ty).acquiredElem()
-                elemOut = Acquire("OF_2to3", ObjectFifoPort.Produce, 1, memRef_64_ty).acquiredElem()
+                elemIn = Acquire(ObjectFifoPort.Consume, "inOF_L2L1", 1, memRef_256_ty).acquiredElem()
+                elemOut = Acquire(ObjectFifoPort.Produce, "OF_2to3", 1, memRef_64_ty).acquiredElem()
 
                 Call(rgba2grayLine, [elemIn, elemOut, integerConstant(64)])
 
@@ -78,23 +78,23 @@ def edge_detect():
             Store(v0, kernel, [2, 2])
             
             # Preamble : Top Border
-            elemsInPre = Acquire("OF_2to3", ObjectFifoPort.Consume, 2, memRef_64_ty).acquiredElem()
-            elemPreOut = Acquire("OF_3to4", ObjectFifoPort.Produce, 1, memRef_64_ty).acquiredElem()
+            elemsInPre = Acquire(ObjectFifoPort.Consume, "OF_2to3", 2, memRef_64_ty).acquiredElem()
+            elemPreOut = Acquire(ObjectFifoPort.Produce, "OF_3to4", 1, memRef_64_ty).acquiredElem()
             Call(filter2dLine, [elemsInPre[0], elemsInPre[0], elemsInPre[1], elemPreOut, integerConstant(64), kernel])
             Release(ObjectFifoPort.Produce, "OF_3to4", 1) 
 
             # Steady State : Middle
             @forLoop(lowerBound = 1, upperBound = 35, step = 1)
             def loopBody():
-                elemsIn = Acquire("OF_2to3", ObjectFifoPort.Consume, 3, memRef_64_ty).acquiredElem()
-                elemOut = Acquire("OF_3to4", ObjectFifoPort.Produce, 1, memRef_64_ty).acquiredElem()
+                elemsIn = Acquire(ObjectFifoPort.Consume, "OF_2to3", 3, memRef_64_ty).acquiredElem()
+                elemOut = Acquire(ObjectFifoPort.Produce, "OF_3to4", 1, memRef_64_ty).acquiredElem()
                 Call(filter2dLine, [elemsIn[0], elemsIn[1], elemsIn[2], elemOut, integerConstant(64), kernel])
                 Release(ObjectFifoPort.Consume, "OF_2to3", 1)
                 Release(ObjectFifoPort.Produce, "OF_3to4", 1)
 
             # Postamble : Bottom Border
-            elemsInPost = Acquire("OF_2to3", ObjectFifoPort.Consume, 2, memRef_64_ty).acquiredElem()
-            elemPostOut = Acquire("OF_3to4", ObjectFifoPort.Produce, 1, memRef_64_ty).acquiredElem()
+            elemsInPost = Acquire(ObjectFifoPort.Consume, "OF_2to3", 2, memRef_64_ty).acquiredElem()
+            elemPostOut = Acquire(ObjectFifoPort.Produce, "OF_3to4", 1, memRef_64_ty).acquiredElem()
             Call(filter2dLine, [elemsInPost[0], elemsInPost[1], elemsInPost[1], elemPostOut, integerConstant(64), kernel])
             Release(ObjectFifoPort.Consume, "OF_2to3", 2)
             Release(ObjectFifoPort.Produce, "OF_3to4", 1) 
@@ -106,8 +106,8 @@ def edge_detect():
             vTyp = integerConstant(0, int8_ty)
             @forLoop(lowerBound = 0, upperBound = 36, step = 1)
             def loopBody():
-                elemIn = Acquire("OF_3to4", ObjectFifoPort.Consume, 1, memRef_64_ty).acquiredElem()
-                elemOut = Acquire("OF_4to5", ObjectFifoPort.Produce, 1, memRef_64_ty).acquiredElem()
+                elemIn = Acquire(ObjectFifoPort.Consume, "OF_3to4", 1, memRef_64_ty).acquiredElem()
+                elemOut = Acquire(ObjectFifoPort.Produce, "OF_4to5", 1, memRef_64_ty).acquiredElem()
 
                 Call(thresholdLine, [elemIn, elemOut, integerConstant(64), vThr, vMax, vTyp])
 
@@ -118,17 +118,17 @@ def edge_detect():
         def coreBody():
             @forLoop(lowerBound = 0, upperBound = 36, step = 1)
             def loopBody():
-                elemIn = Acquire("OF_4to5", ObjectFifoPort.Consume, 1, memRef_64_ty).acquiredElem()
-                elemOut = Acquire("OF_5to5", ObjectFifoPort.Produce, 1, memRef_256_ty).acquiredElem()
+                elemIn = Acquire(ObjectFifoPort.Consume, "OF_4to5", 1, memRef_64_ty).acquiredElem()
+                elemOut = Acquire(ObjectFifoPort.Produce, "OF_5to5", 1, memRef_256_ty).acquiredElem()
 
                 Call(gray2rgbaLine, [elemIn, elemOut, integerConstant(64)])
 
                 Release(ObjectFifoPort.Consume, "OF_4to5", 1)
                 Release(ObjectFifoPort.Produce, "OF_5to5", 1)
 
-                elemIn1 = Acquire("OF_5to5", ObjectFifoPort.Consume, 1, memRef_256_ty).acquiredElem()
-                elemIn2 = Acquire("inOF_L2L1", ObjectFifoPort.Consume, 1, memRef_256_ty).acquiredElem()
-                elemOut2 = Acquire("outOF_L1L2", ObjectFifoPort.Produce, 1, memRef_256_ty).acquiredElem()
+                elemIn1 = Acquire(ObjectFifoPort.Consume, "OF_5to5", 1, memRef_256_ty).acquiredElem()
+                elemIn2 = Acquire(ObjectFifoPort.Consume, "inOF_L2L1", 1, memRef_256_ty).acquiredElem()
+                elemOut2 = Acquire(ObjectFifoPort.Produce, "outOF_L1L2", 1, memRef_256_ty).acquiredElem()
 
                 alpha = integerConstant(16384, int16_ty)
                 beta = integerConstant(16384, int16_ty)
