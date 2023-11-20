ed96d84e0bdbb5634d178f6940e0b44f3a87f4d3
Kristof Denolf <kristof.denolf@amd.com>
2023-11-20 15:23:39 -0700

    applied format patch

--- reference_designs/log_hello_world/decodelog.hpp
+++ reference_designs/log_hello_world/decodelog.hpp
@@ -1,4 +1,5 @@
-//===- decodelog.hpp ---------------------------------------000---*- C++ -*-===//
+//===- decodelog.hpp ---------------------------------------000---*- C++
+//-*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -9,8 +10,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "xrt/xrt_bo.h"
-#include <filesystem>
 #include <boost/tokenizer.hpp>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <map>
--- reference_designs/log_hello_world/ipulog.h
+++ reference_designs/log_hello_world/ipulog.h
@@ -1,4 +1,5 @@
-//===- ipulog.h --------------------------------------------000---*- C++ -*-===//
+//===- ipulog.h --------------------------------------------000---*- C++
+//-*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
