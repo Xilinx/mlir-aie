aaaebf9115b2ac9b1dd06558b0624a348b4e4263
Joseph Melber <jgmelber@gmail.com>
2023-12-05 10:43:11 -0700

    Update reference_designs/ipu-xrt/vision_pipelines/color_threshold/aie2_colorThreshold.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- reference_designs/ipu-xrt/vision_pipelines/color_threshold/aie2_colorThreshold.py
+++ reference_designs/ipu-xrt/vision_pipelines/color_threshold/aie2_colorThreshold.py
@@ -129,9 +129,13 @@ def color_threshold():
 
                     # RTPs written from the instruction stream must be read right before the kernel
                     # after the ObjectFIFO acquires
-                    thresholdValue = arith.trunci(T.i16(), memref.load(rtpComputeTile2, [0]))
-                    maxValue       = arith.trunci(T.i16(), memref.load(rtpComputeTile2, [1]))
-                    thresholdType  = arith.trunci(T.i8(),  memref.load(rtpComputeTile2, [2]))
+                    thresholdValue = arith.trunci(
+                        T.i16(), memref.load(rtpComputeTile2, [0])
+                    )
+                    maxValue = arith.trunci(T.i16(), memref.load(rtpComputeTile2, [1]))
+                    thresholdType = arith.trunci(
+                        T.i8(), memref.load(rtpComputeTile2, [2])
+                    )
                     # maxValue = arith.constant(255, T.i16())
                     # thresholdValue = arith.constant(50, T.i16())
                     # thresholdType = arith.constant(0, T.i8())
