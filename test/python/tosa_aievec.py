e4296994f609608d232d0954187e2cc02ff0c81c
Maksim Levental <maksim.levental@gmail.com>
2023-12-12 22:47:04 -0600

    fix enable_python_tests (#862)

--- test/python/tosa_aievec.py
+++ test/python/tosa_aievec.py
@@ -41,35 +41,3 @@ def test_emit():
     assert hasattr(demo_fun1, "emit")
     assert inspect.ismethod(demo_fun1.emit)
     demo_fun1.emit()
-
-
-def test_location_tracking():
-    with mlir_mod_ctx() as ctx:
-
-        frameinfo = getframeinfo(currentframe())
-
-        @func
-        def demo_fun1():
-            one = arith.constant(1)
-            return one
-
-        demo_fun1.emit()
-
-    asm = get_asm(ctx.module.operation)
-    correct = dedent(
-        f"""\
-    module {{
-      func.func @demo_fun1() -> i32 {{
-        %c1_i32 = arith.constant 1 : i32 THIS_DIR/tosa_aievec.py:{frameinfo.lineno + 4}:18
-        return %c1_i32 : i32 [unknown]
-      }} THIS_DIR/tosa_aievec.py:{frameinfo.lineno + 2}:9
-    }} [unknown]
-    #loc = [unknown]
-    #loc1 = THIS_DIR/tosa_aievec.py:{frameinfo.lineno + 2}:9
-    #loc2 = THIS_DIR/tosa_aievec.py:{frameinfo.lineno + 4}:18
-    """
-    )
-    assert asm == correct
-
-
-test_location_tracking()
