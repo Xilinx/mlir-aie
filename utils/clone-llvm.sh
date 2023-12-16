04c272112440706dc2e471eddbe57a26a4525bac
Maksim Levental <maksim.levental@gmail.com>
2023-12-15 19:54:51 -0600

    expand test matrix (#868)

--- utils/clone-llvm.sh
+++ utils/clone-llvm.sh
@@ -13,11 +13,23 @@
 ##===----------------------------------------------------------------------===##
 
 # The LLVM commit to use.
-# TODO: create a branch or a tag instead, to avoid fetching main and
-# this commit later.
-commithash=d36b483f4f1109f53399ef82fda32f2c04d4ef44
+COMMITHASH=d36b483
+DATETIME=2023121521
+WHEEL_VERSION=18.0.0.$DATETIME+$COMMITHASH
+############################################################################################
+# The way to bump `COMMITHASH`:
+#   1. Find the hash you want (`git rev-parse --short=8 HEAD` or just copy paste from github);
+#   2. Go to mlir-aie github actions and launch an MLIR Distro workflow to build against that hash (see docs/Dev.md);
+#   3. Look under the Get latest LLVM commit job -> Get llvm-project commit step -> DATETIME;
+#   4. Record it here and push up a PR; the PR will fail until MLIR Distro workflow.
+############################################################################################
 
 here=$PWD
+
+if [ x"$1" == x--get-wheel-version ]; then
+  echo $WHEEL_VERSION
+  exit 0
+fi
 
 # Use --worktree <directory-of-local-LLVM-repo> to reuse some existing
 # local LLVM git repository
@@ -27,7 +39,7 @@ if [ x"$1" == x--llvm-worktree ]; then
     cd $git_central_llvm_repo_dir
     # Use force just in case there are various experimental iterations
     # after you have removed the llvm directory
-    git worktree add --force "$here"/llvm $commithash
+    git worktree add --force "$here"/llvm $COMMITHASH
   )
 else
   # Fetch main first just to clone
@@ -35,7 +47,7 @@ else
   (
     cd llvm
     # Then fetch the interesting part
-    git fetch --depth=1 origin $commithash
-    git checkout $commithash
+    git fetch --depth=1 origin $COMMITHASH
+    git checkout $COMMITHASH
   )
 fi
