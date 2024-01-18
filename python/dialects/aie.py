56ee3958dfb2a54017b777697d32909d68a03c63
abisca <andrab@amd.com>
2024-01-18 09:40:30 -0700

    Fix

--- python/dialects/aie.py
+++ python/dialects/aie.py
@@ -200,17 +200,15 @@ class objectfifo_link(ObjectFifoLinkOp):
         self,
         fifoIns,
         fifoOuts,
-        fifoInRefs = map(
-            lambda i: i if isinstance(i, str) else i.sym_name.value, fifoIns
-        )
-        fifoOutRefs = map(
-            lambda i: i if isinstance(i, str) else i.sym_name.value, fifoOuts
-        )
+    ):
+        if not isinstance(fifoIns, List):
             fifoIns = [fifoIns]
         if not isinstance(fifoOuts, List):
             fifoOuts = [fifoOuts]
-        fifoInRefs = map(lambda i: i if isinstance(i, str) else i.sym_name.value, fifoIns)
-        fifoOutRefs = map(lambda i: i if isinstance(i, str) else i.sym_name.value, fifoOuts)
+        fifoInRefs = 
+            map(lambda i: i if isinstance(i, str) else i.sym_name.value, fifoIns)
+        fifoOutRefs = 
+            map(lambda i: i if isinstance(i, str) else i.sym_name.value, fifoOuts)
         super().__init__(
             fifoIns=fifoInRefs,
             fifoOuts=fifoOutRefs,
