5001d6fa6a8ebccb9f8dd202e297d1d6d3a4d472
Jack Lo <36210336+jackl-xilinx@users.noreply.github.com>
2024-02-07 12:02:56 -0800

    Update quick_setup.sh to remove cert check for github

--- utils/quick_setup.sh
+++ utils/quick_setup.sh
@@ -61,12 +61,12 @@ if test -f "$VPP"; then
   AIETOOLS="`dirname $VPP`/../aietools"
   mkdir -p my_install
   pushd my_install
-  wget -q --show-progress https://github.com/Xilinx/mlir-aie/releases/download/latest-wheels/mlir_aie-0.0.1.2024020616+bde53fc-py3-none-manylinux_2_35_x86_64.whl
+  wget -q --show-progress --no-check-certificate https://github.com/Xilinx/mlir-aie/releases/download/latest-wheels/mlir_aie-0.0.1.2024020616+bde53fc-py3-none-manylinux_2_35_x86_64.whl
   unzip -q mlir_aie-*-py3-none-manylinux_*_x86_64.whl
   sed -i "s^TARGET_AIE_LIBDIR=.*^TARGET_AIE_LIBDIR=\"$AIETOOLS/data/versal_prod/lib\"^g" mlir_aie/bin/xchesscc_wrapper
   sed -i "s^TARGET_AIE2_LIBDIR=.*^TARGET_AIE2_LIBDIR=\"$AIETOOLS/data/aie_ml/lib\"^g" mlir_aie/bin/xchesscc_wrapper
   sed -i "s^AIETOOLS=.*^AIETOOLS=\"$AIETOOLS\"^g" mlir_aie/bin/xchesscc_wrapper
-  wget -q --show-progress https://github.com/Xilinx/mlir-aie/releases/download/mlir-distro/mlir-19.0.0.2024013022+24923214-py3-none-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl
+  wget -q --show-progress --no-check-certificate https://github.com/Xilinx/mlir-aie/releases/download/mlir-distro/mlir-19.0.0.2024013022+24923214-py3-none-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl
   unzip -q mlir-*-py3-none-manylinux_*_x86_64.manylinux_*_x86_64.whl
   pip install https://github.com/makslevental/mlir-python-extras/archive/d84f05582adb2eed07145dabce1e03e13d0e29a6.zip
   rm -rf mlir*.whl
