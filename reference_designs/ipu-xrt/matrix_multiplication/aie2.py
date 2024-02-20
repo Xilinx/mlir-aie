62b51b841815eb390ec8ef40b70e7e7ae5cf9435
andrej <an.roesti@gmail.com>
2024-02-20 12:53:32 -0800

    add tracing to matrix multiply reference design

--- reference_designs/ipu-xrt/matrix_multiplication/aie2.py
+++ reference_designs/ipu-xrt/matrix_multiplication/aie2.py
@@ -5,16 +5,16 @@
 #
 # (c) Copyright 2023 AMD Inc.
 
-from aie.dialects.aie import *
-from aie.dialects.aiex import *
-from aie.dialects.scf import *
+import aie.dialects.aie  as AIE
+import aie.dialects.aiex as AIEX 
+import aie.dialects.scf  as SCF 
 from aie.extras.context import mlir_mod_ctx
 
 
 def my_matmul():
-    M = 128
-    K = 128
-    N = 128
+    M = 256
+    K = 256
+    N = 256
     m = 64
     k = 32
     n = 64
@@ -23,6 +23,10 @@ def my_matmul():
     t = 4
     word_size_in = 2
     word_size_out = 4
+
+    vectorized = True
+    enable_tracing = True
+    trace_size = 4096
 
     A_sz_in_i32s = M * K * word_size_in // 4
     B_sz_in_i32s = K * N * word_size_in // 4
@@ -48,40 +52,40 @@ def my_matmul():
     N_in_i32s_out = N * word_size_out // 4
     m_x_N_in_i32s_out = m * N * word_size_out // 4
 
-    vectorized = True
 
     with mlir_mod_ctx() as ctx:
 
-        @device(AIEDevice.ipu)
+        @AIE.device(AIE.AIEDevice.ipu)
         def device_body():
-            memref_a_ty = T.memref(m, k, T.bf16())
-            memref_b_ty = T.memref(k, n, T.bf16())
-            memref_c_ty = T.memref(m, n, T.f32())
+            memref_a_ty = AIE.T.memref(m, k, AIE.T.bf16())
+            memref_b_ty = AIE.T.memref(k, n, AIE.T.bf16())
+            memref_c_ty = AIE.T.memref(m, n, AIE.T.f32())
 
-            ofifo_memref_a_ty = TypeAttr.get(ObjectFifoType.get(memref_a_ty))
-            ofifo_memref_b_ty = TypeAttr.get(ObjectFifoType.get(memref_b_ty))
-            ofifo_memref_c_ty = TypeAttr.get(ObjectFifoType.get(memref_c_ty))
+            ofifo_memref_a_ty = SCF.TypeAttr.get(AIE.ObjectFifoType.get(memref_a_ty))
+            ofifo_memref_b_ty = SCF.TypeAttr.get(AIE.ObjectFifoType.get(memref_b_ty))
+            ofifo_memref_c_ty = SCF.TypeAttr.get(AIE.ObjectFifoType.get(memref_c_ty))
 
             # AIE Core Function declarations
-            zero_scalar = external_func("zero_scalar_f32", inputs=[memref_c_ty])
-            zero = external_func("zero_f32", inputs=[memref_c_ty])
-            matmul_scalar = external_func(
+            zero_scalar = AIE.external_func("zero_scalar_f32", inputs=[memref_c_ty])
+            zero = AIE.external_func("zero_f32", inputs=[memref_c_ty])
+            matmul_scalar = AIE.external_func(
                 "matmul_scalar_bf16_f32",
                 inputs=[memref_a_ty, memref_b_ty, memref_c_ty],
             )
-            matmul = external_func(
+            matmul = AIE.external_func(
                 "matmul_bf16_f32", inputs=[memref_a_ty, memref_b_ty, memref_c_ty]
             )
 
             # Tile declarations
-            shim_tile = tile(0, 0)
-            mem_tile = tile(0, 1)
-            compute_tile2 = tile(0, 2)
+            shim_tile = AIE.tile(0, 0)
+            mem_tile  = AIE.tile(0, 1)
+            compute_tile2_col, compute_tile2_row = 0, 2
+            compute_tile2 = AIE.tile(compute_tile2_col, compute_tile2_row)
 
             # AIE-array data movement with object fifos
             # Input A
-            objectfifo("inA", shim_tile, [mem_tile], 2, ofifo_memref_a_ty, [], [])
-            objectfifo(
+            AIE.objectfifo("inA", shim_tile, [mem_tile], 2, ofifo_memref_a_ty, [], [])
+            AIE.objectfifo(
                 "memA",
                 mem_tile,
                 [compute_tile2],
@@ -95,11 +99,11 @@ def my_matmul():
                 ],
                 [],
             )
-            objectfifo_link(["inA"], ["memA"])
+            AIE.objectfifo_link(["inA"], ["memA"])
 
             # Input B
-            objectfifo("inB", shim_tile, [mem_tile], 2, ofifo_memref_b_ty, [], [])
-            objectfifo(
+            AIE.objectfifo("inB", shim_tile, [mem_tile], 2, ofifo_memref_b_ty, [], [])
+            AIE.objectfifo(
                 "memB",
                 mem_tile,
                 [compute_tile2],
@@ -113,11 +117,11 @@ def my_matmul():
                 ],
                 [],
             )
-            objectfifo_link(["inB"], ["memB"])
+            AIE.objectfifo_link(["inB"], ["memB"])
 
             # Output C
-            objectfifo("memC", compute_tile2, [mem_tile], 2, ofifo_memref_c_ty, [], [])
-            objectfifo(
+            AIE.objectfifo("memC", compute_tile2, [mem_tile], 2, ofifo_memref_c_ty, [], [])
+            AIE.objectfifo(
                 "outC",
                 mem_tile,
                 [shim_tile],
@@ -131,50 +135,107 @@ def my_matmul():
                 ],
                 [],
             )
-            objectfifo_link(["memC"], ["outC"])
+            AIE.objectfifo_link(["memC"], ["outC"])
+
+            # Set up a circuit-switched flow from core to shim for tracing information
+            if enable_tracing:
+                AIE.flow(compute_tile2, AIE.WireBundle.Trace, 0, shim_tile, AIE.WireBundle.DMA, 1)
 
             # Set up compute tiles
 
             # Compute tile 2
-            @core(compute_tile2, "mm.o")
+            @AIE.core(compute_tile2, "mm.o")
             def core_body():
-                for _ in for_(0xFFFFFFFF):
-                    for _ in for_(tiles):
-                        elem_out = acquire(
-                            ObjectFifoPort.Produce, "memC", 1, memref_c_ty
+                for _ in SCF.for_(0xFFFFFFFF):
+                    for _ in SCF.for_(tiles):
+                        elem_out = AIE.acquire(
+                            AIE.ObjectFifoPort.Produce, "memC", 1, memref_c_ty
                         ).acquired_elem()
                         if vectorized:
-                            Call(zero, [elem_out])
+                            AIE.Call(zero, [elem_out])
                         else:
-                            Call(zero_scalar, [elem_out])
+                            AIE.Call(zero_scalar, [elem_out])
 
-                        for _ in for_(K_div_k):
-                            elem_in_a = acquire(
-                                ObjectFifoPort.Consume, "memA", 1, memref_a_ty
+                        for _ in SCF.for_(K_div_k):
+                            elem_in_a = AIE.acquire(
+                                AIE.ObjectFifoPort.Consume, "memA", 1, memref_a_ty
                             ).acquired_elem()
-                            elem_in_b = acquire(
-                                ObjectFifoPort.Consume, "memB", 1, memref_b_ty
+                            elem_in_b = AIE.acquire(
+                                AIE.ObjectFifoPort.Consume, "memB", 1, memref_b_ty
                             ).acquired_elem()
                             if vectorized:
-                                Call(matmul, [elem_in_a, elem_in_b, elem_out])
+                                AIE.Call(matmul, [elem_in_a, elem_in_b, elem_out])
                             else:
-                                Call(matmul_scalar, [elem_in_a, elem_in_b, elem_out])
-                            objectfifo_release(ObjectFifoPort.Consume, "memA", 1)
-                            objectfifo_release(ObjectFifoPort.Consume, "memB", 1)
-                            yield_([])
+                                AIE.Call(matmul_scalar, [elem_in_a, elem_in_b, elem_out])
+                            AIE.objectfifo_release(AIE.ObjectFifoPort.Consume, "memA", 1)
+                            AIE.objectfifo_release(AIE.ObjectFifoPort.Consume, "memB", 1)
+                            SCF.yield_([])
 
-                        objectfifo_release(ObjectFifoPort.Produce, "memC", 1)
-                        yield_([])
-                    yield_([])
+                        AIE.objectfifo_release(AIE.ObjectFifoPort.Produce, "memC", 1)
+                        SCF.yield_([])
+                    SCF.yield_([])
 
             # To/from AIE-array data movement
 
-            @FuncOp.from_py_func(
-                T.memref(A_sz_in_i32s, T.i32()),
-                T.memref(B_sz_in_i32s, T.i32()),
-                T.memref(C_sz_in_i32s, T.i32()),
+            @AIE.FuncOp.from_py_func(
+                AIE.T.memref(A_sz_in_i32s, AIE.T.i32()),
+                AIE.T.memref(B_sz_in_i32s, AIE.T.i32()),
+                AIE.T.memref(C_sz_in_i32s, AIE.T.i32()),
             )
             def sequence(A, B, C):
+
+                # Configure tracing, see https://github.com/Xilinx/mlir-aie/blob/resnet/docs/Tracing.md
+                if enable_tracing:
+                    # 0x340D0: Trace Control 0
+                    #          0xAABB---C
+                    #            AA        <- Event to stop trace capture
+                    #              BB      <- Event to start trace capture
+                    #                   C  <- Trace mode, 00=event=time, 01=event-PC, 10=execution
+                    # Configure so that "Event 1" (always true) causes tracing to start
+                    AIEX._generated_ipu_write32(column=compute_tile2_col, row=compute_tile2_row, address=0x340D0, value=0x00010000)
+                    # 0x340D4: Trace Control 1
+                    AIEX._generated_ipu_write32(column=compute_tile2_col, row=compute_tile2_row, address=0x340D4, value=0x00000000)
+                    # 0x340E0: Trace Event Group 1  (Which events to trace)
+                    #          0xAABBCCDD    AA, BB, CC, DD <- four event slots
+                    AIEX._generated_ipu_write32(column=compute_tile2_col, row=compute_tile2_row, address=0x340E0, value=0x4B222125)
+                    # 0x340E4: Trace Event Group 2  (Which events to trace)
+                    #          0xAABBCCDD    AA, BB, CC, DD <- four event slots
+                    AIEX._generated_ipu_write32(column=compute_tile2_col, row=compute_tile2_row, address=0x340E4, value=0x00000000)
+
+                    # Configure a buffer descriptor to write tracing information that has been routed into this shim tile
+                    # out to host DDR memory
+                    trace_bd_id = 13  # use BD 13 for writing trace output from compute tile to DDR host memory
+                    output_size = C_sz_in_bytes
+                    AIEX._generated_ipu_writebd_shimtile(
+                            bd_id              =  trace_bd_id,
+                            buffer_length      =   trace_size,
+                            buffer_offset      =  output_size,
+                            enable_packet      =            0,
+                            out_of_order_id    =            0,
+                            packet_id          =            0,
+                            packet_type        =            0,
+                            column             =            0,
+                            column_num         =            1,
+                            d0_size            =            0,
+                            d0_stride          =            0,
+                            d1_size            =            0,
+                            d1_stride          =            0,
+                            d2_stride          =            0, 
+                            ddr_id             =            2,
+                            iteration_current  =            0,
+                            iteration_size     =            0,
+                            iteration_stride   =            0,
+                            lock_acq_enable    =            0,
+                            lock_acq_id        =            0,
+                            lock_acq_val       =            0,
+                            lock_rel_id        =            0,
+                            lock_rel_val       =            0,
+                            next_bd            =            0,
+                            use_next_bd        =            0,
+                            valid_bd           =            1)
+                    # Set start BD to our shim bd_Id (3)
+                    AIEX._generated_ipu_write32(column=0, row=0, address=0x1D20C, value=trace_bd_id)
+
                 # only do 5 tile rows at a time before synchronizing, so we can reuse BDs
                 rows_per_block = 5
                 for tile_row_block in range(
@@ -186,7 +247,7 @@ def my_matmul():
                     num_tile_rows = min(
                         [rows_per_block, M_div_m - tile_row_block * rows_per_block]
                     )
-                    ipu_dma_memcpy_nd(
+                    AIEX.ipu_dma_memcpy_nd(
                         metadata="outC",
                         bd_id=0,
                         mem=C,
@@ -202,7 +263,7 @@ def my_matmul():
                             * word_size_in
                             // 4
                         )
-                        ipu_dma_memcpy_nd(
+                        AIEX.ipu_dma_memcpy_nd(
                             metadata="inA",
                             bd_id=2 * tile_row + 1,
                             mem=A,
@@ -210,7 +271,7 @@ def my_matmul():
                             sizes=[N_div_n, K_div_k, m, k_in_i32s],
                             strides=[0, k_in_i32s, K_in_i32s],
                         )
-                        ipu_dma_memcpy_nd(
+                        AIEX.ipu_dma_memcpy_nd(
                             metadata="inB",
                             bd_id=2 * tile_row + 2,
                             mem=B,
@@ -218,7 +279,7 @@ def my_matmul():
                             strides=[n_in_i32s, k_x_N_in_i32s, N_in_i32s],
                         )
 
-                    ipu_sync(column=0, row=0, direction=0, channel=0)
+                    AIEX.ipu_sync(column=0, row=0, direction=0, channel=0)
 
     print(ctx.module)
 
