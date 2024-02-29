13a253ac1fbcf6dc9befed98795259bef7b6fdf7
makslevental <maksim.levental@gmail.com>
2024-02-28 20:52:29 -0600

    patch in eric's change to bootgen/CMake

--- runtime_lib/xaiengine/aiert.cmake
+++ runtime_lib/xaiengine/aiert.cmake
@@ -76,6 +76,10 @@ function(add_aiert_library TARGET XAIE_SOURCE)
     ${XAIE_SOURCE}/util)
 
   add_library(${TARGET} ${LIBTYPE} ${libsources})
-  target_compile_options(${TARGET} PRIVATE -fPIC -Wno-gnu-designator)
+  set_property(TARGET ${TARGET} PROPERTY C_STANDARD 99)
+
+  if (NOT CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    target_compile_options(${TARGET} PRIVATE -fPIC -Wno-gnu-designator)
+  endif()
 
 endfunction()
