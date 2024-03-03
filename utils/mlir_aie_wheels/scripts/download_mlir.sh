c6028c4ad668f431b265533f3c199f2a7ba52afd
max <maksim.levental@gmail.com>
2024-03-02 23:21:14 -0600

    try upgrading to macos-12

--- utils/mlir_aie_wheels/scripts/download_mlir.sh
+++ utils/mlir_aie_wheels/scripts/download_mlir.sh
@@ -12,6 +12,8 @@ fi
 if [ x"$CIBW_ARCHS" == x"arm64" ] || [ x"$CIBW_ARCHS" == x"aarch64" ]; then
   if [ x"$MATRIX_OS" == x"macos-11" ] && [ x"$CIBW_ARCHS" == x"arm64" ]; then
     PLAT=macosx_11_0_arm64
+  elif [ x"$MATRIX_OS" == x"macos-14" ] && [ x"$CIBW_ARCHS" == x"arm64" ]; then
+    PLAT=macosx_11_0_arm64
   elif [ x"$MATRIX_OS" == x"ubuntu-20.04" ] && [ x"$CIBW_ARCHS" == x"aarch64" ]; then
     PLAT=linux_aarch64
   fi
