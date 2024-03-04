4ef9251d402a6be22af256b36ed6d7687648d1ff
makslevental <maksim.levental@gmail.com>
2024-03-04 17:55:10 -0600

    rename bdnum -> bdid

--- lib/Dialect/AIE/IR/AIEDialect.cpp
+++ lib/Dialect/AIE/IR/AIEDialect.cpp
@@ -359,12 +359,10 @@ void AIEDialect::initialize() {
   addAttributes<
 #define GET_ATTRDEF_LIST
 #include "aie/Dialect/AIE/IR/AIEAttrs.cpp.inc"
-
       >();
   addOperations<
 #define GET_OP_LIST
 #include "aie/Dialect/AIE/IR/AIEOps.cpp.inc"
-
       >();
   addInterfaces<AIEInlinerInterface, AIEDialectFoldInterface>();
 }
