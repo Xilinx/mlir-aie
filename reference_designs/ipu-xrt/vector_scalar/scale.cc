0a5c94c82829b1633035ab2b11ebfe01612a4ec5
Jack Lo <36210336+jackl-xilinx@users.noreply.github.com>
2024-03-10 22:52:05 -0700

    Vectorize vec scalar (#1115)

--- reference_designs/ipu-xrt/vector_scalar/scale.cc
+++ reference_designs/ipu-xrt/vector_scalar/scale.cc
@@ -22,7 +22,7 @@
 
 #include <aie_api/aie.hpp>
 
-template <typename T_in, typename T_out, int N>
+template <typename T_in, typename T_out, const int N>
 void scale(T_in *a, T_out *c, T_in factor) {
   event0();
   for (int i = 0; i < N; i++) {
@@ -31,9 +31,32 @@ void scale(T_in *a, T_out *c, T_in factor) {
   event1();
 }
 
+// Assume factor is at least 16
+template <typename T_in, typename T_out, const int N>
+void scale_vectorized(T_in *a, T_out *c, T_in factor) {
+  constexpr int vec_factor = 16;
+  event0();
+  T_in *__restrict pA1 = a;
+  T_out *__restrict pC1 = c;
+  const int F = N / vec_factor;
+  for (int i = 0; i < F; i++)
+    chess_prepare_for_pipelining chess_loop_range(16, ) {
+      aie::vector<T_in, vec_factor> A0 = aie::load_v<vec_factor>(pA1);
+      pA1 += vec_factor;
+      aie::accum<acc64, vec_factor> cout = aie::mul(A0, factor);
+      aie::store_v(pC1, cout.to_vector<T_out>(0));
+      pC1 += vec_factor;
+    }
+  event1();
+}
+
 extern "C" {
 
 void scale_int32(int32_t *a_in, int32_t *c_out) {
+  scale_vectorized<int32_t, int32_t, 1024>(a_in, c_out, 3);
+}
+
+void scale_scalar_int32(int32_t *a_in, int32_t *c_out) {
   scale<int32_t, int32_t, 1024>(a_in, c_out, 3);
 }
 
