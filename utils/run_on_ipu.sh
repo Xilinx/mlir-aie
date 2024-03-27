36df259e1d596689ce33b182a2b874042e9c9944
Ronan Keryell <ronan.keryell@amd.com>
2024-03-26 17:33:36 -0700

    Handle new firmware location for RyzenAI and Linux 6.8

--- utils/run_on_ipu.sh
+++ utils/run_on_ipu.sh
@@ -3,7 +3,22 @@
 # (c) Copyright 2023 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+# Execute a command while pre-staging for AIE driver some .xclbin firmware and
+# clean up the firmware stage after execution.
+# This script assumes that the path of one xclbin
+# appears in the argument list
+
+# Old AIE driver
+# Where the device driver finds the firmware to be loaded.
 FIRMWARE_DIR=/lib/firmware/amdipu/1502
+# The AIE device model
+NPU_DEVICE=Phoenix
+if [[ -d /lib/firmware/amdnpu/1502_00 ]]; then
+  # New AIE driver naming and firmware location starting with Linux 6.8
+  FIRMWARE_DIR=/lib/firmware/amdnpu/1502_00
+  NPU_DEVICE=NPU1
+fi
+
 XRT_DIR=/opt/xilinx/xrt
 source $XRT_DIR/setup.sh
 
@@ -25,20 +40,25 @@ if [ x"$1" == x"--clean-xclbin" ]; then
 fi
 
 XCLBIN_FN=""
+# Analyze all the command arguments to find some .xclbin
 for f in "$@"; do
   if [ -f "$f" ]; then
     filename=$(basename "$f")
     extension="${f##*.}"
     if [ x"$extension" = x"xclbin" ]; then
       XCLBIN_FN="$filename"
-      $XRT_DIR/amdxdna/setup_xclbin_firmware.sh -dev Phoenix -xclbin $XCLBIN_FN
+      # Stage the .xclbin to be loaded by the command execution later
+      $XRT_DIR/amdxdna/setup_xclbin_firmware.sh -dev $NPU_DEVICE -xclbin $XCLBIN_FN
+      break
     fi
   fi
 done
 
+# Execute the commands and its arguments
 "$@"
 err=$?
 
+# TODO: use setup_xclbin_firmware.sh -clean instead
 if [ x"$XCLBIN_FN" != x"" ]; then
   rm_xclbin $XCLBIN_FN
 fi
