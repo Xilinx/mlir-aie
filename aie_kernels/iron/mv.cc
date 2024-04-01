7f0a70f64b94ee73c2053d76cd987add149abcff
andrej <an.roesti@gmail.com>
2024-04-01 15:24:18 -0700

    format

--- aie_kernels/iron/mv.cc
+++ aie_kernels/iron/mv.cc
@@ -40,7 +40,7 @@ void matvec_scalar(T_in *a, T_in *b, T_out *c) {
 template <typename T_in, typename T_out, typename T_acc, unsigned m, unsigned k,
           unsigned r, unsigned s>
 void matvec_vectorized(T_in *__restrict a, T_in *__restrict b,
-                      T_out *__restrict c) {
+                       T_out *__restrict c) {
   static_assert(m % r == 0 && k % 2 == 0);
   static_assert(s == 8); // s is fixed to 8 because that is the number of
                          // column vectors (a_vec_0_0..a_vec_3_1) we create
@@ -80,12 +80,12 @@ void matvec_vectorized(T_in *__restrict a, T_in *__restrict b,
         aie::accum<T_acc, r> c_acc_in;
         c_acc_in.from_vector(aie::load_v<r>(c_ptr));
 
-        const aie::vector<T_in, 2 * r> a_vec_0 = aie::load_v<2 * r>(a_ptr);
-        const aie::vector<T_in, 2 * r> a_vec_1 =
+        const aie::vector<T_in, 2 *r> a_vec_0 = aie::load_v<2 * r>(a_ptr);
+        const aie::vector<T_in, 2 *r> a_vec_1 =
             aie::load_v<2 * r>(a_ptr + 2 * m);
-        const aie::vector<T_in, 2 * r> a_vec_2 =
+        const aie::vector<T_in, 2 *r> a_vec_2 =
             aie::load_v<2 * r>(a_ptr + 4 * m);
-        const aie::vector<T_in, 2 * r> a_vec_3 =
+        const aie::vector<T_in, 2 *r> a_vec_3 =
             aie::load_v<2 * r>(a_ptr + 6 * m);
 
         // The even/odd calls below extract the interleaved columns of A.
@@ -141,27 +141,27 @@ extern "C" {
                              ctype_acc)                                        \
   void matvec_scalar_##mlir_type_in##_##mlir_type_out(                         \
       ctype_in *a_in, ctype_in *b_in, ctype_out *c_out) {                      \
-    matvec_scalar<ctype_in, ctype_out, 32, 32>(a_in, b_in, c_out);              \
+    matvec_scalar<ctype_in, ctype_out, 32, 32>(a_in, b_in, c_out);             \
   }
 
 #define matvec_vectorized_c_func(ctype_in, mlir_type_in, ctype_out,            \
                                  mlir_type_out, ctype_acc)                     \
   void matvec_vectorized_##mlir_type_in##_##mlir_type_out(                     \
       ctype_in *a_in, ctype_in *b_in, ctype_out *c_out) {                      \
-    matvec_vectorized<ctype_in, ctype_out, ctype_acc, 32, 32, 16, 8>(           \
+    matvec_vectorized<ctype_in, ctype_out, ctype_acc, 32, 32, 16, 8>(          \
         a_in, b_in, c_out);                                                    \
   }
 
 #define zero_vectorized_c_func(ctype_in, mlir_type_in, ctype_out,              \
                                mlir_type_out, ctype_acc)                       \
   void zero_vectorized_##mlir_type_out(ctype_out *c_out) {                     \
-    zero_vectorized<ctype_out, 32, 1, 32>(c_out);                               \
+    zero_vectorized<ctype_out, 32, 1, 32>(c_out);                              \
   }
 
 #define zero_scalar_c_func(ctype_in, mlir_type_in, ctype_out, mlir_type_out,   \
                            ctype_acc)                                          \
   void zero_scalar_##mlir_type_out(ctype_out *c_out) {                         \
-    zero_scalar<ctype_out, 32, 1>(c_out);                                       \
+    zero_scalar<ctype_out, 32, 1>(c_out);                                      \
   }
 
 combos(matvec_scalar_c_func) combos(matvec_vectorized_c_func)
