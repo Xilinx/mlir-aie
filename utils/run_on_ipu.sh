ff515d0c0c0d404594b0f4e545ba9f8e5af26bf5
Stephen Neuendorffer <stephen.neuendorffer@amd.com>
2024-04-10 10:50:52 -0700

    fix?

--- utils/run_on_ipu.sh
+++ utils/run_on_ipu.sh
@@ -21,7 +21,7 @@ if [[ -d /lib/firmware/amdnpu/1502_00 ]]; then
   NPU_DEVICE=NPU1
 fi
 # Don't require root.
-XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
+export XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
 
 XRT_DIR=/opt/xilinx/xrt
 source $XRT_DIR/setup.sh
