ae8eca5f282816428f1d915c085803280bf40502
Jack Lo <jack.lo@amd.com>
2024-04-10 07:25:06 -0700

    Extracted more helper functions into test_utils

--- programming_guide/section-1/aie2.py
+++ programming_guide/section-1/aie2.py
@@ -1,4 +1,3 @@
-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
@@ -6,8 +5,9 @@
 #
 # (c) Copyright 2023 AMD Inc.
 
-from aie.dialects.aie import *                     # primary mlir-aie dialect definitions
-from aie.extras.context import mlir_mod_ctx        # mlir ctx wrapper 
+from aie.dialects.aie import *  # primary mlir-aie dialect definitions
+from aie.extras.context import mlir_mod_ctx  # mlir ctx wrapper
+
 
 # AI Engine structural design function
 def mlir_aie_design():
@@ -26,6 +26,6 @@ def mlir_aie_design():
     # Print the mlir conversion
     print(ctx.module)
 
+
 # Call design function to generate mlir code to stdout
 mlir_aie_design()
-
--- programming_guide/section-3/test_3.py
+++ programming_guide/section-3/test_3.py
@@ -15,13 +15,16 @@ import test_utils
 # Configure this to match your design's buffer size
 # ------------------------------------------------------
 INOUT0_VOLUME = 64  # Input only, 64x uint32_t in this example
-INOUT1_VOLUME = 64  # Output only, 64x uint32_t in this example
+INOUT1_VOLUME = 64  # Not used in this example
+INOUT2_VOLUME = 64  # Output only, 64x uint32_t in this example
 
 INOUT0_DATATYPE = np.uint32
 INOUT1_DATATYPE = np.uint32
+INOUT2_DATATYPE = np.uint32
 
 INOUT0_SIZE = INOUT0_VOLUME * INOUT0_DATATYPE().itemsize
 INOUT1_SIZE = INOUT1_VOLUME * INOUT1_DATATYPE().itemsize
+INOUT2_SIZE = INOUT2_VOLUME * INOUT2_DATATYPE().itemsize
 
 
 def main(opts):
@@ -35,7 +38,7 @@ def main(opts):
     # ------------------------------------------------------
     # Get device, load the xclbin & kernel and register them
     # ------------------------------------------------------
-    (device, kernel) = test_utils.init_xrt_load_kernel(opts)
+    (device, kernel) = init_xrt_load_kernel(opts)
 
     # ------------------------------------------------------
     # Initialize input/ output buffer sizes and sync them
@@ -43,6 +46,7 @@ def main(opts):
     bo_instr = xrt.bo(device, len(instr_v) * 4, xrt.bo.cacheable, kernel.group_id(0))
     bo_inout0 = xrt.bo(device, INOUT0_SIZE, xrt.bo.host_only, kernel.group_id(2))
     bo_inout1 = xrt.bo(device, INOUT1_SIZE, xrt.bo.host_only, kernel.group_id(3))
+    bo_inout2 = xrt.bo(device, INOUT2_SIZE, xrt.bo.host_only, kernel.group_id(4))
 
     # Initialize instruction buffer
     bo_instr.write(instr_v, 0)
@@ -50,13 +54,16 @@ def main(opts):
     # Initialize data buffers
     inout0 = np.arange(1, INOUT0_VOLUME + 1, dtype=INOUT0_DATATYPE)
     inout1 = np.zeros(INOUT1_VOLUME, dtype=INOUT1_DATATYPE)
+    inout2 = np.zeros(INOUT2_VOLUME, dtype=INOUT2_DATATYPE)
     bo_inout0.write(inout0, 0)
     bo_inout1.write(inout1, 0)
+    bo_inout2.write(inout2, 0)
 
     # Sync buffers to update input buffer values
     bo_instr.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE)
     bo_inout0.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE)
     bo_inout1.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE)
+    bo_inout2.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE)
 
     # ------------------------------------------------------
     # Initialize run configs
@@ -70,13 +77,13 @@ def main(opts):
     # Run kernel
     if opts.verbosity >= 1:
         print("Running Kernel.")
-    h = kernel(bo_instr, len(instr_v), bo_inout0, bo_inout1)
+    h = kernel(bo_instr, len(instr_v), bo_inout0, bo_inout1, bo_inout2)
     h.wait()
-    bo_inout1.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_FROM_DEVICE)
+    bo_inout2.sync(xrt.xclBOSyncDirection.XCL_BO_SYNC_BO_FROM_DEVICE)
 
     # Copy output results and verify they are correct
-    out_size = INOUT1_SIZE
-    output_buffer = bo_inout1.read(out_size, 0).view(INOUT1_DATATYPE)
+    out_size = INOUT2_SIZE
+    output_buffer = bo_inout2.read(out_size, 0).view(INOUT2_DATATYPE)
     if opts.verify:
         if opts.verbosity >= 1:
             print("Verifying results ...")
