5d19b5f0c839cba55cef4af336786ac984956fa1
singagan <gagan.posted@gmail.com>
2024-04-10 14:48:40 -0600

    running python test

--- programming_examples/utils/mlutils.py
+++ programming_examples/utils/mlutils.py
@@ -1,6 +1,6 @@
 import os
 from torch.utils.data import Dataset
-from PIL import Image
+# from PIL import Image
 import json
 import sys
 import csv
@@ -9,7 +9,7 @@ import argparse
 import numpy as np
 # import cv2
 import numpy as np
-from PIL import Image
+# from PIL import Image
 from pathlib import Path
 import torch
 import torch.nn as nn
@@ -17,43 +17,43 @@ import torch.nn as nn
 import math
 
 
-class ImageNetKaggle(Dataset):
-    def __init__(self, root, split, transform=None):
-        self.samples = []
-        self.targets = []
-        self.transform = transform
-        self.syn_to_class = {}
-        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
-            json_file = json.load(f)
-            for class_id, v in json_file.items():
-                self.syn_to_class[v[0]] = int(class_id)
-        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
-            self.val_to_syn = json.load(f)
-        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
-        for entry in os.listdir(samples_dir):
-            if split == "train":
-                syn_id = entry
-                target = self.syn_to_class[syn_id]
-                syn_folder = os.path.join(samples_dir, syn_id)
-                for sample in os.listdir(syn_folder):
-                    sample_path = os.path.join(syn_folder, sample)
-                    self.samples.append(sample_path)
-                    self.targets.append(target)
-            elif split == "val":
-                syn_id = self.val_to_syn[entry]
-                target = self.syn_to_class[syn_id]
-                sample_path = os.path.join(samples_dir, entry)
-                self.samples.append(sample_path)
-                self.targets.append(target)
+# class ImageNetKaggle(Dataset):
+#     def __init__(self, root, split, transform=None):
+#         self.samples = []
+#         self.targets = []
+#         self.transform = transform
+#         self.syn_to_class = {}
+#         with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
+#             json_file = json.load(f)
+#             for class_id, v in json_file.items():
+#                 self.syn_to_class[v[0]] = int(class_id)
+#         with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
+#             self.val_to_syn = json.load(f)
+#         samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+#         for entry in os.listdir(samples_dir):
+#             if split == "train":
+#                 syn_id = entry
+#                 target = self.syn_to_class[syn_id]
+#                 syn_folder = os.path.join(samples_dir, syn_id)
+#                 for sample in os.listdir(syn_folder):
+#                     sample_path = os.path.join(syn_folder, sample)
+#                     self.samples.append(sample_path)
+#                     self.targets.append(target)
+#             elif split == "val":
+#                 syn_id = self.val_to_syn[entry]
+#                 target = self.syn_to_class[syn_id]
+#                 sample_path = os.path.join(samples_dir, entry)
+#                 self.samples.append(sample_path)
+#                 self.targets.append(target)
 
-    def __len__(self):
-        return len(self.samples)
+#     def __len__(self):
+#         return len(self.samples)
 
-    def __getitem__(self, idx):
-        x = Image.open(self.samples[idx]).convert("RGB")
-        if self.transform:
-            x = self.transform(x)
-        return x, self.targets[idx]
+#     def __getitem__(self, idx):
+#         x = Image.open(self.samples[idx]).convert("RGB")
+#         if self.transform:
+#             x = self.transform(x)
+#         return x, self.targets[idx]
 
 
 class CSVLogger:
