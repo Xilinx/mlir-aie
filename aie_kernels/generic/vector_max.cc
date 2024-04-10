54c313fb72ce9e8e0e13b8bb4c21d8c202aeb600
pjr <pjr@xilinx.com>
2024-04-10 17:09:53 -0600

    [TESTS] Min max tests

--- aie_kernels/generic/vector_max.cc
+++ aie_kernels/generic/vector_max.cc
@@ -5,40 +5,42 @@
 
 #include <aie_api/aie.hpp>
 
-void vector(int32_t *restrict v1, int32_t *restrict v2) {
-  int32_t v3 = 4;
-  int32_t v4 = 8;
-  int32_t v5 = 16;
-  int32_t v6 = 32;
-  int32_t v7 = 0;
-  v16int32 v8 = broadcast_to_v16int32((int32_t)-2147483648);
-  size_t v9 = 0;
-  size_t v10 = 1024;
-  size_t v11 = 16;
-  v16int32 v12;
-  v16int32 v13 = v8;
-  for (size_t v14 = v9; v14 < v10; v14 += v11)
+void vector(int32_t *restrict in, int32_t *restrict out) {
+
+  v16int32 tiny = broadcast_to_v16int32((int32_t)-2147483648);
+  int32_t input_size = 1024;
+  int32_t vector_size = 16;
+  v16int32 after_vector;
+  v16int32 running_max = tiny;
+  for (int32_t i = 0; i < input_size; i += vector_size)
     chess_prepare_for_pipelining chess_loop_range(64, 64) {
-      v16int32 v15 = *(v16int32 *)(v1 + v14);
-      v16int32 v16 = max(v13, v15);
-      v13 = v16;
+      v16int32 next = *(v16int32 *)(in + i);
+      v16int32 test = max(running_max, next);
+      running_max = test;
     }
-  v12 = v13;
-  v16int32 v17 = shift_bytes(v12, v12, v6);
-  v16int32 v18 = max(v12, v17);
-  v16int32 v19 = shift_bytes(v18, v18, v5);
-  v16int32 v20 = max(v18, v19);
-  v16int32 v21 = shift_bytes(v20, v20, v4);
-  v16int32 v22 = max(v20, v21);
-  v16int32 v23 = shift_bytes(v22, v22, v3);
-  v16int32 v24 = max(v22, v23);
-  int32_t v25 = extract_elem(v24, v7);
-  *(int32_t *)v2 = v25;
+  after_vector = running_max;
+  v16int32 first = shift_bytes(after_vector, after_vector, 32);
+  v16int32 second = max(after_vector, first);
+  v16int32 second_shift = shift_bytes(second, second, 16);
+  v16int32 third = max(second, second_shift);
+  v16int32 third_shift = shift_bytes(third, third, 8);
+  v16int32 fourth = max(third, third_shift);
+  v16int32 fourth_shift = shift_bytes(fourth, fourth, 4);
+  v16int32 fifth = max(fourth, fourth_shift);
+  int32_t last = extract_elem(fifth, 0);
+  *(int32_t *)out = last;
   return;
 }
 
-void scalar(int32_t *restrict v1, int32_t *restrict v2) {
-  *(int32_t *)v2 = 27; // what could be bigger
+void scalar(int32_t *restrict in, int32_t *restrict out) {
+  size_t input_size = 1024;
+  int32_t running_max = (int32_t)-2147483648;
+  for (int32_t i = 0; i < input_size; i++) {
+    if (in[i] > running_max)
+      running_max = in[i];
+  }
+  *(int32_t *)out = running_max;
+
   return;
 }
 
