ffd7043fdb2ed86168593a5a3e93fb7922c929dd
jamestcl-amd <linjames@xilinx.com>
2024-04-11 13:58:17 -0700

    Add e2e unit test for fp32xfp32 mul_elem

---
+++ test/unit_tests/aievec_tests/floatxfloat_mul_elem/defines.h
@@ -0,0 +1,4 @@
+#pragma once
+constexpr unsigned const IN0_SIZE = 1024;
+constexpr unsigned const IN1_SIZE = 1024;
+constexpr unsigned const OUT0_SIZE = 1024;
---
+++ test/unit_tests/aievec_tests/floatxfloat_mul_elem/testbench.cc
@@ -0,0 +1,71 @@
+#include "../common/testbench.h"
+#include "defines.h"
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef TO_CPP
+void dut(float *restrict in0, float *restrict in1, float *restrict out0);
+#elif TO_LLVM
+extern "C" {
+void dut(float *in0_allocated, float *in0_aligned, int64_t in0_offset,
+         int64_t in0_sizes_0, int64_t in0_strides_0, float *in1_allocated,
+         float *in1_aligned, int64_t in1_offset, int64_t in1_sizes_0,
+         int64_t in1_strides_0, float *out0_allocated, float *out0_aligned,
+         int64_t out0_offset, int64_t out0_sizes_0, int64_t out0_strides_0);
+}
+#endif
+
+void dut_ref(float *in0, float *in1, float *out0);
+
+alignas(32) float g_in0[IN0_SIZE];
+alignas(32) float g_in1[IN1_SIZE];
+alignas(32) float g_out0[OUT0_SIZE];
+alignas(32) float g_out0Ref[OUT0_SIZE];
+
+int main(int argc, char *argv[]) {
+  std::string dataDir(TO_STR(DATA_DIR));
+  srand(10);
+  std::generate(g_in0, g_in0 + IN0_SIZE,
+                [&]() { return random_float(-60, 60, 10); });
+  std::generate(g_in1, g_in1 + IN1_SIZE,
+                [&]() { return random_float(-60, 60, 10); });
+
+  writeData(g_in0, IN0_SIZE, dataDir + "/in0.txt");
+  writeData(g_in1, IN1_SIZE, dataDir + "/in1.txt");
+
+  chess_memory_fence();
+  auto cyclesBegin = chess_cycle_count();
+#ifdef TO_CPP
+  dut(g_in0, g_in1, g_out0);
+#elif TO_LLVM
+  dut(g_in0, g_in0, 0, 0, 0, g_in1, g_in1, 0, 0, 0, g_out0, g_out0, 0, 0, 0);
+#endif
+  auto cyclesEnd = chess_cycle_count();
+  chess_memory_fence();
+
+  auto cycleCount = (int)(cyclesEnd - cyclesBegin);
+  reportCycleCount(cycleCount, dataDir + "/cycle_count.txt");
+
+  writeData(g_out0, OUT0_SIZE, dataDir + "/out0.txt");
+
+  dut_ref(g_in0, g_in1, g_out0Ref);
+  writeData(g_out0Ref, OUT0_SIZE, dataDir + "/out0_ref.txt");
+
+  bool ok = true;
+  ok &= checkData(g_out0, g_out0Ref, OUT0_SIZE, 0, 1e-3, 1e-3);
+
+  if (ok)
+    printf("TEST PASSED\n");
+  else
+    printf("TEST FAILED\n");
+
+  return ok ? 0 : 1;
+}
+
+void dut_ref(float *in0, float *in1, float *out0) {
+  for (unsigned k = 0; k < OUT0_SIZE; k += 1) {
+    out0[k] = in0[k] * in1[k];
+  }
+}
