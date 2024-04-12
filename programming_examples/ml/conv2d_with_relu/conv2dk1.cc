1ce4d9a852e86a3d7284b70ca8f4bd346aa5f6cc
singagan <gagan.posted@gmail.com>
2024-04-12 02:12:39 -0600

    clang-format

--- programming_examples/ml/conv2d_with_relu/conv2dk1.cc
+++ programming_examples/ml/conv2d_with_relu/conv2dk1.cc
@@ -37,111 +37,111 @@ const int32_t UMAX = 255;
 // multiple of 4 but there is some strange scheduling happening now so for
 // now, we do not.
 //*****************************************************************************
-  void conv2dk1_i8_vector(int8_t *input, int8_t *kernels, uint8_t *output,
-                          const int32_t input_width, const int32_t input_channels,
-                          const int32_t output_channels, const int scale) {
-    event0();
+void conv2dk1_i8_vector(int8_t *input, int8_t *kernels, uint8_t *output,
+                        const int32_t input_width, const int32_t input_channels,
+                        const int32_t output_channels, const int scale) {
+  event0();
 
-    using MMUL4x8x8 = aie::mmul<4, 8, 8, int8, int8>;
-    ::aie::set_saturation(
-        aie::saturation_mode::saturate); // Needed to saturate properly to uint8
-    ::aie::set_rounding(
-        aie::rounding_mode::positive_inf); // Needed to saturate properly to uint8
+  using MMUL4x8x8 = aie::mmul<4, 8, 8, int8, int8>;
+  ::aie::set_saturation(
+      aie::saturation_mode::saturate); // Needed to saturate properly to uint8
+  ::aie::set_rounding(
+      aie::rounding_mode::positive_inf); // Needed to saturate properly to uint8
 
-    uint8_t *restrict out_ptr = output;
+  uint8_t *restrict out_ptr = output;
 
-    const int scaleT = scale;
+  const int scaleT = scale;
 
-    MMUL4x8x8 acc_tmp[8];
-    for (int x = 0; x < 8; x++) {
-      acc_tmp[x] = aie::zeros<acc32, 32>();
-    }
+  MMUL4x8x8 acc_tmp[8];
+  for (int x = 0; x < 8; x++) {
+    acc_tmp[x] = aie::zeros<acc32, 32>();
+  }
 
-    // TODO Keeping this variable gives a wrong behavior and bad schedule!
-    const int iw = input_width;
-    const int iw_32 = (input_width / 4) / 8;
+  // TODO Keeping this variable gives a wrong behavior and bad schedule!
+  const int iw = input_width;
+  const int iw_32 = (input_width / 4) / 8;
 
-    // const int iw_32_rem = (input_width / 4) % 8;
-    // const int iw_32_rem = (32 / 4) % 8;
-    assert((input_width / 4) % 8 == 0);
-    const int iw_32_rem = 0; // TODO - See restriction
+  // const int iw_32_rem = (input_width / 4) % 8;
+  // const int iw_32_rem = (32 / 4) % 8;
+  assert((input_width / 4) % 8 == 0);
+  const int iw_32_rem = 0; // TODO - See restriction
 
-    assert((input_channels / 8) > 2); // Assume IC >= 16
+  assert((input_channels / 8) > 2); // Assume IC >= 16
 
-    if (iw_32 > 0) {
+  if (iw_32 > 0) {
 
-      for (int oc = 0; oc < (output_channels / 8); oc++) {
-        for (int iw_32c = 0; iw_32c < iw_32; iw_32c++) {
-          for (int ic = 0; ic < (input_channels / 8); ic++)
-            chess_prepare_for_pipelining chess_loop_range(2, ) {
-              aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
-              kernels += 64; // wts ic0..7(oc0..7)
-
-              for (int x = 0; x < 8; x++) {
-                aie::vector<int8, 32> in_a = aie::load_v<32>(input);
-                input += 32; // act oc0..3(ic0..7)
-                acc_tmp[x].mac(in_a, in_b);
-              }
-              input += (iw * 8) - 256; // Move to next ic/8 position
-            }
-          // input ptr just moves to next section
-          for (int xx = 0; xx < 8; xx++) {
-            aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
-            aie::store_v(out_ptr, o1);
-            out_ptr += 32;
-            acc_tmp[xx] = aie::zeros<acc32, 32>();
-          }
-          input -= ((input_channels / 8) * iw * 8) -
-                   256; // reset to next input_width/32 block
-          kernels -=
-              (input_channels / 8) * 64; // reset kernel back to beginning of ic/8
-        }
-        input -= (iw_32) * 256; // 8*32, reset beginning of input ptr
-        kernels += (input_channels / 8) * 64; // move to next oc/8 weights
-        out_ptr += (iw_32_rem *
-                    32); // move to next oc/8 (skip remainder section if present)
-      }
-
-    } // if(iw_32 > 0) {
-
-    if (iw_32_rem > 0) {
-
-      const int ocs = output_channels;
-      const int ics = input_channels;
-
-      for (int oc = 0; oc < (ocs / 8); oc++) {
-        for (int ic = 0; ic < (ics / 8); ic++)
+    for (int oc = 0; oc < (output_channels / 8); oc++) {
+      for (int iw_32c = 0; iw_32c < iw_32; iw_32c++) {
+        for (int ic = 0; ic < (input_channels / 8); ic++)
           chess_prepare_for_pipelining chess_loop_range(2, ) {
             aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
             kernels += 64; // wts ic0..7(oc0..7)
 
-            for (int x = 0; x < iw_32_rem; x++) {
+            for (int x = 0; x < 8; x++) {
               aie::vector<int8, 32> in_a = aie::load_v<32>(input);
               input += 32; // act oc0..3(ic0..7)
               acc_tmp[x].mac(in_a, in_b);
             }
-            input += (iw * 8) - (iw_32_rem * 32); // Move to next ic/8 position
+            input += (iw * 8) - 256; // Move to next ic/8 position
           }
         // input ptr just moves to next section
-        for (int xx = 0; xx < iw_32_rem; xx++) {
+        for (int xx = 0; xx < 8; xx++) {
           aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
           aie::store_v(out_ptr, o1);
           out_ptr += 32;
           acc_tmp[xx] = aie::zeros<acc32, 32>();
         }
-        // input   -= ((ics-1)/8)*(iw*8)+(iw_32_rem*32); // reset to beginning of
-        // input ptr for remainder
-        input -= 448; // reset to beginning of input ptr for remainder
-        // kernel ptr already at next oc/8
-        out_ptr += (iw * 8) -
-                   (iw_32_rem *
-                    32); // move to next oc/8 (skip remainder section if present)
+        input -= ((input_channels / 8) * iw * 8) -
+                 256; // reset to next input_width/32 block
+        kernels -=
+            (input_channels / 8) * 64; // reset kernel back to beginning of ic/8
       }
+      input -= (iw_32)*256; // 8*32, reset beginning of input ptr
+      kernels += (input_channels / 8) * 64; // move to next oc/8 weights
+      out_ptr += (iw_32_rem *
+                  32); // move to next oc/8 (skip remainder section if present)
+    }
 
-    } // if(iw_32_rem > 0)
+  } // if(iw_32 > 0) {
 
-    event1();
-  }
+  if (iw_32_rem > 0) {
+
+    const int ocs = output_channels;
+    const int ics = input_channels;
+
+    for (int oc = 0; oc < (ocs / 8); oc++) {
+      for (int ic = 0; ic < (ics / 8); ic++)
+        chess_prepare_for_pipelining chess_loop_range(2, ) {
+          aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
+          kernels += 64; // wts ic0..7(oc0..7)
+
+          for (int x = 0; x < iw_32_rem; x++) {
+            aie::vector<int8, 32> in_a = aie::load_v<32>(input);
+            input += 32; // act oc0..3(ic0..7)
+            acc_tmp[x].mac(in_a, in_b);
+          }
+          input += (iw * 8) - (iw_32_rem * 32); // Move to next ic/8 position
+        }
+      // input ptr just moves to next section
+      for (int xx = 0; xx < iw_32_rem; xx++) {
+        aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
+        aie::store_v(out_ptr, o1);
+        out_ptr += 32;
+        acc_tmp[xx] = aie::zeros<acc32, 32>();
+      }
+      // input   -= ((ics-1)/8)*(iw*8)+(iw_32_rem*32); // reset to beginning of
+      // input ptr for remainder
+      input -= 448; // reset to beginning of input ptr for remainder
+      // kernel ptr already at next oc/8
+      out_ptr += (iw * 8) -
+                 (iw_32_rem *
+                  32); // move to next oc/8 (skip remainder section if present)
+    }
+
+  } // if(iw_32_rem > 0)
+
+  event1();
+}
 
 #else // UINT8_ACT
 
@@ -158,112 +158,112 @@ const int32_t UMAX = 255;
 // multiple of 4 but there is some strange scheduling happening now so for
 // now, we do not.
 //*****************************************************************************
-  void conv2dk1_ui8_vector(uint8_t *input, int8_t *kernels, uint8_t *output,
-                           const int32_t input_width,
-                           const int32_t input_channels,
-                           const int32_t output_channels, const int scale) {
-    event0();
+void conv2dk1_ui8_vector(uint8_t *input, int8_t *kernels, uint8_t *output,
+                         const int32_t input_width,
+                         const int32_t input_channels,
+                         const int32_t output_channels, const int scale) {
+  event0();
 
-    using MMUL4x8x8 = aie::mmul<4, 8, 8, uint8, int8>;
-    ::aie::set_saturation(
-        aie::saturation_mode::saturate); // Needed to saturate properly to uint8
-    ::aie::set_rounding(
-        aie::rounding_mode::positive_inf); // Needed to saturate properly to uint8
+  using MMUL4x8x8 = aie::mmul<4, 8, 8, uint8, int8>;
+  ::aie::set_saturation(
+      aie::saturation_mode::saturate); // Needed to saturate properly to uint8
+  ::aie::set_rounding(
+      aie::rounding_mode::positive_inf); // Needed to saturate properly to uint8
 
-    uint8_t *restrict out_ptr = output;
+  uint8_t *restrict out_ptr = output;
 
-    const int scaleT = scale;
+  const int scaleT = scale;
 
-    MMUL4x8x8 acc_tmp[8];
-    for (int x = 0; x < 8; x++) {
-      acc_tmp[x] = aie::zeros<acc32, 32>();
-    }
+  MMUL4x8x8 acc_tmp[8];
+  for (int x = 0; x < 8; x++) {
+    acc_tmp[x] = aie::zeros<acc32, 32>();
+  }
 
-    // TODO Keeping this variable gives a wrong behavior and bad schedule!
-    const int iw = input_width;
-    const int iw_32 = (input_width / 4) / 8;
+  // TODO Keeping this variable gives a wrong behavior and bad schedule!
+  const int iw = input_width;
+  const int iw_32 = (input_width / 4) / 8;
 
-    // const int iw_32_rem = (input_width / 4) % 8;
-    // const int iw_32_rem = (32 / 4) % 8;
-    assert((input_width / 4) % 8 == 0);
-    const int iw_32_rem = 0; // TODO - See restriction
+  // const int iw_32_rem = (input_width / 4) % 8;
+  // const int iw_32_rem = (32 / 4) % 8;
+  assert((input_width / 4) % 8 == 0);
+  const int iw_32_rem = 0; // TODO - See restriction
 
-    assert((input_channels / 8) > 2); // Assume IC >= 16
+  assert((input_channels / 8) > 2); // Assume IC >= 16
 
-    if (iw_32 > 0) {
+  if (iw_32 > 0) {
 
-      for (int oc = 0; oc < (output_channels / 8); oc++) {
-        for (int iw_32c = 0; iw_32c < iw_32; iw_32c++) {
-          for (int ic = 0; ic < (input_channels / 8); ic++)
-            chess_prepare_for_pipelining chess_loop_range(2, ) {
-              aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
-              kernels += 64; // wts ic0..7(oc0..7)
-
-              for (int x = 0; x < 8; x++) {
-                aie::vector<uint8, 32> in_a = aie::load_v<32>(input);
-                input += 32; // act oc0..3(ic0..7)
-                acc_tmp[x].mac(in_a, in_b);
-              }
-              input += (iw * 8) - 256; // Move to next ic/8 position
-            }
-          // input ptr just moves to next section
-          for (int xx = 0; xx < 8; xx++) {
-            aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
-            aie::store_v(out_ptr, o1);
-            out_ptr += 32;
-            acc_tmp[xx] = aie::zeros<acc32, 32>();
-          }
-          input -= ((input_channels / 8) * iw * 8) -
-                   256; // reset to next input_width/32 block
-          kernels -=
-              (input_channels / 8) * 64; // reset kernel back to beginning of ic/8
-        }
-        input -= (iw_32) * 256; // 8*32, reset beginning of input ptr
-        kernels += (input_channels / 8) * 64; // move to next oc/8 weights
-        out_ptr += (iw_32_rem *
-                    32); // move to next oc/8 (skip remainder section if present)
-      }
-
-    } // if(iw_32 > 0) {
-
-    if (iw_32_rem > 0) {
-
-      const int ocs = output_channels;
-      const int ics = input_channels;
-
-      for (int oc = 0; oc < (ocs / 8); oc++) {
-        for (int ic = 0; ic < (ics / 8); ic++)
+    for (int oc = 0; oc < (output_channels / 8); oc++) {
+      for (int iw_32c = 0; iw_32c < iw_32; iw_32c++) {
+        for (int ic = 0; ic < (input_channels / 8); ic++)
           chess_prepare_for_pipelining chess_loop_range(2, ) {
             aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
             kernels += 64; // wts ic0..7(oc0..7)
 
-            for (int x = 0; x < iw_32_rem; x++) {
+            for (int x = 0; x < 8; x++) {
               aie::vector<uint8, 32> in_a = aie::load_v<32>(input);
               input += 32; // act oc0..3(ic0..7)
               acc_tmp[x].mac(in_a, in_b);
             }
-            input += (iw * 8) - (iw_32_rem * 32); // Move to next ic/8 position
+            input += (iw * 8) - 256; // Move to next ic/8 position
           }
         // input ptr just moves to next section
-        for (int xx = 0; xx < iw_32_rem; xx++) {
+        for (int xx = 0; xx < 8; xx++) {
           aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
           aie::store_v(out_ptr, o1);
           out_ptr += 32;
           acc_tmp[xx] = aie::zeros<acc32, 32>();
         }
-        // input   -= ((ics-1)/8)*(iw*8)+(iw_32_rem*32); // reset to beginning of
-        // input ptr for remainder
-        input -= 448; // reset to beginning of input ptr for remainder
-        // kernel ptr already at next oc/8
-        out_ptr += (iw * 8) -
-                   (iw_32_rem *
-                    32); // move to next oc/8 (skip remainder section if present)
+        input -= ((input_channels / 8) * iw * 8) -
+                 256; // reset to next input_width/32 block
+        kernels -=
+            (input_channels / 8) * 64; // reset kernel back to beginning of ic/8
       }
+      input -= (iw_32)*256; // 8*32, reset beginning of input ptr
+      kernels += (input_channels / 8) * 64; // move to next oc/8 weights
+      out_ptr += (iw_32_rem *
+                  32); // move to next oc/8 (skip remainder section if present)
+    }
 
-    } // if(iw_32_rem > 0)
+  } // if(iw_32 > 0) {
 
-    event1();
-  }
+  if (iw_32_rem > 0) {
+
+    const int ocs = output_channels;
+    const int ics = input_channels;
+
+    for (int oc = 0; oc < (ocs / 8); oc++) {
+      for (int ic = 0; ic < (ics / 8); ic++)
+        chess_prepare_for_pipelining chess_loop_range(2, ) {
+          aie::vector<int8, 64> in_b = aie::load_v<64>(kernels);
+          kernels += 64; // wts ic0..7(oc0..7)
+
+          for (int x = 0; x < iw_32_rem; x++) {
+            aie::vector<uint8, 32> in_a = aie::load_v<32>(input);
+            input += 32; // act oc0..3(ic0..7)
+            acc_tmp[x].mac(in_a, in_b);
+          }
+          input += (iw * 8) - (iw_32_rem * 32); // Move to next ic/8 position
+        }
+      // input ptr just moves to next section
+      for (int xx = 0; xx < iw_32_rem; xx++) {
+        aie::vector<uint8, 32> o1 = acc_tmp[xx].to_vector<uint8>(scaleT);
+        aie::store_v(out_ptr, o1);
+        out_ptr += 32;
+        acc_tmp[xx] = aie::zeros<acc32, 32>();
+      }
+      // input   -= ((ics-1)/8)*(iw*8)+(iw_32_rem*32); // reset to beginning of
+      // input ptr for remainder
+      input -= 448; // reset to beginning of input ptr for remainder
+      // kernel ptr already at next oc/8
+      out_ptr += (iw * 8) -
+                 (iw_32_rem *
+                  32); // move to next oc/8 (skip remainder section if present)
+    }
+
+  } // if(iw_32_rem > 0)
+
+  event1();
+}
 
 #endif // UINT8_ACT
 
@@ -272,24 +272,24 @@ const int32_t UMAX = 255;
 //*****************************************************************************
 extern "C" {
 
-  #ifdef INT8_ACT
+#ifdef INT8_ACT
 
-    void conv2dk1_i8(int8_t *input, int8_t *kernels, uint8_t *output,
-                     const int32_t input_width, const int32_t input_channels,
-                     const int32_t output_channels, const int scale) {
-      conv2dk1_i8_vector(input, kernels, output, input_width, input_channels,
-                         output_channels, scale);
-    }
+void conv2dk1_i8(int8_t *input, int8_t *kernels, uint8_t *output,
+                 const int32_t input_width, const int32_t input_channels,
+                 const int32_t output_channels, const int scale) {
+  conv2dk1_i8_vector(input, kernels, output, input_width, input_channels,
+                     output_channels, scale);
+}
 
-  #else // UINT8_ACT
+#else // UINT8_ACT
 
-    void conv2dk1_ui8(uint8_t *input, int8_t *kernels, uint8_t *output,
-                      const int32_t input_width, const int32_t input_channels,
-                      const int32_t output_channels, const int scale) {
-      conv2dk1_ui8_vector(input, kernels, output, input_width, input_channels,
-                          output_channels, scale);
-    }
+void conv2dk1_ui8(uint8_t *input, int8_t *kernels, uint8_t *output,
+                  const int32_t input_width, const int32_t input_channels,
+                  const int32_t output_channels, const int scale) {
+  conv2dk1_ui8_vector(input, kernels, output, input_width, input_channels,
+                      output_channels, scale);
+}
 
-  #endif // UINT8_ACT
+#endif // UINT8_ACT
 
 } // extern "C"
