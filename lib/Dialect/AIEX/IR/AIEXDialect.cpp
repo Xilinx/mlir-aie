0ee9bb50c5e81c17cc8b02e11e38e3b84558a250
Jorn Tuyls <jorn.tuyls@gmail.com>
2024-04-12 07:06:14 -0700

    [AIEX] Add IpuDmaWaitOp to standard lowering pass and fix device checks

--- lib/Dialect/AIEX/IR/AIEXDialect.cpp
+++ lib/Dialect/AIEX/IR/AIEXDialect.cpp
@@ -107,9 +107,9 @@ LogicalResult AIEX::IpuDmaMemcpyNdOp::verify() {
 
 LogicalResult AIEX::IpuDmaWaitOp::verify() {
   AIE::DeviceOp dev = (*this)->getParentOfType<AIE::DeviceOp>();
-  if (!dev)
-    return emitOpError("couldn't find parent of type DeviceOp");
-  if (!dev.lookupSymbol(getSymbol()))
+  // Some passes (e.g. aie-standard-lowering) use aiex ops outside a DeviceOp,
+  // so we can't expect the device to always exist.
+  if (dev && !dev.lookupSymbol(getSymbol()))
     return emitOpError("couldn't find symbol in parent device");
   return success();
 }
