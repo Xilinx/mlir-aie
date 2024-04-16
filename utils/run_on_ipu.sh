8e9790c837245422b0a8f4613c27209ee3a42307
Jeff Fifield <jeff.fifield@amd.com>
2024-04-16 12:54:57 -0600

    Update run_on_ipu.sh
    cleanup

--- utils/run_on_ipu.sh
+++ utils/run_on_ipu.sh
@@ -3,11 +3,6 @@
 # (c) Copyright 2023-2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Execute a command while pre-staging for AIE driver some .xclbin firmware and
-# clean up the firmware stage after execution.
-# This script assumes that the path of one xclbin
-# appears in the argument list
-
 # Don't require root.
 export XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
 
