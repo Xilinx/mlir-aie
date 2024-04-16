a3e8aaf173d272faeb6f31979e88dbdf4a728994
Joseph Melber <jgmelber@gmail.com>
2024-04-16 10:52:58 -0600

    Remove 6.8 kernel / updated driver check

--- utils/run_on_ipu.sh
+++ utils/run_on_ipu.sh
@@ -8,19 +8,6 @@
 # This script assumes that the path of one xclbin
 # appears in the argument list
 
-# Old AIE driver
-linux_driver_version=""
-# Where the device driver finds the firmware to be loaded.
-FIRMWARE_DIR=/lib/firmware/amdipu/1502
-# The AIE device model
-NPU_DEVICE=Phoenix
-if [[ -d /lib/firmware/amdnpu/1502_00 ]]; then
-  # New AIE driver naming and firmware location starting with Linux 6.8
-  linux_driver_version="6.8"
-  FIRMWARE_DIR=/lib/firmware/amdnpu/1502_00
-  NPU_DEVICE=NPU1
-fi
-
 # Don't require root.
 export XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
 
