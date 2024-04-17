260f1dd97dd502a3eb4d6f576cc37666303e25e2
Jack Lo <jack.lo@amd.com>
2024-04-16 21:14:04 -0700

    Added license header and trace.py support

--- python/utils/ml.py
+++ python/utils/ml.py
@@ -1,3 +1,11 @@
+# ml.py -*- Python -*-
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# (c) Copyright 2024 Advanced Micro Devices, Inc.
+
 import os
 from torch.utils.data import Dataset
 
--- python/utils/xrt.py
+++ python/utils/xrt.py
@@ -1,3 +1,11 @@
+# ml.py -*- Python -*-
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# (c) Copyright 2024 Advanced Micro Devices, Inc.
+
 # from npu.runtime
 import pyxrt as xrt
 
