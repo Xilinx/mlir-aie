45b58e2e6c5b2f9a0d826565d6e59e711204a329
Kristof Denolf <kristof.denolf@amd.com>
2024-04-17 14:16:28 -0600

    apply black reformat

--- programming_guide/section-1/aie2.py
+++ programming_guide/section-1/aie2.py
@@ -8,6 +8,7 @@
 from aie.dialects.aie import *  # primary mlir-aie dialect definitions
 from aie.extras.context import mlir_mod_ctx  # mlir ctx wrapper
 
+
 # AI Engine structural design function
 def mlir_aie_design():
     # Device declaration - aie2 device ipu
@@ -18,8 +19,9 @@ def mlir_aie_design():
         ComputeTile1 = tile(1, 3)
         ComputeTile2 = tile(2, 3)
         ComputeTile3 = tile(2, 4)
-    
+
+
 # Call design function in a mlir context to generate mlir code to stdout
 with mlir_mod_ctx() as ctx:
     mlir_aie_design()
-    print(ctx.module) # Print the mlir conversion to stdout
+    print(ctx.module)  # Print the mlir conversion to stdout
