81ff8e9202729b031ad438c439468e0e2f3a4d4a
Joseph Melber <jgmelber@gmail.com>
2024-04-16 23:26:43 -0600

    [ASPLOS] Passthrough Kernel README and test.py (#1262)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Kristof Denolf <kristof.denolf@amd.com>
    Co-authored-by: Jack Lo <jack.lo@amd.com>
    Co-authored-by: Jack Lo <36210336+jackl-xilinx@users.noreply.github.com>

--- python/utils/test.py
+++ python/utils/test.py
@@ -1,4 +1,4 @@
-# ml.py -*- Python -*-
+# test.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
