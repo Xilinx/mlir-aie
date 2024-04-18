57f7409bba6d68e1e9deeadf58545c61a4b20e58
Gagandeep Singh <gagan.posted@gmail.com>
2024-04-18 13:35:31 -0600

    copyright fixes

--- programming_examples/ml/resnet/layers_conv2_x/mwe.py
+++ programming_examples/ml/resnet/layers_conv2_x/mwe.py
@@ -1,8 +1,13 @@
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+
 import torch
 import torch.nn as nn
 import sys
-
-sys.path.append("../../utils")
 import time
 import os
 import numpy as np
