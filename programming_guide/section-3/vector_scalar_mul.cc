6c5221be763e3e277d2ca60f22aa44882f97488a
Kristof Denolf <kristof.denolf@amd.com>
2024-04-18 17:24:57 -0600

    fight the dog

--- programming_guide/section-3/vector_scalar_mul.cc
+++ programming_guide/section-3/vector_scalar_mul.cc
@@ -1,4 +1,4 @@
-//===- vector_scaler_mul.cc --------------------------------------*- C++ -*-===//
+//===- vector_scaler_mul.cc -------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,7 +7,6 @@
 // Copyright (C) 2024, Advanced Micro Devices, Inc.
 //
 //===----------------------------------------------------------------------===//
-
 
 #include <stdint.h>
 #include <stdio.h>
