8d5b32ec9f185b45d6cca2608cb747b8751ac504
Joseph Melber <jmelber@xilinx.com>
2024-04-18 11:49:37 -0600

    Rename ml requirements

--- utils/quick_setup.sh
+++ utils/quick_setup.sh
@@ -74,7 +74,7 @@ if test -f "$VPP"; then
   export PYTHONPATH=`realpath mlir_aie/python`:$PYTHONPATH
   popd
   python3 -m pip install --upgrade --force-reinstall -r python/requirements.txt
-  python3 -m pip install --upgrade --force-reinstall -r python/ml_requirements.txt
+  python3 -m pip install --upgrade --force-reinstall -r python/requirements_ml.txt
   pushd programming_examples
 else
   echo "Vitis not found! Exiting..."
