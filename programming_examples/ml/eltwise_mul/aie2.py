c371918401e4f81582dd373e3e46f8f564f00914
Philip James-Roxby <phil.jamesroxby@gmail.com>
2024-04-19 17:07:25 -0600

    Update programming_examples/ml/eltwise_mul/aie2.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- programming_examples/ml/eltwise_mul/aie2.py
+++ programming_examples/ml/eltwise_mul/aie2.py
@@ -164,7 +164,7 @@ except ValueError:
 with mlir_mod_ctx() as ctx:
     my_eltwise_mul(trace_size)
     res = ctx.module.operation.verify()
-    if (res == True):
+    if res == True:
         print(ctx.module)
     else:
         print(res)
