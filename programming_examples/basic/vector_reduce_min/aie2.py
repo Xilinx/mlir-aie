a76a6fabafec9e570f9ceea898f5708ac8357dec
pjr <pjr@xilinx.com>
2024-04-22 11:49:33 -0600

    [PYTHON] Move context to new format

--- programming_examples/basic/vector_reduce_min/aie2.py
+++ programming_examples/basic/vector_reduce_min/aie2.py
@@ -21,61 +21,61 @@ def my_reduce_min():
 
     buffer_depth = 2
 
-    with mlir_mod_ctx() as ctx:
+    if len(sys.argv) != 3:
+        raise ValueError("[ERROR] Need 2 command line arguments (Device name, Col)")
 
-        if len(sys.argv) != 3:
-            raise ValueError("[ERROR] Need 2 command line arguments (Device name, Col)")
+    if sys.argv[1] == "ipu":
+        dev = AIEDevice.ipu
+    elif sys.argv[1] == "xcvc1902":
+        dev = AIEDevice.xcvc1902
+    else:
+        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
 
-        if sys.argv[1] == "ipu":
-            dev = AIEDevice.ipu
-        elif sys.argv[1] == "xcvc1902":
-            dev = AIEDevice.xcvc1902
-        else:
-            raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
+    @device(dev)
+    def device_body():
+        memRef_I_ty = T.memref(N, T.i32())
+        memRef_O_ty = T.memref(1, T.i32())
 
-        @device(dev)
-        def device_body():
-            memRef_I_ty = T.memref(N, T.i32())
-            memRef_O_ty = T.memref(1, T.i32())
+        # AIE Core Function declarations
+        reduce_min_vector = external_func(
+            "reduce_min_vector", inputs=[memRef_I_ty, memRef_O_ty, T.i32()]
+        )
 
-            # AIE Core Function declarations
-            reduce_min_vector = external_func(
-                "reduce_min_vector", inputs=[memRef_I_ty, memRef_O_ty, T.i32()]
-            )
+        # Tile declarations
+        ShimTile = tile(int(sys.argv[2]), 0)
+        ComputeTile2 = tile(int(sys.argv[2]), 2)
 
-            # Tile declarations
-            ShimTile = tile(int(sys.argv[2]), 0)
-            ComputeTile2 = tile(int(sys.argv[2]), 2)
+        # AIE-array data movement with object fifos
+        of_in = object_fifo("in", ShimTile, ComputeTile2, buffer_depth, memRef_I_ty)
+        of_out = object_fifo("out", ComputeTile2, ShimTile, buffer_depth, memRef_O_ty)
 
-            # AIE-array data movement with object fifos
-            of_in = object_fifo("in", ShimTile, ComputeTile2, buffer_depth, memRef_I_ty)
-            of_out = object_fifo(
-                "out", ComputeTile2, ShimTile, buffer_depth, memRef_O_ty
-            )
+        # Set up compute tiles
 
-            # Set up compute tiles
+        # Compute tile 2
+        @core(ComputeTile2, "reduce_min.cc.o")
+        def core_body():
+            for _ in for_(0xFFFFFFFF):
+                elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
+                elem_in = of_in.acquire(ObjectFifoPort.Consume, 1)
+                call(reduce_min_vector, [elem_in, elem_out, N])
+                of_in.release(ObjectFifoPort.Consume, 1)
+                of_out.release(ObjectFifoPort.Produce, 1)
+                yield_([])
 
-            # Compute tile 2
-            @core(ComputeTile2, "reduce_min.cc.o")
-            def core_body():
-                for _ in for_(0xFFFFFFFF):
-                    elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
-                    elem_in = of_in.acquire(ObjectFifoPort.Consume, 1)
-                    call(reduce_min_vector, [elem_in, elem_out, N])
-                    of_in.release(ObjectFifoPort.Consume, 1)
-                    of_out.release(ObjectFifoPort.Produce, 1)
-                    yield_([])
+        # To/from AIE-array data movement
+        tensor_ty = T.memref(N, T.i32())
 
-            # To/from AIE-array data movement
-            tensor_ty = T.memref(N, T.i32())
-
-            @FuncOp.from_py_func(tensor_ty, tensor_ty)
-            def sequence(A, C):
-                ipu_dma_memcpy_nd(metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, 1])
-                ipu_dma_memcpy_nd(metadata="in", bd_id=1, mem=A, sizes=[1, 1, 1, N])
-                ipu_sync(column=0, row=0, direction=0, channel=0)
-
-    print(ctx.module)
+        @FuncOp.from_py_func(tensor_ty, tensor_ty)
+        def sequence(A, C):
+            ipu_dma_memcpy_nd(metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, 1])
+            ipu_dma_memcpy_nd(metadata="in", bd_id=1, mem=A, sizes=[1, 1, 1, N])
+            ipu_sync(column=0, row=0, direction=0, channel=0)
 
 
-my_reduce_min()
+with mlir_mod_ctx() as ctx:
+    my_reduce_min()
+    res = ctx.module.operation.verify()
+    if res == True:
+        print(ctx.module)
+    else:
+        print(res)
