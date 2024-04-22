4ff5135916e4fd163d711d87cab8dcee64c62f03
Joseph Melber <jmelber@xilinx.com>
2024-04-22 10:14:36 -0600

    Add torch to lit cfg for programming_examples

--- programming_examples/lit.cfg.py
+++ programming_examples/lit.cfg.py
@@ -104,6 +104,14 @@ else:
     opencv_flags = ""
 config.substitutions.append(("%opencv_flags", opencv_flags))
 
+try:
+     import torch
+
+     config.available_features.add("torch")
+ except:
+     print("torch not found")
+     pass
+
 VitisSysrootFlag = ""
 if config.aieHostTarget == "x86_64":
     config.substitutions.append(("%aieHostTargetTriplet%", "x86_64-unknown-linux-gnu"))
