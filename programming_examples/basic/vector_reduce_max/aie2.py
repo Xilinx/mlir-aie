2a9c996a19413818214a6fa99edba760be969d01
Jack Lo <jack.lo@amd.com>
2024-04-22 10:06:22 -0700

    Updated Makefile VPATH and quick_reference.md

--- programming_examples/basic/vector_reduce_max/aie2.py
+++ programming_examples/basic/vector_reduce_max/aie2.py
@@ -56,7 +56,7 @@ def my_reduce_max():
             # Set up compute tiles
 
             # Compute tile 2
-            @core(ComputeTile2, "reduce_max.cc.o")
+            @core(ComputeTile2, "reduce_max.o")
             def core_body():
                 for _ in for_(0xFFFFFFFF):
                     elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
