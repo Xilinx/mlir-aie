singagan/mlir-aie_strix

Extend target model to represent virtualized NPU devices.
Previously, the code made certain poor assumptions about the size
of designs targetting the Phoenix NPU.  This patch attempts to
create a consistent model behind these devices, enabling the ability
to target a variety of single-column targets, in addition to the
entire 5 column array.

The main complexity arises because in the 5 column array, column zero
has no shimDMA, whereas 'virtual' designs include a number of
adjacent columns where every column has a shim DMA.  This patch
introduces different TargetModels for these different situations.
The differences also need to be taken into account when generating
CDOs/XCLBins.  Ideally, this information would exist in exactly one
place in the generated blob, unfortunately, it seems that the current
driver model requires this information to be replicated in multiple
places.

Note that this patch does change some APIs.  In particular, the
'starting column' is no longer a parameter to APIs which create
CDOs/XCLBins and is instead handled directly through the TargetModel.
In addition, the 'npu' device is gone and we now have an explicit
'npu1' device for Phoenix hardware and 'npu1_1col', 'npu1_2col',
'npu1_3col', 'npu1_4col' to represent partitions.

Let's also document all these devices while we're at it.

programming_examples/ml/resnet/layers_conv2_x/aie2.py
@@ -34,7 +34,7 @@ def resnet_conv_x():
 
     with mlir_mod_ctx() as ctx:
 
-        @device(AIEDevice.npu)
+        @device(AIEDevice.npu1_1col)
         def deviceBody():
 
             # define types

test/npu-xrt/cascade_flows/aie.mlir
@@ -6,7 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 module {
-  aie.device(npu) {
+  aie.device(npu1_1col) {
     %t00 = aie.tile(0, 0)
     %t01 = aie.tile(0, 1)
     %t03 = aie.tile(0, 3)

test/npu-xrt/two_col/aie.mlir
@@ -1,5 +1,5 @@
 module {
-  aie.device(npu) {
+  aie.device(npu1_1col) {
     %0 = aie.tile(0, 0)
     %1 = aie.tile(0, 1)
     %2 = aie.tile(0, 2)

Xilinx/mlir-aie

programming_examples/ml/resnet/layers_conv2_x/aie2.py
@@ -34,7 +34,7 @@ def resnet_conv_x():
 
     with mlir_mod_ctx() as ctx:
 
-        @device(AIEDevice.npu)
+        @device(AIEDevice.npu1_3col)
         def deviceBody():
 
             # define types

test/npu-xrt/cascade_flows/aie.mlir
@@ -6,7 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 module {
-  aie.device(npu) {
+  aie.device(npu1_2col) {
     %t00 = aie.tile(0, 0)
     %t01 = aie.tile(0, 1)
     %t03 = aie.tile(0, 3)

test/npu-xrt/two_col/aie.mlir
@@ -1,5 +1,5 @@
 module {
-  aie.device(npu) {
+  aie.device(npu1_2col) {
     %0 = aie.tile(0, 0)
     %1 = aie.tile(0, 1)
     %2 = aie.tile(0, 2)