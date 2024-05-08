2bcbc23ca6004698818a102391ba26d6cd1d333d
Joseph Melber <jgmelber@gmail.com>
2024-05-08 10:28:31 -0600

    Update run_on_npu.sh for 6.8.8
    Remove XRT_HACK_UNSECURE_LOADING_XCLBIN

--- utils/run_on_npu.sh
+++ utils/run_on_npu.sh
@@ -3,8 +3,8 @@
 # (c) Copyright 2023-2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Don't require root.
-export XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
+# # Don't require root.
+# export XRT_HACK_UNSECURE_LOADING_XCLBIN=1 
 
 XRT_DIR=/opt/xilinx/xrt
 source $XRT_DIR/setup.sh
