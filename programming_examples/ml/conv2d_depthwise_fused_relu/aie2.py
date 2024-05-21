071386b4f6e0de22a13e1578ca82c52fa0f7f4c7
Gagandeep Singh <gagan.posted@gmail.com>
2024-05-21 10:42:48 -0600

    depthwise convolution

---
+++ programming_examples/ml/conv2d_depthwise_fused_relu/aie2.py
@@ -0,0 +1,252 @@
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+
+import sys
+
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.dialects.scf import *
+from aie.extras.dialects.ext import memref, arith
+from aie.extras.context import mlir_mod_ctx
+
+tensorInW = 7
+tensorInH = 7
+tensorInC = 32
+
+tensorL1InC = tensorInC
+tensorL1OutC = tensorL1InC // 4
+
+tensorL2InC = tensorInC
+tensorL2OutC = tensorInC
+
+tensorL3InC = tensorL2OutC
+tensorL3OutC = tensorL3InC * 4
+
+if len(sys.argv) == 3:
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+
+enableTrace = False
+trace_size = 16384
+traceSizeInInt32s = trace_size // 4
+
+
+def conv2dk1():
+    with mlir_mod_ctx() as ctx:
+
+        @device(AIEDevice.npu1_1col)
+        def device_body():
+
+            # define types
+            uint8_ty = IntegerType.get_unsigned(8)
+            int8_ty = IntegerType.get_signless(8)
+            int16_ty = IntegerType.get_signless(16)
+            int32_ty = IntegerType.get_signless(32)
+
+            tensorLayer2In_ty = MemRefType.get(
+                (
+                    tensorInW,
+                    1,
+                    tensorL2InC,
+                ),
+                uint8_ty,
+            )
+            weightsLayer2_ty = MemRefType.get(
+                (3 * 3 * tensorL2OutC * 1,), int8_ty
+            )
+            tensorLayer2Out_ty = MemRefType.get(
+                (
+                    tensorInW,
+                    1,
+                    tensorL2OutC,
+                ),
+                uint8_ty,
+            )
+
+            # AIE Core Function declarations
+            conv2dk3 = external_func(
+                "conv2dk3_ui8",
+                inputs=[
+                    tensorLayer2In_ty,
+                    tensorLayer2In_ty,
+                    tensorLayer2In_ty,
+                    weightsLayer2_ty,
+                    tensorLayer2Out_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                    int32_ty,
+                ],
+            )
+
+            # Tile declarations
+            ShimTile = tile(0, 0)
+            MemTile = tile(0, 1)
+            ComputeTile2 = tile(0, 2)
+            compute_tile2_col, compute_tile2_row = 0, 2
+
+            # AIE-array data movement with object fifos
+            # Input
+            of_inOF_act_L3L2 = object_fifo(
+                "inOF_act_L3L2", ShimTile, MemTile, 2, tensorLayer2In_ty
+            )
+            of_act_2_3_5 = object_fifo("act_2_3_5", MemTile, ComputeTile2, 2, tensorLayer2In_ty)
+            object_fifo_link(of_inOF_act_L3L2, of_act_2_3_5)
+
+            # wts
+            wts_buf_01 = object_fifo(
+                "wts_buf_01", ShimTile, [ComputeTile2], 1, weightsLayer2_ty
+            )
+
+            # Output
+            act_3_4 = object_fifo("act_3_4", ComputeTile2, [MemTile], 2, tensorLayer2Out_ty)
+            of_outOFL2L3 = object_fifo("outOFL2L3", MemTile, [ShimTile], 2, tensorLayer2Out_ty)
+            object_fifo_link(act_3_4, of_outOFL2L3)
+
+            # Set up compute tiles
+
+            rtp2 = Buffer(ComputeTile2, [16], T.i32(), "rtp2")
+
+            # # Compute tile 2
+            @core(ComputeTile2, "conv2dk3.o")
+            def core_body():
+                scale = 8
+                for _ in for_(sys.maxsize):
+
+                    # acquire weights and rtps once
+                    element0Weights = wts_buf_01.acquire(ObjectFifoPort.Consume, 1)
+                    # scale = memref.load(rtpComputeTile3, 0)
+
+                    # pre-amble: top row
+                    elementActivactionsIn = of_act_2_3_5.acquire(
+                        ObjectFifoPort.Consume, 2
+                    )
+                    element0ActivactionsOut = act_3_4.acquire(ObjectFifoPort.Produce, 1)
+                    res = call(
+                        conv2dk3,
+                        [
+                            elementActivactionsIn[0],
+                            elementActivactionsIn[0],
+                            elementActivactionsIn[1],
+                            element0Weights,
+                            element0ActivactionsOut,
+                            tensorInW,
+                            1,
+                            tensorL2OutC,
+                            3,
+                            3,
+                            0,
+                            scale,
+                            0,
+                        ],
+                    )
+                    objectfifo_release(ObjectFifoPort.Produce, "act_3_4", 1)
+
+                    # middle
+                    for _ in for_(tensorInH - 2):
+                        elementActivactionsIn = of_act_2_3_5.acquire(
+                            ObjectFifoPort.Consume, 3
+                        )
+                        element0ActivactionsOut = act_3_4.acquire(
+                            ObjectFifoPort.Produce, 1
+                        )
+                        res = call(
+                            conv2dk3,
+                            [
+                                elementActivactionsIn[0],
+                                elementActivactionsIn[1],
+                                elementActivactionsIn[2],
+                                element0Weights,
+                                element0ActivactionsOut,
+                                tensorInW,
+                                1,
+                                tensorL2OutC,
+                                3,
+                                3,
+                                1,
+                                scale,
+                                0,
+                            ],
+                        )
+
+                        objectfifo_release(ObjectFifoPort.Consume, "act_2_3_5", 1)
+                        objectfifo_release(ObjectFifoPort.Produce, "act_3_4", 1)
+                        yield_([])
+
+                    # last part
+                    elementActivactionsIn = of_act_2_3_5.acquire(
+                        ObjectFifoPort.Consume, 2
+                    )
+                    element0ActivactionsOut = act_3_4.acquire(ObjectFifoPort.Produce, 1)
+                    res = call(
+                        conv2dk3,
+                        [
+                            elementActivactionsIn[0],
+                            elementActivactionsIn[1],
+                            elementActivactionsIn[1],
+                            element0Weights,
+                            element0ActivactionsOut,
+                            tensorInW,
+                            1,
+                            tensorL2OutC,
+                            3,
+                            3,
+                            2,
+                            scale,
+                            0,
+                        ],
+                    )
+
+                    objectfifo_release(ObjectFifoPort.Consume, "act_2_3_5", 2)
+                    objectfifo_release(ObjectFifoPort.Produce, "act_3_4", 1)
+
+                    objectfifo_release(ObjectFifoPort.Consume, "wts_buf_01", 1)
+                    yield_([])
+
+
+            # # instruction stream generation
+            activationsInSize32b = (tensorInW * tensorInH * tensorInC) // 4
+            acitivationsOutSize32b = activationsInSize32b
+            totalWeightsSize32b = (
+               3 * 3 * tensorL2OutC * 1
+            ) // 4
+
+            activationsInL3_ty = MemRefType.get((activationsInSize32b,), int32_ty)
+            weightsInL3_ty = MemRefType.get((totalWeightsSize32b,), int32_ty)
+
+            @FuncOp.from_py_func(activationsInL3_ty, weightsInL3_ty, activationsInL3_ty)
+            def sequence(inputFromL3, weightsFromL3, outputToL3):
+                NpuWriteRTPOp("rtp2", col=0, row=2, index=0, value=8)
+
+                npu_dma_memcpy_nd(
+                    metadata="inOF_act_L3L2",
+                    bd_id=0,
+                    mem=inputFromL3,
+                    sizes=[1, 1, 1, activationsInSize32b],
+                )
+                npu_dma_memcpy_nd(
+                    metadata="outOFL2L3",
+                    bd_id=2,
+                    mem=outputToL3,
+                    sizes=[1, 1, 1, acitivationsOutSize32b],
+                )
+                npu_dma_memcpy_nd(
+                    metadata="wts_buf_01",
+                    bd_id=1,
+                    mem=weightsFromL3,
+                    sizes=[1, 1, 1, totalWeightsSize32b],
+                )
+                npu_sync(column=0, row=0, direction=0, channel=0)
+
+    print(ctx.module)
+
+
+conv2dk1()
---
+++ programming_examples/ml/conv2d_depthwise_fused_relu/test.py
@@ -0,0 +1,241 @@
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+
+import torch
+import torch.nn as nn
+import sys
+import math
+from aie.utils.ml import DataShaper
+import time
+import os
+import numpy as np
+from aie.utils.xrt import setup_aie, extract_trace, write_out_trace, execute
+import aie.utils.test as test_utils
+from brevitas.nn import QuantConv2d, QuantIdentity, QuantReLU
+from brevitas.quant.fixed_point import (
+    Int8ActPerTensorFixedPoint,
+    Int8WeightPerTensorFixedPoint,
+    Uint8ActPerTensorFixedPoint,
+)
+torch.use_deterministic_algorithms(True)
+torch.manual_seed(0)
+
+
+def main(opts):
+    design = "conv2d_depthwise_with_relu"
+    xclbin_path = opts.xclbin
+    insts_path = opts.instr
+
+    log_folder = "log/"
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    num_iter = 1
+    npu_time_total = 0
+    npu_time_min = 9999999
+    npu_time_max = 0
+    trace_size = 16384
+    enable_trace = False
+    trace_file = "log/trace_" + design + ".txt"
+    # ------------------------------------------------------
+    # Configure this to match your design's buffer size
+    # ------------------------------------------------------
+    dtype_in = np.dtype("uint8")
+    dtype_wts = np.dtype("int8")
+    dtype_out = np.dtype("uint8")
+
+    shape_total_wts = (576, 1)
+    shape_in_act = (7, 4, 7, 8)  #'YCXC8' , 'CYX'
+    shape_in_wts1 = (1, 4, 3, 3, 8, 1)  # out,in,ky,kx,in8,out8
+    shape_out = (7, 4, 7, 8)
+    quant_conv1 = QuantConv2d(
+        32,
+        32,
+        kernel_size=3,
+        padding=1,
+        padding_mode="zeros",
+        bit_width=8,
+        weight_bit_width=8,
+        groups=32,
+        bias=False,
+        weight_quant=Int8WeightPerTensorFixedPoint,
+        return_quant_tensor=True,
+    )
+    quant_conv1.eval()
+    quant_id_1 = QuantIdentity(
+        act_quant=Uint8ActPerTensorFixedPoint, bit_width=8, return_quant_tensor=True
+    )
+    quant_relu1 = QuantReLU(
+        act_quant=Uint8ActPerTensorFixedPoint, bit_width=8, return_quant_tensor=True
+    )
+    
+    
+    # ------------------------------------------------------
+    # Initialize activation, weights, scaling factor for int8 model
+    # ------------------------------------------------------
+    input = torch.randn(1, 32, 7, 7)
+    q_inp = quant_id_1(input)
+    int_inp = q_inp.int(float_datatype=True)
+
+    # int_inp = torch.randint(1, 100, (1, 64, 7, 7)).type(torch.FloatTensor)
+    
+    # int_weight = torch.randint(50, 100, (64, 64, 1, 1)).type(torch.FloatTensor)
+    conv_scale = 0.0039  # scale to convert int8 output to floating point
+    relu_scale = 0.0078  # scale to convert int8 output to floating point
+    min = 0
+    max = 255
+
+    # ------------------------------------------------------
+    # Get device, load the xclbin & kernel and register them
+    # ------------------------------------------------------
+    app = setup_aie(
+        xclbin_path,
+        insts_path,
+        shape_in_act,
+        dtype_in,
+        shape_total_wts,
+        dtype_wts,
+        shape_out,
+        dtype_out,
+        enable_trace=enable_trace,
+        trace_size=trace_size,
+    )
+    class QuantConv2d3x3(nn.Module):
+        def __init__(self, in_planes=32, planes=32):
+            super(QuantConv2d3x3, self).__init__()
+            self.quant_id_1 = QuantIdentity(
+                act_quant=Uint8ActPerTensorFixedPoint,
+                bit_width=8,
+                return_quant_tensor=True,
+            )
+            self.conv1 = QuantConv2d(
+                in_planes,
+                planes,
+                kernel_size=3,
+                padding=1,
+                padding_mode="zeros",
+                bit_width=8,
+                groups=in_planes,
+                weight_bit_width=8,
+                bias=False,
+                weight_quant=Int8WeightPerTensorFixedPoint,
+                return_quant_tensor=True,
+            )
+            self.relu1 = QuantReLU(
+                act_quant=Uint8ActPerTensorFixedPoint,
+                bit_width=8,
+                return_quant_tensor=True,
+            )
+
+        def forward(self, x):
+            out_q = self.quant_id_1(x)
+            out = self.conv1(out_q)
+            out = self.relu1(out)
+            return out
+
+    quant_bottleneck_model = QuantConv2d3x3()
+    quant_bottleneck_model.eval()
+    int_weight = quant_bottleneck_model.conv1.quant_weight().int(float_datatype=True)
+    q_bottleneck_out = quant_bottleneck_model(input)
+    golden_output = q_bottleneck_out.int(float_datatype=True).data.numpy().astype(dtype_out)
+    print("Golden::Brevitas::", golden_output)
+    print(input.shape)
+    print(int_weight.shape)
+    print(q_bottleneck_out.shape)
+    inp_scale1 = quant_bottleneck_model.quant_id_1.quant_act_scale()
+    inp_scale2 = quant_bottleneck_model.relu1.quant_act_scale()
+    weight_scale1 = quant_bottleneck_model.conv1.quant_weight_scale()
+    combined_scale1 = -torch.log2(inp_scale1 * weight_scale1 / inp_scale2)
+    
+    print("combined_scale after first conv3x3:", combined_scale1.item())
+    # ------------------------------------------------------
+    # Define your golden reference
+    # ------------------------------------------------------
+    # class conv2d_relu_int_model(nn.Module):
+    #     def __init__(self, in_planes=64, planes=64):
+    #         super(conv2d_relu_int_model, self).__init__()
+    #         self.conv = nn.Conv2d(64, 64, kernel_size=1, bias=False)
+    #         self.relu = nn.ReLU()
+
+    #     def forward(self, x):
+    #         out_int = self.conv(x)
+    #         out_float = out_int * conv_scale
+    #         out_int = self.relu(out_float)
+    #         out_float = relu_scale * torch.clamp(
+    #             torch.round(out_int / relu_scale), min, max
+    #         )  # converting to int to do proper clipping
+    #         return out_float
+
+    # # ------------------------------------------------------
+    # # Pytorch baseline
+    # # ------------------------------------------------------
+    # model = conv2d_relu_int_model()
+    # model.eval()
+    # model.conv.weight.data.copy_(int_weight)
+    # golden_output = model(int_inp)
+
+    # ------------------------------------------------------
+    # Reorder input data-layout
+    # ------------------------------------------------------
+    ds = DataShaper()
+    before_input = int_inp.squeeze().data.numpy().astype(dtype_in)
+    before_input.tofile(
+        log_folder + "/before_ifm_mem_fmt_1x1.txt", sep=",", format="%d"
+    )
+    ifm_mem_fmt = ds.reorder_mat(before_input, "YCXC8", "CYX")
+    ifm_mem_fmt.tofile(log_folder + "/after_ifm_mem_fmt.txt", sep=",", format="%d")
+    print(int_weight.shape)
+    int_weight.data.numpy().astype(dtype_wts).tofile(log_folder + "/before_weights_mem_fmt_final.txt", sep=",", format="%d")
+    wts1 = ds.reorder_mat(int_weight.data.numpy().astype(dtype_wts), "OIYXI1O8", "OIYX")
+    total_wts = np.concatenate((wts1), axis=None)
+    print(total_wts.shape)
+    print(int_weight)
+    total_wts.tofile(log_folder + "/after_weights_mem_fmt_final.txt", sep=",", format="%d")
+
+    # ------------------------------------------------------
+    # Main run loop
+    # ------------------------------------------------------
+    for i in range(num_iter):
+        start = time.time_ns()
+        aie_output = execute(app, ifm_mem_fmt, total_wts) 
+        stop = time.time_ns()
+        npu_time = stop - start
+        npu_time_total = npu_time_total + npu_time
+
+    # ------------------------------------------------------
+    # Reorder output data-layout
+    # ------------------------------------------------------
+    temp_out = aie_output.reshape(7, 4, 7, 8)
+    temp_out = ds.reorder_mat(temp_out, "CDYX", "YCXD")
+    ofm_mem_fmt = temp_out.reshape(32, 7, 7)
+    ofm_mem_fmt.tofile(
+        log_folder + "/after_ofm_mem_fmt_final.txt", sep=",", format="%d"
+    )
+    ofm_mem_fmt_out = torch.from_numpy(ofm_mem_fmt).unsqueeze(0)
+    print(ofm_mem_fmt)
+    # ------------------------------------------------------
+    # Compare the AIE output and the golden reference
+    # ------------------------------------------------------
+    print("\nAvg NPU time: {}us.".format(int((npu_time_total / num_iter) / 1000)))
+
+    # if np.allclose(
+    #     ofm_mem_fmt_out.detach().numpy(),
+    #     golden_output.detach().numpy(),
+    #     rtol=0,
+    #     atol=2 * relu_scale,
+    # ):
+    #     print("\nPASS!\n")
+    #     exit(0)
+    # else:
+    #     print("\nFailed.\n")
+    #     exit(-1)
+
+
+if __name__ == "__main__":
+    p = test_utils.create_default_argparser()
+    opts = p.parse_args(sys.argv[1:])
+    main(opts)
