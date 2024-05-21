ebc71a1fc4a5d225c8479cfbb171e46b3a90877a
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-05-21 14:41:45 -0600

    A few programming guide hardware type fixes

--- programming_guide/section-2/section-2e/01_single_double_buffer/single_buffer.py
+++ programming_guide/section-2/section-2e/01_single_double_buffer/single_buffer.py
@@ -15,7 +15,7 @@ from aie.extras.context import mlir_mod_ctx
 def single_buffer():
     with mlir_mod_ctx() as ctx:
 
-        @device(AIEDevice.npu)
+        @device(AIEDevice.npu1_1col)
         def device_body():
             memRef_16_ty = T.memref(16, T.i32())
 
