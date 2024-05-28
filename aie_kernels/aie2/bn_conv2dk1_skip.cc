cd8de442d25f3ad052d0d3977ae709600a7275a1
Gagandeep Singh <gagan.posted@gmail.com>
2024-05-28 15:39:23 -0600

    functional correct bn10+bn11+bn12

---
+++ aie_kernels/aie2/bn_conv2dk1_skip.cc
@@ -0,0 +1,123 @@
+//===- conv2dk1_skip_init.cc -------------------------------------------------*-
+// C++
+//-*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2024, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+// #define __AIENGINE__ 1
+#define __AIENGINE__ 2
+#define NOCPP
+#define __AIEARCH__ 20
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define REL_WRITE 0
+#define REL_READ 1
+
+#include <aie_api/aie.hpp>
+
+#ifdef SCALAR
+
+const int32_t MIN = 128;
+const int32_t MAX = 127;
+const int32_t UMAX = 255;
+
+//*****************************************************************************
+// conv2d 1x1 skip - scalar
+// act: uint8, wts: int8, skip: int8, out: int8
+//*****************************************************************************
+void conv2dk1_skip_ui8_i8_i8_scalar(
+    uint8_t *input0, int8_t *kernels, int8_t *output, int8_t *skip, 
+    const int32_t input_width, const int32_t input_channels, const int32_t output_channels,
+    const int scale, const int skip_scale) {
+  event0();
+
+  int x, ic, ic2, oc, oc8, ic8, ic8b;
+
+  const int scaleT = scale;
+  const int skip_scaleT = skip_scale;
+  // const int scaleT = 10;
+  // const int skip_scaleT = 0;
+
+  for (oc = 0; oc < output_channels / 8; oc++) {
+    for (oc8 = 0; oc8 < 8; oc8++) {
+      for (x = 0; x < input_width; x++) { // col of output image
+        int sum = 0;
+        int sum_srs = 0;
+        int64_t skip_sum = 0;
+        int skip_sum_srs_final = 0;
+        int skip_sum_srs_final_out = 0;
+        int skip_temp = 0;
+        for (ic = 0; ic < input_channels / 8; ic++) {
+          for (ic8 = 0; ic8 < 8; ic8++) {
+            // int val = input0[ic * input_width + x];
+            int val = input0[(ic * input_width * 8) + (x * 8) + ic8];
+            // int k = kernels[oc * input_channels + ic];
+            int k = kernels[(oc * (input_channels / 8) * 64) + (ic * 64) +
+                            (ic8 * 8) + oc8];
+            sum += val * k;
+          }
+        }
+        // scale for convolution
+        sum_srs = (sum + (1 << (scaleT - 1))) >> scaleT;
+        sum_srs = (sum_srs > MAX)    ? MAX
+                  : (sum_srs < -MIN) ? -MIN
+                                     : sum_srs; // clip
+        // //clip
+
+        skip_temp = skip[(oc * input_width * 8) + (x * 8) + oc8];
+        skip_sum = sum_srs + skip_temp;
+
+        skip_sum_srs_final =
+            (skip_sum + (1 << (skip_scaleT - 1))) >> skip_scaleT;
+        skip_sum_srs_final_out = (skip_sum_srs_final > MAX) ? MAX
+                                 : (skip_sum_srs_final < -MIN)
+                                     ? -MIN
+                                     : skip_sum_srs_final; // clip
+
+        // output[oc * input_width + x] = skip_sum_srs_final_out;
+        output[(oc * input_width * 8) + (x * 8) + oc8] = skip_sum_srs_final_out;
+
+        // output[oc * input_width + x] = sum;
+        // output[oc * input_width + x] = sum+skip[oc * input_width + x];
+      }
+    }
+  }
+
+  event1();
+}
+#else // Vector
+
+
+#endif // Vector
+//*****************************************************************************
+// conv2d 1x1 skip wrappers
+//*****************************************************************************
+extern "C" {
+
+#ifdef SCALAR
+
+
+void conv2dk1_skip_ui8_i8_i8(uint8_t *input0,int8_t *kernels,
+                       int8_t *output, int8_t *skip,
+                       const int32_t input_width, const int32_t input_channels,
+                       const int32_t output_channels, const int scale,
+                       const int skip_scale) {
+  conv2dk1_skip_ui8_i8_i8_scalar(input0,  kernels, output, skip, input_width,
+                           input_channels, output_channels, scale, skip_scale);
+}
+
+#else // Vector
+
+
+#endif // Vector
+
+} // extern "C"
---
+++ aie_kernels/aie2/bn_conv2dk3_dw.cc
@@ -0,0 +1,368 @@
+//===- conv2dk3.cc -------------------------------------------------*- C++
+//-*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2024, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+// #define __AIENGINE__ 1
+#define __AIENGINE__ 2
+#define NOCPP
+#define __AIEARCH__ 20
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <aie_api/aie.hpp>
+
+#define REL_WRITE 0
+#define REL_READ 1
+
+enum region { top, middle, bottom };
+
+#ifdef SCALAR
+
+const int32_t MAX = 255;
+
+//*****************************************************************************
+// conv2d 3x3 - scalar
+// act: uint8, wts: int8, out: uint8
+//*****************************************************************************
+  #ifdef STRIDE2
+
+  void conv2dk3_stride2_ui8_scalar(uint8_t *line0, uint8_t *line1, uint8_t *line2,
+                          int8_t *wts, uint8_t *output,
+                          const int32_t input_width,
+                          const int32_t input_channels,
+                          const int32_t output_channels,
+                          const int32_t kernel_width,
+                          const int32_t kernel_height, const int32_t check,
+                          const int scale, const int channel_offset) 
+    {
+event0();
+
+    int x, ki,c_div_8,c8;
+    int32_t sum;
+    int sum_srs;
+    int wts_indx_0 = 0, wts_indx_1 = 0, wts_indx_2 = 0;
+    int in_indx_0 = 0;
+    int32_t output_width=input_width/2;
+    // for (oc = (0+channel_offset)/8; oc < (output_channels+channel_offset)/8;
+    // oc++) {
+    int CHANNEL_REMAIN=output_channels / 8;
+    int VECTOR_SIZE=8;
+    for (c_div_8 = 0; c_div_8 < CHANNEL_REMAIN; c_div_8++) {
+          for (c8 = 0; c8 < VECTOR_SIZE; c8++) {
+            //left border
+              sum = 0;
+              sum_srs = 0;
+              for (ki = 1; ki < kernel_width; ki++) {
+                // wts format - orig is oc,ic,ky,kx, reformat is
+                // oc,ic,k0..k8,ic8,oc8
+                int wts_indx_0 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +0*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_1 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +1*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_2 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +2*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int in_indx_0=
+                c_div_8 * input_width * VECTOR_SIZE
+                +(0 + ki-1) * VECTOR_SIZE
+                + c8;
+                if (check != top)
+                  sum += line0[in_indx_0] * wts[wts_indx_0];
+                sum += line1[in_indx_0] * wts[wts_indx_1];
+                if (check != bottom)
+                  sum += line2[in_indx_0] * wts[wts_indx_2];
+              }
+            sum_srs = (sum + (1 << (scale - 1))) >> scale;
+            sum_srs = (sum_srs > MAX) ? MAX : (sum_srs < 0) ? 0 : sum_srs;
+            output[(c_div_8 * output_width * VECTOR_SIZE) + c8] = sum_srs;
+
+            for (x = 1; x < output_width; x++) { // middle part of row
+              sum = 0;
+              sum_srs = 0;
+              for (ki = 0; ki < kernel_width; ki++) {
+                // wts format - orig is oc,ic,ky,kx, reformat is
+                // oc,ic,k0..k8,ic8,oc8
+                int wts_indx_0 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +0*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_1 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +1*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_2 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +2*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int in_indx_0=
+                c_div_8 * input_width * VECTOR_SIZE
+                +(2*x-1 + ki) * VECTOR_SIZE
+                + c8;
+                if (check != top)
+                  sum += line0[in_indx_0] * wts[wts_indx_0];
+                sum += line1[in_indx_0] * wts[wts_indx_1];
+                if (check != bottom)
+                  sum += line2[in_indx_0] * wts[wts_indx_2];
+              }
+            sum_srs = (sum + (1 << (scale - 1))) >> scale;
+            sum_srs = (sum_srs > MAX) ? MAX : (sum_srs < 0) ? 0 : sum_srs;
+            output[(c_div_8 * output_width * VECTOR_SIZE) + x * VECTOR_SIZE + c8] = sum_srs;
+            }
+          
+          }
+    }
+
+    event1();
+  }
+  #else
+  void conv2dk3_ui8_scalar(uint8_t *line0, uint8_t *line1, uint8_t *line2,
+                          int8_t *wts, uint8_t *output,
+                          const int32_t input_width,
+                          const int32_t input_channels,
+                          const int32_t output_channels,
+                          const int32_t kernel_width,
+                          const int32_t kernel_height, const int32_t check,
+                          const int scale, const int channel_offset) 
+    {
+    event0();
+
+    int x, ki,c_div_8,c8;
+    int32_t sum;
+    int sum_srs;
+    int wts_indx_0 = 0, wts_indx_1 = 0, wts_indx_2 = 0;
+    int in_indx_0 = 0;
+    // for (oc = (0+channel_offset)/8; oc < (output_channels+channel_offset)/8;
+    // oc++) {
+    int CHANNEL_REMAIN=output_channels / 8;
+    int VECTOR_SIZE=8;
+    for (c_div_8 = 0; c_div_8 < CHANNEL_REMAIN; c_div_8++) {
+          for (c8 = 0; c8 < VECTOR_SIZE; c8++) {
+            //left border
+              sum = 0;
+              sum_srs = 0;
+              for (ki = 1; ki < kernel_width; ki++) {
+                // wts format - orig is oc,ic,ky,kx, reformat is
+                // oc,ic,k0..k8,ic8,oc8
+                int wts_indx_0 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +0*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_1 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +1*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_2 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +2*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int in_indx_0=
+                c_div_8 * input_width * VECTOR_SIZE
+                +(0 + ki-1) * VECTOR_SIZE
+                + c8;
+                if (check != top)
+                  sum += line0[in_indx_0] * wts[wts_indx_0];
+                sum += line1[in_indx_0] * wts[wts_indx_1];
+                if (check != bottom)
+                  sum += line2[in_indx_0] * wts[wts_indx_2];
+              }
+            sum_srs = (sum + (1 << (scale - 1))) >> scale;
+            sum_srs = (sum_srs > MAX) ? MAX : (sum_srs < 0) ? 0 : sum_srs;
+            output[(c_div_8 * input_width * VECTOR_SIZE) + c8] = sum_srs;
+
+            //right border
+              sum = 0;
+              sum_srs = 0;
+              for (ki = 0; ki < kernel_width-1; ki++) {
+                // wts format - orig is oc,ic,ky,kx, reformat is
+                // oc,ic,k0..k8,ic8,oc8
+                int wts_indx_0 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +0*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_1 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +1*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_2 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +2*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                  
+                int in_indx_0=
+                c_div_8 * input_width * VECTOR_SIZE
+                +(input_width-2 + ki) * VECTOR_SIZE
+                + c8;
+                if (check != top)
+                  sum += line0[in_indx_0] * wts[wts_indx_0];
+                sum += line1[in_indx_0] * wts[wts_indx_1];
+                if (check != bottom)
+                  sum += line2[in_indx_0] * wts[wts_indx_2];
+              }
+            sum_srs = (sum + (1 << (scale - 1))) >> scale;
+            sum_srs = (sum_srs > MAX) ? MAX : (sum_srs < 0) ? 0 : sum_srs;
+            output[(c_div_8 * input_width * VECTOR_SIZE) + (input_width-1) * VECTOR_SIZE + c8] = sum_srs;
+
+            for (x = 1; x < input_width - 1; x++) { // middle part of row
+              sum = 0;
+              sum_srs = 0;
+              for (ki = 0; ki < kernel_width; ki++) {
+                // wts format - orig is oc,ic,ky,kx, reformat is
+                // oc,ic,k0..k8,ic8,oc8
+                int wts_indx_0 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +0*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_1 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +1*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int wts_indx_2 =
+                    +3*3*VECTOR_SIZE*c_div_8
+                    +2*3*VECTOR_SIZE
+                    +ki*VECTOR_SIZE
+                    +c8;
+                int in_indx_0=
+                c_div_8 * input_width * VECTOR_SIZE
+                +(x-1 + ki) * VECTOR_SIZE
+                + c8;
+                if (check != top)
+                  sum += line0[in_indx_0] * wts[wts_indx_0];
+                sum += line1[in_indx_0] * wts[wts_indx_1];
+                if (check != bottom)
+                  sum += line2[in_indx_0] * wts[wts_indx_2];
+              }
+            sum_srs = (sum + (1 << (scale - 1))) >> scale;
+            sum_srs = (sum_srs > MAX) ? MAX : (sum_srs < 0) ? 0 : sum_srs;
+            output[(c_div_8 * input_width * VECTOR_SIZE) + x * VECTOR_SIZE + c8] = sum_srs;
+            // output[oc * (input_width) +  x] = sum;
+            }
+          
+          }
+    }
+
+    event1();
+  }
+  #endif //STRIDE
+#else // Vector
+
+
+#endif // Vector
+
+extern "C" {
+
+#ifdef BN10
+    #ifdef SCALAR
+
+    #ifdef STRIDE2
+    void bn10_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_stride2_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #else
+    void bn10_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #endif 
+    #endif 
+#endif // BN
+#ifdef BN12
+    #ifdef SCALAR
+
+    #ifdef STRIDE2
+    void bn12_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_stride2_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #else
+    void bn12_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #endif 
+    #endif 
+#endif // BN
+#ifdef BN11
+    #ifdef SCALAR
+
+    #ifdef STRIDE2
+    void bn11_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_stride2_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #else
+    void bn11_conv2dk3_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+                      uint8_t *output, const int32_t input_width,
+                      const int32_t input_channels, const int32_t output_channels,
+                      const int32_t kernel_width, const int32_t kernel_height,
+                      const int32_t check, const int scale,
+                      const int channel_offset) {
+      conv2dk3_ui8_scalar(line0, line1, line2, wts, output, input_width,
+                          input_channels, output_channels, kernel_width,
+                          kernel_height, check, scale, channel_offset);
+    }
+    #endif 
+    #endif 
+
+  #endif 
+
+
+}
