5b8c7831676cac9c07bab3daae1d8b1d00f3aebd
AndraBisca <andrab@amd.com>
2024-05-29 16:22:35 -0600

    fix python example

--- test/python/objFifo.py
+++ test/python/objFifo.py
@@ -25,6 +25,7 @@ from util import construct_and_print_module
 # CHECK:      %tile_0_2 = aie.tile(0, 2)
 # CHECK:      %tile_1_2 = aie.tile(1, 2)
 # CHECK:      aie.objectfifo @of0(%tile_0_2, {%tile_1_2}, 2 : i32) : !aie.objectfifo<memref<256xi32>>
+# CHECK:      aie.objectfifo @of1(%tile_0_1, {%tile_1_2}, 2 : i32) {memtile_repeat = 4 : i32} : !aie.objectfifo<memref<256xi32>>
 # CHECK:      %core_1_2 = aie.core(%tile_1_2) {
 # CHECK:        %0 = aie.objectfifo.acquire @of0(Consume, 1) : !aie.objectfifosubview<memref<256xi32>>
 # CHECK:        %1 = aie.objectfifo.subview.access %0[0] : !aie.objectfifosubview<memref<256xi32>> -> memref<256xi32>
@@ -42,10 +43,12 @@ def objFifo_example():
     dev_block = Block.create_at_start(dev.body_region)
     with InsertionPoint(dev_block):
         S = tile(0, 2)
+        M = tile(0, 1)
         T_ = tile(1, 2)
 
         of0 = object_fifo("of0", S, T_, 2, T.memref(256, T.i32()))
-        of0.set_memtile_repeat(4)
+        of1 = object_fifo("of1", M, T_, 2, T.memref(256, T.i32()))
+        of1.set_memtile_repeat(4)
 
         C = Core(T_)
         bb = Block.create_at_start(C.body)
