31395a021ed4691db0d4b158e4dcde53c4a626a6
Andra Bisca <andra.bisca@gmail.com>
2024-06-03 17:58:54 +0200

    Update test/objectFifo-stateful-transform/memtile_repeat_test.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/objectFifo-stateful-transform/memtile_repeat_test.cpp
+++ test/objectFifo-stateful-transform/memtile_repeat_test.cpp
@@ -5,7 +5,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 // Copyright (C) 2024, Advanced Micro Devices, Inc.
-// 
+//
 //===----------------------------------------------------------------------===//
 
 // RUN: aie-opt --aie-objectFifo-stateful-transform %s | FileCheck %s
