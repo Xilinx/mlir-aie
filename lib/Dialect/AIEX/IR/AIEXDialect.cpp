05012fa1e3462c677e77b7b17026852aeb282bf3
Pranathi Vasireddy <pvasired@amd.com>
2024-06-05 09:11:40 -0600

    Supports sub-word data widths for strides, sizes and offsets in NpuDmaMemcpyNdOp

--- lib/Dialect/AIEX/IR/AIEXDialect.cpp
+++ lib/Dialect/AIEX/IR/AIEXDialect.cpp
@@ -66,8 +66,10 @@ LogicalResult AIEX::BroadcastPacketOp::verify() {
 
 LogicalResult AIEX::NpuDmaMemcpyNdOp::verify() {
   MemRefType buffer = getMemref().getType();
-  if (buffer.getElementTypeBitWidth() != 32)
-    return emitOpError("must be used with memref type with element width 32.");
+  if (buffer.getElementTypeBitWidth() > 32)
+      return emitOpError("Maximum element bit width allowed is 32 bits. ");
+  else if((buffer.getNumElements()*buffer.getElementTypeBitWidth()) < 32)
+      return emitOpError("Minimum bandwidth required is 32 bits. ");
   if (!llvm::all_of(getMixedStrides(), [](OpFoldResult s) {
         return getConstantIntValue(s).has_value();
       }))
--- lib/Dialect/AIEX/Transforms/AIEDmaToNpu.cpp
+++ lib/Dialect/AIEX/Transforms/AIEDmaToNpu.cpp
@@ -255,6 +255,18 @@ public:
         llvm::reverse(op.getMixedOffsets()),
         [](OpFoldResult s) { return getConstantIntValue(s).value(); });
 
+    MemRefType buffer = op.getMemref().getType();
+    auto elemWidth = buffer.getElementTypeBitWidth();
+      if(elemWidth < 32){
+        if(!strides.empty()){
+          for (int i=0; i < 3; i++){
+            strides[i] = (strides[i]*elemWidth)/32;
+          }
+       }
+       if(!sizes.empty()) sizes[0] = (sizes[0]*elemWidth)/32;
+       if(!offsets.empty())offsets[0] = (offsets[0]*elemWidth)/32;
+     }
+
     // column
     column = IntegerAttr::get(i32ty, col);
 
