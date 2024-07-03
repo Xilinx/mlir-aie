3da945b91524a7f9be599ba97b4fa0f027a7e830
andrej <an.roesti@gmail.com>
2024-07-03 11:24:53 -0600

    add row-wise bias addition programming example

---
+++ programming_examples/basic/row_wise_bias_add/aie2.py
@@ -0,0 +1,79 @@
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# (c) Copyright 2024 AMD Inc.
+
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.dialects.scf import *
+from aie.extras.dialects.ext import memref, arith
+from aie.extras.context import mlir_mod_ctx
+
+import sys
+
+
+def row_wise_bias_add(M, N, m, n):
+
+    assert M % m == 0
+    assert N % n == 0
+
+    @device(AIEDevice.npu1_1col)
+    def device_body():
+
+        complete_in_memref = T.memref(m*n, T.f32())
+        complete_bias_memref = T.memref(n, T.f32())
+        complete_out_memref = T.memref(m*n, T.f32())
+
+        in_memref = T.memref(m*n, T.f32())
+        bias_memref = T.memref(n, T.f32())
+        out_memref = T.memref(m*n, T.f32())
+
+        kernel_func = external_func(f"row_wise_bias_add_f32_f32", inputs=[in_memref, bias_memref, out_memref])
+
+        shim_tile = tile(0, 0)
+        compute_tile = tile(0, 2)
+
+        in_fifo = object_fifo("in_fifo", shim_tile, compute_tile, 2, in_memref)
+        bias_fifo = object_fifo("bias_fifo", shim_tile, compute_tile, 2, bias_memref)
+        out_fifo = object_fifo("out_fifo", compute_tile, shim_tile, 2, out_memref)
+
+        @core(compute_tile, "kernel.o")
+        def core_body():
+            for _ in for_(0xFFFFFFFF):
+                for _ in for_(N//n):
+                    elem_bias = bias_fifo.acquire(ObjectFifoPort.Consume, 1)
+                    for i in for_(M//m):
+                        elem_in = in_fifo.acquire(ObjectFifoPort.Consume, 1)
+                        elem_out = out_fifo.acquire(ObjectFifoPort.Produce, 1)
+                        call(kernel_func, [elem_in, elem_bias, elem_out])
+                        out_fifo.release(ObjectFifoPort.Produce, 1)
+                        in_fifo.release(ObjectFifoPort.Consume, 1)
+                        yield_([])
+                    bias_fifo.release(ObjectFifoPort.Consume, 1)
+                    yield_([])
+                yield_([])
+
+        @FuncOp.from_py_func(complete_in_memref, complete_bias_memref, complete_out_memref)
+        def sequence(inp, bias, out):
+            npu_dma_memcpy_nd(
+                metadata=in_fifo.sym_name.value, bd_id=0, mem=inp, sizes=[1, N//n, M, n], strides=[0, n, N, 1]
+            )
+            npu_dma_memcpy_nd(
+                metadata=bias_fifo.sym_name.value, bd_id=1, mem=bias, sizes=[1, 1, N//n, n], strides=[0, 0, n, 1]
+            )
+            npu_dma_memcpy_nd(
+                metadata=out_fifo.sym_name.value, bd_id=2, mem=out, sizes=[1, N//n, M, n], strides=[0, n, N, 1]
+            )
+            npu_sync(column=0, row=0, direction=0, channel=0)
+
+
+# Declares that subsequent code is in mlir-aie context
+with mlir_mod_ctx() as ctx:
+    row_wise_bias_add(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    res = ctx.module.operation.verify()
+    if res == True:
+        print(ctx.module)
+    else:
+        print(res)
---
+++ programming_examples/basic/row_wise_bias_add/test.cpp
@@ -0,0 +1,149 @@
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2024 AMD Inc.
+
+#include <cassert>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+
+#ifndef XCLBIN
+#define XCLBIN "build/final.xclbin"
+#endif
+
+#ifndef INSTS_TXT
+#define INSTS_TXT "build/insts.txt"
+#endif
+
+#ifndef KERNEL_NAME
+#define KERNEL_NAME "MLIR_AIE"
+#endif
+
+#define IN_SIZE (M * N * sizeof(float)) // in bytes
+#define BIAS_SIZE (N * sizeof(float)) // in bytes
+#define OUT_SIZE (M * N * sizeof(float)) // in bytes
+
+std::vector<uint32_t> load_instr_sequence(std::string instr_path) {
+  std::ifstream instr_file(instr_path);
+  std::string line;
+  std::vector<uint32_t> instr_v;
+  while (std::getline(instr_file, line)) {
+    std::istringstream iss(line);
+    uint32_t a;
+    if (!(iss >> std::hex >> a)) {
+      throw std::runtime_error("Unable to parse instruction file\n");
+    }
+    instr_v.push_back(a);
+  }
+  return instr_v;
+}
+
+void print_matrix(float *buf, int n_rows, int n_cols) {
+  for (int row = 0; row < n_rows; row++) {
+    for (int col = 0; col < n_cols; col++) {
+      std::cout << std::setw(4) << buf[row * n_cols + col] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(int argc, const char *argv[]) {
+
+  std::vector<uint32_t> instr_v = load_instr_sequence(INSTS_TXT);
+  assert(instr_v.size() > 0);
+
+  // Get a device handle
+  unsigned int device_index = 0;
+  xrt::device device = xrt::device(device_index);
+
+  // Load the xclbin
+  xrt::xclbin xclbin = xrt::xclbin(XCLBIN);
+
+  // Get the kernel from the xclbin
+  std::vector<xrt::xclbin::kernel> xkernels = xclbin.get_kernels();
+  xrt::xclbin::kernel xkernel = *std::find_if(
+      xkernels.begin(), xkernels.end(), [](xrt::xclbin::kernel &k) {
+        return k.get_name().rfind(KERNEL_NAME, 0) == 0;
+      });
+  std::string kernel_name = xkernel.get_name();
+  assert(strcmp(kernel_name.c_str(), KERNEL_NAME) == 0);
+
+  device.register_xclbin(xclbin);
+
+  // get a hardware context
+  xrt::hw_context context(device, xclbin.get_uuid());
+
+  // get a kernel handle
+  auto kernel = xrt::kernel(context, kernel_name);
+
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_in =
+      xrt::bo(device, IN_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  auto bo_bias =
+      xrt::bo(device, BIAS_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_out =
+      xrt::bo(device, OUT_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+
+  float *buf_in = bo_in.map<float *>();
+  for (int i = 0; i < IN_SIZE / sizeof(buf_in[0]); i++) {
+    buf_in[i] = i;
+  }
+  float *buf_bias = bo_bias.map<float *>();
+  for (int i = 0; i < BIAS_SIZE / sizeof(buf_bias[0]); i++) {
+    buf_bias[i] = 3*i;
+  }
+  float *buf_out = bo_out.map<float *>();
+  memset(buf_out, 0, OUT_SIZE);
+
+  // Instruction buffer for DMA configuration
+  void *buf_instr = bo_instr.map<void *>();
+  memcpy(buf_instr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_in.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_bias.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_out.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  unsigned int opcode = 3;
+  auto run = kernel(opcode, bo_instr, instr_v.size(), bo_in, bo_bias, bo_out);
+  ert_cmd_state r = run.wait();
+  if (r != ERT_CMD_STATE_COMPLETED) {
+    std::cout << "Kernel did not complete. Returned status: " << r << "\n";
+    return 1;
+  }
+
+  bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+  float ref[M * N] = {};
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      ref[i*N + j] = buf_in[i*N + j] + buf_bias[j];
+    }
+  }
+
+  if(M <= 64 && N <= 64) {
+    std::cout << "Input:" << std::endl;
+    print_matrix(buf_in, M, N);
+    std::cout << "Bias:" << std::endl;
+    print_matrix(buf_bias, 1, N);
+    std::cout << "Expected:" << std::endl;
+    print_matrix(ref, M, N);
+    std::cout << "Output:" << std::endl;
+    print_matrix(buf_out, M, N);
+  }
+
+  if (memcmp(ref, buf_out, sizeof(ref)) == 0) {
+    std::cout << "PASS!" << std::endl;
+  } else {
+    std::cout << "FAIL." << std::endl;
+  }
+
+  return 0;
+}
