0b2a37bf9b68678499c6c602494d4b79540a44cb
andrej <an.roesti@gmail.com>
2024-08-15 10:56:12 -0600

    add test

--- test/python/dma_tasks.py
+++ test/python/dma_tasks.py
@@ -8,10 +8,12 @@ from aie.dialects.aie import *
 from aie.dialects.aiex import *
 import aie.ir as ir
 
+
 with mlir_mod_ctx() as ctx:
 
     @device(AIEDevice.npu1_1col)
     def device_body():
+        shim_tile = tile(0, 0)
         # CHECK: %[[tile:.+]] = aie.tile
         mem_tile = tile(0, 1)
 
@@ -20,11 +22,13 @@ with mlir_mod_ctx() as ctx:
         # CHECK: %[[buf1:.+]] = aie.buffer
         buf1 = buffer(mem_tile, (32,), T.i16())
 
-        @runtime_sequence()
-        def seq():
+        of0 = object_fifo("of0", shim_tile, mem_tile, 2, T.memref(256, T.i32()))
+
+        @runtime_sequence(T.memref(256, T.i32()))
+        def seq(a):
             # CHECK: %dma_configure_task_0_1 = aiex.dma_configure_task(%[[tile]], MM2S, 1) {
-            tsk = dma_configure_task(mem_tile, DMAChannelDir.MM2S, 1)
-            with bds(tsk) as bd:
+            tsk0 = dma_configure_task(mem_tile, DMAChannelDir.MM2S, 1)
+            with bds(tsk0) as bd:
                 with bd[0]:
                     # CHECK:   aie.next_bd ^bb1
                     next_bd(bd[1])
@@ -41,7 +45,11 @@ with mlir_mod_ctx() as ctx:
                     dma_bd(buf1)
                     # CHECK:   aie.next_bd ^bb1
                     next_bd(bd[1])
-                pass
-            pass
+
+            tsk1 = dma_configure_task_for(of0)
+            with bds(tsk1) as bd:
+                with bd[0]:
+                    dma_bd(a)
+                    EndOp()
 
     print(ctx.module)
