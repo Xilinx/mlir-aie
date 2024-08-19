dbdd070d57cd1262543a848e523303bcba03e57d
erweiw <erwei.wang@amd.com>
2024-08-19 12:57:36 -0600

    Have AIEGenerateColumnControlOverlay generate packet flows for column control streams

--- lib/Dialect/AIE/Transforms/AIEGenerateColumnControlOverlay.cpp
+++ lib/Dialect/AIE/Transforms/AIEGenerateColumnControlOverlay.cpp
@@ -72,6 +72,22 @@ struct AIEGenerateColumnControlOverlayPass
         shimTile = builder.create<AIE::TileOp>(builder.getUnknownLoc(), col, 0);
 
       if (clRouteShimCTRLToTCT) {
+        // Get all tile ops on column col
+        SmallVector<AIE::TileOp> tilesOnCol;
+        for (auto &[tId, tOp] : tiles) {
+          if (tId.col != col)
+            continue;
+          tilesOnCol.push_back(tOp);
+        }
+
+        int unusedPacketIdFrom = getUnusedPacketIdFrom(device);
+        SmallVector<int> availableShimChans = {
+            0}; // Only using SHIM dest SOUTH 0 for TCT.
+        // Create packet flows per col
+        generatePacketFlowsForControl(
+            builder, device, targetModel, shimTile, AIE::WireBundle::South,
+            availableShimChans, tilesOnCol, AIE::WireBundle::Ctrl, 0,
+            unusedPacketIdFrom, false);
       }
       if (clRouteShimDmaToTileCTRL) {
         // Get all tile ops on column col
@@ -83,29 +99,94 @@ struct AIEGenerateColumnControlOverlayPass
               targetModel.isMemTile(tId.col, tId.row))
             tilesOnCol.push_back(tOp);
         }
-        // Create packet flows per col
-        SmallVector<int> thresholdsToNextChannel;
-        int numShimDmaMM2SChans = targetModel.getNumSourceShimMuxConnections(
+
+        int unusedPacketIdFrom = getUnusedPacketIdFrom(device);
+        int numShimChans = targetModel.getNumSourceShimMuxConnections(
             shimTile.getCol(), shimTile.getRow(), AIE::WireBundle::DMA);
-        for (int i = 1; i < numShimDmaMM2SChans + 1; i++)
-          thresholdsToNextChannel.push_back(tilesOnCol.size() /
-                                            numShimDmaMM2SChans * i);
-        int unusedIdFrom =
-            0; // TODO: get unused packet header for control packets
-        int ctrlPktFlowID = unusedIdFrom;
-        int currShimChan = 0;
-        for (int i = 0; i < (int)tilesOnCol.size(); i++) {
-          builder.setInsertionPointToEnd(device.getBody());
-          (void)createPacketFlowOp(builder, ctrlPktFlowID, shimTile,
-                                   AIE::WireBundle::DMA, currShimChan,
-                                   tilesOnCol[i], AIE::WireBundle::Ctrl, 0);
-          if (i >= thresholdsToNextChannel[currShimChan]) {
-            currShimChan++;
-            ctrlPktFlowID = unusedIdFrom;
-          }
-        }
+        SmallVector<int> availableShimChans = getAvailableShimChans(
+            device, numShimChans, shimTile, AIE::WireBundle::DMA, true);
+        // Create packet flows per col
+        generatePacketFlowsForControl(builder, device, targetModel, shimTile,
+                                      AIE::WireBundle::DMA, availableShimChans,
+                                      tilesOnCol, AIE::WireBundle::Ctrl, 0,
+                                      unusedPacketIdFrom, true);
       }
     }
+  }
+
+  // Create packet flows per col which moves control packets to and from shim
+  // dma
+  void generatePacketFlowsForControl(OpBuilder builder, DeviceOp device,
+                                     const AIETargetModel &targetModel,
+                                     TileOp shimTile, WireBundle shimWireBundle,
+                                     SmallVector<int> availableShimChans,
+                                     SmallVector<AIE::TileOp> coreOrMemTiles,
+                                     WireBundle coreOrMemWireBundle,
+                                     int coreOrMemChanId,
+                                     int unusedPacketIdFrom, bool isShimMM2S) {
+    // Get a set of available shim dma channels
+    int numShimDmaChans = targetModel.getNumSourceShimMuxConnections(
+        shimTile.getCol(), shimTile.getRow(), shimWireBundle);
+    // Create packet flows
+    SmallVector<int> thresholdsToNextShimChannel;
+    for (int i = 1; i < (int)availableShimChans.size() + 1; i++)
+      thresholdsToNextShimChannel.push_back(coreOrMemTiles.size() /
+                                            availableShimChans.size() * i);
+    int ctrlPktFlowID = unusedPacketIdFrom;
+    int shimChanIdx = 0;
+    for (int i = 0; i < (int)coreOrMemTiles.size(); i++) {
+      builder.setInsertionPointToEnd(device.getBody());
+      if (isShimMM2S)
+        (void)createPacketFlowOp(
+            builder, ctrlPktFlowID, shimTile, shimWireBundle,
+            availableShimChans[shimChanIdx], coreOrMemTiles[i],
+            coreOrMemWireBundle, coreOrMemChanId);
+      else
+        (void)createPacketFlowOp(builder, ctrlPktFlowID, coreOrMemTiles[i],
+                                 coreOrMemWireBundle, coreOrMemChanId, shimTile,
+                                 shimWireBundle,
+                                 availableShimChans[shimChanIdx]);
+      if (i >= thresholdsToNextShimChannel[shimChanIdx]) {
+        shimChanIdx++;
+        ctrlPktFlowID = unusedPacketIdFrom;
+      }
+    }
+  }
+
+  int getUnusedPacketIdFrom(DeviceOp device) {
+    int unusedPacketIdFrom = 0;
+    device.walk([&](AIE::PacketFlowOp pOp) {
+      unusedPacketIdFrom = std::max(unusedPacketIdFrom, pOp.IDInt());
+    });
+    return unusedPacketIdFrom + 1;
+  }
+
+  // Get a vector of shim channels not reserved by any circuit-switched aie.flow
+  // op
+  SmallVector<int> getAvailableShimChans(DeviceOp device, int numShimChans,
+                                         TileOp shimTile,
+                                         WireBundle shimWireBundle,
+                                         bool isShimMM2S) {
+    SmallVector<int> availableShimChans;
+    for (int i = 0; i < numShimChans; i++) {
+      bool isShimChanReservedByAIEFlowOp = false;
+      device.walk([&](AIE::FlowOp fOp) {
+        if (isShimMM2S && fOp.getSource() == shimTile &&
+            fOp.getSourceBundle() == shimWireBundle &&
+            fOp.getSourceChannel() == i) {
+          isShimChanReservedByAIEFlowOp = true;
+          return;
+        } else if (!isShimMM2S && fOp.getDest() == shimTile &&
+                   fOp.getDestBundle() == shimWireBundle &&
+                   fOp.getDestChannel() == i) {
+          isShimChanReservedByAIEFlowOp = true;
+          return;
+        }
+      });
+      if (!isShimChanReservedByAIEFlowOp)
+        availableShimChans.push_back(i);
+    }
+    return availableShimChans;
   }
 };
 
