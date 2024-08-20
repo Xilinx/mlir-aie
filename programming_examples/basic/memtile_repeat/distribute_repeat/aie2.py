6b26f1c8613f49bae6e1a94cb08e1e661b0350f5
Andra Bisca <andra.bisca@gmail.com>
2024-08-20 12:31:49 +0200

    Update programming_examples/basic/memtile_repeat/distribute_repeat/aie2.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- programming_examples/basic/memtile_repeat/distribute_repeat/aie2.py
+++ programming_examples/basic/memtile_repeat/distribute_repeat/aie2.py
@@ -102,7 +102,9 @@ def distribute_repeat():
 
             @FuncOp.from_py_func(tensor_in_ty, tensor_in_ty, tensor_out_ty)
             def sequence(A, B, C):
-                npu_dma_memcpy_nd(metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, out_size])
+                npu_dma_memcpy_nd(
+                    metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, out_size]
+                )
                 npu_dma_memcpy_nd(metadata="in", bd_id=1, mem=A, sizes=[1, 1, 1, N])
                 npu_sync(column=0, row=0, direction=0, channel=0)
 
