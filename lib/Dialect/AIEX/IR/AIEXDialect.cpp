68453579e321949a5178206dac05237a818081a6
erweiw <erwei.wang@amd.com>
2024-08-29 17:29:08 -0600

    Remove unused methods

--- lib/Dialect/AIEX/IR/AIEXDialect.cpp
+++ lib/Dialect/AIEX/IR/AIEXDialect.cpp
@@ -642,23 +642,3 @@ LogicalResult AIEX::DMAStartBdChainOp::verify() {
   }
   return success();
 }
-
-//===----------------------------------------------------------------------===//
-// NpuControlPacketOp
-//===----------------------------------------------------------------------===//
-
-uint32_t AIEX::NpuControlPacketOp::getRow() {
-  const auto &targetModel = AIE::getTargetModel(*this);
-  uint32_t addr = getAddress();
-  uint32_t rowInt =
-      (addr & (0xff << targetModel.getRowShift())) >> targetModel.getRowShift();
-  return rowInt;
-}
-
-uint32_t AIEX::NpuControlPacketOp::getColumn() {
-  const auto &targetModel = AIE::getTargetModel(*this);
-  uint32_t addr = getAddress();
-  uint32_t colInt = (addr & (0xff << targetModel.getColumnShift())) >>
-                    targetModel.getColumnShift();
-  return colInt;
-}
