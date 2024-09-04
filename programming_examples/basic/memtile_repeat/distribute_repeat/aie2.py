914d68e308e18b5fe4ded7461533f2d891d6b7a5
Erika Hunhoff <erika.hunhoff@amd.com>
2024-09-04 13:22:31 -0600

    distribute repeat is working locally

--- programming_examples/basic/memtile_repeat/distribute_repeat/aie2.py
+++ programming_examples/basic/memtile_repeat/distribute_repeat/aie2.py
@@ -5,18 +5,18 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
 # (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
-
 import sys
 
 from aie.dialects.aie import *
 from aie.dialects.aiex import *
 from aie.dialects.scf import *
-from aie.extras.dialects.ext import memref, arith
+from aie.extras.dialects.ext import arith
 from aie.extras.context import mlir_mod_ctx
 
+N = 4096
 dev = AIEDevice.npu1_1col
 col = 0
-N = 36
+repeat_counter = 6
 
 if len(sys.argv) > 1:
     N = int(sys.argv[1])
@@ -32,7 +32,7 @@ if len(sys.argv) > 2:
 if len(sys.argv) > 3:
     col = int(sys.argv[3])
 
-repeat_counter = 6
+assert N % 2 == 0, "N must be even"
 out_size = N * (repeat_counter + 1)
 
 
@@ -41,9 +41,9 @@ def distribute_repeat():
 
         @device(dev)
         def device_body():
-            memRef_in_ty = T.memref(N, T.i32())
+            memRef_ty = T.memref(N, T.i32())
+            memRef_half_ty = T.memref(N // 2, T.i32())
             memRef_out_ty = T.memref(out_size, T.i32())
-            memRef_18_ty = T.memref(N // 2, T.i32())
 
             # Tile declarations
             ShimTile = tile(col, 0)
@@ -52,15 +52,15 @@ def distribute_repeat():
             ComputeTile3 = tile(col, 3)
 
             # AIE-array data movement with object fifos
-            of_in = object_fifo("in", ShimTile, MemTile, 1, memRef_in_ty)
-            of_in2 = object_fifo("in2", MemTile, ComputeTile2, 2, memRef_18_ty)
-            of_in3 = object_fifo("in3", MemTile, ComputeTile3, 2, memRef_18_ty)
+            of_in = object_fifo("in", ShimTile, MemTile, 1, memRef_ty)
+            of_in2 = object_fifo("in2", MemTile, ComputeTile2, 2, memRef_half_ty)
+            of_in3 = object_fifo("in3", MemTile, ComputeTile3, 2, memRef_half_ty)
             of_in2.set_memtile_repeat(repeat_counter)
             of_in3.set_memtile_repeat(repeat_counter)
             object_fifo_link(of_in, [of_in2, of_in3], [], [0, N // 2])
 
-            of_out2 = object_fifo("out2", ComputeTile2, MemTile, 2, memRef_18_ty)
-            of_out3 = object_fifo("out3", ComputeTile3, MemTile, 2, memRef_18_ty)
+            of_out2 = object_fifo("out2", ComputeTile2, MemTile, 2, memRef_half_ty)
+            of_out3 = object_fifo("out3", ComputeTile3, MemTile, 2, memRef_half_ty)
             of_out = object_fifo("out", MemTile, ShimTile, 1, memRef_out_ty)
             object_fifo_link([of_out2, of_out3], of_out, [0, out_size // 2], [])
 
@@ -97,10 +97,10 @@ def distribute_repeat():
                     yield_([])
 
             # To/from AIE-array data movement
+            tensor_ty = T.memref(N, T.i32())
             tensor_out_ty = T.memref(out_size, T.i32())
-            tensor_in_ty = T.memref(N, T.i32())
 
-            @runtime_sequence(tensor_in_ty, tensor_in_ty, tensor_out_ty)
+            @runtime_sequence(tensor_ty, tensor_ty, tensor_out_ty)
             def sequence(A, B, C):
                 npu_dma_memcpy_nd(
                     metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, out_size]
