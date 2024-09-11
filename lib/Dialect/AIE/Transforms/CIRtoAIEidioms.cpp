b68da02fd2fe03885b07aef3eca77d876ffd8283
Ronan Keryell <ronan.keryell@amd.com>
2024-09-11 12:25:58 -0700

    [CIR] Generate aie.device operation from C++ aie::device<> use

--- lib/Dialect/AIE/Transforms/CIRtoAIEidioms.cpp
+++ lib/Dialect/AIE/Transforms/CIRtoAIEidioms.cpp
@@ -10,15 +10,70 @@
 #include "aie/Dialect/AIE/IR/AIEDialect.h"
 #include "aie/Dialect/AIE/Transforms/AIEPasses.h"
 
+#include "mlir/IR/Builders.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Support/LLVM.h"
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/IR/CIRTypes.h"
+#include "llvm/Support/Regex.h"
 
 namespace xilinx::AIE {
 
+namespace {
+struct DeviceNameAndLoc {
+  std::string name;
+  mlir::Location loc;
+};
+
+std::optional<DeviceNameAndLoc> getDeviceName(mlir::ModuleOp m) {
+  std::optional<DeviceNameAndLoc> deviceName;
+  m->walk([&](mlir::cir::CallOp call) {
+    // Look at the member function call to aie::device::run()
+    if (auto calleeName = call.getCallee();
+        // \todo Make this mangle-free
+        calleeName != "_ZN3aie6deviceILNS_3$_0E0EE3runEv")
+      return;
+    // Dive into the type of the object which is passed as the first parameter
+    // to the member function
+    auto aieDeviceInstance = call.getArgOperand(0);
+    // Get to the aie::device type
+    auto aieDeviceType = mlir::cast<mlir::cir::StructType>(
+        mlir::cast<mlir::cir::PointerType>(aieDeviceInstance.getType())
+            .getPointee());
+    // The struct has a name like "aie::device<aie::npu1>" and the "npu1" is
+    // used directly for the MLIR aie.device attribute
+    static const llvm::Regex deviceNamePattern{"^aie::device<aie::([^>]+)>$"};
+    if (llvm::SmallVector<llvm::StringRef> matches;
+        deviceNamePattern.match(aieDeviceType.getName(), &matches))
+      deviceName = {std::string{matches[1]}, call.getLoc()};
+    // \todo optimize with early exit walk::interrupt()?
+  });
+  return deviceName;
+}
+
+} // namespace
+
 struct CIRtoAIEidiomsPass : CIRtoAIEidiomsBase<CIRtoAIEidiomsPass> {
   void runOnOperation() override {
-
-    //ModuleOp m = getOperation();
-    return signalPassFailure();
+    mlir::ModuleOp m = getOperation();
+    if (std::optional<DeviceNameAndLoc> deviceName = getDeviceName(m);
+        deviceName) {
+      mlir::OpBuilder builder{m};
+      // Parse the device name into the right integer attribute
+      auto deviceId =
+          *xilinx::AIE::symbolizeEnum<xilinx::AIE::AIEDevice>(deviceName->name);
+      auto device =
+          builder.create<xilinx::AIE::DeviceOp>(deviceName->loc, deviceId);
+      // Move the module region block into the device region
+      device.getRegion().takeBody(m.getBodyRegion());
+      // Recreate an empty region in the module
+      new (&m.getRegion()) mlir::Region{m};
+      // Create an empty block in the region
+      m.getRegion().emplaceBlock();
+      // Insert the aie.device in the module block
+      builder.setInsertionPointToStart(m.getBody());
+      builder.insert(device);
+    }
   }
 };
 
