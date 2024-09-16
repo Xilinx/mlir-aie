978f55eeaa557cfd64badbea68ad7436cfca4b74
Pranathi Vasireddy <pvasired@amd.com>
2024-09-16 16:50:55 -0600

    Merge latest changes

--- python/compiler/aiecc/main.py
+++ python/compiler/aiecc/main.py
@@ -37,7 +37,7 @@ from aie.ir import Context, Location, Module
 from aie.passmanager import PassManager
 
 INPUT_WITH_ADDRESSES_PIPELINE = (
-    lambda basic_alloc_scheme=False, ctrl_pkt_overlay=False: (
+    lambda scheme="", ctrl_pkt_overlay=False: (
         Pipeline()
         .lower_affine()
         .add_pass("aie-canonicalize-device")
@@ -56,7 +56,7 @@ INPUT_WITH_ADDRESSES_PIPELINE = (
                 "aie-generate-column-control-overlay",
                 route_shim_to_tile_ctrl=ctrl_pkt_overlay,
             )
-            .add_pass("aie-assign-buffer-addresses", basic_alloc=basic_alloc_scheme),
+            .add_pass("aie-assign-buffer-addresses", alloc_scheme=scheme),
         )
         .convert_scf_to_cf()
     )
@@ -1048,18 +1048,12 @@ class FlowRunner:
             )
 
             file_with_addresses = self.prepend_tmp("input_with_addresses.mlir")
-<<<<<<< HEAD
             if opts.alloc_scheme:
                 pass_pipeline = INPUT_WITH_ADDRESSES_PIPELINE(
                     opts.alloc_scheme
                 ).materialize(module=True)
             else:
                 pass_pipeline = INPUT_WITH_ADDRESSES_PIPELINE().materialize(module=True)
-=======
-            pass_pipeline = INPUT_WITH_ADDRESSES_PIPELINE(
-                opts.basic_alloc_scheme, opts.ctrl_pkt_overlay
-            ).materialize(module=True)
->>>>>>> 932a5df230ed255e9053777f0e07ddde1324531c
             run_passes(
                 pass_pipeline,
                 self.mlir_module_str,
