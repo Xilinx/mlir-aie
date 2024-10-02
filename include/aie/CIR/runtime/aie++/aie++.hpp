c0dd56b4e75a8dc69784bcb26119b6c9d45fe656
Ronan Keryell <ronan.keryell@amd.com>
2024-10-02 13:18:08 -0700

    [CIR][header] Makes aie::buffer a real data type to be cleaner
    Also simplify some type names by removing some `_t`.

--- include/aie/CIR/runtime/aie++/aie++.hpp
+++ include/aie/CIR/runtime/aie++/aie++.hpp
@@ -9,16 +9,25 @@ namespace aie {
 
 // using namespace std::literals::string_view_literals;
 
-template <typename T, std::size_t Size> using buffer_t = std::array<T, Size>;
+// template <typename T, std::size_t Size> using buffer_t = std::array<T, Size>;
+template <typename T, std::size_t Size>
+struct buffer{
+  using storage_t = std::array<T, Size>;
+  storage_t storage;
+  operator storage_t&() { return storage; }
+  // The previous is not enough
+  decltype(auto) operator [](std::size_t index) { return storage[index]; }
+
+};
 
 // Typically compiled as:
-// !ty_22aie3A3Atile_t3C12C_43E22 = !cir.struct<struct "aie::tile_t<1, 4>"
+// !ty_22aie3A3Atile3C12C_43E22 = !cir.struct<struct "aie::tile<1, 4>"
 // {!cir.int<u, 8>}>
-template <int X, int Y> struct tile_t {
+template <int X, int Y> struct tile {
   static constexpr auto x() { return X; }
   static constexpr auto y() { return Y; }
   template <int X2, int Y2>
-  friend constexpr auto operator==(const tile_t &, const tile_t<X2, Y2> &) {
+  friend constexpr auto operator==(const tile &, const tile<X2, Y2> &) {
     return X == X2 && Y == Y2;
   };
   /*  template <int X1, int Y1, int X2, int Y2>
@@ -26,7 +35,7 @@ template <int X, int Y> struct tile_t {
       return std::strong_ordering::equal;
     };*/
   /*
-  friend constexpr auto operator<=>(const tile_t &a, const tile_t &b) {
+  friend constexpr auto operator<=>(const tile &a, const tile &b) {
 //    return operator<=>(std::array { a.x, a.y }, std::array { b.x, b.y });
     return operator<=>(a.x, a.y);
   }
@@ -34,13 +43,13 @@ template <int X, int Y> struct tile_t {
 
   template <typename Code> static inline Code tile_code;
 
-  template <typename T, std::size_t Size> buffer_t<T, Size> buffer() __attribute__((annotate("aie.tile.buffer"))) {
+  template <typename T, std::size_t Size> buffer<T, Size> buffer() __attribute__((annotate("aie.tile.buffer"))) {
     return {};
   }
 
   // Typically compiled as:
-  // cir.call @_ZN3aie6tile_tILi1ELi4EE7programIZ4mainE3$_0EEvOT_(%2, %7) :
-  // (!cir.ptr<!ty_22aie3A3Atile_t3C12C_43E22>, !cir.ptr<!ty_22anon2E122>) -> ()
+  // cir.call @_ZN3aie4tileILi1ELi4EE7programIZ4mainE3$_0EEvOT_(%2, %7) :
+  // (!cir.ptr<!ty_22aie3A3Atile3C12C_43E22>, !cir.ptr<!ty_22anon2E122>) -> ()
   // loc(#loc63)
   void program(auto &&code) __attribute__((annotate("aie.tile.program"))) {
     // Use this since std::function crashes ClangIR 2024/09/12
@@ -50,7 +59,7 @@ template <int X, int Y> struct tile_t {
   }
 };
 
-template <int X, int Y> inline constexpr tile_t<X, Y> tile;
+//template <int X, int Y> inline constexpr tile<X, Y> tile;
 
 template <typename Storage> struct tile_handle {
   Storage tile_memory;
@@ -66,7 +75,7 @@ enum : std::int8_t { npu1 = 42};
 // "aie::device<aie::npu1>" {!cir.int<u, 8>}>
 template <auto Name = npu1> struct device {
   template <int X, int Y>
-  tile_t<X, Y> tile()
+  tile<X, Y> tile()
       __attribute__((annotate("aie.device.tile", X, Y, Name, std::to_underlying(Name)))) {
     return {};
   };
