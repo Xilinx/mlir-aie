73d9f3a701130545bd9e60c063c4d5dcf5f0809c
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-02 15:20:32 -0600

    fix typo

--- python/extras/dialects/ext/func.py
+++ python/extras/dialects/ext/func.py
@@ -50,9 +50,11 @@ def call(
             raise ValueError(
                 "unexpected third argument when constructing a call" + "to a function"
             )
-        if len(arguments) != len(callee_or_results.function_type.value.inputs):
+        if len(arguments_or_callee) != len(
+            callee_or_results.function_type.value.inputs
+        ):
             raise ValueError(
-                f"Expected {len(callee_or_results.function_type.value.inputs)} arguments, but got {len(arguments)} arguments"
+                f"Expected {len(callee_or_results.function_type.value.inputs)} arguments, but got {len(arguments_or_callee)} arguments"
             )
         args = []
         for i, a in enumerate(arguments_or_callee):
