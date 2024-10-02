990a5ff81c03f1bba3648247a7ffa0223eb3e148
Ronan Keryell <ronan.keryell@amd.com>
2024-10-02 08:16:12 -0700

    [CIR] Add runtime header prototype for aie++ and test example
    To do: integrate this into CMake infrastructure.

---
+++ include/aie/CIR/runtime/aie++/aie++.hpp
@@ -0,0 +1,83 @@
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <utility>
+
+// #include <string_view>
+
+namespace aie {
+
+// using namespace std::literals::string_view_literals;
+
+template <typename T, std::size_t Size> using buffer_t = std::array<T, Size>;
+
+// Typically compiled as:
+// !ty_22aie3A3Atile_t3C12C_43E22 = !cir.struct<struct "aie::tile_t<1, 4>"
+// {!cir.int<u, 8>}>
+template <int X, int Y> struct tile_t {
+  static constexpr auto x() { return X; }
+  static constexpr auto y() { return Y; }
+  template <int X2, int Y2>
+  friend constexpr auto operator==(const tile_t &, const tile_t<X2, Y2> &) {
+    return X == X2 && Y == Y2;
+  };
+  /*  template <int X1, int Y1, int X2, int Y2>
+    friend auto operator<=>(const tile_t<X1, Y1> &, const tile_t<X2, Y2> &) {
+      return std::strong_ordering::equal;
+    };*/
+  /*
+  friend constexpr auto operator<=>(const tile_t &a, const tile_t &b) {
+//    return operator<=>(std::array { a.x, a.y }, std::array { b.x, b.y });
+    return operator<=>(a.x, a.y);
+  }
+*/
+
+  template <typename Code> static inline Code tile_code;
+
+  template <typename T, std::size_t Size> buffer_t<T, Size> buffer() __attribute__((annotate("aie.tile.buffer"))) {
+    return {};
+  }
+
+  // Typically compiled as:
+  // cir.call @_ZN3aie6tile_tILi1ELi4EE7programIZ4mainE3$_0EEvOT_(%2, %7) :
+  // (!cir.ptr<!ty_22aie3A3Atile_t3C12C_43E22>, !cir.ptr<!ty_22anon2E122>) -> ()
+  // loc(#loc63)
+  void program(auto &&code) __attribute__((annotate("aie.tile.program"))) {
+    // Use this since std::function crashes ClangIR 2024/09/12
+    tile_code<decltype(&code)> = &code;
+    // Just to instantiate the lambda body while waiting for std::function
+    code();
+  }
+};
+
+template <int X, int Y> inline constexpr tile_t<X, Y> tile;
+
+template <typename Storage> struct tile_handle {
+  Storage tile_memory;
+  constexpr tile_handle(Storage tile_memory) : tile_memory{tile_memory} {};
+  constexpr auto &mem() { return tile_memory; }
+};
+
+// Inject in aie:: to ease use
+enum : std::int8_t { npu1 = 42};
+
+// Typically compiled as:
+// !ty_22aie3A3Adevice3Caie3A3Anpu13E22 = !cir.struct<struct
+// "aie::device<aie::npu1>" {!cir.int<u, 8>}>
+template <auto Name = npu1> struct device {
+  template <int X, int Y>
+  tile_t<X, Y> tile()
+      __attribute__((annotate("aie.device.tile", X, Y, Name, std::to_underlying(Name)))) {
+    return {};
+  };
+
+  void constexpr run() {}
+};
+
+template <typename T> struct tile_storage_t {
+  using content = T;
+};
+
+template <typename T> inline constexpr tile_storage_t<T> tile_storage;
+
+} // namespace aie
---
+++ test/CIR/aie++/example.cpp
@@ -0,0 +1,33 @@
+/*
+https://github.com/Xilinx/mlir-aie/blob/main/mlir_tutorials/tutorial-1/answers/aie_q5.mlir
+module @tutorial_1 {
+  // Declare tile object of the AIE class located at position col 1, row 4
+  %tile14 = aie.tile(1, 4)
+
+  // Declare buffer for tile(1, 4) with symbolic name "a14" and
+  // size 256 deep x int32 wide. By default, the address of
+  // this buffer begins after the stack (1024 Bytes offset) and
+  // all subsequent buffers are allocated one after another in memory.
+  %buf = aie.buffer(%tile14) { sym_name = "a14" } : memref<8192xi32>
+
+  // Define the algorithm for the core of tile(1, 4)
+  // buf[3] = 14
+  %core14 = aie.core(%tile14) {
+    %val = arith.constant 14 : i32 // declare a constant (int32)
+    %idx = arith.constant 3 : index // declare a constant (index)
+    memref.store %val, %buf[%idx] : memref<8192xi32> // store val in buf[3]
+    aie.end
+  }
+}
+*/
+
+#include "aie++.hpp"
+
+int main() {
+  aie::device<aie::npu1> d;
+  auto t = d.tile<1, 4>();
+  auto b = t.buffer<int, 8192>();
+  t.program([&] { b[3] = 14; });
+  d.tile<2, 3>().program([]{});
+  d.run();
+}
