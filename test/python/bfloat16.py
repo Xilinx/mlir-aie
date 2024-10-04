77f654224c602bcf2dcfef37e4013b3765804248
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-04 14:24:28 -0600

    Actually fix bfloat16 conversion test

--- test/python/bfloat16.py
+++ test/python/bfloat16.py
@@ -1,19 +1,22 @@
 # Copyright (C) 2022, Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 import numpy as np
-
-# RUN: %PYTHON %s | FileCheck %s
 from aie.extras import types as T
 from aie.extras.util import bfloat16, _pseudo_bfloat16, np_dtype_to_mlir_type
+from util import construct_and_print_module
+
+# RUN: %PYTHON %s | FileCheck %s
 
 
+# CHECK-LABEL: bfloat16Conversion
+# CHECK: PASS!
+@construct_and_print_module
 def bfloat16Conversion():
     assert np.dtype(bfloat16).itemsize == 2
     assert _pseudo_bfloat16 != np.float16
     assert np_dtype_to_mlir_type(_pseudo_bfloat16) == T.bf16()
     try:
-        # Checks if bfloat16 library is available
-        from bfloat16 import real_bfloat16
+        from bfloat16 import bfloat16 as real_bfloat16
 
         assert real_bfloat16 == bfloat16
         assert np_dtype_to_mlir_type(real_bfloat16) == T.bf16()
@@ -21,8 +24,6 @@ def bfloat16Conversion():
         assert np_dtype_to_mlir_type(real_bfloat16) == np_dtype_to_mlir_type(
             _pseudo_bfloat16
         )
-    except:
-        # CHECK: Warning! bfloat16 python package not available, so using placeholder bfloat16 type
+    except ModuleNotFoundError:
         pass
-    # CHECK: PASS
-    print("PASS")
+    print("PASS!")
