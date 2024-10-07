4ed0a2de5bfb3334457929f1bdbb6c8f8c25ab0f
Jeff Fifield <jeff.fifield@amd.com>
2024-10-07 10:48:36 -0600

    update llvm to 0e2970f0 (#1822)

--- utils/clone-llvm.sh
+++ utils/clone-llvm.sh
@@ -13,8 +13,8 @@
 ##===----------------------------------------------------------------------===##
 
 # The LLVM commit to use.
-LLVM_PROJECT_COMMIT=bfa8519bbb48942648c9ca58d2380b48313894d1
-DATETIME=2024093013
+LLVM_PROJECT_COMMIT=0e2970f0ad723a3b4e56d18858f4fd215415002f
+DATETIME=2024100714
 WHEEL_VERSION=20.0.0.$DATETIME+${LLVM_PROJECT_COMMIT:0:8}
 
 ############################################################################################
