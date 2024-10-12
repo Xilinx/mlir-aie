b10276e1f4360e121f027dbc01c3f237a567f60d
Joseph Melber <jgmelber@gmail.com>
2024-10-11 21:37:19 -0600

    Update quick_setup.sh

--- utils/quick_setup.sh
+++ utils/quick_setup.sh
@@ -69,14 +69,11 @@ if test -f "$VPP"; then
   rm -rf mlir*.whl
   rm -rf llvm_aie*.whl
   # pip install https://github.com/makslevental/mlir-python-extras/archive/d84f05582adb2eed07145dabce1e03e13d0e29a6.zip
-  export PATH=`realpath llvm-aie/bin`:`realpath mlir_aie/bin`:`realpath mlir/bin`:$PATH
-  export LD_LIBRARY_PATH=`realpath llvm-aie/lib`:`realpath mlir_aie/lib`:`realpath mlir/lib`:$LD_LIBRARY_PATH
-  export PYTHONPATH=`realpath mlir_aie/python`:$PYTHONPATH
-  export PEANO_DIR=`realpath llvm-aie`
   popd
   python3 -m pip install --upgrade --force-reinstall --no-cache-dir -r python/requirements.txt
   python3 -m pip install --upgrade --force-reinstall --no-cache-dir -r python/requirements_ml.txt
   python3 -m pip install --upgrade --force-reinstall --no-cache-dir -r python/requirements_bfloat16.txt || echo "Failed to install bfloat16, that's ok!"
+  source utils/setup_iron_env.sh
   pushd programming_examples
 else
   echo "Vitis not found! Exiting..."
