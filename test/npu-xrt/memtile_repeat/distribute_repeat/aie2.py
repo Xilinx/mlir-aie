64e67a37d2bd01e764c3477a3433151413ff93f1
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-16 14:56:13 -0600

    Update test.exe arguments for memtile repeat and dyn of tests

--- test/npu-xrt/memtile_repeat/distribute_repeat/aie2.py
+++ test/npu-xrt/memtile_repeat/distribute_repeat/aie2.py
@@ -10,7 +10,7 @@
 # RUN: %python %S/aie2.py > ./aie2.mlir
 # RUN: %python aiecc.py --no-aiesim --aie-generate-cdo --aie-generate-npu --aie-generate-xclbin --no-compile-host --xclbin-name=final.xclbin --npu-insts-name=insts.txt ./aie2.mlir
 # RUN: clang %S/test.cpp -o test.exe -std=c++17 -Wall %xrt_flags -lrt -lstdc++ %test_utils_flags
-# RUN: %run_on_npu ./test.exe | FileCheck %s
+# RUN: %run_on_npu ./test.exe -x final.xclbin -i insts.txt | FileCheck %s
 # CHECK: PASS!
 import numpy as np
 import sys
