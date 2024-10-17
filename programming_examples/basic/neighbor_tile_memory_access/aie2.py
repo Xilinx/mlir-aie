e0ec2ca6a20b4b1a4d140e9b46e6c1f9e2175118
Pranathi Vasireddy <pvasired@amd.com>
2024-10-17 16:56:25 -0600

    Lit test change

--- programming_examples/basic/neighbor_tile_memory_access/aie2.py
+++ programming_examples/basic/neighbor_tile_memory_access/aie2.py
@@ -8,12 +8,13 @@
 
 # Adapted from vector_scalar_add/aie2.py but with link between ComputeTiles
 
+import numpy as np
+import sys
+
 from aie.dialects.aie import *
 from aie.dialects.aiex import *
 from aie.extras.context import mlir_mod_ctx
-from aie.extras.dialects.ext.scf import _for as range_
-
-import sys
+from aie.helpers.dialects.ext.scf import _for as range_
 
 PROBLEM_SIZE = 1024
 MEM_TILE_WIDTH = 64
@@ -23,40 +24,40 @@ AIE_TILE_WIDTH = 32
 def my_vector_bias_add():
     @device(AIEDevice.npu1_1col)
     def device_body():
-        memRef_aie_tile_ty = T.memref(AIE_TILE_WIDTH, T.i32())
+        mem_tile_ty = np.ndarray[(AIE_TILE_WIDTH,), np.dtype[np.int32]]
+        aie_tile_ty = np.ndarray[(AIE_TILE_WIDTH,), np.dtype[np.int32]]
+        all_data_ty = np.ndarray[(PROBLEM_SIZE,), np.dtype[np.int32]]
 
         # Tile declarations
         ShimTile = tile(0, 0)
         ComputeTile2 = tile(0, 2)
-        ComputeTile3 = tile(0, 4)
+        ComputeTile3 = tile(0, 3)
 
         # AIE-array data movement with object fifos
         # Input
-        of_in0 = object_fifo("in0", ShimTile, ComputeTile2, 2, memRef_aie_tile_ty)
-        of_in1 = object_fifo("in1", ComputeTile2, ComputeTile3, 2, memRef_aie_tile_ty)
+        of_in0 = object_fifo("in0", ShimTile, ComputeTile2, 2, mem_tile_ty)
+        of_in1 = object_fifo("in1", ComputeTile2, ComputeTile3, 2, aie_tile_ty)
         object_fifo_link(of_in0, of_in1)
 
         # Output
-        of_out0 = object_fifo("out0", ComputeTile3, ShimTile, 2, memRef_aie_tile_ty)
+        of_out0 = object_fifo("out0", ComputeTile3, ShimTile, 2, mem_tile_ty)
 
         # Set up compute tiles
 
-        # Compute tile 2
+        # Compute tile 3
         @core(ComputeTile3)
         def core_body():
             # Effective while(1)
             for _ in range_(sys.maxsize):
                 elem_in = of_in1.acquire(ObjectFifoPort.Consume, 1)
                 elem_out = of_out0.acquire(ObjectFifoPort.Produce, 1)
-                for i in range_(AIE_TILE_WIDTH):
+                for i in range_(AIE_TILE_WIDTH): 
                     elem_out[i] = elem_in[i] + 1
                 of_in1.release(ObjectFifoPort.Consume, 1)
                 of_out0.release(ObjectFifoPort.Produce, 1)
 
         # To/from AIE-array data movement
-        tensor_ty = T.memref(PROBLEM_SIZE, T.i32())
-
-        @runtime_sequence(tensor_ty, tensor_ty)
+        @runtime_sequence(all_data_ty, all_data_ty)
         def sequence(inTensor, outTensor):
             npu_dma_memcpy_nd(
                 metadata=of_in0,
