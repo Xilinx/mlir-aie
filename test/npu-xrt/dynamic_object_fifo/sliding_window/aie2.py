fc90931598fd9e8b226f049dc313ec79e49f1932
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-17 09:40:18 -0600

    Fix incorrect import in dynamic object fifo test

--- test/npu-xrt/dynamic_object_fifo/sliding_window/aie2.py
+++ test/npu-xrt/dynamic_object_fifo/sliding_window/aie2.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from aie.dialects.aie import *
 from aie.dialects.aiex import *
-from aie.extras.dialects.ext.scf import _for as range_
+from aie.helpers.dialects.ext.scf import _for as range_
 from aie.extras.context import mlir_mod_ctx
 
 N = 100
