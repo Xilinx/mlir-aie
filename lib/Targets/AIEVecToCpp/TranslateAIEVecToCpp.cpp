6bc3fb88a3f185bef61f5bc46d1c0b2abf56bc97
Joseph Melber <jgmelber@gmail.com>
2024-10-18 12:21:45 -0600

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/Targets/AIEVecToCpp/TranslateAIEVecToCpp.cpp
+++ lib/Targets/AIEVecToCpp/TranslateAIEVecToCpp.cpp
@@ -1473,10 +1473,12 @@ static LogicalResult printOperation(CppEmitter &emitter,
   os << opname;
   os << "(" << lhsName;
   if ((lsize == 32) && iType)
-    os << " ," << "undef_v16int32()";
+    os << " ,"
+       << "undef_v16int32()";
   os << " ," << rhsName;
   if ((lsize == 32) && iType)
-    os << " , " << "broadcast_zero_s32()";
+    os << " , "
+       << "broadcast_zero_s32()";
   os << ")";
   return success();
 }
