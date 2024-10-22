1071ee0235d295d493c2b52f2eb78d76d880cd1e
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-22 16:37:08 -0600

    make tensortile test formatting a bit more sane

--- test/python/tensortiler2d/util.py
+++ test/python/tensortiler2d/util.py
@@ -2,3 +2,15 @@
 def construct_test(f):
     print("\nTEST:", f.__name__)
     f()
+
+
+# This is a utility function used in creating tests to pretty print arrays in a way that's easy to see tiles
+def array_printer(arr):
+    pad = len(str(len(arr) * len(arr[0])))
+    print("[")
+    for i in range(len(arr)):
+        print("\t[", end="")
+        for j in range(len(arr[0])):
+            print(f"{arr[i][j]:{pad}},", end="")
+        print("],")
+    print("]")
