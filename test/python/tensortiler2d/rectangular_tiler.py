babf9e7f0c7e61e104186737897d41e081375134
Erika Hunhoff <erika.hunhoff@amd.com>
2024-10-22 14:52:00 -0600

    Add a few more tests, remove template

---
+++ test/python/tensortiler2d/rectangular_tiler.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+from aie.helpers.tensortiler.tensortiler2D import TensorTiler2D
+from util import construct_test
+
+# RUN: %python %s | FileCheck %s
+
+
+# CHECK-LABEL: rectangular_tiler
+@construct_test
+def rectangular_tiler():
+    tiler = TensorTiler2D(16, 8, 4, 4)
+    access_order = tiler.access_order()
+    reference_access = np.array(
+        [
+            [0, 1, 2, 3, 16, 17, 18, 19],
+            [4, 5, 6, 7, 20, 21, 22, 23],
+            [8, 9, 10, 11, 24, 25, 26, 27],
+            [12, 13, 14, 15, 28, 29, 30, 31],
+            [32, 33, 34, 35, 48, 49, 50, 51],
+            [36, 37, 38, 39, 52, 53, 54, 55],
+            [40, 41, 42, 43, 56, 57, 58, 59],
+            [44, 45, 46, 47, 60, 61, 62, 63],
+            [64, 65, 66, 67, 80, 81, 82, 83],
+            [68, 69, 70, 71, 84, 85, 86, 87],
+            [72, 73, 74, 75, 88, 89, 90, 91],
+            [76, 77, 78, 79, 92, 93, 94, 95],
+            [96, 97, 98, 99, 112, 113, 114, 115],
+            [100, 101, 102, 103, 116, 117, 118, 119],
+            [104, 105, 106, 107, 120, 121, 122, 123],
+            [108, 109, 110, 111, 124, 125, 126, 127],
+        ],
+        dtype=TensorTiler2D.DTYPE,
+    )
+    assert (reference_access == access_order).all()
+
+    tile1_reference_order = np.array(
+        [
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [0, 1, 2, 3, -1, -1, -1, -1],
+            [4, 5, 6, 7, -1, -1, -1, -1],
+            [8, 9, 10, 11, -1, -1, -1, -1],
+            [12, 13, 14, 15, -1, -1, -1, -1],
+        ],
+        dtype=TensorTiler2D.DTYPE,
+    )
+
+    tile_count = 0
+    for t in tiler.tile_iter():
+        if tile_count == 6:
+            tile_access_order = t.access_order()
+            assert (tile_access_order == tile1_reference_order).all()
+        tile_count += 1
+    assert tile_count == (16 // 4) * (8 // 4)
+
+    # CHECK: Pass!
+    print("Pass!")
---
+++ test/python/tensortiler2d/rectangular_tiler2.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+from aie.helpers.tensortiler.tensortiler2D import TensorTiler2D
+from util import construct_test
+
+# RUN: %python %s | FileCheck %s
+
+
+# CHECK-LABEL: rectangular_tiler2
+@construct_test
+def rectangular_tiler2():
+    tiler = TensorTiler2D(12, 8, 3, 2)
+    access_order = tiler.access_order()
+    reference_access = np.array(
+        [
+            [0, 1, 6, 7, 12, 13, 18, 19],
+            [2, 3, 8, 9, 14, 15, 20, 21],
+            [4, 5, 10, 11, 16, 17, 22, 23],
+            [24, 25, 30, 31, 36, 37, 42, 43],
+            [26, 27, 32, 33, 38, 39, 44, 45],
+            [28, 29, 34, 35, 40, 41, 46, 47],
+            [48, 49, 54, 55, 60, 61, 66, 67],
+            [50, 51, 56, 57, 62, 63, 68, 69],
+            [52, 53, 58, 59, 64, 65, 70, 71],
+            [72, 73, 78, 79, 84, 85, 90, 91],
+            [74, 75, 80, 81, 86, 87, 92, 93],
+            [76, 77, 82, 83, 88, 89, 94, 95],
+        ],
+        dtype=TensorTiler2D.DTYPE,
+    )
+    assert (reference_access == access_order).all()
+
+    tile1_reference_order = np.array(
+        [
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [0, 1, -1, -1, -1, -1, -1, -1],
+            [2, 3, -1, -1, -1, -1, -1, -1],
+            [4, 5, -1, -1, -1, -1, -1, -1],
+        ],
+        dtype=TensorTiler2D.DTYPE,
+    )
+
+    tile_count = 0
+    for t in tiler.tile_iter():
+        if tile_count == 12:
+            tile_access_order = t.access_order()
+            assert (tile_access_order == tile1_reference_order).all()
+        tile_count += 1
+    assert tile_count == (12 // 3) * (8 // 2)
+
+    # CHECK: Pass!
+    print("Pass!")
