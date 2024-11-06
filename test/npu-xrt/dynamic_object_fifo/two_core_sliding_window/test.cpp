dd87d0b71544cb453dd0a5c3fda0cd5fe2389dc9
Pranathi Vasireddy <pvasired@amd.com>
2024-11-06 14:38:08 -0700

    Checking with working test file

--- test/npu-xrt/dynamic_object_fifo/two_core_sliding_window/test.cpp
+++ test/npu-xrt/dynamic_object_fifo/two_core_sliding_window/test.cpp
@@ -14,11 +14,11 @@
 #include "xrt/xrt_kernel.h"
 
 #ifndef XCLBIN
-#define XCLBIN "final.xclbin"
+#define XCLBIN "build/final.xclbin"
 #endif
 
 #ifndef INSTS_TXT
-#define INSTS_TXT "insts.txt"
+#define INSTS_TXT "build/insts.txt"
 #endif
 
 #ifndef KERNEL_NAME
@@ -33,9 +33,24 @@
 
 #include "test_utils.h"
 
+std::vector<uint32_t> load_instr_sequence(std::string instr_path) {
+  std::ifstream instr_file(instr_path);
+  std::string line;
+  std::vector<uint32_t> instr_v;
+  while (std::getline(instr_file, line)) {
+    std::istringstream iss(line);
+    uint32_t a;
+    if (!(iss >> std::hex >> a)) {
+      throw std::runtime_error("Unable to parse instruction file\n");
+    }
+    instr_v.push_back(a);
+  }
+  return instr_v;
+}
+
 int main(int argc, const char *argv[]) {
 
-  std::vector<uint32_t> instr_v = test_utils::load_instr_sequence(INSTS_TXT);
+  std::vector<uint32_t> instr_v = load_instr_sequence(INSTS_TXT);
   assert(instr_v.size() > 0);
 
   // Get a device handle
