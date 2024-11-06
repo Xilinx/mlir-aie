2b6e8ec7c58cddf009781e80b2f8f85a85d16e6c
Pranathi Vasireddy <pvasired@amd.com>
2024-11-06 16:24:02 -0700

    Rest of them

--- test/npu-xrt/dynamic_object_fifo/sliding_window/test.cpp
+++ test/npu-xrt/dynamic_object_fifo/sliding_window/test.cpp
@@ -14,11 +14,11 @@
 #include "xrt/xrt_kernel.h"
 
 #ifndef XCLBIN
-#define XCLBIN "build/final.xclbin"
+#define XCLBIN "final.xclbin"
 #endif
 
 #ifndef INSTS_TXT
-#define INSTS_TXT "build/insts.txt"
+#define INSTS_TXT "insts.txt"
 #endif
 
 #ifndef KERNEL_NAME
@@ -28,25 +28,11 @@
 #define INPUT_SIZE (100 * sizeof(int))  // in bytes
 #define OUTPUT_SIZE (100 * sizeof(int)) // in bytes
 #define WIDTH_SIZE (10 * sizeof(int))   // in bytes
+#define WIDTH 10
 #define INPUT_ROWS INPUT_SIZE / WIDTH_SIZE
 #define OUTPUT_ROWS OUTPUT_SIZE / WIDTH_SIZE
 
 #include "test_utils.h"
-
-std::vector<uint32_t> load_instr_sequence(std::string instr_path) {
-  std::ifstream instr_file(instr_path);
-  std::string line;
-  std::vector<uint32_t> instr_v;
-  while (std::getline(instr_file, line)) {
-    std::istringstream iss(line);
-    uint32_t a;
-    if (!(iss >> std::hex >> a)) {
-      throw std::runtime_error("Unable to parse instruction file\n");
-    }
-    instr_v.push_back(a);
-  }
-  return instr_v;
-}
 
 int main(int argc, const char *argv[]) {
 
