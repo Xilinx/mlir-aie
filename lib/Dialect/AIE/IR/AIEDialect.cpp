844f68cf600917e27347da32600647f4319f26af
Pranathi Vasireddy <pvasired@amd.com>
2024-11-12 09:43:02 -0700

    Revert "Change in AIEDialect"
    This reverts commit 42a81907c97c7f4d8c0fa9b5c0c190885be56508.

--- lib/Dialect/AIE/IR/AIEDialect.cpp
+++ lib/Dialect/AIE/IR/AIEDialect.cpp
@@ -1883,7 +1883,7 @@ LogicalResult DMABDOp::verify() {
     if (!dims.has_value())
       return emitOpError() << "Padding requires n-d data layouts expressed as"
                            << " wrap(s) and stride(s).";
-    if (paddims.has_value() && dims->size() != paddims->size())
+    if (dims->size() != paddims->size())
       return emitOpError() << "Mismatch number of dimensions between padding(s)"
                            << " and wrap(s) and stride(s).";
     if (!targetModel.isMemTile(parentTileId.col, parentTileId.row))
