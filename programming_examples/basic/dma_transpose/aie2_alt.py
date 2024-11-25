4dd7386ff300ee8267cc1c6fedf2317bec015c8c
Erika Hunhoff <erika.hunhoff@amd.com>
2024-11-25 16:13:22 -0700

    Another example

--- programming_examples/basic/dma_transpose/aie2_alt.py
+++ programming_examples/basic/dma_transpose/aie2_alt.py
@@ -1,4 +1,4 @@
-# dma_transpose/aie2.py -*- Python -*-
+# dma_transpose/aie2_alt.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
--- programming_examples/basic/row_wise_bias_add/aie2_iron.py
+++ programming_examples/basic/row_wise_bias_add/aie2_iron.py
@@ -1,4 +1,3 @@
-# passthrough_kernel/aie2.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
