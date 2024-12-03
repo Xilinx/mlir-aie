bb43c9592e967b3e7b834c037985a38c9973fb1c
Pranathi Vasireddy <pvasired@amd.com>
2024-12-03 15:08:31 -0700

    Removing core to see the problem

--- test/npu-xrt/dynamic_object_fifo/nested_loops/aie2.py
+++ test/npu-xrt/dynamic_object_fifo/nested_loops/aie2.py
@@ -7,83 +7,12 @@
 
 # REQUIRES: ryzen_ai, valid_xchess_license
 #
+# RUN: xchesscc_wrapper aie2 -I %aietools/include -c %S/kernel.cc -o ./kernel.o
 # RUN: %python %S/aie2.py > ./aie2.mlir
-# RUN: aie-opt --aie-objectFifo-stateful-transform=dynamic-objFifos ./aie2.mlir | FileCheck %s
-# CHECK: %tile_0_0 = aie.tile(0, 0)
-# CHECK: %tile_0_2 = aie.tile(0, 2)
-# CHECK: %out_cons_prod_lock = aie.lock(%tile_0_0, 2) {init = 1 : i32, sym_name = "out_cons_prod_lock"}
-# CHECK: %out_cons_cons_lock = aie.lock(%tile_0_0, 3) {init = 0 : i32, sym_name = "out_cons_cons_lock"}
-# CHECK: %out_buff_0 = aie.buffer(%tile_0_2) {sym_name = "out_buff_0"} : memref<10xi32> 
-# CHECK: %out_buff_1 = aie.buffer(%tile_0_2) {sym_name = "out_buff_1"} : memref<10xi32> 
-# CHECK: %out_prod_lock = aie.lock(%tile_0_2, 2) {init = 2 : i32, sym_name = "out_prod_lock"}
-# CHECK: %out_cons_lock = aie.lock(%tile_0_2, 3) {init = 0 : i32, sym_name = "out_cons_lock"}
-# CHECK: %in_cons_buff_0 = aie.buffer(%tile_0_2) {sym_name = "in_cons_buff_0"} : memref<10xi32> 
-# CHECK: %in_cons_buff_1 = aie.buffer(%tile_0_2) {sym_name = "in_cons_buff_1"} : memref<10xi32> 
-# CHECK: %in_cons_prod_lock = aie.lock(%tile_0_2, 0) {init = 2 : i32, sym_name = "in_cons_prod_lock"}
-# CHECK: %in_cons_cons_lock = aie.lock(%tile_0_2, 1) {init = 0 : i32, sym_name = "in_cons_cons_lock"}
-# CHECK: %in_prod_lock = aie.lock(%tile_0_0, 0) {init = 1 : i32, sym_name = "in_prod_lock"}
-# CHECK: %in_cons_lock = aie.lock(%tile_0_0, 1) {init = 0 : i32, sym_name = "in_cons_lock"}
-# CHECK: aie.flow(%tile_0_0, DMA : 0, %tile_0_2, DMA : 0)
-# CHECK: aie.flow(%tile_0_2, DMA : 0, %tile_0_0, DMA : 0)
-# CHECK: func.func private @passthrough_10_i32(memref<10xi32>, memref<10xi32>)
-# CHECK: %buffer_0_2 = aie.buffer(%tile_0_2) : memref<2xindex> 
-# CHECK: %core_0_2 = aie.core(%tile_0_2) {
-# CHECK:   %c0 = arith.constant 0 : index
-# CHECK:   %c0_0 = arith.constant 0 : index
-# CHECK:   %c2 = arith.constant 2 : index
-# CHECK:   memref.store %c0, %buffer_0_2[%c0_0] : memref<2xindex>
-# CHECK:   %c1 = arith.constant 1 : index
-# CHECK:   %c2_1 = arith.constant 2 : index
-# CHECK:   memref.store %c0, %buffer_0_2[%c1] : memref<2xindex>
-# CHECK:   %c0_2 = arith.constant 0 : index
-# CHECK:   %c5 = arith.constant 5 : index
-# CHECK:   %c1_3 = arith.constant 1 : index
-# CHECK:   scf.for %arg0 = %c0_2 to %c5 step %c1_3 {
-# CHECK:     aie.use_lock(%in_cons_cons_lock, AcquireGreaterEqual, 1)
-# CHECK:     %0 = memref.load %buffer_0_2[%c1] : memref<2xindex>
-# CHECK:     %1 = scf.index_switch %0 -> memref<10xi32> 
-# CHECK:     case 0 {
-# CHECK:       scf.yield %in_cons_buff_0 : memref<10xi32>
-# CHECK:     }
-# CHECK:     case 1 {
-# CHECK:       scf.yield %in_cons_buff_1 : memref<10xi32>
-# CHECK:     }
-# CHECK:     default {
-# CHECK:       scf.yield %in_cons_buff_0 : memref<10xi32>
-# CHECK:     }
-# CHECK:     %c0_4 = arith.constant 0 : index
-# CHECK:     %c5_5 = arith.constant 5 : index
-# CHECK:     %c1_6 = arith.constant 1 : index
-# CHECK:     scf.for %arg1 = %c0_4 to %c5_5 step %c1_6 {
-# CHECK:       aie.use_lock(%out_prod_lock, AcquireGreaterEqual, 1)
-# CHECK:       %5 = memref.load %buffer_0_2[%c0_0] : memref<2xindex>
-# CHECK:       %6 = scf.index_switch %5 -> memref<10xi32> 
-# CHECK:       case 0 {
-# CHECK:         scf.yield %out_buff_0 : memref<10xi32>
-# CHECK:       }
-# CHECK:       case 1 {
-# CHECK:         scf.yield %out_buff_1 : memref<10xi32>
-# CHECK:       }
-# CHECK:       default {
-# CHECK:         scf.yield %out_buff_0 : memref<10xi32>
-# CHECK:       }
-# CHECK:       func.call @passthrough_10_i32(%1, %6) : (memref<10xi32>, memref<10xi32>) -> ()
-# CHECK:       aie.use_lock(%out_cons_lock, Release, 1)
-# CHECK:       %7 = memref.load %buffer_0_2[%c0_0] : memref<2xindex>
-# CHECK:       %c1_8 = arith.constant 1 : index
-# CHECK:       %8 = arith.addi %7, %c1_8 : index
-# CHECK:       %9 = arith.remsi %8, %c2 : index
-# CHECK:       memref.store %9, %buffer_0_2[%c0_0] : memref<2xindex>
-# CHECK:     }
-# CHECK:     aie.use_lock(%in_cons_prod_lock, Release, 1)
-# CHECK:     %2 = memref.load %buffer_0_2[%c1] : memref<2xindex>
-# CHECK:     %c1_7 = arith.constant 1 : index
-# CHECK:     %3 = arith.addi %2, %c1_7 : index
-# CHECK:     %4 = arith.remsi %3, %c2_1 : index
-# CHECK:     memref.store %4, %buffer_0_2[%c1] : memref<2xindex>
-# CHECK:   }
-# CHECK:   aie.end
-# CHECK: } {link_with = "kernel.o"}
+# RUN: clang %S/test.cpp -o test.exe -std=c++17 -Wall %xrt_flags -lrt -lstdc++ %test_utils_flags
+# RUN: %python aiecc.py --no-aiesim --aie-generate-cdo --no-compile-host --aie-generate-xclbin --xclbin-name=final.xclbin --dynamic-objFifos --aie-generate-npu --npu-insts-name=insts.txt ./aie2.mlir
+# RUN: %run_on_npu ./test.exe -x final.xclbin -k MLIR_AIE -i insts.txt | FileCheck %s
+# CHECK: PASS!
 
 import numpy as np
 
@@ -119,16 +48,6 @@ def nested_loops():
                 "passthrough_10_i32", inputs=[tensor_ty, tensor_ty]
             )
 
-            # Set up compute tiles
-            @core(ComputeTile, "kernel.o")
-            def core_body():
-                for _ in range_(5):
-                    elemIn = of_in.acquire(ObjectFifoPort.Consume, 1)
-                    for _ in range_(5):
-                        elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
-                        passthrough_10_i32(elemIn, elemOut)
-                        of_out.release(ObjectFifoPort.Produce, 1)
-                    of_in.release(ObjectFifoPort.Consume, 1)
 
             # To/from AIE-array data movement
             @runtime_sequence(tensor_ty, tensor_ty)
