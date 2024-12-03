7797c5da91c38b813d9e725daeccca3a0a048bf3
Pranathi Vasireddy <pvasired@amd.com>
2024-12-03 15:25:20 -0700

    Remove inner loop

--- test/npu-xrt/dynamic_object_fifo/nested_loops/aie2.py
+++ test/npu-xrt/dynamic_object_fifo/nested_loops/aie2.py
@@ -48,6 +48,16 @@ def nested_loops():
                 "passthrough_10_i32", inputs=[tensor_ty, tensor_ty]
             )
 
+            # Set up compute tiles
+            @core(ComputeTile, "kernel.o")
+            def core_body():
+                for _ in range_(5):
+                    elemIn = of_in.acquire(ObjectFifoPort.Consume, 1)
+                    # for _ in range_(5):
+                    #     elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                    #     passthrough_10_i32(elemIn, elemOut)
+                    #     of_out.release(ObjectFifoPort.Produce, 1)
+                    of_in.release(ObjectFifoPort.Consume, 1)
 
             # To/from AIE-array data movement
             @runtime_sequence(tensor_ty, tensor_ty)
