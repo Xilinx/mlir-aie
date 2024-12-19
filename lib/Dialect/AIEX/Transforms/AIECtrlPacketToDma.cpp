f8cc6b22d40a04ce45d936be5e9222e219ae699a
AndraBisca <andrab@amd.com>
2024-12-19 04:09:43 -0700

    Restore format change

--- lib/Dialect/AIEX/Transforms/AIECtrlPacketToDma.cpp
+++ lib/Dialect/AIEX/Transforms/AIECtrlPacketToDma.cpp
@@ -125,7 +125,7 @@ struct AIECtrlPacketToDmaPass : AIECtrlPacketToDmaBase<AIECtrlPacketToDmaPass> {
 
               StringRef metadata = builder.getStringAttr(shimDmaAllocName);
               builder.create<NpuDmaMemcpyNdOp>(
-                  builder.getUnknownLoc(), newBlockArg, SmallVector<Value>{},
+                  builder.getUnknownLoc(), newBlockArg,
                   SmallVector<Value>{}, SmallVector<Value>{},
                   SmallVector<Value>{}, ArrayRef(staticOffsets),
                   ArrayRef(staticSizes), ArrayRef(staticStrides),
