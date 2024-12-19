Fix softmax with functional kernel (#1998)
Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

programming_examples/ml/softmax/softmax_alt.py
@@ -46,7 +46,7 @@ def vector_softmax(trace_size):
         # AIE Core Function declarations
 
         softmax_bf16_vector = external_func(
-            "softmax_bf16_vector", inputs=[tile_ty, tile_ty]
+            "softmax_bf16", inputs=[tile_ty, tile_ty, np.int32]
         )
 
         # Tile declarations
@@ -100,7 +100,7 @@ def vector_softmax(trace_size):
                         elem_out = outC_fifos[i].acquire(ObjectFifoPort.Produce, 1)
                         elem_in_a = inA_fifos[i].acquire(ObjectFifoPort.Consume, 1)
 
-                        softmax_bf16_vector(elem_in_a, elem_out)
+                        softmax_bf16_vector(elem_in_a, elem_out, n)
 
                         inA_fifos[i].release(ObjectFifoPort.Consume, 1)
                         outC_fifos[i].release(ObjectFifoPort.Produce, 1)