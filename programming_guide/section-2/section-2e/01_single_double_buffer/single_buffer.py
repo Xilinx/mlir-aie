5c883365f210daf00cac4671862677a417b9a47c
AndraBisca <andrab@amd.com>
2025-01-29 08:22:17 -0700

    Modify practical examples section 2e

--- programming_guide/section-2/section-2e/01_single_double_buffer/single_buffer.py
+++ programming_guide/section-2/section-2e/01_single_double_buffer/single_buffer.py
@@ -1,57 +1,56 @@
+# single_buffer.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# (c) Copyright 2024 AMD Inc.
+# (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
 import numpy as np
-from aie.dialects.aie import *
-from aie.dialects.aiex import *
-from aie.helpers.dialects.ext.scf import _for as range_
-from aie.extras.context import mlir_mod_ctx
+import sys
 
+from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
+from aie.iron.placers import SequentialPlacer
+from aie.iron.device import NPU1Col1
+from aie.iron.controlflow import range_
 
-def single_buffer():
-    with mlir_mod_ctx() as ctx:
+dev = NPU1Col1()
 
-        @device(AIEDevice.npu1_1col)
-        def device_body():
-            data_ty = np.ndarray[(16,), np.dtype[np.int32]]
+# Define tensor types
+data_ty = np.ndarray[(16,), np.dtype[np.int32]]
 
-            # Tile declarations
-            ComputeTile2 = tile(0, 2)
-            ComputeTile3 = tile(0, 3)
+# Dataflow with ObjectFifos
+of_in = ObjectFifo(data_ty, name="in", default_depth=1)
 
-            # AIE-array data movement with object fifos
-            # Input
-            of_in = object_fifo(
-                "in", ComputeTile2, ComputeTile3, 1, data_ty
-            )  # single buffer
+# Task for the core to perform
+def core_fn(of_in):
+    # Effective while(1)
+    for _ in range_(8):
+        elem_out = of_in.acquire(1)
+        for i in range_(16):
+            elem_out[i] = 1
+        of_in.release(1)
 
-            # Set up compute tiles
-            # Compute tile 2
-            @core(ComputeTile2)
-            def core_body():
-                # Effective while(1)
-                for _ in range_(8):
-                    elem_out = of_in.acquire(ObjectFifoPort.Produce, 1)
-                    for i in range_(16):
-                        elem_out[i] = 1
-                    of_in.release(ObjectFifoPort.Produce, 1)
+def core_fn2(of_in):
+    # Effective while(1)
+    for _ in range_(8):
+        elem_in = of_in.acquire(1)
+        of_in.release(1)
+        
+# Create a worker to perform the task
+my_worker = Worker(core_fn, [of_in.prod()])
+my_worker2 = Worker(core_fn2, [of_in.cons()])
 
-            # Compute tile 3
-            @core(ComputeTile3)
-            def core_body():
-                # Effective while(1)
-                for _ in range_(8):
-                    elem_in = of_in.acquire(ObjectFifoPort.Consume, 1)
-                    of_in.release(ObjectFifoPort.Consume, 1)
+# Runtime operations to move data to/from the AIE-array
+rt = Runtime()
+with rt.sequence(data_ty, data_ty, data_ty) as (_, _, _):
+    rt.start(my_worker)
+    rt.start(my_worker2)
 
-    res = ctx.module.operation.verify()
-    if res == True:
-        print(ctx.module)
-    else:
-        print(res)
+# Create the program from the device type and runtime
+my_program = Program(dev, rt)
 
+# Place components (assign them resources on the device) and generate an MLIR module
+module = my_program.resolve_program(SequentialPlacer())
 
-single_buffer()
+# Print the generated MLIR
+print(module)
