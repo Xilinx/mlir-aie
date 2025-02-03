dd31269d5021b05b8af384742948805c772cfdda
Endri Taka <endri.taka@amd.com>
2025-02-03 14:55:54 -0700

    MatMul changes

--- programming_examples/basic/matrix_multiplication/whole_array/whole_array.py
+++ programming_examples/basic/matrix_multiplication/whole_array/whole_array.py
@@ -36,6 +36,7 @@ def main():
     argparser.add_argument("-m", type=int, default=64)
     argparser.add_argument("-k", type=int, default=64)
     argparser.add_argument("-n", type=int, default=32)
+    argparser.add_argument("-mtk", type=int, default=512)
     argparser.add_argument("--n-aie-cols", type=int, choices=[1, 2, 4], default=4)
     argparser.add_argument("--b-col-maj", type=int, choices=[0, 1], default=0)
     argparser.add_argument(
@@ -63,6 +64,7 @@ def main():
             args.m,
             args.k,
             args.n,
+            args.mtk,
             args.n_aie_cols,
             args.dtype_in,
             args.dtype_out,
@@ -88,6 +90,7 @@ def my_matmul(
     m,
     k,
     n,
+    mtk,
     n_aie_cols,
     dtype_in_str,
     dtype_out_str,
@@ -95,6 +98,8 @@ def my_matmul(
     trace_size,
     generate_taps=False,
 ):
+    
+    # This design will have always 4 rows and 4 cols (to maximize performance)
     n_aie_rows = 4
     n_aie_cores = n_aie_rows * n_aie_cols
 
@@ -145,11 +150,18 @@ def my_matmul(
     assert k % s == 0
     assert n % t == 0
 
-    if b_col_maj:
-        # These assertions are probably too broad.
-        assert m % 32 == 0
-        assert k % 32 == 0
-        assert n % 32 == 0
+
+    # K needs to be divisible by mtk
+    assert K % mtk == 0
+
+    # also mtk as a bigger block needs to be divisible by k
+    assert mtk % k == 0
+
+    # if b_col_maj:
+    #     # These assertions are probably too broad.
+    #     assert m % 32 == 0
+    #     assert k % 32 == 0
+    #     assert n % 32 == 0
 
     # If you get errors during CDO generation due to running out of program
     # memory, it may be because too much code is generated due to ObjectFIFO
@@ -159,7 +171,7 @@ def my_matmul(
 
     n_tiles_per_core = (M // m) * (N // n) // n_aie_cores
 
-    n_A_tiles_per_shim = n_aie_rows // n_aie_cols
+    # n_A_tiles_per_shim = n_aie_rows // n_aie_cols
 
     dev = None
     if n_aie_cols == 1:
@@ -177,7 +189,7 @@ def my_matmul(
 
     @device(dev)
     def device_body():
-        A_l2_ty = np.ndarray[(m * k * n_A_tiles_per_shim,), np.dtype[dtype_in]]
+        A_l2_ty = np.ndarray[(m * mtk,), np.dtype[dtype_in]] # it will read a bigger m * mtk block in MemTile
         B_l2_ty = np.ndarray[(k * n,), np.dtype[dtype_in]]
         C_l2_ty = np.ndarray[(m * n * n_aie_rows,), np.dtype[dtype_out]]
         A_l1_ty = np.ndarray[(m, k), np.dtype[dtype_in]]
@@ -188,8 +200,8 @@ def my_matmul(
         zero = external_func(f"zero_{dtype_out_str}", inputs=[C_l1_ty])
         matmul_vectorized_func_name = (
             f"matmul_{dtype_in_str}_{dtype_out_str}"
-            if not b_col_maj
-            else f"matmul_{dtype_in_str}_{dtype_out_str}_b_col_maj"
+            # if not b_col_maj
+            # else f"matmul_{dtype_in_str}_{dtype_out_str}_b_col_maj"
         )
         matmul = external_func(
             matmul_vectorized_func_name,
@@ -218,17 +230,46 @@ def my_matmul(
         for row in range(n_aie_rows):
             A_l2l1_fifos[row] = object_fifo(
                 f"A_L2L1_{row}",
-                mem_tiles[row // n_A_tiles_per_shim],
+                mem_tiles[row],
                 core_tiles[row][0:n_aie_cols],  # broadcast along one row
                 fifo_depth,
                 A_l1_ty,
+                # MM2S expressing the (m * mtk) tile
                 [
-                    (m // r, r * k),
+                    (mtk // k, m * k),
                     (k // s, s),
-                    (r, k),
+                    (m, k),
                     (s, 1),
                 ],
+
+                # S2MM in compute tiles, so eack (m * k) tile 
+                # have the correct data layout. 
+                # We have 4 compute tiles, so each needs it's own
+                # S2MM transformation. TODO: change that to compact format in python using for _ in range(n_aie_cols)
+                [
+                    [
+                        (k // s, r * s),
+                        (m // r, r * k),
+                        (r * s, 1),
+                    ],
+                    [
+                        (k // s, r * s),
+                        (m // r, r * k),
+                        (r * s, 1),
+                    ],
+                    [
+                        (k // s, r * s),
+                        (m // r, r * k),
+                        (r * s, 1),
+                    ],
+                    [
+                        (k // s, r * s),
+                        (m // r, r * k),
+                        (r * s, 1),
+                    ],
+                ],
             )
+
         for col in range(n_aie_cols):
             A_l3l2_fifos[col] = object_fifo(
                 f"A_L3L2_{col}",
@@ -236,24 +277,30 @@ def my_matmul(
                 mem_tiles[col],
                 fifo_depth,
                 A_l2_ty,
+                None,
+                # S2MM in MemTile to convert m * mtk block from row-maj
+                # to tiled m*k blocks
+                [
+                   [
+                       (m, k),
+                       (mtk // k, m * k),
+                       (k, 1),
+                   ]
+                ],
             )
-            # If n_cols == n_rows, n_A_tiles_per_shim is 1 and
-            # this simply links a_l3l2_fifos[col] to a_l2l1_fifos[row] directly,
-            # where col == row.
-            # If n_cols < n_rows, each column receives multiple rows of
-            # tiles; distribute it along rows of AIE cores.
-            start_row = col * n_A_tiles_per_shim
-            stop_row = start_row + n_A_tiles_per_shim
-            if stop_row - start_row > 1:
-                of_offsets = [m * k * i for i in range(stop_row - start_row)]
-            else:
-                of_offsets = []
+            
+
+            # here just do a link between the A OFs
+            of_offsets = []
             object_fifo_link(
                 A_l3l2_fifos[col],
-                [A_l2l1_fifos[row] for row in range(start_row, stop_row)],
+                A_l2l1_fifos[col],
                 [],
                 of_offsets,
             )
+
+            # repeat test here
+            A_l2l1_fifos[col].set_repeat_count(N//n//n_aie_cols)
 
         # Input B
         for col in range(n_aie_cols):
@@ -263,16 +310,6 @@ def my_matmul(
                 mem_tiles[col],
                 fifo_depth,
                 B_l2_ty,
-                None,
-                # S2MM conversion of col-maj order K*n
-                # into blocked row-maj order of k*n blocks
-                [
-                    [
-                        (n, 1), 
-                        # (K // k, k*n), 
-                        (k, n),
-                    ]
-                ],
             )
             B_l2l1_fifos[col] = object_fifo(
                 f"B_L2L1_{col}",
@@ -289,13 +326,13 @@ def my_matmul(
                         (s, n),
                         (t, 1),
                     ]
-                    if not b_col_maj
-                    else [
-                        (n // t, t * k),
-                        (k // s, s),
-                        (t, k),
-                        (s, 1),
-                    ]
+                    # if not b_col_maj
+                    # else [
+                    #     (n // t, t * k),
+                    #     (k // s, s),
+                    #     (t, k),
+                    #     (s, 1),
+                    # ]
                 ),
             )
             object_fifo_link(B_l3l2_fifos[col], B_l2l1_fifos[col])
@@ -458,16 +495,22 @@ def my_matmul(
                                 (row_base + tile_row) * n_aie_rows * m * K
                             )  # base address for this transfer block for all BDs
                             A_row_offset = (
-                                col * n_A_tiles_per_shim * m * K
+                                col * m * K
                             )  # base address for the shim in this column
                             A_offset = A_block_offset + A_row_offset
-                            A_sizes = [
-                                N // n // n_aie_cols,
-                                K // k,
-                                m * n_A_tiles_per_shim,
-                                k,
-                            ]
+
+                            # A_sizes = [N // n // n_aie_cols, K // k, m, k,]
+                            # A_strides = [0, k, K, 1]
+
+                            # change those to mtk representation
+                            # A_sizes = [N // n // n_aie_cols, K // mtk, m, mtk]
+                            # A_strides = [0, mtk, K, 1]
+
+                            # repeat OF test
+                            A_sizes = [1, K // k, m, k,]
                             A_strides = [0, k, K, 1]
+
+
                             npu_dma_memcpy_nd(
                                 metadata=A_l3l2_fifos[col],
                                 bd_id=bd_id_base + 2 * tile_row + 1,
