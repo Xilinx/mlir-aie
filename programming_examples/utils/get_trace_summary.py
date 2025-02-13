b0d2e19d924d92971c37a634030b3927aaa048c2
Jack Lo <jack.lo@amd.com>
2025-02-13 13:34:46 -0700

    Fixed format with black

--- programming_examples/utils/get_trace_summary.py
+++ programming_examples/utils/get_trace_summary.py
@@ -5,13 +5,14 @@ import sys
 import re
 import trace_utils
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--filename", help="Trace file", required=True)
-    #parser.add_argument("--mlir", help="mlir source file", required=True)
-    #parser.add_argument(
+    # parser.add_argument("--mlir", help="mlir source file", required=True)
+    # parser.add_argument(
     #    "--colshift", help="column shift adjustment to source mlir", required=False
-    #)
+    # )
     parser.add_argument("--debug", help="debug mode", required=False)
     # TODO tracelabels removed since we can have multiple sets of labels for each pkt_type & loc combination
     # parser.add_argument('--tracelabels',
@@ -19,8 +20,18 @@ def parse_args():
     #         help='Labels for traces', required=False)
     return parser.parse_args(sys.argv[1:])
 
+
 opts = parse_args()
 cycles = trace_utils.get_cycles_summary(opts.filename)
 
 print("Total number of full kernel invocations is " + str(len(cycles)))
-print("First/Min/Avg/Max is " + str(cycles[0]) + "/ " + str(min(cycles)) + "/ " + str(sum(cycles)/len(cycles)) + "/ " + str(max(cycles)))
+print(
+    "First/Min/Avg/Max is "
+    + str(cycles[0])
+    + "/ "
+    + str(min(cycles))
+    + "/ "
+    + str(sum(cycles) / len(cycles))
+    + "/ "
+    + str(max(cycles))
+)
