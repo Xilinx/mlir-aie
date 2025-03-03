48b9de0d478abd274a2308230b6fe59ea4170878
Pranathi Vasireddy <pvasired@amd.com>
2025-03-03 08:55:44 -0700

    Else condition

--- lib/Dialect/AIEX/IR/AIEXDialect.cpp
+++ lib/Dialect/AIEX/IR/AIEXDialect.cpp
@@ -401,7 +401,8 @@ LogicalResult AIEX::NpuDmaMemcpyNdOp::verify() {
                                   skipTransformationChecks))) {
       return failure();
     }
-  }
+  } else
+    return emitOpError("Shim DMA Allocation Op not found.");
 
   // packet header
   if (auto packetInfo = getPacket()) {
