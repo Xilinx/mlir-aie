942c2ed5ffdc3d897099849f831250af0b104e86
Joseph Melber <jgmelber@gmail.com>
2025-03-04 11:09:28 -0700

    Update header conv2dk1_i8.cc
    Co-authored-by: Jeff Fifield <jeff.fifield@amd.com>

--- aie_kernels/aie2p/conv2dk1_i8.cc
+++ aie_kernels/aie2p/conv2dk1_i8.cc
@@ -1,5 +1,5 @@
-//===- conv2dk1.cc -------------------------------------------------*- C++
-//-*-===//
+//===- conv2dk1.cc ----------------------------------------------*- C++ -*-===//
+
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
