1ed86f6386a041cec5f0bc01d517706a4c6967b1
Joseph Melber <jmelber@xilinx.com>
2025-03-19 22:28:23 -0600

    Use txt for ctrl packets still

--- test/npu-xrt/ctrl_packet_reconfig/test.cpp
+++ test/npu-xrt/ctrl_packet_reconfig/test.cpp
@@ -35,10 +35,10 @@ int main(int argc, const char *argv[]) {
       test_utils::load_instr_binary("aie2_run_seq.bin");
   // AIE configuration as control packet streams
   std::vector<uint32_t> instr3_cfg_v =
-      test_utils::load_instr_binary("ctrlpkt_dma_seq.bin");
+      test_utils::load_instr_sequence("ctrlpkt_dma_seq.txt");
   // AIE configuration control packets' raw data
   std::vector<uint32_t> ctrlPackets =
-      test_utils::load_instr_binary("ctrlpkt.bin");
+      test_utils::load_instr_sequence("ctrlpkt.txt");
 
   // Start the XRT test code
   // Get a device handle
--- test/npu-xrt/ctrl_packet_reconfig_4x1_cores/test.cpp
+++ test/npu-xrt/ctrl_packet_reconfig_4x1_cores/test.cpp
@@ -30,9 +30,9 @@ int main(int argc, const char *argv[]) {
   std::vector<uint32_t> instr_v =
       test_utils::load_instr_binary("aie2_run_seq.bin");
   std::vector<uint32_t> ctrlpkt_instr_v =
-      test_utils::load_instr_binary("ctrlpkt_dma_seq.bin");
+      test_utils::load_instr_binary("ctrlpkt_dma_seq.txt");
   std::vector<uint32_t> ctrlPackets =
-      test_utils::load_instr_binary("ctrlpkt.bin");
+      test_utils::load_instr_binary("ctrlpkt.txt");
 
   // Start the XRT test code
   // Get a device handle
