cee96e2a24c72f61518d73600b45a85b4b371748
endtaka-amd <endri.taka@amd.com>
2025-03-21 14:56:09 -0600

    DDR BW Kr results

---
+++ vitis_setup_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################
+# Setup Vitis AIE Essentials
+#################################################################################
+export AIETOOLS_ROOT=/tools/ryzen_ai-1.3.0/vitis_aie_essentials
+export PATH=$PATH:${AIETOOLS_ROOT}/bin
+export LM_LICENSE_FILE=/opt/Xilinx.lic
+
+source ironenv/bin/activate
+
+export NPU2=1
+
+source /opt/xilinx/xrt/setup.sh
+
+source utils/env_setup.sh
