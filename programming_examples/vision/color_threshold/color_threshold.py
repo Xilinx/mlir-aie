b8fe9fc3a91f717648ffc873626d2744c236fecc
AndraBisca <andrab@amd.com>
2025-03-28 10:38:26 -0600

    Update vision examples

--- programming_examples/vision/color_threshold/color_threshold.py
+++ programming_examples/vision/color_threshold/color_threshold.py
@@ -9,7 +9,7 @@ import sys
 
 from aie.iron import GlobalBuffer, Kernel, ObjectFifo, Program, Runtime, Worker
 from aie.iron.placers import SequentialPlacer
-from aie.iron.device import NPU1Col1, NPU2
+from aie.iron.device import NPU1Col1, NPU2Col1
 
 from aie.extras.dialects.ext import arith
 from aie.helpers.util import np_ndarray_type_get_shape
@@ -121,7 +121,7 @@ try:
     if device_name == "npu":
         dev = NPU1Col1()
     elif device_name == "npu2":
-        dev = NPU2()
+        dev = NPU2Col1()
     else:
         raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
     width = 512 if (len(sys.argv) != 4) else int(sys.argv[2])
