cd1061227443e94957f8737e56ec1fa47ad91b56
AndraBisca <andrab@amd.com>
2025-03-28 10:42:38 -0600

    Update some of the ML examples

--- programming_examples/ml/conv2d/conv2d.py
+++ programming_examples/ml/conv2d/conv2d.py
@@ -9,7 +9,7 @@ import sys
 
 from aie.iron import GlobalBuffer, Kernel, ObjectFifo, Program, Runtime, Worker
 from aie.iron.placers import SequentialPlacer
-from aie.iron.device import NPU1Col1, NPU2
+from aie.iron.device import NPU1Col1, NPU2Col1
 from aie.iron.controlflow import range_
 
 
@@ -133,7 +133,7 @@ try:
     if device_name == "npu":
         dev = NPU1Col1()
     elif device_name == "npu2":
-        dev = NPU2()
+        dev = NPU2Col1()
     else:
         raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
     width = int(sys.argv[2])
