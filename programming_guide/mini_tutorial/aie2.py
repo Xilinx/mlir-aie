7aafde53c58c432343394d51d0f6d70c3be4d7be
AndraBisca <andrab@amd.com>
2025-04-17 13:19:59 -0600

    Update aie2.py

--- programming_guide/mini_tutorial/aie2.py
+++ programming_guide/mini_tutorial/aie2.py
@@ -28,7 +28,7 @@ data_ty = np.ndarray[(data_size,), np.dtype[np.int32]]
 # The IRON placement step relies on ObjectFifoHandles to infer the endpoints of ObjectFifos.
 # Each ObjectFifo can have one producer and one or multiple consumer ObjectFifoHandles.
 # These endpoints can be used by the Workers, the Runtime or to generate connecting ObjectFifos
-# (as with forward(), split(), join()).
+# (with forward(), split(), or join()).
 of_in = ObjectFifo(data_ty, name="in")
 of_out = ObjectFifo(data_ty, name="out")
 
@@ -49,14 +49,17 @@ my_worker = Worker(core_fn, [of_in.cons(), of_out.prod()])
 # To/from AIE-array runtime data movement
 # The arguments of the runtime sequence describe buffers that will be available on the host side;
 # the body of the sequence contains tasks which describe how those buffers are moved into the AIE-array.
-# Runtime sequence tasks are submitted to and executed by a dedicated command processor in the order
-# they are submitted in. Tasks which are set to 'wait' will block until a token associated with their
-# completion is generated.
+# Runtime sequence tasks are submitted to and executed by a dedicated command processor in order.
+# Tasks which are set to 'wait' will block the dedicated command processor until a token associated
+# with their completion is generated.
+# The host processor waits for the full runtime sequence to complete before reading the buffers.
+
 # Note: The blocking instruction submitted to the dedicated command processor will be satisfied by
 # any token, not necessarily the one emitted by the task that generated the instruction.
-# Note: The task to start the workers currently only registers the input workers as part of the
-# Program, but does not launch the Workers themselves. This means that this task can be added in
-# the sequence at any point and does not need to be the first one.
+
+# Note: The task to start the workers currently only registers the workers as part of the Program, but
+# does not launch the Workers themselves. This means that this task can be added in the sequence at
+# any point and does not need to be the first one.
 rt = Runtime()
 with rt.sequence(data_ty, data_ty, data_ty) as (a_in, _, c_out):
     rt.start(my_worker)
@@ -68,8 +71,8 @@ my_program = Program(dev, rt)
 
 # Place components (assign them resources on the device) and generate an MLIR module
 # The placer will use available information, such as the ObjectFifoHandles, to place the components.
-# After the placement is complete, the resolve_program() will check that each component has
-# sufficient information to be lowered to its MLIR equivalent.
+# After the placement is complete, the resolve_program() will check that each component has sufficient
+# information to be lowered to its MLIR equivalent.
 # At this point, the program is also verified and will report underlying MLIR errors, if any.
 # You can see a list of available placers in python/iron/placers.py
 module = my_program.resolve_program(SequentialPlacer())
