646b78d7d5cecdb2099797ff4daa2be653d35314
Joseph Melber <jmelber@xilinx.com>
2025-04-18 12:32:07 -0600

    Fix headers

--- programming_guide/mini_tutorial/exercise_3/exercise_3.py
+++ programming_guide/mini_tutorial/exercise_3/exercise_3.py
@@ -1,4 +1,4 @@
-# aie2.py -*- Python -*-
+# exercise_3.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
