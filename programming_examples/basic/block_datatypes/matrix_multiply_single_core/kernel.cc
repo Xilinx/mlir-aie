e8d358be15d6db459f6b7f2860d597b4d2933db4
L-roro <luis.romerorodriguez@epfl.ch>
2025-04-22 09:41:23 -0600

    First iteration of bfp examples

---
+++ programming_examples/basic/block_datatypes/matrix_multiply_single_core/kernel.cc
@@ -0,0 +1,77 @@
+//===- kernel.cc ------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2025, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#ifdef _CLANGD
+#define __AIENGINE__ 2
+#define __AIE_ARCH__ 21
+#define __AIEARCH__ 21
+#endif
+
+#include <aie_api/aie.hpp>
+
+extern "C" {
+
+// Note that the naming for bfp vector types is somewhat confusing.
+// The v64bfp16ebs8 type is a vector of 8 bfp16ebs8 elements, which corresponds
+// to 64 floating points in total
+void bfp16_matrix_multiplication(bfp16ebs8 *__restrict inA,
+                                 bfp16ebs8 *__restrict inB,
+                                 bfp16ebs8 *__restrict out) {
+  // We cannot load the bfp16ebs8 vectors directly doing this:
+  // aie::vector<bfloat16, 64> matrixA = aie::load_v<64>(inA);
+  // The reason is that the bfp16ebs8 type is not necessarily aligned and the
+  // memory interface must be adapted through the use of buffer streams for both
+  // input and output.
+  aie::block_vector<bfp16ebs8, 64> matrixA;
+  aie::block_vector_input_buffer_stream<bfp16ebs8, 64> inStreamA(inA);
+  inStreamA >> matrixA;
+
+  aie::block_vector<bfp16ebs8, 64> matrixB;
+  aie::block_vector_input_buffer_stream<bfp16ebs8, 64> inStreamB(inB);
+  inStreamB >> matrixB;
+
+  aie::accum<accfloat, 64> res = mul_8x8_8x8T(matrixA, matrixB);
+
+  aie::block_vector_output_buffer_stream<bfp16ebs8, 64> outStream(out);
+  outStream << res.to_vector<bfp16ebs8>();
+}
+
+void bf16_to_bfp_conversion(bfloat16 *__restrict inA, bfloat16 *__restrict inB,
+                            bfp16ebs8 *__restrict outA,
+                            bfp16ebs8 *__restrict outB) {
+  // Here there are two conversion paths for bf16 to bfp16ebs8. This split is
+  // meant to optimize resource usage in the inner loop, since we would
+  // otherwise be using the same slot in the core.
+
+  // The conversion for inA to bfp16 is carried out from an accumulator
+  aie::vector<bfloat16, 64> vecA = aie::load_v<64>(inA);
+  aie::accum<accfloat, 64> accA(vecA);
+
+  // The conversion for inB to bfp16 is carried out through an element-wise
+  // multiplication with ones
+  aie::vector<bfloat16, 64> vecB = aie::load_v<64>(inB);
+  // Porperly aligning dot products between blocks for a matrix multiplication
+  // implies transposing before the conversion!
+  aie::vector<bfloat16, 64> vecBT =
+      aie::detail::transpose<bfloat16, 64>::run(vecB, 8, 8);
+  aie::accum<accfloat, 64> accB =
+      mul_elem_64(vecBT, concat(broadcast_one_to_v32bfloat16(),
+                                broadcast_one_to_v32bfloat16()));
+
+  // This is doubling of the output array has to be done because we cannot
+  // concatenate the result. Unfortunately, vectors of size 128 are currently
+  // unsupported for bfp so no concatenation is possible.
+  aie::block_vector_output_buffer_stream<bfp16ebs8, 64> outStreamA(outA);
+  aie::block_vector_output_buffer_stream<bfp16ebs8, 64> outStreamB(outB);
+  outStreamA << accA.to_vector<bfp16ebs8>();
+  outStreamB << accB.to_vector<bfp16ebs8>();
+}
+
+} // extern "C"
---
+++ programming_examples/basic/block_datatypes/vector_passthrough/README.md
@@ -0,0 +1,55 @@
+<!---//===- README.md --------------------------*- Markdown -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2022, Advanced Micro Devices, Inc.
+// 
+//===----------------------------------------------------------------------===//-->
+
+# <ins>Vector Vector Add</ins>
+
+A simple binary operator, which uses a single AIE core to add two vectors together.  The overall vector size in this design is `256` and it processed by the core in smaller sub tiles of size `16`.  It shows how simple it can be to just feed data into the AIEs using the `ObjectFifo` abstraction, and drain the results back to external memory.  This reference design can be run on either a Ryzen™ AI NPU or a VCK5000.
+
+Both input vectors are brought into a Compute tile from a Shim tile. In the placed design, the value of `col` is dependent on whether the application is targeting NPU or VCK5000. The AIE tile performs the summation operations and the Shim tile brings the data back out to external memory.
+
+## Source Files Overview
+
+1. `vector_vector_add.py`: A Python script that defines the AIE array structural design using MLIR-AIE operations. This generates MLIR that is then compiled using `aiecc.py` to produce design binaries (ie. XCLBIN and inst.txt for the NPU in Ryzen™ AI). 
+
+1. `vector_vector_add_placed.py`: An alternative version of the design in `vector_vector_add.py`, that is expressed in a lower-level version of IRON.
+
+1. `test.cpp`: This C++ code is a testbench for the design example targeting Ryzen™ AI (AIE-ML). The code is responsible for loading the compiled XCLBIN file, configuring the AIE module, providing input data, and executing the AIE design on the NPU. After executing, the program verifies the results.
+
+1. `test_vck5000.cpp`: This C++ code is a testbench for the design example targeting the VCK5000 PCIe card (AIE). The code is responsible for configuring the AIEs, allocating memory, providing input data, and executing the AIE design on the VCK5000. After executing, the program verifies the results.
+
+## Ryzen™ AI Usage
+
+### C++ Testbench
+
+### Compilation
+
+To compile the design:
+```shell
+make
+```
+
+To compile the placed design:
+```shell
+env use_placed=1 make
+```
+
+To compile the C++ testbench:
+```shell
+make vector_vector_add.exe
+```
+
+### C++ Testbench
+
+To run the design:
+
+```shell
+make run
+```
+
