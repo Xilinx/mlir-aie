4ff8237e08f68ca289e5f969d13809dc519f5faf
Joseph Melber <jmelber@xilinx.com>
2025-04-24 17:57:11 -0600

    Add detect_npu_device concept

--- python/iron/config.py
+++ python/iron/config.py
@@ -6,7 +6,48 @@
 #
 # (c) Copyright 2025 Advanced Micro Devices, Inc.
 
+import subprocess
+
+from iron.device import NPU1Col4, NPU2
+
+
+def detect_npu_device():
+    try:
+        # Run `xrt-smi examine` and capture output
+        result = subprocess.run(
+            ["/opt/xilinx/xrt/bin/xrt-smi", "examine"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+        )
+        output = result.stdout
+
+        # Match strings for NPU2
+        # Set's generic "whole array" devices, this is overkill...
+        if any(
+            keyword.lower() in output.lower()
+            for keyword in [
+                "NPU Strix",
+                "NPU Strix Halo",
+                "NPU Kracken",
+                "RyzenAI-npu4",
+                "RyzenAI-npu6",
+            ]
+        ):
+            return NPU2()
+        else:
+            return NPU1Col4()
+
+    except FileNotFoundError:
+        raise RuntimeError("xrt-smi not found. Make sure XRT is installed and set up.")
+    except subprocess.CalledProcessError:
+        raise RuntimeError("Failed to run xrt-smi examine.")
+
+
 config = {}
+
+config["device"] = detect_npu_device()
 
 
 def set_current_device(device):
