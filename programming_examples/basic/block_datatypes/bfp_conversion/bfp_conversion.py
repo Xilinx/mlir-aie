6685a86b223d8a76b5f534f3bfaf3a4e537390b4
L-roro <luis.romerorodriguez@epfl.ch>
2025-04-28 03:25:44 -0600

    Fix Copyright dates

--- programming_examples/basic/block_datatypes/bfp_conversion/bfp_conversion.py
+++ programming_examples/basic/block_datatypes/bfp_conversion/bfp_conversion.py
@@ -1,4 +1,4 @@
-# vector_vector_add/vector_vector_add.py -*- Python -*-
+# bfp_conversion.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
