819ba19bbb447f02e57c67eb1b4eee69bd1ed6ab
Jack Lo <jack.lo@amd.com>
2025-04-29 11:22:38 -0600

    New unified pragmas

---
+++ aie_kernels/optimization_pragmas.h
@@ -0,0 +1,22 @@
+#if defined(__chess__)
+#define AIE_PIPELINE [[using chess: prepare_for_pipelining]]
+#define AIE_LOOP_MIN_ITERATION_COUNT(x) [[using chess: min_loop_count(x)]]
+#define AIE_LOOP_MAX_ITERATION_COUNT(x) [[using chess: max_loop_count(x)]]
+#define AIE_LOOP_RANGE(a, ...)                                                 \
+  [[chess::min_loop_count(a)]] __VA_OPT__(                                     \
+      [[chess::max_loop_count(__VA_ARGS__)]])
+
+#elif defined(__AIECC__)
+#ifndef __STRINGIFY
+#define __STRINGIFY(a) #a
+#endif
+
+#define AIE_PIPELINE
+#define AIE_LOOP_MIN_ITERATION_COUNT(x)                                        \
+  _Pragma(__STRINGIFY(clang loop min_iteration_count(x)))
+#define AIE_LOOP_MAX_ITERATION_COUNT(x)                                        \
+  _Pragma(__STRINGIFY(clang loop max_iteration_count(x)))
+#define AIE_LOOP_RANGE(a, ...)                                                 \
+  AIE_LOOP_MIN_ITERATION_COUNT(a)                                              \
+  __VA_OPT__(AIE_LOOP_MAX_ITERATION_COUNT(__VA_ARGS__))
+#endif
