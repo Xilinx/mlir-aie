05d6dd5c4fc1a166d474bf24312a28e16c26f57e
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-04-29 21:38:31 -0700

    Add initial core function

--- python/iron/__init__.py
+++ python/iron/__init__.py
@@ -1,5 +1,5 @@
 from .globalbuffer import GlobalBuffer
-from .kernel import Kernel
+from .kernel import CoreFunction, Kernel
 from .localbuffer import LocalBuffer
 from .program import Program
 from .worker import Worker, WorkerRuntimeBarrier
