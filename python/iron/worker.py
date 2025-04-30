cd7bf34068a73505e557b1815095738d5427a00b
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-04-29 21:38:31 -0700

    Add initial core function

--- python/iron/worker.py
+++ python/iron/worker.py
@@ -13,9 +13,9 @@ from ..dialects.aie import core, lock, use_lock
 from ..dialects.aiex import set_lock_value, LockAction
 from ..helpers.dialects.scf import _for as range_
 from .device import PlacementTile, AnyComputeTile, Tile
-from .dataflow.objectfifo import ObjectFifoHandle, ObjectFifo
+from .dataflow.objectfifo import ObjectFifoHandle
 from .dataflow.endpoint import ObjectFifoEndpoint
-from .kernel import Kernel, ExternalFunction
+from .kernel import Kernel, ExternalFunction, CoreFunction
 from .buffer import Buffer
 from .resolvable import Resolvable
 
@@ -82,7 +82,9 @@ class Worker(ObjectFifoEndpoint):
 
         # Check arguments to the core. Some information is saved for resolution.
         for arg in self.fn_args:
-            if isinstance(arg, (Kernel, ExternalFunction)):
+            if isinstance(arg, (Kernel, ExternalFunction)) or isinstance(
+                arg, (Kernel, CoreFunction)
+            ):
                 bin_names.add(arg.bin_name)
             elif isinstance(arg, ObjectFifoHandle):
                 arg.endpoint = self
