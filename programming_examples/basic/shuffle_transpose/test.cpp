05d3d3fb7a45c78e21c507a96cc055a702573fd6
Hashim Sharif <hasharif@amd.com>
2025-05-08 14:59:01 -0600

    shuffle_transpose: using cxx options

--- programming_examples/basic/shuffle_transpose/test.cpp
+++ programming_examples/basic/shuffle_transpose/test.cpp
@@ -13,6 +13,7 @@
 #include "xrt/xrt_device.h"
 #include "xrt/xrt_kernel.h"
 
+#include "cxxopts.hpp"
 #include "test_utils.h"
 
 #ifndef XCLBIN
@@ -28,6 +29,7 @@
 #endif
 
 /* This example performs a 16x16 INT8 transpose.
+   M and N are set to 16 in Makefile.
    kernel.cc includes an AIE kernel that is specific to 16x16 */
 
 #define IN_SIZE (M * N * sizeof(uint8_t))  // in bytes
@@ -44,7 +46,31 @@ void print_matrix(uint8_t *buf, int n_rows, int n_cols) {
 
 int main(int argc, const char *argv[]) {
 
-  std::vector<uint32_t> instr_v = test_utils::load_instr_binary(INSTS_BIN);
+  // Program arguments parsing
+  cxxopts::Options options("Shuffle Transpose Test",
+                           "Test the Shuffle transpose kernel");
+
+  options.add_options()("help,h", "produce help message")(
+      "xclbin,x", "the input xclbin path", cxxopts::value<std::string>())(
+      "kernel,k", "the kernel name in the XCLBIN (for instance PP_PRE_FD)",
+      cxxopts::value<std::string>())("verbosity,v",
+                                     "the verbosity of the output",
+                                     cxxopts::value<int>()->default_value("0"))(
+      "instr,i",
+      "path of file containing userspace instructions to be sent to the LX6",
+      cxxopts::value<std::string>());
+
+  auto vm = options.parse(argc, argv);
+
+  // Check required options
+  if (!vm.count("xclbin") || !vm.count("kernel") || !vm.count("instr")) {
+    std::cerr << "Error: Required options missing\n\n";
+    std::cerr << "Usage:\n" << options.help() << std::endl;
+    return 1;
+  }
+  
+  std::vector<uint32_t> instr_v =
+    test_utils::load_instr_binary(vm["instr"].as<std::string>());
   assert(instr_v.size() > 0);
 
   // Get a device handle
@@ -52,16 +78,20 @@ int main(int argc, const char *argv[]) {
   xrt::device device = xrt::device(device_index);
 
   // Load the xclbin
-  xrt::xclbin xclbin = xrt::xclbin(XCLBIN);
+  xrt::xclbin xclbin = xrt::xclbin(vm["xclbin"].as<std::string>());
 
+  std::string Node = vm["kernel"].as<std::string>();
   // Get the kernel from the xclbin
   std::vector<xrt::xclbin::kernel> xkernels = xclbin.get_kernels();
-  xrt::xclbin::kernel xkernel = *std::find_if(
-      xkernels.begin(), xkernels.end(), [](xrt::xclbin::kernel &k) {
-        return k.get_name().rfind(KERNEL_NAME, 0) == 0;
-      });
+  auto xkernel = *std::find_if(xkernels.begin(), xkernels.end(),
+                               [Node](xrt::xclbin::kernel &k) {
+                                 auto name = k.get_name();
+                                 std::cout << "Name: " << name << std::endl;
+                                 return name.rfind(Node, 0) == 0;
+                               });
+
   std::string kernel_name = xkernel.get_name();
-  assert(strcmp(kernel_name.c_str(), KERNEL_NAME) == 0);
+  assert(strcmp(kernel_name.c_str(), Node.c_str()) == 0);
 
   device.register_xclbin(xclbin);
 
