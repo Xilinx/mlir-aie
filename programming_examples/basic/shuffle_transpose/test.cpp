0dcb2e41e1219c663cab3538e22af94005301c47
Hashim Sharif <hasharif@amd.com>
2025-05-08 14:19:14 -0600

    shuffle_transpose: using test_utils::load_instr_binary

--- programming_examples/basic/shuffle_transpose/test.cpp
+++ programming_examples/basic/shuffle_transpose/test.cpp
@@ -13,6 +13,8 @@
 #include "xrt/xrt_device.h"
 #include "xrt/xrt_kernel.h"
 
+#include "test_utils.h"
+
 #ifndef XCLBIN
 #define XCLBIN "build/final.xclbin"
 #endif
@@ -40,34 +42,9 @@ void print_matrix(uint8_t *buf, int n_rows, int n_cols) {
   }
 }
 
-std::vector<uint32_t> load_instr_binary(std::string instr_path) {
-  // Open file in binary mode
-  std::ifstream instr_file(instr_path, std::ios::binary);
-  if (!instr_file.is_open()) {
-    throw std::runtime_error("Unable to open instruction file\n");
-  }
-
-  // Get the size of the file
-  instr_file.seekg(0, std::ios::end);
-  std::streamsize size = instr_file.tellg();
-  instr_file.seekg(0, std::ios::beg);
-
-  // Check that the file size is a multiple of 4 bytes (size of uint32_t)
-  if (size % 4 != 0) {
-    throw std::runtime_error("File size is not a multiple of 4 bytes\n");
-  }
-
-  // Allocate vector and read the binary data
-  std::vector<uint32_t> instr_v(size / 4);
-  if (!instr_file.read(reinterpret_cast<char *>(instr_v.data()), size)) {
-    throw std::runtime_error("Failed to read instruction file\n");
-  }
-  return instr_v;
-}
-
 int main(int argc, const char *argv[]) {
 
-  std::vector<uint32_t> instr_v = load_instr_binary(INSTS_BIN);
+  std::vector<uint32_t> instr_v = test_utils::load_instr_binary(INSTS_BIN);
   assert(instr_v.size() > 0);
 
   // Get a device handle
