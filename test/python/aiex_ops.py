1cdb7f073b96e8cb4766d241fbb5519d57d336a3
joeldushouyu <joeldushouyu@gmail.com>
2025-05-13 16:04:36 -0400

    fix: add packet_header into aiex_ops.py test

--- test/python/aiex_ops.py
+++ test/python/aiex_ops.py
@@ -41,7 +41,7 @@ def NpuDmaMemcpyNdOp():
     def device_body():
         @runtime_sequence(np.ndarray[(100,), np.dtype[np.int8]])
         def sequence(B):
-            #CHECK: aiex.npu.dma_memcpy_nd(%arg0[0, 0, 0, 0][1, 1, 1, 100][0, 0, 0, 1]) {id = 1 : i64, issue_token = true, metadata = @objFifo_out0} : memref<100xi8>
+            #CHECK: aiex.npu.dma_memcpy_nd(%arg0[0, 0, 0, 0][1, 1, 1, 100][0, 0, 0, 1], packet = <pkt_type = 1, pkt_id = 4>) {id = 1 : i64, issue_token = true, metadata = @objFifo_out0} : memref<100xi8>
             npu_dma_memcpy_nd(
                 metadata="objFifo_out0",
                 bd_id=1,
@@ -49,5 +49,6 @@ def NpuDmaMemcpyNdOp():
                 offsets=[0,0,0,0],
                 sizes=[1,1,1,100],
                 strides=[0,0,0,1],
+                packet=(1,4),
                 issue_token=True
             )
