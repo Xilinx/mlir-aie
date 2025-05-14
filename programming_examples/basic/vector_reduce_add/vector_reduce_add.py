15d954ff9a21765b7a034b278b069f8313068826
Vit Zemanek <2vitzemanek@gmail.com>
2025-05-14 17:41:49 +0200

    now works with the newer host-NPU buffer method already present in most other examples. supports tiled computation. kernel code uses templates.

--- programming_examples/basic/vector_reduce_add/vector_reduce_add.py
+++ programming_examples/basic/vector_reduce_add/vector_reduce_add.py
@@ -1,62 +1,115 @@
-# vector_reduce_add/vector_reduce_add.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+# (c) Copyright 2024 AMD Inc.
+from ml_dtypes import bfloat16
 import numpy as np
 import sys
 
 from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
 from aie.iron.placers import SequentialPlacer
-from aie.iron.device import NPU1Col1, NPU2Col1
+from aie.iron.device import NPU1Col1, NPU2
+from aie.iron.controlflow import range_
+from aie.helpers.util import np_ndarray_type_get_shape
 
 
-if len(sys.argv) > 2:
-    if sys.argv[1] == "npu":
-        dev = NPU1Col1()
-    elif sys.argv[1] == "npu2":
-        dev = NPU2Col1()
-    else:
-        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
+def my_relu(dev, trace_size):
+    N = 65536 #tiles with reduce_add produce a segmentation error
 
+    # Tile sizes and types
+    n = 256
+    N_div_n = N // n
 
-def my_reduce_add():
-    N = 1024
+    n_cores = 1
+    tiles = N_div_n // n_cores
 
-    in_ty = np.ndarray[(N,), np.dtype[np.int32]]
-    out_ty = np.ndarray[(1,), np.dtype[np.int32]]
+    tensor_ty = np.ndarray[(N,), np.dtype[np.int32]]
+    tile_ty = np.ndarray[(n,), np.dtype[np.int32]]
+    out_ty = np.ndarray[(n,), np.dtype[np.int32]]
 
-    # AIE-array data movement with object fifos
-    of_in = ObjectFifo(in_ty, name="in")
-    of_out = ObjectFifo(out_ty, name="out")
+    # Type used in the tile memory
+    A_ty = np.ndarray[(n,), np.dtype[np.int32]]
+    C_ty = np.ndarray[(n,), np.dtype[np.int32]] #DMAs only transfer multiples of 32 bits
+
+    # Type used in the memory tile which aggregates across the 4 cores
+    A_memTile_ty = np.ndarray[(n * n_cores,), np.dtype[np.int32]]
+    C_memTile_ty = np.ndarray[(n * n_cores,), np.dtype[np.int32]]
 
     # AIE Core Function declarations
-    reduce_add_vector = Kernel(
-        "reduce_add_vector", "reduce_add.cc.o", [in_ty, out_ty, np.int32]
+    relu = Kernel("reduce_add_vector", "reduce_add.o", [tile_ty, out_ty, np.int32])
+
+    # AIE-array data movement with object fifos
+    # Input A
+    inA = ObjectFifo(A_memTile_ty, name="inA")
+    of_offsets = [
+        (np.prod(np_ndarray_type_get_shape(A_memTile_ty)) // n_cores) * i
+        for i in range(n_cores)
+    ]
+    inA_fifos = inA.cons().split(
+        of_offsets,
+        obj_types=[A_ty] * n_cores,
+        names=[f"memA{i}" for i in range(n_cores)],
     )
 
-    # A task for a core to perform
-    def core_body(of_in, of_out, reduce_add_vector):
-        elem_out = of_out.acquire(1)
-        elem_in = of_in.acquire(1)
-        reduce_add_vector(elem_in, elem_out, N)
-        of_in.release(1)
-        of_out.release(1)
+    # Output C
+    outC = ObjectFifo(C_memTile_ty, name="outC")
+    of_offsets = [
+        (np.prod(np_ndarray_type_get_shape(C_memTile_ty)) // n_cores) * i
+        for i in range(n_cores)
+    ]
+    outC_fifos = outC.prod().join(
+        of_offsets,
+        obj_types=[C_ty] * n_cores,
+        names=[f"memC{i}" for i in range(n_cores)],
+    )
 
-    # Create a worker to run a task on a core
-    worker = Worker(core_body, fn_args=[of_in.cons(), of_out.prod(), reduce_add_vector])
+    # Task for cores to perform
+    def core_fn(of_a, of_c, relu_fn):
+        for _ in range_(tiles):
+            elem_out = of_c.acquire(1)
+            elem_in_a = of_a.acquire(1)
+            relu_fn(elem_in_a, elem_out, n)
+            of_a.release(1)
+            of_c.release(1)
+
+    # Create workers to perform the task
+    workers = []
+    for i in range(n_cores):
+        workers.append(
+            Worker(
+                core_fn,
+                fn_args=[
+                    inA_fifos[i].cons(),
+                    outC_fifos[i].prod(),
+                    relu,
+                ],
+            )
+        )
 
     # Runtime operations to move data to/from the AIE-array
     rt = Runtime()
-    with rt.sequence(in_ty, out_ty) as (a_in, c_out):
-        rt.start(worker)
-        rt.fill(of_in.prod(), a_in)
-        rt.drain(of_out.cons(), c_out, wait=True)
+    with rt.sequence(tensor_ty, tensor_ty, tensor_ty) as (A, B, C):
+        rt.start(*workers)
+        rt.fill(inA.prod(), A)
+        rt.drain(outC.cons(), C, wait=True)
 
-    # Place program components (assign them resources on the device) and generate an MLIR module
+    # Place components (assign them resources on the device) and generate an MLIR module
     return Program(dev, rt).resolve_program(SequentialPlacer())
 
 
-print(my_reduce_add())
+try:
+    device_name = str(sys.argv[1])
+    if device_name == "npu":
+        dev = NPU1Col1()
+    elif device_name == "npu2":
+        dev = NPU2()
+    else:
+        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[2]))
+    trace_size = 0 if (len(sys.argv) != 3) else int(sys.argv[2])
+except ValueError:
+    print("Argument is not an integer")
+
+module = my_relu(dev, trace_size)
+print(module)
