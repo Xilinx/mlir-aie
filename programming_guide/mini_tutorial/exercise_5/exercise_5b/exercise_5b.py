fd332dac3db6ea5bf51b092d2fe8da4ba216f432
AndraBisca <andrab@amd.com>
2025-05-28 12:44:12 -0600

    Add runtime sequence section to mini tutorial

--- programming_guide/mini_tutorial/exercise_5/exercise_5b/exercise_5b.py
+++ programming_guide/mini_tutorial/exercise_5/exercise_5b/exercise_5b.py
@@ -1,4 +1,4 @@
-# exercise_3.py -*- Python -*-
+# exercise_5b.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
@@ -6,21 +6,51 @@
 #
 # (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
 
+import os
+import glob
 import sys
 import numpy as np
 
 from aie.iron import Program, Runtime, Worker, ObjectFifo
 from aie.iron.placers import SequentialPlacer
 from aie.iron.controlflow import range_
+from aie.helpers.taplib import TensorAccessPattern, TensorAccessSequence
 
 import aie.iron as iron
 
+# Define tensor shape
+data_height = 3
+data_width = 16
+
 
 @iron.jit(is_placed=False)
-def exercise_3(input0, output):
-    num_elements = output.numel()
-    data_type = output.dtype
-    tile_ty = np.ndarray[(num_elements,), np.dtype[data_type]]
+def exercise_5b(input0, output):
+    # Define tile size
+    tile_height = 3
+    tile_width = 8
+    tile_size = tile_height * tile_width
+
+    data_size = input0.numel()
+    element_type = input0.dtype
+    data_ty = np.ndarray[(data_size,), np.dtype[element_type]]
+    tile_ty = np.ndarray[(tile_size,), np.dtype[element_type]]
+
+    # Define runtime tensor access pattern (tap)
+    tensor_dims = (data_height, data_width)
+    tap1 = TensorAccessPattern(
+        tensor_dims, offset=0, sizes=[1, 1, 3, 8], strides=[0, 0, 16, 1]
+    )
+    tap2 = TensorAccessPattern(
+        tensor_dims, offset=8, sizes=[1, 1, 3, 8], strides=[0, 0, 16, 1]
+    )
+
+    # Create a TensorTileSequence from a list of taps
+    taps = TensorAccessSequence.from_taps([tap1, tap2])
+
+    i = 0
+    for t in taps:
+        t.visualize(show_arrows=True, file_path=f"plot{i}.png")
+        i += 1
 
     # Dataflow with ObjectFifos
     of_in = ObjectFifo(tile_ty, name="in")
@@ -30,7 +60,7 @@ def exercise_3(input0, output):
     def core_fn(of_in, of_out):
         elem_in = of_in.acquire(1)
         elem_out = of_out.acquire(1)
-        for i in range_(num_elements):
+        for i in range_(tile_size):
             elem_out[i] = elem_in[i]
         of_in.release(1)
         of_out.release(1)
@@ -40,9 +70,10 @@ def exercise_3(input0, output):
 
     # To/from AIE-array runtime data movement
     rt = Runtime()
-    with rt.sequence(tile_ty, tile_ty) as (a_in, c_out):
+    with rt.sequence(data_ty, data_ty) as (a_in, c_out):
         rt.start(my_worker)
-        rt.fill(of_in.prod(), a_in)
+        for t in taps:
+            rt.fill(of_in.prod(), a_in, t)
         rt.drain(of_out.cons(), c_out, wait=True)
 
     # Create the program from the device type and runtime
@@ -54,28 +85,38 @@ def exercise_3(input0, output):
 
 def main():
     # Define tensor shapes and data types
-    num_elements = 48
-    data_type = np.int32
+    data_size = data_height * data_width
+    element_type = np.int32
+
+    # Delete existing plot*.png files
+    for file in glob.glob("plot*.png"):
+        try:
+            os.remove(file)
+        except OSError as e:
+            print(f"Error deleting {file}: {e}")
 
     # Construct an input tensor and an output zeroed tensor
     # The two tensors are in memory accessible to the NPU
-    input0 = iron.arange(num_elements, dtype=data_type, device="npu")
-    output = iron.zeros_like(input0)
+    input0 = iron.arange(data_size, dtype=element_type, device="npu")
+    output = iron.zeros(data_size, dtype=element_type, device="npu")
 
     # JIT-compile the kernel then launches the kernel with the given arguments. Future calls
     # to the kernel will use the same compiled kernel and loaded code objects
-    exercise_3(input0, output)
+    exercise_5b(input0, output)
 
     # Check the correctness of the result
-    e = np.equal(input0.numpy(), output.numpy())
-    errors = np.size(e) - np.count_nonzero(e)
-
-    # Print the results
-    print(f"{'input0':>4} = {'output':>4}")
-    print("-" * 34)
-    count = input0.numel()
-    for idx, (a, c) in enumerate(zip(input0[:count], output[:count])):
-        print(f"{idx:2}: {a:4} = {c:4}")
+    errors = 0
+    for index, (actual, ref) in enumerate(
+        zip(
+            output,
+            [k * 8 + j * 16 + i for k in range(2) for j in range(3) for i in range(8)],
+        )
+    ):
+        if actual != ref:
+            print(f"Error in output {actual} != {ref}")
+            errors += 1
+        else:
+            print(f"Correct output {actual} == {ref}")
 
     # If the result is correct, exit with a success code.
     # Otherwise, exit with a failure code
