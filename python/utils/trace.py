8bf074a36801f6cb635768076b0ee161cb260850
Jack Lo <jack.lo@amd.com>
2025-05-30 15:35:04 -0600

    format fix

--- python/utils/trace.py
+++ python/utils/trace.py
@@ -276,7 +276,6 @@ def pack4bytes(b3, b2, b1, b0):
     return w
 
 
-
 # This function configures the a tile's memory trace unit given a set of configurations as described below:
 #
 # function arguments:
@@ -317,10 +316,10 @@ def configure_coremem_tracing_aie2(
             f"At most 8 events can be traced at once, have {len(events)}."
         )
     events = (events + [MemEvent.NONE] * 8)[:8]
-    
+
     # Reorder events so they match the event order for display
     ordered_events = [events[p] for p in [3, 2, 1, 0, 7, 6, 5, 4]]
-    
+
     # Assure all selected events are valid
     ordered_events = [
         e if isinstance(e, GenericEvent) else GenericEvent(e) for e in ordered_events
@@ -337,7 +336,7 @@ def configure_coremem_tracing_aie2(
                 "For example: "
                 f"configure_simple_tracing_aie2( ..., events=[PortEvent(CoreEvent.{event.code.name}, 1, master=True)])"
             )
-            
+
     # 140D0: Trace Control 0
     #          0xAABB---C
     #            AA        <- Event to stop trace capture
@@ -356,7 +355,7 @@ def configure_coremem_tracing_aie2(
         row=int(tile.row),
         address=0x140D4,
         value=((packet_type & 0x7) << 12) | (packet_id & 0x1F) if enable_packet else 0,
-    )    
+    )
     # 0x140E0: Trace Event Group 1  (Which events to trace)
     #          0xAABBCCDD    AA, BB, CC, DD <- four event slots
     npu_write32(
@@ -373,7 +372,7 @@ def configure_coremem_tracing_aie2(
         address=0x140E4,
         value=pack4bytes(*(e.code.value for e in ordered_events[4:8])),
     )
-    
+
     # Event specific register writes
     all_reg_writes = {}
     for e in ordered_events:
@@ -387,8 +386,8 @@ def configure_coremem_tracing_aie2(
         npu_write32(
             column=int(tile.col), row=int(tile.row), address=addr, value=value
         )  # For backwards compatibility, allow integers for start/stop events
-        
-    
+
+
 # This function configures the a tile's trace unit given a set of configurations as described below:
 #
 # function arguments:
@@ -934,7 +933,6 @@ def configure_coremem_packet_tracing_aie2(
     )
 
 
-
 # Wrapper to configure the core tile and shim tile for packet tracing. This does
 # the following:
 # 1. Configure core tile based on start/ stop, events, and flow id. The flow id
@@ -1118,10 +1116,10 @@ def configure_shimtile_packet_tracing_aie2(
 # * `tiles to trace` - array of tiles to trace
 # * `shim tile` - Single shim tile to configure for writing trace packets to DDR
 def configure_packet_tracing_flow(tiles_to_trace, shim):
-    
+
     exist_traces = []
     for i in range(len(tiles_to_trace)):
-        
+
         if tiles_to_trace[i] not in exist_traces:
             packetflow(
                 i + 1,
@@ -1133,7 +1131,7 @@ def configure_packet_tracing_flow(tiles_to_trace, shim):
                 1,
                 keep_pkt_header=True,
             )
-            
+
             exist_traces.append(tiles_to_trace[i])
         else:
             # Ct's memory trace?
@@ -1165,7 +1163,9 @@ def configure_shim_trace_start_aie2(
     brdcst_num=15,
     user_event=ShimTileEvent.USER_EVENT_1,  # 0x7F, 127: user even t#1
 ):
-    configure_timer_ctrl_coretile_aie2(shim, user_event) #TODO: should have call configure_timer_ctrl_shimtile_aie2() instead?
+    configure_timer_ctrl_coretile_aie2(
+        shim, user_event
+    )  # TODO: should have call configure_timer_ctrl_shimtile_aie2() instead?
     configure_broadcast_core_aie2(shim, brdcst_num, user_event)
     configure_event_gen_core_aie2(shim, user_event)
 
@@ -1184,7 +1184,7 @@ def gen_trace_done_aie2(
 # `configure_*tile_packet_tracing_aie2`. A key distinction is made to choose the right start and stop
 # event depending on the tile type. We pass in 3 sets of optional event arguments that allows them
 # to be customized depending on the tile type.
-# 
+#
 # NOTE: We configure the shimdma to bd 15 and channel 1 be default which has the potential to
 # conflict if that channel or bd is used by the lowering steps to configure the shim data movers.
 # We also configure each tile in the array tiles_to_trace with a incrementing packet id which matches
@@ -1295,7 +1295,7 @@ def configure_packet_tracing_aie2(
             ShimTileEvent.DMA_S2MM_1_STREAM_STARVATION,
         ]
     if coremem_events == None:
-        coremem_events=[
+        coremem_events = [
             MemEvent.DMA_S2MM_0_START_TASK,
             MemEvent.DMA_MM2S_0_START_TASK,
             MemEvent.CONFLICT_DM_BANK_0,
@@ -1306,8 +1306,8 @@ def configure_packet_tracing_aie2(
             MemEvent.EDGE_DETECTION_EVENT_1,
         ]
 
-    start_core_mem_broadcast_event= MemEvent(107+start_broadcast_num)
-    stop_core_mem_broadcast_event = MemEvent(107+ stop_broadcast_num)
+    start_core_mem_broadcast_event = MemEvent(107 + start_broadcast_num)
+    stop_core_mem_broadcast_event = MemEvent(107 + stop_broadcast_num)
     start_core_broadcast_event = CoreEvent(107 + start_broadcast_num)
     stop_core_broadcast_event = CoreEvent(107 + stop_broadcast_num)
     start_memtile_broadcast_event = MemTileEvent(142 + start_broadcast_num)
@@ -1339,7 +1339,9 @@ def configure_packet_tracing_aie2(
                     packet_id=i + 1,
                     packet_type=PacketType.SHIMTILE,
                 )
-                configure_timer_ctrl_shimtile_aie2(tiles_to_trace[i], start_shimtile_broadcast_event)
+                configure_timer_ctrl_shimtile_aie2(
+                    tiles_to_trace[i], start_shimtile_broadcast_event
+                )
         elif isMemTile(tiles_to_trace[i]):
             configure_memtile_tracing_aie2(
                 tile=tiles_to_trace[i],
@@ -1350,7 +1352,9 @@ def configure_packet_tracing_aie2(
                 packet_id=i + 1,
                 packet_type=PacketType.MEMTILE,
             )
-            configure_timer_ctrl_memtile_aie2(tiles_to_trace[i], start_memtile_broadcast_event)
+            configure_timer_ctrl_memtile_aie2(
+                tiles_to_trace[i], start_memtile_broadcast_event
+            )
         elif isCoreTile(tiles_to_trace[i]):
             if tiles_to_trace[i] not in exist_core_tile_traces:
                 configure_coretile_tracing_aie2(
@@ -1362,7 +1366,9 @@ def configure_packet_tracing_aie2(
                     packet_id=i + 1,
                     packet_type=PacketType.CORE,
                 )
-                configure_timer_ctrl_coretile_aie2(tiles_to_trace[i], start_core_broadcast_event)
+                configure_timer_ctrl_coretile_aie2(
+                    tiles_to_trace[i], start_core_broadcast_event
+                )
                 exist_core_tile_traces.append(tiles_to_trace[i])
             else:
                 configure_coremem_tracing_aie2(
@@ -1374,7 +1380,9 @@ def configure_packet_tracing_aie2(
                     packet_id=i + 1,
                     packet_type=PacketType.MEM,
                 )
-                configure_timer_ctrl_coremem_aie2(tiles_to_trace[i], start_core_mem_broadcast_event)
+                configure_timer_ctrl_coremem_aie2(
+                    tiles_to_trace[i], start_core_mem_broadcast_event
+                )
         else:
             raise ValueError(
                 "Invalid tile("
