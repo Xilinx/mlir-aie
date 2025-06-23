d2a22e6d05610011ecc3b65140561e24e1c82849
André Rösti <an.roesti@gmail.com>
2025-06-23 15:26:06 -0600

    [GEMM] Fix placed variant whole array 8 col (#2409)

--- programming_examples/basic/matrix_multiplication/whole_array/whole_array_placed.py
+++ programming_examples/basic/matrix_multiplication/whole_array/whole_array_placed.py
@@ -527,7 +527,7 @@ def my_matmul(
                             #     |                |
                             #      ----------------
                             tile_offset = (
-                                (row_base + tile_row) * n_aie_cols + col
+                                (row_base + tile_row) * n_shim_mem_A + col
                             ) % len(A_tiles)
 
                             # always equal to n_aie_rows since we have n_aie_rows row tiles for matrix A
