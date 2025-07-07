b0f4287a7341e237fd88e429b336d8d2b88a09cd
Pranathi Vasireddy <pvasired@amd.com>
2025-07-07 11:51:11 -0600

    Working sliding conditional example

--- programming_examples/basic/sliding/aie2.py
+++ programming_examples/basic/sliding/aie2.py
@@ -37,43 +37,59 @@ def sliding_window():
             # Tile declarations
             ShimTile = tile(col, 0)
             ComputeTile = tile(col, 2)
+            ComputeTile2 = tile(col, 4)
 
-            # AIE-array data movement with object fifos
-            of_in = object_fifo("in", ShimTile, ComputeTile, 3, subtensor_ty)
-            of_out = object_fifo("out", ComputeTile, ShimTile, 2, subtensor_ty)
+            # AIE-array data movement with object fifos             
+            of_in = object_fifo("in", ShimTile, ComputeTile, 2, subtensor_ty)
+            of_in2 = object_fifo("in2", ComputeTile, ComputeTile2, 3, subtensor_ty)
+            of_out = object_fifo("out", ComputeTile2, ShimTile, 2, subtensor_ty)
 
             # AIE Core Function declarations
+            passthrough_10_i32 = external_func(
+                 "passthrough_10_i32", inputs=[subtensor_ty, subtensor_ty]
+            )
             add_10_i32 = external_func(
                 "add_10_i32", inputs=[subtensor_ty, subtensor_ty, subtensor_ty]
             )
-
-            # Set up compute tiles
             @core(ComputeTile, "kernel.o")
             def core_body():
+                for _ in range_(10):
+                    elemOut = of_in2.acquire(ObjectFifoPort.Produce, 1)
+                    elemIn = of_in.acquire(ObjectFifoPort.Consume, 1)
+                    call(passthrough_10_i32, [elemIn, elemOut])
+                    of_in.release(ObjectFifoPort.Consume, 1)
+                    of_in2.release(ObjectFifoPort.Produce, 1)
+
+            # Set up compute tiles
+            @core(ComputeTile2, "kernel.o")
+            def core_body():
                 for i in range_(10):
-                    elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
                     with if_(i == 0) as if_op:
-                        elemInPre = of_in.acquire(ObjectFifoPort.Consume, 1)
+                        elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                        elemInPre = of_in2.acquire(ObjectFifoPort.Consume, 1)
                         add_10_i32(elemInPre, elemInPre, elemOut)
-                        of_in.release(ObjectFifoPort.Consume, 1)
+                        of_out.release(ObjectFifoPort.Produce, 1)
                     with else_(if_op):
                         with if_(i == 9) as if_op1:
-                            elemsInPost = of_in.acquire(ObjectFifoPort.Consume, 2)
+                            elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                            elemsInPost = of_in2.acquire(ObjectFifoPort.Consume, 2)
                             add_10_i32(elemsInPost[0], elemsInPost[1], elemOut)
-                            of_in.release(ObjectFifoPort.Consume, 2)
+                            of_in2.release(ObjectFifoPort.Consume, 2)
+                            of_out.release(ObjectFifoPort.Produce, 1)
                         with else_(if_op1):
-                            elemsIn = of_in.acquire(ObjectFifoPort.Consume, 2)
+                            elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                            elemsIn = of_in2.acquire(ObjectFifoPort.Consume, 2)
                             add_10_i32(elemsIn[0], elemsIn[1], elemOut)
-                            of_in.release(ObjectFifoPort.Consume, 2)
-                of_out.release(ObjectFifoPort.Produce, 1)
+                            of_in2.release(ObjectFifoPort.Consume, 1)
+                            of_out.release(ObjectFifoPort.Produce, 1)
 
             # To/from AIE-array data movement
             tensor_ty = np.ndarray[(N,), np.dtype[np.int32]]
 
             @runtime_sequence(tensor_ty, tensor_ty)
             def sequence(A, C):
-                npu_dma_memcpy_nd(metadata=of_in, bd_id=1, mem=A, sizes=[1, 1, 1, N])
-                npu_dma_memcpy_nd(metadata=of_out, bd_id=0, mem=C, sizes=[1, 1, 1, N])
+                npu_dma_memcpy_nd(metadata="in", bd_id=1, mem=A, sizes=[1, 1, 1, N])
+                npu_dma_memcpy_nd(metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, N])
                 dma_wait(of_out)
 
     print(ctx.module)
