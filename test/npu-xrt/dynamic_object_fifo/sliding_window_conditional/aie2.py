8d5a8c55fb46ceb2f355baef67c3ab1127b52399
Pranathi Vasireddy <pvasired@amd.com>
2025-07-10 16:35:54 -0600

    Adding both NPUs

--- test/npu-xrt/dynamic_object_fifo/sliding_window_conditional/aie2.py
+++ test/npu-xrt/dynamic_object_fifo/sliding_window_conditional/aie2.py
@@ -9,8 +9,11 @@
 #
 # RUN: xchesscc_wrapper aie2 -I %aietools/include -c %S/kernel.cc -o ./kernel.o
 # RUN: %python %S/aie2.py > ./aie2.mlir
+# RUN: %run_on_npu1% sed 's/NPUDEVICE/npu1_1col/g' -i aie2.mlir
+# RUN: %run_on_npu2% sed 's/NPUDEVICE/npu2_1col/g' -i aie2.mlir
 # RUN: %python aiecc.py --no-aiesim --aie-generate-npu-insts --aie-generate-xclbin --no-compile-host --dynamic-objFifos --xclbin-name=final.xclbin --npu-insts-name=insts.bin ./aie2.mlir
 # RUN: clang %S/test.cpp -o test.exe -std=c++17 -Wall %xrt_flags -lrt -lstdc++ %test_utils_flags
+# RUN: %run_on_npu1% ./test.exe
 # RUN: %run_on_npu2% ./test.exe
 
 import numpy as np
