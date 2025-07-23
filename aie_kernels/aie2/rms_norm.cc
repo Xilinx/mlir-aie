aa98fd328a1486a4b6c7a8f07645de13bfaa8b7e
Pranathi Vasireddy <pvasired@amd.com>
2025-07-23 10:38:05 -0600

    RMSNorm First draft

---
+++ aie_kernels/aie2/rms_norm.cc
@@ -0,0 +1,55 @@
+//===- layernorm.cc -------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2025, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include <aie_api/aie.hpp>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+template <typename T, int N>
+void rms_norm(const T *restrict input, T *restrict output, int32_t rows,
+              int32_t cols) {
+  event0();
+  constexpr float epsilon = 1e-5f;
+  const float gamma = 1.0f;
+
+  for (int c = 0; c < cols; c++) {
+    float sum_sq = 0.0f;
+    for (int r = 0; r < rows; r++) {
+      float x = float(input[r * cols + c]);
+      sum_sq += x * x;
+    }
+    float inv_rms = aie::invsqrt(sum_sq / rows + epsilon);
+
+    // Normalize each value in the column
+    for (int r = 0; r < rows; r += N) {
+      T *output_ptr = output + r * cols + c;
+      const T *input_ptr = input + r * cols + c;
+      ::aie::vector<T, N> input_v;
+      for (int i = 0; i < N; i++) {
+        input_v[i] = input_ptr[i * cols];
+      }
+      ::aie::vector<T, N> inv_rms_v = ::aie::broadcast<T, N>(inv_rms);
+      ::aie::vector<T, N> gamma_v = ::aie::broadcast<T, N>(gamma);
+      ::aie::vector<T, N> norm_v = ::aie::mul(input_v, inv_rms_v);
+      ::aie::vector<T, N> out_v = ::aie::mul(norm_v, gamma_v);
+      for (int i = 0; i < N; i++) {
+        output_ptr[i * cols] = out_v[i];
+      }
+    }
+  }
+  event1();
+}
+
+extern "C" {
+void rms_norm(bfloat16 *input, bfloat16 *output, int32_t rows, int32_t cols) {
+  rms_norm<bfloat16, 16>(input, output, rows, cols);
+}
+}
---
+++ programming_examples/ml/rmsnorm/rmsnorm.py
@@ -0,0 +1,80 @@
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# (c) Copyright 2025 Advanced Micro Devices, Inc.
+
+import numpy as np
+import argparse
+import sys
+
+from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
+from aie.iron.placers import SequentialPlacer
+from aie.iron.device import NPU1Col1, NPU2Col1
+from ml_dtypes import bfloat16
+
+
+def rmsnorm(dev, rows, cols, trace_size):
+    enable_trace = 1 if trace_size > 0 else None
+
+    # Define tensor types
+
+    in_volume = rows * cols
+
+    dtype = np.ndarray[(in_volume,), np.dtype[bfloat16]]
+
+    of_in = ObjectFifo(dtype, name="in")
+    of_out = ObjectFifo(dtype, name="out")
+
+    rms_norm_kernel = Kernel(
+        "rms_norm", "rms_norm.o", [dtype, dtype, np.int32, np.int32]
+    )
+
+    def core_body(of_in, of_out, rms_norm_kernel):
+        elem_in = of_in.acquire(1)
+        elem_out = of_out.acquire(1)
+        rms_norm_kernel(elem_in, elem_out, rows, cols)
+        of_in.release(1)
+        of_out.release(1)
+
+    worker = Worker(
+        core_body,
+        fn_args=[of_in.cons(), of_out.prod(), rms_norm_kernel],
+        trace=enable_trace,
+    )
+
+    rt = Runtime()
+    with rt.sequence(dtype, dtype) as (a_in, c_out):
+        rt.enable_trace(enable_trace)
+        rt.start(worker)
+        rt.fill(of_in.prod(), a_in)
+        rt.drain(of_out.cons(), c_out, wait=True)
+    return Program(dev, rt).resolve_program(SequentialPlacer())
+
+
+p = argparse.ArgumentParser()
+p.add_argument("-d", "--dev", required=True, dest="device", help="AIE Device")
+p.add_argument("-r", "--rows", required=True, dest="rows", help="Row size")
+p.add_argument("-c", "--cols", required=True, dest="cols", help="Col size")
+p.add_argument(
+    "-t",
+    "--trace_size",
+    required=False,
+    dest="trace_size",
+    default=0,
+    help="Trace buffer size",
+)
+opts = p.parse_args(sys.argv[1:])
+
+if opts.device == "npu":
+    dev = NPU1Col1()
+elif opts.device == "npu2":
+    dev = NPU2Col1()
+else:
+    raise ValueError("[ERROR] Device name {} is unknown".format(opts.device))
+
+rows = int(opts.rows)
+cols = int(opts.cols)
+trace_size = int(opts.trace_size)
+
+print(rmsnorm(dev, rows, cols, trace_size))
