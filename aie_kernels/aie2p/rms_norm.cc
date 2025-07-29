9a258aba8cab781f6d24ef287c17bced740c640a
pvasireddy-amd <pvasired@amd.com>
2025-07-29 11:29:21 -0600

    name change rms_norm.cc

--- aie_kernels/aie2p/rms_norm.cc
+++ aie_kernels/aie2p/rms_norm.cc
@@ -1,4 +1,4 @@
-//===- layernorm.cc -------------------------------------------*- C++ -*-===//
+//===- rmsnorm.cc -------------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
