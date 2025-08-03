f811bb91a3cd59cee5e359b839734d3aa470798b
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-08-03 15:57:55 -0700

    Use `XFAIL`

--- test/npu-xrt/dma_task_large_linear/aie2.py
+++ test/npu-xrt/dma_task_large_linear/aie2.py
@@ -6,7 +6,7 @@
 # (c) Copyright 2024 AMD Inc.
 
 # REQUIRES: ryzen_ai_npu1, peano
-# UNSUPPORTED: xchesscc_segmentation_fault
+# XFAIL: xchesscc_segmentation_fault
 #
 # RUN: %python %S/aie2.py > ./aie2.mlir
 # RUN: %python aiecc.py --no-aiesim --no-xchesscc --aie-generate-npu-insts --aie-generate-xclbin --no-compile-host --xclbin-name=final.xclbin --npu-insts-name=insts.bin ./aie2.mlir
