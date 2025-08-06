cbeca7f8ab041e097213d7bfb013c509740f331d
pvasireddy-amd <pvasired@amd.com>
2025-08-06 11:42:10 -0600

    Update rms_norm.cc

--- aie_kernels/aie2p/rms_norm.cc
+++ aie_kernels/aie2p/rms_norm.cc
@@ -69,43 +69,8 @@ void rms_norm(const T *restrict input, T *restrict output, int32_t cols) {
   event1();
 }
 
-template <typename T, int N>
-void rms_norm2(const T *restrict input, T *restrict output, int32_t rows,
-               int32_t cols) {
-  event0();
-  constexpr float epsilon = 1e-5f;
-  const float gamma = 1.0f;
-  ::aie::vector<T, N> gamma_v = ::aie::broadcast<T, N>(gamma);
-
-  for (int r = 0; r < rows; r++) {
-    T final_sum_sq = 0.0f;
-    ::aie::vector<T, N> add_res = ::aie::zeros<T, N>();
-    for (int i = 0; i < cols; i = i + N) {
-      ::aie::vector<T, N> reg_a = ::aie::load_v<N>(input + r * cols + i);
-      ::aie::vector<T, N> square_v = ::aie::mul(reg_a, reg_a);
-      add_res = ::aie::add(square_v, add_res);
-    }
-    // Reduce the final_sum_sq across all lanes
-    for (int i = 0; i < N; i++)
-      final_sum_sq = final_sum_sq + add_res[i];
-    T inv_rms = aie::invsqrt(final_sum_sq / cols + epsilon);
-    // Normalize the input and apply gamma
-    ::aie::vector<T, N> inv_rms_v = ::aie::broadcast<T, N>(inv_rms);
-    for (int i = 0; i < cols; i = i + N) {
-      ::aie::vector<T, N> reg_a = ::aie::load_v<N>(input + r * cols + i);
-      ::aie::vector<T, N> norm_v = ::aie::mul(reg_a, inv_rms_v);
-      ::aie::vector<T, N> out_v = ::aie::mul(norm_v, gamma_v);
-      ::aie::store_v(output + r * cols + i, out_v);
-    }
-  }
-  event1();
-}
-
 extern "C" {
 void rms_norm(bfloat16 *input, bfloat16 *output, int32_t cols) {
   rms_norm<bfloat16, 16>(input, output, cols);
-}
-void rms_norm2(bfloat16 *input, bfloat16 *output, int32_t rows, int32_t cols) {
-  rms_norm2<bfloat16, 16>(input, output, rows, cols);
 }
 }
