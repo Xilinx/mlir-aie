98ffc2b1488327e044a2976689ac3d7f2b709d20
Joseph Melber <jmelber@xilinx.com>
2025-08-06 17:09:28 -0600

    Formatting kernels

--- aie_kernels/aie2/swiglu.cc
+++ aie_kernels/aie2/swiglu.cc
@@ -16,10 +16,10 @@
 using namespace aie;
 
 void swiglu_tanh_approx_bf16(bfloat16 *restrict input_vector,
-                           bfloat16 *restrict weight_vector_1,
-                           bfloat16 *restrict weight_vector_2,
-                           bfloat16 *restrict output_vector,
-                           const int32_t vector_size) {
+                             bfloat16 *restrict weight_vector_1,
+                             bfloat16 *restrict weight_vector_2,
+                             bfloat16 *restrict output_vector,
+                             const int32_t vector_size) {
   event0();
 
   int num_elems = vector_size;
@@ -44,11 +44,9 @@ void swiglu_tanh_approx_bf16(bfloat16 *restrict input_vector,
     weight_2 = *it_wt_2++;
 
     // Compute the first part of the SWiGLU: x * w1
-    aie::vector<bfloat16, 16> mul_input_weight_1 = 
-        aie::mul(input, weight_1);
+    aie::vector<bfloat16, 16> mul_input_weight_1 = aie::mul(input, weight_1);
     // Compute the second part of the SWiGLU: x * w2
-    aie::vector<bfloat16, 16> mul_input_weight_2 = 
-        aie::mul(input, weight_2);
+    aie::vector<bfloat16, 16> mul_input_weight_2 = aie::mul(input, weight_2);
 
     // Compute tanh approximation
     auto half_x = aie::mul(mul_input_weight_2, register_0_5);
@@ -57,7 +55,7 @@ void swiglu_tanh_approx_bf16(bfloat16 *restrict input_vector,
     aie::vector<bfloat16, 16> sigmoid_approx =
         aie::mul(tanh_half_x_approx, register_0_5);
     // Compute output: x * tanh_approx
-    aie::vector<bfloat16, 16> silu_output = 
+    aie::vector<bfloat16, 16> silu_output =
         aie::mul(mul_input_weight_2, sigmoid_approx);
 
     // Multiply the first part with the second part
@@ -74,7 +72,8 @@ void swiglu_tanh_approx_bf16(bfloat16 *restrict input_vector,
 
 extern "C" {
 
-void swiglu_bf16(bfloat16 *restrict input, bfloat16 *restrict weights_1, bfloat16 *restrict weights_2, bfloat16 *restrict output) {
+void swiglu_bf16(bfloat16 *restrict input, bfloat16 *restrict weights_1,
+                 bfloat16 *restrict weights_2, bfloat16 *restrict output) {
   int32_t input_size = 1024; // Assuming input size is a multiple of 16
   swiglu_tanh_approx_bf16(input, weights_1, weights_2, output, input_size);
 }
