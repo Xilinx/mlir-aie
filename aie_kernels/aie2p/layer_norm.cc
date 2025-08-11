b215205e3ddc64c4e36c08c4520de7e5ff121401
pvasireddy-amd <pvasired@amd.com>
2025-08-11 16:13:19 -0600

    Update layer_norm.cc

--- aie_kernels/aie2p/layer_norm.cc
+++ aie_kernels/aie2p/layer_norm.cc
@@ -1,7 +1,7 @@
 //===- layernorm.cc -------------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
- // See https://llvm.org/LICENSE.txt for license information.
+// See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 // Copyright (C) 2025, Advanced Micro Devices, Inc.
