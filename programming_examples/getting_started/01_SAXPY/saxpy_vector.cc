6127b16f3b2a646ae31076ea9ce846a94e06b97a
Andra Bisca <andra.bisca@gmail.com>
2025-08-15 18:24:39 +0200

    Update programming_examples/getting_started/01_SAXPY/saxpy_vector.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- programming_examples/getting_started/01_SAXPY/saxpy_vector.cc
+++ programming_examples/getting_started/01_SAXPY/saxpy_vector.cc
@@ -10,21 +10,21 @@
 #include <aie_api/aie.hpp>
 
 extern "C" {
-   void saxpy(bfloat16* restrict x, bfloat16* restrict y, bfloat16* restrict z) {
-       event0();
-       ::aie::vector<bfloat16, 64> a_v = ::aie::broadcast<bfloat16, 64>(3f);
-       #pragma clang loop min_iteration_count(4)
-       for (int i = 0; i < 4096; i += 64) {
-           ::aie::vector<bfloat16, 64> x_v = ::aie::load_v<64>(x);
-           x += 64;
-           ::aie::vector<bfloat16, 64> y_v = ::aie::load_v<64>(y);
-           y += 64;
-           ::aie::accum<accfloat, 64> ax_v = ::aie::mul(x_v, a_v);
-           ::aie::vector<bfloat16, 64> ax_v_converted = ax_v.to_vector<bfloat16>();
-           ::aie::vector<bfloat16, 64> z_v = ::aie::add(ax_v_converted, y_v);
-           ::aie::store_v(z, z_v);
-           z += 64;
-       }
-       event1();
-   }
+void saxpy(bfloat16 *restrict x, bfloat16 *restrict y, bfloat16 *restrict z) {
+  event0();
+  ::aie::vector<bfloat16, 64> a_v = ::aie::broadcast<bfloat16, 64>(3f);
+#pragma clang loop min_iteration_count(4)
+  for (int i = 0; i < 4096; i += 64) {
+    ::aie::vector<bfloat16, 64> x_v = ::aie::load_v<64>(x);
+    x += 64;
+    ::aie::vector<bfloat16, 64> y_v = ::aie::load_v<64>(y);
+    y += 64;
+    ::aie::accum<accfloat, 64> ax_v = ::aie::mul(x_v, a_v);
+    ::aie::vector<bfloat16, 64> ax_v_converted = ax_v.to_vector<bfloat16>();
+    ::aie::vector<bfloat16, 64> z_v = ::aie::add(ax_v_converted, y_v);
+    ::aie::store_v(z, z_v);
+    z += 64;
+  }
+  event1();
+}
 }
