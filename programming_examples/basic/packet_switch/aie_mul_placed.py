e0b4217be85d42dc2278745b204456ebd8ada420
AndraBisca <andrab@amd.com>
2025-08-15 09:26:31 -0600

    Typo

--- programming_examples/basic/packet_switch/aie_mul_placed.py
+++ programming_examples/basic/packet_switch/aie_mul_placed.py
@@ -1,4 +1,4 @@
-# packet_switch/aie_add_mul_placed.py -*- Python -*-
+# packet_switch/aie_mul_placed.py -*- Python -*-
 #
 # This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
