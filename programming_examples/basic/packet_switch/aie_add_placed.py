a018f10853c58600ae55b7c705141604fcb7a0e8
AndraBisca <andrab@amd.com>
2025-08-15 09:36:54 -0600

    Python format

--- programming_examples/basic/packet_switch/aie_add_placed.py
+++ programming_examples/basic/packet_switch/aie_add_placed.py
@@ -43,15 +43,13 @@ def packet_switch_kernel(dev, in_out_size):
         MemTile_0_1 = tile(0, 1)
         CT_0_2 = tile(0, 2)
         CT_0_3 = tile(0, 3)
-        
+
         # Data allocations and synchronization with aie.buffer() and aie.lock()
         # (TODO: use objectfifo once it also supports packet_flow)
-        # aie.buffer() and aie.lock() is the underhood implementation of aie.objectfifo, which can be 
+        # aie.buffer() and aie.lock() is the underhood implementation of aie.objectfifo, which can be
         # verified by running "aie-opt -aie-objectFifo-stateful-transform aie_add.mlir"
         # core_0_2
-        core02_buff_in = buffer(
-            tile=CT_0_2, datatype=vector_ty, name="core02_buff_in"
-        )
+        core02_buff_in = buffer(tile=CT_0_2, datatype=vector_ty, name="core02_buff_in")
         core02_prod_lock_in = lock(
             tile=CT_0_2, lock_id=0, init=1, sym_name="core02_prod_lock_in"
         )
@@ -69,9 +67,7 @@ def packet_switch_kernel(dev, in_out_size):
         )
 
         # core_0_3
-        core03_buff_in = buffer(
-            tile=CT_0_3, datatype=vector_ty, name="core03_buff_in"
-        )
+        core03_buff_in = buffer(tile=CT_0_3, datatype=vector_ty, name="core03_buff_in")
         core03_prod_lock_in = aie.lock(
             CT_0_3, lock_id=0, init=1, sym_name="core03_prod_lock_in"
         )
@@ -181,12 +177,8 @@ def packet_switch_kernel(dev, in_out_size):
         def core_body():
             for _ in range_(sys.maxsize):
                 # Acquire locks to read core02_buff_in and write core02_buff_out
-                use_lock(
-                    core02_cons_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
-                use_lock(
-                    core02_prod_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core02_cons_lock_in, LockAction.AcquireGreaterEqual, value=1)
+                use_lock(core02_prod_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 add_func(core02_buff_in, core02_buff_out)
                 # Release locks to write core02_buff_in and read core02_buff_out
                 use_lock(core02_prod_lock_in, LockAction.Release, value=1)
@@ -198,9 +190,7 @@ def packet_switch_kernel(dev, in_out_size):
             s0 = dma_start(DMAChannelDir.S2MM, 0, dest=block[1], chain=block[2])
             with block[1]:
                 # Write data from stream to core02_buff_in
-                use_lock(
-                    core02_prod_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core02_prod_lock_in, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(core02_buff_in)
                 use_lock(core02_cons_lock_in, LockAction.Release, value=1)
                 next_bd(block[1])
@@ -209,9 +199,7 @@ def packet_switch_kernel(dev, in_out_size):
             with block[3]:
                 # Write data from core02_buff_out + packet header to stream
                 # Data is then routed to memtile_0_1
-                use_lock(
-                    core02_cons_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core02_cons_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(core02_buff_out, packet=(0, 4))
                 use_lock(core02_prod_lock_out, LockAction.Release, value=1)
                 next_bd(block[3])
@@ -223,12 +211,8 @@ def packet_switch_kernel(dev, in_out_size):
         def core_body():
             for _ in range_(sys.maxsize):
                 # Acquire locks to read core03_buff_in and write core03_buff_out
-                use_lock(
-                    core03_cons_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
-                use_lock(
-                    core03_prod_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core03_cons_lock_in, LockAction.AcquireGreaterEqual, value=1)
+                use_lock(core03_prod_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 mult_func(core03_buff_in, core03_buff_out)
                 # Release locks to write core03_buff_in and read core03_buff_out
                 use_lock(core03_prod_lock_in, LockAction.Release, value=1)
@@ -240,9 +224,7 @@ def packet_switch_kernel(dev, in_out_size):
             s0 = dma_start(DMAChannelDir.S2MM, 0, dest=block[1], chain=block[2])
             with block[1]:
                 # Write data from stream to core03_buff_in
-                use_lock(
-                    core03_prod_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core03_prod_lock_in, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(core03_buff_in)
                 use_lock(core03_cons_lock_in, LockAction.Release, value=1)
                 next_bd(block[1])
@@ -251,25 +233,23 @@ def packet_switch_kernel(dev, in_out_size):
             with block[3]:
                 # Write data from core03_buff_out + packet header to stream
                 # Data is then routed to memtile_0_1
-                use_lock(
-                    core03_cons_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(core03_cons_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(core03_buff_out, packet=(0, 6))
                 use_lock(core03_prod_lock_out, LockAction.Release, value=1)
                 next_bd(block[3])
             with block[4]:
                 EndOp()
 
-         # memtile_0_1 DMA logic
+        # memtile_0_1 DMA logic
         @memtile_dma(MemTile_0_1)
         def m(block):
             s0 = dma_start(DMAChannelDir.S2MM, 0, dest=block[1], chain=block[2])
             with block[1]:
                 # Write data from stream to mem01_buff_in
-                use_lock(
-                    mem01_prod_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
-                dma_bd(mem01_buff_in)  # First 4 byte is the packet header becasue in packet_flow(pkt_id=0) configured "keep_pkt_header"
+                use_lock(mem01_prod_lock_in, LockAction.AcquireGreaterEqual, value=1)
+                dma_bd(
+                    mem01_buff_in
+                )  # First 4 byte is the packet header becasue in packet_flow(pkt_id=0) configured "keep_pkt_header"
                 use_lock(mem01_cons_lock_in, LockAction.Release, value=1)
                 next_bd(block[1])
             with block[2]:
@@ -279,9 +259,7 @@ def packet_switch_kernel(dev, in_out_size):
                 # Sends the message to corresponding ComputeTile core. This works because the packet header from
                 # Shimtile to Memtile is saved in the buffer and the packet_flow from Memtile to ComputeTile uses
                 # the same packet_id (0 or 1).
-                use_lock(
-                    mem01_cons_lock_in, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(mem01_cons_lock_in, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(mem01_buff_in)
                 use_lock(mem01_prod_lock_in, LockAction.Release, value=1)
                 next_bd(block[3])
@@ -289,9 +267,7 @@ def packet_switch_kernel(dev, in_out_size):
                 s2 = dma_start(DMAChannelDir.S2MM, 2, dest=block[5], chain=block[6])
             with block[5]:
                 # Write data from stream to mem01_buff_out
-                use_lock(
-                    mem01_prod_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(mem01_prod_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(mem01_buff_out)
                 use_lock(mem01_cons_lock_out, LockAction.Release, value=1)
                 next_bd(block[5])
@@ -300,9 +276,7 @@ def packet_switch_kernel(dev, in_out_size):
             with block[7]:
                 # Write data from mem01_buff_out + packet header to stream
                 # Data is then routed to shimtile_0_0
-                use_lock(
-                    mem01_cons_lock_out, LockAction.AcquireGreaterEqual, value=1
-                )
+                use_lock(mem01_cons_lock_out, LockAction.AcquireGreaterEqual, value=1)
                 dma_bd(mem01_buff_out, packet=(0, 2))
                 use_lock(mem01_prod_lock_out, LockAction.Release, value=1)
                 next_bd(block[7])
@@ -323,11 +297,13 @@ def packet_switch_kernel(dev, in_out_size):
                         offset=0,
                         sizes=[1, 1, 1, in_out_size],
                         strides=[0, 0, 0, 1],
-                        packet=(0, 0)
+                        packet=(0, 0),
                     )
                     EndOp()
             # Write data from stream to host buffer B
-            out_task = dma_configure_task(ShimTile_0_0, DMAChannelDir.S2MM, 0, issue_token=True)
+            out_task = dma_configure_task(
+                ShimTile_0_0, DMAChannelDir.S2MM, 0, issue_token=True
+            )
             with bds(out_task) as bd:
                 with bd[0]:
                     shim_dma_bd(
