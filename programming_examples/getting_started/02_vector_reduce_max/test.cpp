9ec67ab4453300aa045b4ae479ecadf8cbf7e469
Pranathi Vasireddy <pvasired@amd.com>
2025-08-19 10:03:37 -0600

    makefile changes

--- programming_examples/getting_started/02_vector_reduce_max/test.cpp
+++ programming_examples/getting_started/02_vector_reduce_max/test.cpp
@@ -1,9 +1,12 @@
+//===- test.cpp -------------------------------------------------*- C++ -*-===//
+//
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2025 AMD Inc.
-
+// Copyright (C) 2025, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
 #include "xrt_test_wrapper.h"
 #include <cstdint>
 #include <stdfloat>
