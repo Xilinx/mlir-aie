d16caca81167b7e3f79eebad7488c25951d3f722
Andra Bisca <andra.bisca@gmail.com>
2025-08-20 15:51:15 +0200

    Update programming_examples/getting_started/01_SAXPY/saxpy_scalar_baseline.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- programming_examples/getting_started/01_SAXPY/saxpy_scalar_baseline.cc
+++ programming_examples/getting_started/01_SAXPY/saxpy_scalar_baseline.cc
@@ -10,12 +10,12 @@
 #include <aie_api/aie.hpp>
 
 extern "C" {
-   void saxpy(bfloat16* x, bfloat16* y, bfloat16* z) {
-       event0();
-       bfloat16 a = 3f;
-       for (int i = 0; i < 4096; ++i) {
-           z[i] = a * x[i] + y[i];
-       }
-       event1();
-   }
+void saxpy(bfloat16 *x, bfloat16 *y, bfloat16 *z) {
+  event0();
+  bfloat16 a = 3f;
+  for (int i = 0; i < 4096; ++i) {
+    z[i] = a * x[i] + y[i];
+  }
+  event1();
+}
 }
