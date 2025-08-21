57a3c81938a6a1e9859022ba1d9fd3f2b54f3ec2
Jack Lo <jack.lo@amd.com>
2025-08-21 09:31:55 -0600

    format fix

--- aie_kernels/aie2p/conv2dk14.h
+++ aie_kernels/aie2p/conv2dk14.h
@@ -14,10 +14,10 @@
 
 extern "C" {
 
-void conv2dk14_i8(int8_t *input, int8_t *kernels, int8_t *output, 
-                const int32_t input_width, const int32_t input_channels,
-                const int32_t output_channels, 
-                const int32_t kernel_width, const int scale);
+void conv2dk14_i8(int8_t *input, int8_t *kernels, int8_t *output,
+                  const int32_t input_width, const int32_t input_channels,
+                  const int32_t output_channels, const int32_t kernel_width,
+                  const int scale);
 
 // void conv2dk14_i8(int8_t *line0, int8_t *line1, int8_t *line2, int8_t *wts,
 //                  uint8_t *output, const int32_t input_width,
@@ -26,12 +26,13 @@ void conv2dk14_i8(int8_t *input, int8_t *kernels, int8_t *output,
 //                  const int32_t check, const int scale,
 //                  const int channel_offset);
 
-// void conv2dk14_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t *wts,
+// void conv2dk14_ui8(uint8_t *line0, uint8_t *line1, uint8_t *line2, int8_t
+// *wts,
 //                   uint8_t *output, const int32_t input_width,
-//                   const int32_t input_channels, const int32_t output_channels,
-//                   const int32_t kernel_width, const int32_t kernel_height,
-//                   const int32_t check, const int scale,
-//                   const int channel_offset);
+//                   const int32_t input_channels, const int32_t
+//                   output_channels, const int32_t kernel_width, const int32_t
+//                   kernel_height, const int32_t check, const int scale, const
+//                   int channel_offset);
 
 } // extern "C"
 
