fa17c13d6a116173277a383390c5aac121d3c9f3
erwei-xilinx <erwei.wang@amd.com>
2025-08-21 18:14:38 -0700

    Set the default aie arch to aie2, if no aie.device op is found

--- lib/Dialect/AIEVec/Utils/Utils.cpp
+++ lib/Dialect/AIEVec/Utils/Utils.cpp
@@ -111,7 +111,7 @@ std::optional<xilinx::AIE::AIEArch> getAIEVersionFromModule(ModuleOp moduleOp) {
   moduleOp.walk([&](xilinx::AIE::DeviceOp d) { deviceOps.push_back(d); });
 
   if (deviceOps.empty())
-    return std::nullopt;
+    return AIE::AIEArch::AIE2;
 
   const auto &targetModel = deviceOps.front().getTargetModel();
   return targetModel.getTargetArch();
