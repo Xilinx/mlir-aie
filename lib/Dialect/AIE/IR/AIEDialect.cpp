d5f64a3fc3192502c4099225ed9cc630f900dbfd
Andra Bisca <andra.bisca@gmail.com>
2025-08-22 17:06:29 +0200

    Update lib/Dialect/AIE/IR/AIEDialect.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/Dialect/AIE/IR/AIEDialect.cpp
+++ lib/Dialect/AIE/IR/AIEDialect.cpp
@@ -593,9 +593,9 @@ LogicalResult ObjectFifoAllocateOp::verify() {
   if (objFifo.getRepeatCount().has_value())
     return emitError("cannot allocate a shared memory module to objectfifo "
                      "with set `repeat_count` attribute");
-  if (!(objFifo.getDimensionsToStream().empty()
-      && objFifo.getDimensionsFromStreamPerConsumer().empty()
-      && objFifo.getPadDimensions().empty()))
+  if (!(objFifo.getDimensionsToStream().empty() &&
+        objFifo.getDimensionsFromStreamPerConsumer().empty() &&
+        objFifo.getPadDimensions().empty()))
     return emitError("cannot allocate a shared memory module to objectfifo "
                      "with set dimensions attributes");
 }
