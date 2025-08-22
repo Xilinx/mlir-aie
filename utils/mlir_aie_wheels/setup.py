100775af1a3197f07bc8a3d4d5a7c82c4e8d2067
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2025-08-22 13:12:00 -0400

    Trying suffix in different place

--- utils/mlir_aie_wheels/setup.py
+++ utils/mlir_aie_wheels/setup.py
@@ -260,7 +260,7 @@ def get_version():
         "DATETIME", f"{now.year}{now.month:02}{now.day:02}{now.hour:02}"
     )
     suffix = "" if check_env("ENABLE_RTTI", 1) else ".no_rtti"
-    return f"{release_version}{suffix}.{timestamp}+{commit_hash}"
+    return f"{release_version}.{timestamp}{suffix}+{commit_hash}"
 
 
 MLIR_AIE_SOURCE_DIR = Path(
