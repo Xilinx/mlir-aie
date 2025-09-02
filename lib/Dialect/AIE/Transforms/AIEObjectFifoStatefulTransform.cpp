b612e626832650d2f7f6b5bc86206c1f780966b8
AndraBisca <andrab@amd.com>
2025-09-02 08:25:45 -0600

    Test

--- lib/Dialect/AIE/Transforms/AIEObjectFifoStatefulTransform.cpp
+++ lib/Dialect/AIE/Transforms/AIEObjectFifoStatefulTransform.cpp
@@ -1897,7 +1897,7 @@ struct AIEObjectFifoStatefulTransformPass
     // Only the objectFifos we split above require DMA communication; the others
     // rely on shared memory and share the same buffers.
     
-    int packetID = getStartPacketID(device);
+    //int packetID = getStartPacketID(device);
     for (auto &[producer, consumers] : splitFifos) {
       int producerChanIndex = fifo_dma_channel_index[producer];
       if (producerChanIndex == -1)
