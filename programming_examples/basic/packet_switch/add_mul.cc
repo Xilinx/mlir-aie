d10d27dd658b4fc530e45bf60a5cda98c0c0ce19
Jeff Fifield <jeff.fifield@amd.com>
2025-09-03 12:55:19 -0600

    Run clang-format (#2553)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- programming_examples/basic/packet_switch/add_mul.cc
+++ programming_examples/basic/packet_switch/add_mul.cc
@@ -17,42 +17,42 @@
 
 template <typename T, int W = 128>
 void mul_aie(T *restrict in0, T *restrict out) {
-    event0();
-    const int vec_factor = 16;
+  event0();
+  const int vec_factor = 16;
 
-    aie::vector<T, vec_factor> In0;
-    aie::accum<acc32, vec_factor> Out;
+  aie::vector<T, vec_factor> In0;
+  aie::accum<acc32, vec_factor> Out;
 
-    const int F = W / vec_factor;
-    for (int i = 0; i < F; i++)
-        chess_prepare_for_pipelining chess_loop_range(6, ) { 
-            In0 = aie::load_v<vec_factor>(in0);
-            Out = aie::mul(In0, (T)2);
-            aie::store_v(out, Out.template to_vector<T>());
-            in0 += vec_factor;
-            out += vec_factor;
-        }
-    event1();
+  const int F = W / vec_factor;
+  for (int i = 0; i < F; i++)
+    chess_prepare_for_pipelining chess_loop_range(6, ) {
+      In0 = aie::load_v<vec_factor>(in0);
+      Out = aie::mul(In0, (T)2);
+      aie::store_v(out, Out.template to_vector<T>());
+      in0 += vec_factor;
+      out += vec_factor;
+    }
+  event1();
 }
 
 template <typename T, int W = 128>
 void add_aie(T *restrict in0, T *restrict out) {
-    event0();
-    const int vec_factor = 16;
+  event0();
+  const int vec_factor = 16;
 
-    aie::vector<T, vec_factor> In0;
-    aie::vector<T, vec_factor> Out;
+  aie::vector<T, vec_factor> In0;
+  aie::vector<T, vec_factor> Out;
 
-    const int F = W / vec_factor;
-    for (int i = 0; i < F; i++)
-        chess_prepare_for_pipelining chess_loop_range(6, ) { 
-            In0 = aie::load_v<vec_factor>(in0);
-            Out = aie::add(In0, (T)2);
-            aie::store_v(out, Out);
-            in0 += vec_factor;
-            out += vec_factor;
-        }
-    event1();
+  const int F = W / vec_factor;
+  for (int i = 0; i < F; i++)
+    chess_prepare_for_pipelining chess_loop_range(6, ) {
+      In0 = aie::load_v<vec_factor>(in0);
+      Out = aie::add(In0, (T)2);
+      aie::store_v(out, Out);
+      in0 += vec_factor;
+      out += vec_factor;
+    }
+  event1();
 }
 
 extern "C" {
@@ -61,17 +61,7 @@ extern "C" {
 #define DIM_S 256
 #endif
 
-void add(
-        int8 *restrict In0,
-        int8 *restrict y
-) {
-  add_aie<int8, DIM_S>(In0, y);
-}
+void add(int8 *restrict In0, int8 *restrict y) { add_aie<int8, DIM_S>(In0, y); }
 
-void mul(
-        int8 *restrict In0,
-        int8 *restrict y
-) {
-  mul_aie<int8, DIM_S>(In0, y);
-}
+void mul(int8 *restrict In0, int8 *restrict y) { mul_aie<int8, DIM_S>(In0, y); }
 } // extern "C"
