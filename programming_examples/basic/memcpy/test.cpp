6f4f9fc737251ee3a156b8c6e1ae296d15dc221d
Joseph Melber <jmelber@xilinx.com>
2025-09-04 16:05:43 -0600

    Don't touch

--- programming_examples/basic/memcpy/test.cpp
+++ programming_examples/basic/memcpy/test.cpp
@@ -1,8 +1,12 @@
+//===- test.cpp -------------------------------------------000---*- C++ -*-===//
+//
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 // Copyright (C) 2025, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
 
 #include "cxxopts.hpp"
 #include <cstdint>
