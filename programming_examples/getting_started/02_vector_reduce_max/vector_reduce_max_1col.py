b738baf2c2f5dc9d0e0ade4ce32dc33cbb9deb36
AndraBisca <andrab@amd.com>
2025-09-04 10:26:59 -0600

    Remove non-JIT files

--- programming_examples/getting_started/02_vector_reduce_max/vector_reduce_max_1col.py
+++ programming_examples/getting_started/02_vector_reduce_max/vector_reduce_max_1col.py
@@ -4,183 +4,233 @@
 #
 # (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
 
+from ml_dtypes import bfloat16
 import numpy as np
-import argparse
 import sys
 
-from aie.iron import (
-    Kernel,
-    ObjectFifo,
-    Program,
-    Runtime,
-    Worker,
-    LocalBuffer,
-)
+import aie.iron as iron
+from aie.iron import ExternalFunction, jit
+from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker, LocalBuffer
 from aie.iron.placers import SequentialPlacer
-from aie.iron.device import NPU1Col1, NPU2Col1
-from aie.helpers.util import np_ndarray_type_get_shape
-from ml_dtypes import bfloat16
 from aie.iron.controlflow import range_
-
-# --------------------------------------------------------------------------
-# Configuration
-# --------------------------------------------------------------------------
-
-devices = {"npu": NPU1Col1(), "npu2": NPU2Col1()}
-if len(sys.argv) != 2 or sys.argv[1] not in devices:
-    print(f"Usage {sys.argv[0]} <{'|'.join(devices.keys())}>")
-    sys.exit(1)
-device = devices[sys.argv[1]]
-
-in1_size = 524288
-out_size = 4
-dtype = bfloat16
-
-n_cores = 4
-n_mem_elems = 2048
-elems_per_core = n_mem_elems // n_cores
-
-in_tensor_size = in1_size // dtype(0).nbytes
-out_tensor_size = out_size // dtype(0).nbytes
-
-num_iter = in_tensor_size // n_mem_elems
-
-assert out_size == 4, "Output buffer must be size 4 (4 bytes = 1 integer)."
+from aie.helpers.taplib import TensorAccessPattern, TensorTiler2D
+from aie.helpers.util import np_ndarray_type_get_shape
+from aie.helpers.dialects.ext.scf import if_, else_
 
 
-# --------------------------------------------------------------------------
-# In-Array Data Movement
-# --------------------------------------------------------------------------
+# JIT decorator for IRON
+# Decorator to compile an IRON kernel into a binary to run on the NPU.
+# Parameters:
+#     - is_placed (bool): Whether the kernel is using explicit or deferred placement API. Defaults to True.
+#     - use_cache (bool): Use cached MLIR module if available. Defaults to True.
+@iron.jit(is_placed=False)
+def vector_reduce_max(input0, output):
+    N = 4096  # Tensor size
+    element_type = output.dtype
 
-in_ty = np.ndarray[(in_tensor_size,), np.dtype[dtype]]
-mem_ty = np.ndarray[(n_mem_elems,), np.dtype[dtype]]
-op_ty = np.ndarray[(elems_per_core,), np.dtype[dtype]]
-out_ty = np.ndarray[(out_tensor_size,), np.dtype[dtype]]
+    in_size = 524288
+    out_size = 4
 
-# Input A and Output C
-of_in = ObjectFifo(mem_ty, name="of_in")
+    n_cores = 4
+    n_mem_elems = 2048
+    elems_per_core = n_mem_elems // n_cores
 
-in_fifos = []
-out_fifos = []
+    in_tensor_size = in_size // element_type(0).nbytes
+    out_tensor_size = out_size // element_type(0).nbytes
 
-if n_cores > 1:
-    of_a_offsets = [
-        (np.prod(np_ndarray_type_get_shape(mem_ty)) // n_cores) * i
-        for i in range(n_cores)
-    ]
-else:
-    of_a_offsets = [0]
+    num_iter = in_tensor_size // n_mem_elems
 
-in_fifos = of_in.cons().split(
-    of_a_offsets,
-    obj_types=[op_ty] * n_cores,
-    names=[f"memA{i}" for i in range(n_cores)],
-)
-for i in range(n_cores):
-    out_fifos.append(ObjectFifo(out_ty, name=f"memC{i}"))
+    assert out_size == 4, "Output buffer must be size 4 (4 bytes = 1 integer)."
 
+    # --------------------------------------------------------------------------
+    # In-Array Data Movement
+    # --------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------
-# Task each core will run
-# --------------------------------------------------------------------------
+    in_ty = np.ndarray[(in_tensor_size,), np.dtype[element_type]]
+    mem_ty = np.ndarray[(n_mem_elems,), np.dtype[element_type]]
+    op_ty = np.ndarray[(elems_per_core,), np.dtype[element_type]]
+    out_ty = np.ndarray[(out_tensor_size,), np.dtype[element_type]]
 
-reduce_max_vector = Kernel(
-    f"reduce_max_vector_bfloat16", "vector_reduce_max.o", [op_ty, out_ty, np.int32]
-)
-compute_max = Kernel(
-    f"compute_max_bfloat16", "vector_reduce_max.o", [out_ty, out_ty, out_ty]
-)
-min_val = np.array([bfloat16(float("-inf"))], dtype=dtype)
+    # Input A and Output C
+    of_in = ObjectFifo(mem_ty, name="of_in")
 
+    in_fifos = []
+    out_fifos = []
 
-def start_core_body(of_in, of_out, reduce_max_vector, compute_max):
-    nextC_buffer = LocalBuffer(
-        type=np.ndarray[(out_tensor_size,), np.dtype[dtype]],
-        initial_value=min_val,
-    )
-    tmp_buffer = LocalBuffer(
-        type=np.ndarray[(out_tensor_size,), np.dtype[dtype]],
-        initial_value=min_val,
-    )
-    elem_out = of_out.acquire(1)
-    for _ in range_(num_iter):
-        elem_in = of_in.acquire(1)
-        reduce_max_vector(elem_in, tmp_buffer, elems_per_core)
-        nextC_buffer[0] = max(nextC_buffer[0], tmp_buffer[0])
-        of_in.release(1)
-    elem_out[0] = nextC_buffer[0]
-    of_out.release(1)
-
-
-def core_body(of_in, of_out, in0, reduce_max_vector, compute_max):
-    nextC_buffer = LocalBuffer(
-        type=np.ndarray[(out_tensor_size,), np.dtype[dtype]],
-        initial_value=min_val,
-    )
-    tmp_buffer = LocalBuffer(
-        type=np.ndarray[(out_tensor_size,), np.dtype[dtype]],
-        initial_value=min_val,
-    )
-
-    for _ in range_(num_iter):
-        elem_in = of_in.acquire(1)
-        reduce_max_vector(elem_in, tmp_buffer, elems_per_core)
-        nextC_buffer[0] = max(nextC_buffer[0], tmp_buffer[0])
-        of_in.release(1)
-
-    elem_out = of_out.acquire(1)
-    elem_in1 = in0.acquire(1)
-    elem_out[0] = max(nextC_buffer[0], elem_in1[0])
-    in0.release(1)
-    of_out.release(1)
-
-
-workers = []
-for i in range(n_cores):
-    if i != n_cores - 1:
-        workers.append(
-            Worker(
-                core_body,
-                fn_args=[
-                    in_fifos[i].cons(),
-                    out_fifos[i].prod(),
-                    out_fifos[i + 1].cons(),
-                    reduce_max_vector,
-                    compute_max,
-                ],
-                trace=None,
-            )
-        )
+    if n_cores > 1:
+        of_a_offsets = [
+            (np.prod(np_ndarray_type_get_shape(mem_ty)) // n_cores) * i
+            for i in range(n_cores)
+        ]
     else:
-        workers.append(
-            Worker(
-                start_core_body,
-                fn_args=[
-                    in_fifos[i].cons(),
-                    out_fifos[i].prod(),
-                    reduce_max_vector,
-                    compute_max,
-                ],
-                trace=None,
-            )
+        of_a_offsets = [0]
+
+    in_fifos = of_in.cons().split(
+        of_a_offsets,
+        obj_types=[op_ty] * n_cores,
+        names=[f"memA{i}" for i in range(n_cores)],
+    )
+    for i in range(n_cores):
+        out_fifos.append(ObjectFifo(out_ty, name=f"memC{i}"))
+
+    # --------------------------------------------------------------------------
+    # Task each core will run
+    # --------------------------------------------------------------------------
+
+    reduce_max_vector = ExternalFunction(
+        f"reduce_max_vector_bfloat16",
+        source_file="vector_reduce_max.cc",
+        arg_types=[op_ty, out_ty, np.int32],
+    )
+    # compute_max = ExternalFunction(
+    #     f"compute_max_bfloat16",
+    #     source_file="vector_reduce_max.cc",
+    #     arg_types=[out_ty, out_ty, out_ty],
+    # )
+    min_val = np.array([bfloat16(float("-inf"))], dtype=element_type)
+
+    def start_core_body(of_in, of_out, reduce_max_vector):
+        nextC_buffer = LocalBuffer(
+            type=np.ndarray[(out_tensor_size,), np.dtype[element_type]],
+            initial_value=min_val,
+        )
+        tmp_buffer = LocalBuffer(
+            type=np.ndarray[(out_tensor_size,), np.dtype[element_type]],
+            initial_value=min_val,
+        )
+        elem_out = of_out.acquire(1)
+        for _ in range_(num_iter):
+            elem_in = of_in.acquire(1)
+            reduce_max_vector(elem_in, tmp_buffer, elems_per_core)
+            #compute_max(nextC_buffer, tmp_buffer, nextC_buffer)
+            with if_(nextC_buffer[0] < tmp_buffer[0]) as if_op:
+                nextC_buffer[0] = tmp_buffer[0]
+            of_in.release(1)
+        elem_out[0] = nextC_buffer[0]
+        of_out.release(1)
+
+    def core_body(of_in, of_out, in0, reduce_max_vector):
+        nextC_buffer = LocalBuffer(
+            type=np.ndarray[(out_tensor_size,), np.dtype[element_type]],
+            initial_value=min_val,
+        )
+        tmp_buffer = LocalBuffer(
+            type=np.ndarray[(out_tensor_size,), np.dtype[element_type]],
+            initial_value=min_val,
         )
 
+        for _ in range_(num_iter):
+            elem_in = of_in.acquire(1)
+            reduce_max_vector(elem_in, tmp_buffer, elems_per_core)
+            #compute_max(nextC_buffer, tmp_buffer, nextC_buffer)
+            #*out = (*in1 > *in2) ? *in1 : *in2;
+            with if_(nextC_buffer[0] < tmp_buffer[0]) as if_op:
+                nextC_buffer[0] = tmp_buffer[0]
+            of_in.release(1)
 
-# --------------------------------------------------------------------------
-# DRAM-NPU data movement and work dispatch
-# --------------------------------------------------------------------------
+        elem_out = of_out.acquire(1)
+        elem_in1 = in0.acquire(1)
+        #compute_max(elem_in1, nextC_buffer, elem_out)
+        with if_(elem_in1[0] > nextC_buffer[0]) as if_op:
+            elem_out[0] = elem_in1[0]
+        with else_(if_op):
+            elem_out[0] = nextC_buffer[0]
+        in0.release(1)
+        of_out.release(1)
 
-rt = Runtime()
-with rt.sequence(in_ty, out_ty) as (a_in, c_out):
-    rt.start(*workers)
-    rt.fill(of_in.prod(), a_in)
-    rt.drain(out_fifos[0].cons(), c_out, wait=True)
+    workers = []
+    for i in range(n_cores):
+        if i != n_cores - 1:
+            workers.append(
+                Worker(
+                    core_body,
+                    fn_args=[
+                        in_fifos[i].cons(),
+                        out_fifos[i].prod(),
+                        out_fifos[i + 1].cons(),
+                        reduce_max_vector,
+                    ],
+                    trace=None,
+                )
+            )
+        else:
+            workers.append(
+                Worker(
+                    start_core_body,
+                    fn_args=[
+                        in_fifos[i].cons(),
+                        out_fifos[i].prod(),
+                        reduce_max_vector,
+                    ],
+                    trace=None,
+                )
+            )
+
+    # --------------------------------------------------------------------------
+    # DRAM-NPU data movement and work dispatch
+    # --------------------------------------------------------------------------
+
+    rt = Runtime()
+    with rt.sequence(in_ty, out_ty) as (a_in, c_out):
+        rt.start(*workers)
+        rt.fill(of_in.prod(), a_in)
+        rt.drain(out_fifos[0].cons(), c_out, wait=True)
+
+    # --------------------------------------------------------------------------
+    # Place and generate MLIR program
+    # --------------------------------------------------------------------------
+
+    my_program = Program(iron.get_current_device(), rt)
+    return my_program.resolve_program(SequentialPlacer())
 
 
-# --------------------------------------------------------------------------
-# Place and generate MLIR program
-# --------------------------------------------------------------------------
+def main():
+    # Define tensor shapes and data types
+    in_size = 524288
+    out_size = 4
+    element_type = bfloat16
 
-program = Program(device, rt)
-module = program.resolve_program(SequentialPlacer())
-print(module)
+    in_tensor_size = in_size // element_type(0).nbytes
+    out_tensor_size = out_size // element_type(0).nbytes
+
+    # Construct an input tensor and an output zeroed tensor
+    # The two tensors are in memory accessible to the NPU
+    input0 = iron.arange(in_tensor_size, dtype=element_type, device="npu")
+    output = iron.arange(out_tensor_size, dtype=element_type, device="npu")
+
+    # JIT-compile the kernel then launches the kernel with the given arguments. Future calls
+    # to the kernel will use the same compiled kernel and loaded code objects
+    vector_reduce_max(input0, output)
+
+    # Check the correctness of the result and print
+    ref_max = 0
+    for i in input0:
+        if i > ref_max:
+            ref_max = i
+
+    errors = 0
+    # for index, (actual, ref) in enumerate(
+    #     zip(
+    #         output,
+    #         ref_max,
+    #     )
+    # ):
+    if output[0] != ref_max:
+        print(f"Error: {output} != {ref_max}")
+        errors += 1
+    else:
+        print(f"Correct output: {output} == {ref_max}")
+
+    # If the result is correct, exit with a success code
+    # Otherwise, exit with a failure code
+    if not errors:
+        print("\nPASS!\n")
+        sys.exit(0)
+    else:
+        print("\nError count: ", errors)
+        print("\nfailed.\n")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
--- programming_examples/getting_started/03_matrix_multiplication_single_core/matrix_multiplication_single_core.py
+++ programming_examples/getting_started/03_matrix_multiplication_single_core/matrix_multiplication_single_core.py
@@ -7,142 +7,167 @@
 import numpy as np
 import sys
 
+import aie.iron as iron
+from aie.iron import ExternalFunction, jit
 from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
 from aie.iron.placers import SequentialPlacer
-from aie.iron.device import NPU1, NPU2
 from aie.iron.controlflow import range_
 from aie.helpers.taplib import TensorAccessPattern, TensorTiler2D
 
-# --------------------------------------------------------------------------
-# Configuration
-# --------------------------------------------------------------------------
 
-devices = {"npu": NPU1(), "npu2": NPU2()}
-if len(sys.argv) != 2 or sys.argv[1] not in devices:
-    print(f"Usage {sys.argv[0]} <{'|'.join(devices.keys())}>")
-    sys.exit(1)
-device = devices[sys.argv[1]]
+# JIT decorator for IRON
+# Decorator to compile an IRON kernel into a binary to run on the NPU.
+# Parameters:
+#     - is_placed (bool): Whether the kernel is using explicit or deferred placement API. Defaults to True.
+#     - use_cache (bool): Use cached MLIR module if available. Defaults to True.
+@iron.jit(is_placed=False)
+def matrix_multiplication_single_core(input0, input1, output):
+    M, K, N = 512, 512, 512  # Problem size
+    m, k, n = 64, 64, 64  # Tile size
+    r, s, t = 8, 2, 8  # Intrinsic size
 
-M, K, N = 512, 512, 512  # Problem size
-m, k, n = 64, 64, 64  # Tile size
-r, s, t = 8, 2, 8  # Intrinsic size
+    element_type = output.dtype
 
+    # --------------------------------------------------------------------------
+    # In-Array Data Movement
+    # --------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------
-# In-Array Data Movement
-# --------------------------------------------------------------------------
+    A_ty = np.ndarray[(M, K), np.dtype[element_type]]
+    B_ty = np.ndarray[(K, N), np.dtype[element_type]]
+    C_ty = np.ndarray[(M, N), np.dtype[element_type]]
+    a_ty = np.ndarray[(m, k), np.dtype[element_type]]
+    b_ty = np.ndarray[(k, n), np.dtype[element_type]]
+    c_ty = np.ndarray[(m, n), np.dtype[element_type]]
 
-A_ty = np.ndarray[(M, K), np.dtype[np.int16]]
-B_ty = np.ndarray[(K, N), np.dtype[np.int16]]
-C_ty = np.ndarray[(M, N), np.dtype[np.int16]]
-a_ty = np.ndarray[(m, k), np.dtype[np.int16]]
-b_ty = np.ndarray[(k, n), np.dtype[np.int16]]
-c_ty = np.ndarray[(m, n), np.dtype[np.int16]]
+    # The following ObjectFIFOs route m*k-, k*n-, and m*n-sized subtiles
+    # (objects) to/from the compute cores via mem tiles, rearranging their data
+    # into r*s-, s*t-, and r*t-sized sub-subtiles.
 
-# The following ObjectFIFOs route m*k-, k*n-, and m*n-sized subtiles
-# (objects) to/from the compute cores via mem tiles, rearranging their data
-# into r*s-, s*t-, and r*t-sized sub-subtiles.
+    fifo_A_L3L2 = ObjectFifo(a_ty, name="A_L3L2")
+    tap_A_L2L1 = TensorTiler2D.group_tiler((m, k), (r, s), (m // r, k // s))[0]
+    fifo_A_L2L1 = fifo_A_L3L2.cons().forward(
+        dims_to_stream=tap_A_L2L1.transformation_dims, 
+        name="A_L2L1"
+    )
 
-fifo_A_L3L2 = ObjectFifo(a_ty, name="A_L3L2")
-tap_A_L2L1 = TensorTiler2D.group_tiler((m, k), (r, s), (m // r, k // s))[0]
-fifo_A_L2L1 = fifo_A_L3L2.cons().forward(
-    dims_to_stream=tap_A_L2L1.transformation_dims, name="A_L2L1"
-)
+    fifo_B_L3L2 = ObjectFifo(b_ty, name="B_L3L2")
+    tap_B_L2L1 = TensorTiler2D.group_tiler((k, n), (s, t), (k // s, n // t))[0]
+    fifo_B_L2L1 = fifo_B_L3L2.cons().forward(
+        dims_to_stream=tap_B_L2L1.transformation_dims, 
+        name="B_L2L1"
+    )
 
-fifo_B_L3L2 = ObjectFifo(b_ty, name="B_L3L2")
-tap_B_L2L1 = TensorTiler2D.group_tiler((k, n), (s, t), (k // s, n // t))[0]
-fifo_B_L2L1 = fifo_B_L3L2.cons().forward(
-    dims_to_stream=tap_B_L2L1.transformation_dims, name="B_L2L1"
-)
+    fifo_C_L1L2 = ObjectFifo(c_ty, name="C_L1L2")
+    tap_C_L1L2 = TensorAccessPattern(
+        tensor_dims=(m, n),
+        offset=0,
+        sizes=[m // r, r, n // t, t],
+        strides=[r * n, t, r * t, 1]
+    )
+    fifo_C_L2L3 = fifo_C_L1L2.cons().forward(
+        dims_to_stream=tap_C_L1L2.transformation_dims, 
+        name="C_L2L3"
+    )
 
-fifo_C_L1L2 = ObjectFifo(c_ty, name="C_L1L2")
-tap_C_L1L2 = TensorAccessPattern(
-    tensor_dims=(m, n),
-    offset=0,
-    sizes=[m // r, r, n // t, t],
-    strides=[r * n, t, r * t, 1],
-)
-fifo_C_L2L3 = fifo_C_L1L2.cons().forward(
-    dims_to_stream=tap_C_L1L2.transformation_dims, name="C_L2L3"
-)
+    # --------------------------------------------------------------------------
+    # Task each core will run
+    # --------------------------------------------------------------------------
 
+    # The kernel repeatedly acquires one subtile of A and B, multiplies them,
+    # and accumulates the result on top of C. As these tiles come in, the DMAs
+    # will have rearranged them into r*s-, s*t-, and r*t-sized subtiles, which
+    # the computation kernel relies on.
 
-# --------------------------------------------------------------------------
-# Task each core will run
-# --------------------------------------------------------------------------
-
-# The kernel repeatedly acquires one subtile of A and B, multiplies them,
-# and accumulates the result on top of C. As these tiles come in, the DMAs
-# will have rearranged them into r*s-, s*t-, and r*t-sized subtiles, which
-# the computation kernel relies on.
-
-zero_kernel = Kernel("zero", f"matrix_multiplication.o", [c_ty])
-matmul_kernel = Kernel(
-    "matrix_multiplication", f"matrix_multiplication.o", [a_ty, b_ty, c_ty]
-)
-
-
-def core_fn(of_a, of_b, of_c, zero, matmul):
-    for _ in range_(M // m * N // n):
-        elem_out = of_c.acquire(1)
-        zero(elem_out)
-        for _ in range_(K // k):
-            elem_in_a = of_a.acquire(1)
-            elem_in_b = of_b.acquire(1)
-            matmul(elem_in_a, elem_in_b, elem_out)
-            of_a.release(1)
-            of_b.release(1)
-        of_c.release(1)
-
-
-worker = Worker(
-    core_fn,
-    [
-        fifo_A_L2L1.cons(),
-        fifo_B_L2L1.cons(),
-        fifo_C_L1L2.prod(),
-        zero_kernel,
-        matmul_kernel,
-    ],
-)
+    # TODO: use when multiple external functions are supported with JIT
+    # zero_kernel = ExternalFunction(
+    #     "zero", source_file="matrix_multiplication.cc", arg_types=[c_ty],
+    # )
+    matmul_kernel = ExternalFunction(
+        "matrix_multiplication",
+        source_file="matrix_multiplication.cc",
+        arg_types=[a_ty, b_ty, c_ty],
+    )
+    def core_fn(of_a, of_b, of_c, matmul):
+        for _ in range_(M // m * N // n):
+            elem_out = of_c.acquire(1)
+            #zero(elem_out)
+            for i in range_(m):
+                for j in range_(n):
+                    elem_out[i, j] = 0
+            for _ in range_(K // k):
+                elem_in_a = of_a.acquire(1)
+                elem_in_b = of_b.acquire(1)
+                matmul(elem_in_a, elem_in_b, elem_out)
+                of_a.release(1)
+                of_b.release(1)
+            of_c.release(1)
+    worker = Worker(core_fn, [fifo_A_L2L1.cons(), fifo_B_L2L1.cons(), fifo_C_L1L2.prod(), matmul_kernel])
 
 
-# --------------------------------------------------------------------------
-# DRAM-NPU data movement and work dispatch
-# --------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
+    # DRAM-NPU data movement and work dispatch
+    # --------------------------------------------------------------------------
 
-# The data movement patterns from DRAM divide the input matrices (sizes
-# M*K, K*N) into m*k- and k*n-sized subtiles and produce output into C in
-# m*n-sized subtiles. Each single "task group" encompasses all data
-# movement required for a single row of the output matrix.
+    # The data movement patterns from DRAM divide the input matrices (sizes 
+    # M*K, K*N) into m*k- and k*n-sized subtiles and produce output into C in
+    # m*n-sized subtiles. Each single "task group" encompasses all data
+    # movement required for a single row of the output matrix.
 
-a_taps = TensorTiler2D.group_tiler((M, K), (m, k), (1, K // k), pattern_repeat=(N // n))
-b_tap = TensorTiler2D.group_tiler(
-    (K, N), (k, n), (K // k, N // n), tile_group_col_major=True
-)[0]
-c_taps = TensorTiler2D.group_tiler((M, N), (m, n), (1, N // n))
+    a_taps = TensorTiler2D.group_tiler((M, K), (m, k), (1, K // k), pattern_repeat=(N // n))
+    b_tap = TensorTiler2D.group_tiler((K, N), (k, n), (K // k, N // n), tile_group_col_major=True)[0]
+    c_taps = TensorTiler2D.group_tiler((M, N), (m, n), (1, N // n))
 
-rt = Runtime()
-with rt.sequence(A_ty, B_ty, C_ty) as (A, B, C):
-    rt.start(worker)
-    for tile_row in range(M // m):
-        task_group = rt.task_group()
-        rt.fill(fifo_A_L3L2.prod(), A, tap=a_taps[tile_row], task_group=task_group)
-        rt.fill(fifo_B_L3L2.prod(), B, tap=b_tap, task_group=task_group)
-        rt.drain(
-            fifo_C_L2L3.cons(),
-            C,
-            tap=c_taps[tile_row],
-            task_group=task_group,
-            wait=True,
-        )
-        rt.finish_task_group(task_group)
+    rt = Runtime()
+    with rt.sequence(A_ty, B_ty, C_ty) as (A, B, C):
+        rt.start(worker)
+        for tile_row in range(M // m):
+            task_group = rt.task_group()
+            rt.fill(fifo_A_L3L2.prod(), A, tap=a_taps[tile_row], task_group=task_group)
+            rt.fill(fifo_B_L3L2.prod(), B, tap=b_tap, task_group=task_group)
+            rt.drain(fifo_C_L2L3.cons(), C, tap=c_taps[tile_row], task_group=task_group, wait=True)
+            rt.finish_task_group(task_group)
+
+    # --------------------------------------------------------------------------
+    # Place and generate MLIR program
+    # --------------------------------------------------------------------------
+
+    my_program = Program(iron.get_current_device(), rt)
+    return my_program.resolve_program(SequentialPlacer())
 
 
-# --------------------------------------------------------------------------
-# Place and generate MLIR program
-# --------------------------------------------------------------------------
+def main():
+    # Define tensor shapes and data types
+    M, K, N = 512, 512, 512
+    element_type = np.int16
 
-program = Program(device, rt)
-module = program.resolve_program(SequentialPlacer())
-print(module)
+    # Construct an input tensors and an output zeroed tensor
+    # The two tensors are in memory accessible to the NPU
+    input0 = iron.randint(0, 256, (M, K), dtype=element_type, device="npu")
+    input1 = iron.randint(0, 256, (K, N), dtype=element_type, device="npu")
+    output = iron.zeros(M*N, dtype=element_type, device="npu")
+
+    # Generate reference pattern
+    #ref_vec = iron.zeros_like(output)
+    ref_vec = np.matmul(input0.numpy(), input1.numpy())
+
+    # JIT-compile the kernel then launches the kernel with the given arguments. Future calls
+    # to the kernel will use the same compiled kernel and loaded code objects
+    matrix_multiplication_single_core(input0, input1, output)
+
+    # Check the correctness of the result
+    e = np.equal(ref_vec.flatten(), output.numpy())
+    errors = np.size(e) - np.count_nonzero(e)
+
+    # If the result is correct, exit with a success code
+    # Otherwise, exit with a failure code
+    if not errors:
+        print("\nPASS!\n")
+        sys.exit(0)
+    else:
+        print("\nError count: ", errors)
+        print("\nfailed.\n")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
