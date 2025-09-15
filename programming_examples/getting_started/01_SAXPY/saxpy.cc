8f029f90e0283c0cce94deceda59bfa8e4c588a2
AndraBisca <andrab@amd.com>
2025-09-15 12:49:57 -0600

    Merge saxpy kernels

--- programming_examples/getting_started/01_SAXPY/saxpy.cc
+++ programming_examples/getting_started/01_SAXPY/saxpy.cc
@@ -27,4 +27,13 @@ void saxpy(bfloat16 *restrict x, bfloat16 *restrict y, bfloat16 *restrict z) {
   }
   event1();
 }
+
+void saxpy_scalar(bfloat16 *x, bfloat16 *y, bfloat16 *z) {
+  event0();
+  float a = 3.f;
+  for (int i = 0; i < 4096; ++i) {
+    z[i] = a * x[i] + y[i];
+  }
+  event1();
+}
 }
