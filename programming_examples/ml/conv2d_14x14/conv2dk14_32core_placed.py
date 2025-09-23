7b9a46746ac4d03af8600c52791887c84edd1f9b
Jack Lo <36210336+jackl-xilinx@users.noreply.github.com>
2025-09-23 15:38:19 -0700

    Add unplaced IRON design for conv2d 14x14 (#2601)

--- programming_examples/ml/conv2d_14x14/conv2dk14_32core_placed.py
+++ programming_examples/ml/conv2d_14x14/conv2dk14_32core_placed.py
@@ -268,8 +268,8 @@ def conv2dk14(
                         tiles_to_trace=tiles_to_trace,
                         shim=shim_tiles[0],
                         trace_size=trace_size,
-                        trace_offset=N_in_bytes,
-                        ddr_id=2,
+                        # trace_offset=N_in_bytes,
+                        # ddr_id=2,
                         coretile_events=[
                             CoreEvent.INSTR_EVENT_0,
                             CoreEvent.INSTR_EVENT_1,
