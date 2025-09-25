aa359aacd9a8c09073d19f446b00760090ec028d
AndraBisca <andrab@amd.com>
2025-09-25 09:43:45 -0600

    Only copy header files to build path

--- aie_kernels/aiekernels.cmake
+++ aie_kernels/aiekernels.cmake
@@ -1,12 +1,15 @@
 function(add_aie_kernels_headers SRCPATH BUILDPATH INSTALLPATH)
-  message("Installing aie_kernels includes from ${SRCPATH} in ${BUILDPATH}")
-
-  # copy header files into build area
-  install(DIRECTORY ${SRCPATH}/ DESTINATION ${BUILDPATH}/aie_kernels)
-
   message("Installing aie_kernels includes from ${SRCPATH} in ${INSTALLPATH}")
-
-  # install area too
+  
+  # copy header files into install area
   install(DIRECTORY ${SRCPATH}/ DESTINATION ${INSTALLPATH}/aie_kernels)
+  
+  # copy header files from install area to build area for testing
+  add_custom_command(
+    OUTPUT ${BUILDPATH}/aie_kernels
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${INSTALLPATH}/aie_kernels ${BUILDPATH}/aie_kernels
+    DEPENDS ${INSTALLPATH}/aie_kernels
+    COMMENT "Copying aie_kernels includes from ${INSTALLPATH} to ${BUILDPATH}"
+  )
 
 endfunction()
