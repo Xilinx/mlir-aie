df53117ccffa9f927e3d3c947b6b7950bcbce7f6
AndraBisca <andrab@amd.com>
2025-10-01 12:44:54 -0600

    Copy header files from src path to build path

--- aie_kernels/aiekernels.cmake
+++ aie_kernels/aiekernels.cmake
@@ -3,13 +3,10 @@ function(add_aie_kernels_headers SRCPATH BUILDPATH INSTALLPATH)
   
   # copy header files into install area
   install(DIRECTORY ${SRCPATH}/ DESTINATION ${INSTALLPATH}/aie_kernels)
+
+  message("Copying aie_kernels includes from ${SRCPATH} to ${BUILDPATH}")
   
   # copy header files from install area to build area for testing
-  add_custom_command(
-    OUTPUT ${BUILDPATH}/aie_kernels
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${INSTALLPATH}/aie_kernels ${BUILDPATH}/aie_kernels
-    DEPENDS ${INSTALLPATH}/aie_kernels
-    COMMENT "Copying aie_kernels includes from ${INSTALLPATH} to ${BUILDPATH}"
-  )
+  file(COPY ${SRCPATH} DESTINATION ${BUILDPATH}/aie_kernels)
 
 endfunction()
--- runtime_lib/aie_api/aieapi.cmake
+++ runtime_lib/aie_api/aieapi.cmake
@@ -5,12 +5,9 @@ function(add_aie_api_headers SRCPATH BUILDPATH INSTALLPATH)
   # copy header files into install area
   install(DIRECTORY ${SRCPATH}/ DESTINATION ${INSTALLPATH}/aie_api)
 
+  message("Copying aie_api includes from ${SRCPATH} to ${BUILDPATH}")
+
   # copy header files from install area to build area for testing
-  add_custom_command(
-    OUTPUT ${BUILDPATH}/aie_api
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${INSTALLPATH}/aie_api ${BUILDPATH}/aie_api
-    DEPENDS ${INSTALLPATH}/aie_api
-    COMMENT "Copying aie_api includes from ${INSTALLPATH} to ${BUILDPATH}"
-  )
+  file(COPY ${SRCPATH} DESTINATION ${BUILDPATH}/aie_api)
 
 endfunction()
