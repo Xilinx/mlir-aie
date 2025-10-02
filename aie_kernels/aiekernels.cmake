2004cb4ece5a11d404d80b457a8052959ffcf4c9
AndraBisca <andrab@amd.com>
2025-10-02 10:08:50 -0600

    Use file glob_recurse to iterate over aie_kernels and aie_api directory structures

--- aie_kernels/aiekernels.cmake
+++ aie_kernels/aiekernels.cmake
@@ -1,12 +1,27 @@
 function(add_aie_kernels_headers SRCPATH BUILDPATH INSTALLPATH)
   message("Installing aie_kernels includes from ${SRCPATH} in ${INSTALLPATH}")
   
-  # copy header files into install area
+  # copy aie_kernels header files into install area
   install(DIRECTORY ${SRCPATH}/ DESTINATION ${INSTALLPATH}/aie_kernels)
 
-  message("Copying aie_kernels includes from ${SRCPATH} to ${BUILDPATH}")
+  message("Copying aie_kernels includes from ${SRCPATH} to ${BUILDPATH}/aie_kernels")
   
-  # copy header files from install area to build area for testing
-  file(COPY ${SRCPATH} DESTINATION ${BUILDPATH}/aie_kernels)
+  # copy aie_kernels header files into build area
+  file(GLOB_RECURSE headers_to_copy "${SRCPATH}/*.h" "${SRCPATH}/*.hpp")
+  foreach(header ${headers_to_copy})
+      file(RELATIVE_PATH rel_path ${SRCPATH} ${header})
+
+      # create target name from file's path to avoid duplication
+      get_filename_component(file_name "${header}" NAME)
+      get_filename_component(parent_dir ${header} DIRECTORY)
+      get_filename_component(module_name ${parent_dir} NAME)
+
+      set(dest ${BUILDPATH}/aie_kernels/${rel_path})
+      add_custom_target(aie-copy-runtime-libs-${module_name}-${file_name} ALL DEPENDS ${dest})
+      add_custom_command(OUTPUT ${dest}
+                      COMMAND ${CMAKE_COMMAND} -E copy ${header} ${dest}
+                      DEPENDS ${header}
+      )
+  endforeach()
 
 endfunction()
