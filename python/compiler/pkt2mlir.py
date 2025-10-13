e38fc468935e90e77a43e64b04c274f4f0518eab
Jeff Fifield <jeff.fifield@amd.com>
2025-10-13 17:01:34 -0600

    install pkt2mlir

--- python/compiler/pkt2mlir.py
+++ python/compiler/pkt2mlir.py
@@ -4,7 +4,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
 
 from aie.compiler.pkt2mlir.main import main
 
