fa13a7f78eee18c139132459aa7dbdcab6cbc3ff
erweiw <erwei.wang@amd.com>
2025-11-07 13:15:14 -0800

    Fixup file header format

--- lib/Dialect/AIE/Transforms/AIEVectorTransferLowering.cpp
+++ lib/Dialect/AIE/Transforms/AIEVectorTransferLowering.cpp
@@ -1,5 +1,4 @@
-//===- AIEVectorTransferLowering.cpp ------------------------------*- C++
-//-*-===//
+//===- AIEVectorTransferLowering.cpp -----------------------------*- C++-*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
