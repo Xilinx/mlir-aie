c5d83be359f5617832638df46b302e32511239df
Jeff Fifield <jeff.fifield@amd.com>
2025-11-08 12:13:16 -0700

    Commit 5: Implement AIEInlineTraceConfigPass
    - Implemented AIEInlineTraceConfig.cpp:
      - Finds all aie.trace.start_config operations
      - Looks up referenced trace.config symbol
      - Clones all aie.trace.reg operations to call site
      - Removes trace.start_config invocation
    - Relaxed parent constraint on aie.trace.reg to allow DeviceOp parent
      (needed for inlined reg ops)
    - Added test_inline_trace_config.mlir
    - Test passes: trace.reg ops successfully inlined

--- lib/Dialect/AIE/Transforms/AIEInlineTraceConfig.cpp
+++ lib/Dialect/AIE/Transforms/AIEInlineTraceConfig.cpp
@@ -22,8 +22,47 @@ using namespace xilinx::AIE;
 
 struct AIEInlineTraceConfigPass : AIEInlineTraceConfigBase<AIEInlineTraceConfigPass> {
   void runOnOperation() override {
-    // Stub implementation for now
-    // Will be implemented in next commit
+    DeviceOp device = getOperation();
+    
+    // Collect all trace.start_config operations
+    SmallVector<TraceStartConfigOp> startConfigs;
+    device.walk([&](TraceStartConfigOp startConfig) {
+      startConfigs.push_back(startConfig);
+    });
+    
+    for (auto startConfig : startConfigs) {
+      OpBuilder builder(startConfig);
+      
+      // Lookup the trace config symbol
+      auto configSymbolName = startConfig.getTraceConfig();
+      auto configOp = dyn_cast_or_null<TraceConfigOp>(
+        SymbolTable::lookupNearestSymbolFrom(device, 
+          builder.getStringAttr(configSymbolName))
+      );
+      
+      if (!configOp) {
+        startConfig.emitError("trace config symbol '")
+          << configSymbolName << "' not found";
+        return signalPassFailure();
+      }
+      
+      // Clone all register write ops from config to call site
+      for (auto &op : configOp.getBody().getOps()) {
+        if (auto regOp = dyn_cast<TraceRegOp>(op)) {
+          // Create new reg op at call site
+          builder.create<TraceRegOp>(
+            regOp.getLoc(),
+            regOp.getRegNameAttr(),
+            regOp.getFieldAttr(),
+            regOp.getValueAttr(),
+            regOp.getCommentAttr()
+          );
+        }
+      }
+      
+      // Remove the start_config invocation
+      startConfig.erase();
+    }
   }
 };
 
