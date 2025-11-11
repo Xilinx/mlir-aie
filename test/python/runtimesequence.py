d68e9a7b91f0933d1d155376042bb1c26eec2997
Erika Hunhoff <erika.hunhoff@amd.com>
2025-11-10 17:13:42 -0700

    Add one more test

--- test/python/runtimesequence.py
+++ test/python/runtimesequence.py
@@ -211,3 +211,41 @@ def rt_default_mixed_sequence(module):
 
     module = Program(NPU2(), rt).resolve_program(SequentialPlacer())
     return module
+
+
+# CHECK-LABEL: TEST: rt_two_task_group_sequence
+# CHECK: aiex.dma_start_task(%0)
+# CHECK: aiex.dma_start_task(%1)
+# CHECK: aiex.dma_start_task(%2)
+# CHECK: aiex.dma_await_task(%0)
+# CHECK: aiex.dma_free_task(%1)
+# CHECK: aiex.dma_free_task(%2)
+@construct_and_print_module
+def rt_two_task_group_sequence(module):
+    n = 1024
+
+    n_ty = np.ndarray[(n,), np.dtype[np.int32]]
+
+    of_0 = ObjectFifo(n_ty, name="of0")
+    of_1 = ObjectFifo(n_ty, name="of1")
+    of_2 = ObjectFifo(n_ty, name="iof2")
+
+    def core_fn(of_0, of_1, of_2):
+        pass
+
+    worker = Worker(core_fn, [of_0.cons(), of_1.cons(), of_2.prod()])
+
+    rt = Runtime()
+    with rt.sequence(n_ty, n_ty, n_ty) as (A, B, C):
+        rt.start(worker)
+
+        tg = rt.task_group()
+        tg2 = rt.task_group()
+        rt.drain(of_2.cons(), C, wait=True, task_group=tg)
+        rt.fill(of_0.prod(), A, task_group=tg)
+        rt.fill(of_1.prod(), B, task_group=tg2)
+        rt.finish_task_group(tg)
+        rt.finish_task_group(tg2)
+
+    module = Program(NPU2(), rt).resolve_program(SequentialPlacer())
+    return module
