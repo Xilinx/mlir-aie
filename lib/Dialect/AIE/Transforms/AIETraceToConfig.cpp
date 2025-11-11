ce025ae4098416413d3e6466cadb77578ad2ef98
Jeff Fifield <jeff.fifield@amd.com>
2025-11-11 09:49:01 -0700

    format

--- lib/Dialect/AIE/Transforms/AIETraceToConfig.cpp
+++ lib/Dialect/AIE/Transforms/AIETraceToConfig.cpp
@@ -24,32 +24,29 @@ struct AIETraceToConfigPass : AIETraceToConfigBase<AIETraceToConfigPass> {
   void runOnOperation() override {
     DeviceOp device = getOperation();
     OpBuilder builder(device);
-    
+
     // Collect all trace operations
     SmallVector<TraceOp> traces;
     device.walk([&](TraceOp trace) { traces.push_back(trace); });
-    
+
     for (auto trace : traces) {
       // Create config symbol name
       std::string configName = (trace.getSymName().str() + "_config");
       auto tile = trace.getTile();
-      
+
       // Insert trace.config after trace declaration
       builder.setInsertionPointAfter(trace);
       auto configOp = builder.create<TraceConfigOp>(
-        trace.getLoc(),
-        tile,
-        builder.getStringAttr(configName)
-      );
-      
+          trace.getLoc(), tile, builder.getStringAttr(configName));
+
       // Build register writes inside config body
-      Block* configBody = new Block();
+      Block *configBody = new Block();
       configOp.getBody().push_back(configBody);
       OpBuilder configBuilder = OpBuilder::atBlockEnd(configBody);
-      
+
       // For prototype: emit simplified register writes
       // We'll emit placeholder values that will be refined later
-      
+
       // 1. Emit Trace_Control0 fields
       // Check for start/stop events
       for (auto &op : trace.getBody().getOps()) {
@@ -59,67 +56,59 @@ struct AIETraceToConfigPass : AIETraceToConfigBase<AIETraceToConfigPass> {
             startEvent = *startOp.getBroadcast();
           } else if (startOp.getEvent()) {
             // For prototype, use placeholder value
-            startEvent = 1;  // TRUE event
+            startEvent = 1; // TRUE event
           }
-          
+
           configBuilder.create<TraceRegOp>(
-            trace.getLoc(),
-            builder.getStringAttr("Trace_Control0"),
-            builder.getStringAttr("Trace_Start_Event"),
-            builder.getI32IntegerAttr(startEvent),
-            builder.getStringAttr("start event")
-          );
+              trace.getLoc(), builder.getStringAttr("Trace_Control0"),
+              builder.getStringAttr("Trace_Start_Event"),
+              builder.getI32IntegerAttr(startEvent),
+              builder.getStringAttr("start event"));
         }
-        
+
         if (auto stopOp = dyn_cast<TraceStopEventOp>(op)) {
           uint32_t stopEvent = 0;
           if (stopOp.getBroadcast()) {
             stopEvent = *stopOp.getBroadcast();
           } else if (stopOp.getEvent()) {
             // For prototype, use placeholder value
-            stopEvent = 0;  // NONE event
+            stopEvent = 0; // NONE event
           }
-          
+
           configBuilder.create<TraceRegOp>(
-            trace.getLoc(),
-            builder.getStringAttr("Trace_Control0"),
-            builder.getStringAttr("Trace_Stop_Event"),
-            builder.getI32IntegerAttr(stopEvent),
-            builder.getStringAttr("stop event")
-          );
+              trace.getLoc(), builder.getStringAttr("Trace_Control0"),
+              builder.getStringAttr("Trace_Stop_Event"),
+              builder.getI32IntegerAttr(stopEvent),
+              builder.getStringAttr("stop event"));
         }
-        
+
         // Emit mode if present
         if (auto modeOp = dyn_cast<TraceModeOp>(op)) {
           configBuilder.create<TraceRegOp>(
-            trace.getLoc(),
-            builder.getStringAttr("Trace_Control0"),
-            builder.getStringAttr("Mode"),
-            builder.getI32IntegerAttr(static_cast<uint32_t>(modeOp.getMode())),
-            builder.getStringAttr("trace mode")
-          );
+              trace.getLoc(), builder.getStringAttr("Trace_Control0"),
+              builder.getStringAttr("Mode"),
+              builder.getI32IntegerAttr(
+                  static_cast<uint32_t>(modeOp.getMode())),
+              builder.getStringAttr("trace mode"));
         }
-        
+
         // Emit packet config if present
         if (auto packetOp = dyn_cast<TracePacketOp>(op)) {
           configBuilder.create<TraceRegOp>(
-            trace.getLoc(),
-            builder.getStringAttr("Trace_Control1"),
-            builder.getStringAttr("ID"),
-            builder.getI32IntegerAttr(packetOp.getId()),
-            builder.getStringAttr("packet ID")
-          );
-          
+              trace.getLoc(), builder.getStringAttr("Trace_Control1"),
+              builder.getStringAttr("ID"),
+              builder.getI32IntegerAttr(packetOp.getId()),
+              builder.getStringAttr("packet ID"));
+
           configBuilder.create<TraceRegOp>(
-            trace.getLoc(),
-            builder.getStringAttr("Trace_Control1"),
-            builder.getStringAttr("Packet_Type"),
-            builder.getI32IntegerAttr(static_cast<uint32_t>(packetOp.getType())),
-            builder.getStringAttr("packet type")
-          );
+              trace.getLoc(), builder.getStringAttr("Trace_Control1"),
+              builder.getStringAttr("Packet_Type"),
+              builder.getI32IntegerAttr(
+                  static_cast<uint32_t>(packetOp.getType())),
+              builder.getStringAttr("packet type"));
         }
       }
-      
+
       // 2. Emit event slots (Trace_Event0 / Trace_Event1)
       SmallVector<TraceEventOp> events;
       for (auto &op : trace.getBody().getOps()) {
@@ -127,37 +116,33 @@ struct AIETraceToConfigPass : AIETraceToConfigBase<AIETraceToConfigPass> {
           events.push_back(eventOp);
         }
       }
-      
+
       for (size_t i = 0; i < events.size() && i < 8; ++i) {
         auto eventName = events[i].getEvent().getName();
-        
+
         // Determine which register and field
         StringRef registerName = (i < 4) ? "Trace_Event0" : "Trace_Event1";
         std::string fieldName = "Trace_Event" + std::to_string(i % 4);
-        
+
         // For prototype: use event name as string value
         // The next pass will resolve this to event code
         configBuilder.create<TraceRegOp>(
-          trace.getLoc(),
-          builder.getStringAttr(registerName),
-          builder.getStringAttr(fieldName),
-          builder.getStringAttr(eventName),
-          builder.getStringAttr("event slot " + std::to_string(i))
-        );
+            trace.getLoc(), builder.getStringAttr(registerName),
+            builder.getStringAttr(fieldName), builder.getStringAttr(eventName),
+            builder.getStringAttr("event slot " + std::to_string(i)));
       }
-      
+
       // Add terminator
       configBuilder.create<EndOp>(trace.getLoc());
-      
+
       // Update all trace.start_config references
       device.walk([&](TraceStartConfigOp startConfig) {
         if (startConfig.getTraceConfig() == trace.getSymName()) {
           startConfig.setTraceConfigAttr(
-            SymbolRefAttr::get(builder.getContext(), configName)
-          );
+              SymbolRefAttr::get(builder.getContext(), configName));
         }
       });
-      
+
       // Remove original trace op
       trace.erase();
     }
