7cb8fb55b2bd336d6a6ece7f17103aa99da932ab
Erwei Wang <erweiw@xilinx.com>
2025-11-12 15:53:58 -0800

    Add a new pass `llvm-loop-opt` which optimizes the generated llvm ir (#2703)

--- include/aie/Dialect/AIE/Transforms/AIEPasses.h
+++ include/aie/Dialect/AIE/Transforms/AIEPasses.h
@@ -41,6 +41,8 @@ std::unique_ptr<mlir::OperationPass<DeviceOp>>
 createAIEVectorToPointerLoopsPass();
 std::unique_ptr<mlir::OperationPass<DeviceOp>>
 createAIEVectorTransferLoweringPass();
+std::unique_ptr<mlir::OperationPass<mlir::LLVM::LLVMFuncOp>>
+createAIELLVMLoopOptPass();
 std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
 createAIEHoistVectorTransferPointersPass();
 std::unique_ptr<mlir::OperationPass<DeviceOp>> createAIEPathfinderPass();
