c6bd6cfc9c6290e0a5adc535b689fc6e8032fb6a
Jeff Fifield <jeff.fifield@amd.com>
2025-11-11 19:11:11 -0700

    clang-format

--- lib/Dialect/AIE/IR/AIETraceOps.cpp
+++ lib/Dialect/AIE/IR/AIETraceOps.cpp
@@ -37,18 +37,18 @@ LogicalResult TraceOp::verify() {
       eventCount++;
     }
   }
-  
+
   // Check max 8 events
   if (eventCount > 8) {
     return emitOpError("trace unit supports maximum 8 events, got ")
            << eventCount;
   }
-  
+
   // Verify tile operand is a TileOp
   if (!getTile().getDefiningOp<TileOp>()) {
     return emitOpError("tile operand must be a TileOp");
   }
-  
+
   return success();
 }
 
@@ -61,7 +61,7 @@ LogicalResult TraceEventOp::verify() {
   if (getEvent().getName().empty()) {
     return emitOpError("event name cannot be empty");
   }
-  
+
   return success();
 }
 
@@ -76,7 +76,7 @@ LogicalResult TracePacketOp::verify() {
   if (id < 1 || id > 31) {
     return emitOpError("packet ID must be in range [1, 31], got ") << id;
   }
-  
+
   return success();
 }
 
@@ -88,15 +88,15 @@ LogicalResult TraceStartEventOp::verify() {
   // Must have either broadcast or event, but not both
   bool hasBroadcast = getBroadcast().has_value();
   bool hasEvent = getEvent().has_value();
-  
+
   if (!hasBroadcast && !hasEvent) {
     return emitOpError("must specify either broadcast or event");
   }
-  
+
   if (hasBroadcast && hasEvent) {
     return emitOpError("cannot specify both broadcast and event");
   }
-  
+
   return success();
 }
 
@@ -108,15 +108,15 @@ LogicalResult TraceStopEventOp::verify() {
   // Must have either broadcast or event, but not both
   bool hasBroadcast = getBroadcast().has_value();
   bool hasEvent = getEvent().has_value();
-  
+
   if (!hasBroadcast && !hasEvent) {
     return emitOpError("must specify either broadcast or event");
   }
-  
+
   if (hasBroadcast && hasEvent) {
     return emitOpError("cannot specify both broadcast and event");
   }
-  
+
   return success();
 }
 
@@ -131,6 +131,6 @@ LogicalResult TraceStartConfigOp::verify() {
   if (symbolName.empty()) {
     return emitOpError("trace config symbol name cannot be empty");
   }
-  
+
   return success();
 }
