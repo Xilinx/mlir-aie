869973569af66443b342979b7ceb9537c90b1bac
Erika Hunhoff <erika.hunhoff@amd.com>
2025-11-24 11:59:19 -0700

    Remove more unncessary changes

--- python/iron/__init__.py
+++ python/iron/__init__.py
@@ -5,8 +5,23 @@ from .worker import Worker, WorkerRuntimeBarrier
 from .runtime import Runtime
 from .dataflow import ObjectFifo
 from .dtype import str_to_dtype, dtype_to_str
+from .hostruntime.jit import jit
+from .hostruntime.config import (
+    get_current_device,
+    set_current_device,
+    set_iron_tensor_class,
+)
+from .hostruntime.tensor import (
+    tensor,
+    ones,
+    zeros,
+    randint,
+    rand,
+    arange,
+    zeros_like,
+    set_iron_tensor_class,
+)
 from .compile.context import CompileContext, get_compile_arg
 from .compile.compileconfig import compileconfig
 from .run.jit import jit, CallableDesign
 from .compile.compilabledesign import CompilableDesign
-from .config import set_current_device, get_current_device, detect_npu_device, set_iron_tensor_class
