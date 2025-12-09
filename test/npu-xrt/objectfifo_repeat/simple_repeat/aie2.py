6f0240cd19f72bf3a9aea90faf0f34307f62bfcd
Pranathi Vasireddy <pvasired@amd.com>
2025-12-09 14:39:55 -0700

    One example to test

--- test/npu-xrt/objectfifo_repeat/simple_repeat/aie2.py
+++ test/npu-xrt/objectfifo_repeat/simple_repeat/aie2.py
@@ -9,7 +9,7 @@
 # REQUIRES: ryzen_ai_npu1, peano
 #
 # RUN: %python %S/aie2.py 4096 > ./aie2.mlir
-# RUN: %python aiecc.py --no-aiesim --no-xchesscc --aie-generate-npu-insts --aie-generate-xclbin --no-compile-host --xclbin-name=final.xclbin --npu-insts-name=insts.bin ./aie2.mlir
+# RUN: %python3 aiecc.py --no-aiesim --no-xchesscc --aie-generate-npu-insts --aie-generate-xclbin --no-compile-host --xclbin-name=final.xclbin --npu-insts-name=insts.bin ./aie2.mlir
 # RUN: clang %S/test.cpp -o test.exe -std=c++17 -Wall %xrt_flags -lrt -lstdc++ %test_utils_flags
 # RUN: %run_on_npu1% ./test.exe -x final.xclbin -i insts.bin -k MLIR_AIE -l 4096
 import numpy as np
