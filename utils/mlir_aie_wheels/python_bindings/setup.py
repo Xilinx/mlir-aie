e202128a4cd7722ace6f7f896e6f840524be7abb
Erika Hunhoff <erika.hunhoff@amd.com>
2025-12-16 16:17:27 -0700

    Try to fix wheels build

--- utils/mlir_aie_wheels/python_bindings/setup.py
+++ utils/mlir_aie_wheels/python_bindings/setup.py
@@ -223,11 +223,6 @@ setup(
     },
     install_requires=[
         str(ir.requirement)
-        + (
-            "; --config-settings=EUDSL_PYTHON_EXTRAS_HOST_PACKAGE_PREFIX=aie"
-            if ir.requirement.name == "eudsl-python-extras"
-            else ""
-        )
         for ir in parse_requirements("requirements.txt", session="hack")
     ],
 )
