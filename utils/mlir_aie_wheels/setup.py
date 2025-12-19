8ced19ca746eea67f213cc5984d067d143f201f3
Erika Hunhoff <erika.hunhoff@amd.com>
2025-12-19 10:54:46 -0700

    attempt to fix ccache and path errors

--- utils/mlir_aie_wheels/setup.py
+++ utils/mlir_aie_wheels/setup.py
@@ -11,7 +11,7 @@ from textwrap import dedent
 from typing import Union
 
 from importlib_metadata import files
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 from setuptools.command.build_ext import build_ext
 from setuptools.command.develop import develop
 from setuptools.command.install import install
@@ -297,6 +297,7 @@ setup(
         "install": InstallWithPth,
     },
     zip_safe=False,
+    packages=find_packages(exclude=["wheelhouse", "python_bindings", "mlir-aie"]),
     python_requires=">=3.10",
     install_requires=parse_requirements(
         Path(MLIR_AIE_SOURCE_DIR) / "python" / "requirements.txt"
