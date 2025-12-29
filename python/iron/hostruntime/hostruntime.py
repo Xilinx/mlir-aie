21713f6b6e0a3a69a4a5c825fb1110cfe9450a33
Erika Hunhoff <erika.hunhoff@amd.com>
2025-12-29 16:28:19 -0700

    make sure buffers are initially synced to npu for tracing

--- python/iron/hostruntime/hostruntime.py
+++ python/iron/hostruntime/hostruntime.py
@@ -161,18 +161,21 @@ class HostRuntime(ABC):
             if len(args) > 0:
                 out_size += args[-1].nbytes
                 # TODO(erika): should really copy previous contents of output into this buffer...? What if it's in/out?
-                args[-1] = tensor(out_size, dtype=np.uint8)
+                args[-1] = tensor(out_size, dtype=np.uint8, device="npu")
             else:
-                out = tensor(out_size, dtype=np.uint8)
+                out = tensor(out_size, dtype=np.uint8, device="npu")
                 args.append(out)
         else:
             while len(args) < trace_config.DEFAULT_TRACE_BUFFER_INDEX:
                 # TODO out always needed so register buf 7 succeeds (not needed in C/C++ host code)
-                filler = tensor((1,), dtype=np.uint32)
+                filler = tensor((1,), dtype=np.uint32, device="npu")
                 args.append(filler)
 
-            trace_buff = tensor((trace_config.trace_size,), dtype=np.uint8)
+            trace_buff = tensor(
+                (trace_config.trace_size,), dtype=np.uint8, device="npu"
+            )
             args.append(trace_buff)
+        return args
 
     @classmethod
     def extract_trace_from_args(
