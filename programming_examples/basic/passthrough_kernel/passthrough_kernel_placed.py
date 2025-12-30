85bdac4e4d61cd4630e9a12a6183011a236cb9a4
Erika Hunhoff <erika.hunhoff@amd.com>
2025-12-30 14:38:23 -0700

    fix formatting

--- programming_examples/basic/passthrough_kernel/passthrough_kernel_placed.py
+++ programming_examples/basic/passthrough_kernel/passthrough_kernel_placed.py
@@ -1,53 +1,92 @@
-#passthrough_kernel / passthrough_kernel_placed.py - *-Python - *-
+# passthrough_kernel/passthrough_kernel_placed.py -*- Python -*-
 #
-#This file is licensed under the Apache License v2 .0 with LLVM Exceptions.
-#See https: // llvm.org/LICENSE.txt for license information.
-#SPDX - License - Identifier : Apache - 2.0 WITH LLVM - exception
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-#(c) Copyright 2024 Advanced Micro Devices, Inc.or its affiliates
-import numpy as np import argparse import sys
+# (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+import numpy as np
+import argparse
+import sys
 
-    from aie.dialects.aie import * from aie.dialects.aiex import * from aie.extras.context import mlir_mod_ctx from aie.iron.controlflow import range_
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.extras.context import mlir_mod_ctx
+from aie.iron.controlflow import range_
 
-    import aie.utils.trace as trace_utils
+import aie.utils.trace as trace_utils
 
-    def my_passthrough_kernel(dev, in1_size, out_size, trace_size) :in1_dtype = np.uint8 out_dtype = np.uint8
 
-                              N = in1_size         // in1_dtype(0).nbytes
-                              lineWidthInBytes = N // 4  # chop input in 4 sub-tensors
+def my_passthrough_kernel(dev, in1_size, out_size, trace_size):
+    in1_dtype = np.uint8
+    out_dtype = np.uint8
 
-                              assert(out_size == in1_size), "Output buffer size must be equal to input buffer size."
+    N = in1_size // in1_dtype(0).nbytes
+    lineWidthInBytes = N // 4  # chop input in 4 sub-tensors
 
-                                     @device(dev) def device_body() :
-#define types
-                                                                  vector_ty = np.ndarray[(N, ), np.dtype[in1_dtype]] line_ty = np.ndarray[(lineWidthInBytes, ), np.dtype[in1_dtype]]
+    assert (
+        out_size == in1_size
+    ), "Output buffer size must be equal to input buffer size."
 
-#AIE Core Function declarations
-                                                                                                                                           passThroughLine = external_func("passThroughLine", inputs =[line_ty, line_ty, np.int32])
+    @device(dev)
+    def device_body():
+        # define types
+        vector_ty = np.ndarray[(N,), np.dtype[in1_dtype]]
+        line_ty = np.ndarray[(lineWidthInBytes,), np.dtype[in1_dtype]]
 
-#Tile declarations
-                                                                                                                                                                                                           ShimTile = tile(0, 0) ComputeTile2 = tile(0, 2)
+        # AIE Core Function declarations
+        passThroughLine = external_func(
+            "passThroughLine", inputs=[line_ty, line_ty, np.int32]
+        )
 
-#Set up a packet - switched flow from core to shim for tracing information
-                                                                                                                                                                                                                                                         tiles_to_trace =[ComputeTile2, ShimTile] if trace_size> 0 :trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile)
+        # Tile declarations
+        ShimTile = tile(0, 0)
+        ComputeTile2 = tile(0, 2)
 
-#AIE - array data movement with object fifos
-                                                                                                                                                                                                                                                                                                                                                                  of_in = object_fifo("in", ShimTile, ComputeTile2, 2, line_ty) of_out = object_fifo("out", ComputeTile2, ShimTile, 2, line_ty)
+        # Set up a packet-switched flow from core to shim for tracing information
+        tiles_to_trace = [ComputeTile2, ShimTile]
+        if trace_size > 0:
+            trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile)
 
-#Set up compute tiles
+        # AIE-array data movement with object fifos
+        of_in = object_fifo("in", ShimTile, ComputeTile2, 2, line_ty)
+        of_out = object_fifo("out", ComputeTile2, ShimTile, 2, line_ty)
 
-#Compute tile 2
-                                                                                                                                                                                                                                                                                                                                                                                                                                                         @core(ComputeTile2, "passThrough.cc.o") def core_body() : for _ in range_(sys.maxsize) :elemOut = of_out.acquire(ObjectFifoPort.Produce, 1) elemIn = of_in.acquire(ObjectFifoPort.Consume, 1) passThroughLine(elemIn, elemOut, lineWidthInBytes) of_in.release(ObjectFifoPort.Consume, 1) of_out.release(ObjectFifoPort.Produce, 1)
+        # Set up compute tiles
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      @runtime_sequence(vector_ty, vector_ty) def sequence(inTensor, outTensor) : if trace_size> 0 :trace_utils.configure_packet_tracing_aie2(tiles_to_trace = tiles_to_trace, shim = ShimTile, trace_size = trace_size, )
+        # Compute tile 2
+        @core(ComputeTile2, "passThrough.cc.o")
+        def core_body():
+            for _ in range_(sys.maxsize):
+                elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                elemIn = of_in.acquire(ObjectFifoPort.Consume, 1)
+                passThroughLine(elemIn, elemOut, lineWidthInBytes)
+                of_in.release(ObjectFifoPort.Consume, 1)
+                of_out.release(ObjectFifoPort.Produce, 1)
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  in_task = shim_dma_single_bd_task(of_in, inTensor, sizes =[1, 1, 1, N], issue_token = True) out_task = shim_dma_single_bd_task(of_out, outTensor, sizes =[1, 1, 1, N], issue_token = True)
+        @runtime_sequence(vector_ty, vector_ty, vector_ty)
+        def sequence(inTensor, outTensor, notUsed):
+            if trace_size > 0:
+                trace_utils.configure_packet_tracing_aie2(
+                    tiles_to_trace=tiles_to_trace,
+                    shim=ShimTile,
+                    trace_size=trace_size,
+                )
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        dma_start_task(in_task, out_task) dma_await_task(in_task, out_task)
+            in_task = shim_dma_single_bd_task(
+                of_in, inTensor, sizes=[1, 1, 1, N], issue_token=True
+            )
+            out_task = shim_dma_single_bd_task(
+                of_out, outTensor, sizes=[1, 1, 1, N], issue_token=True
+            )
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      trace_utils.gen_trace_done_aie2(ShimTile)
+            dma_start_task(in_task, out_task)
+            dma_await_task(in_task, out_task)
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          if len (sys.argv) < 4:
+            trace_utils.gen_trace_done_aie2(ShimTile)
+
+
+if len(sys.argv) < 4:
     raise ValueError("[ERROR] Need at least 4 arguments (dev, in1_size, out_size)")
 
 
