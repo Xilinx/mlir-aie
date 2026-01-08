7a8769e4375547844f8f9f75f4f836ec9cdd59f2
Erika Hunhoff <erika.hunhoff@amd.com>
2026-01-08 12:10:09 -0700

    fixup year

--- python/utils/hostruntime/__init__.py
+++ python/utils/hostruntime/__init__.py
@@ -4,7 +4,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# (c) Copyright 2025 Advanced Micro Devices, Inc.
+# (c) Copyright 2026 Advanced Micro Devices, Inc.
 from ml_dtypes import bfloat16
 import numpy as np
 from .tensor_class import Tensor
