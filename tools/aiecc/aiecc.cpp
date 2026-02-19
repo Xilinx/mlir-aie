7c4e912fe6efae2ce30dd9ad78d7b9272ec05842
Joseph Melber <jgmelber@gmail.com>
2026-02-18 23:47:01 -0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/aiecc/aiecc.cpp
+++ tools/aiecc/aiecc.cpp
@@ -566,19 +566,28 @@ buildInputWithAddressesPipeline(StringRef aieTarget = "aie2") {
   std::ostringstream oss;
   oss << "builtin.module("
       << "convert-vector-to-aievec{aie-target=" << aieTarget.lower()
-      << " target-backend=llvmir}," << "lower-affine,"
-      << "aie-canonicalize-device," << "aie.device(" << "aie-assign-lock-ids,"
-      << "aie-register-objectFifos," << "aie-objectFifo-stateful-transform{"
+      << " target-backend=llvmir},"
+      << "lower-affine,"
+      << "aie-canonicalize-device,"
+      << "aie.device("
+      << "aie-assign-lock-ids,"
+      << "aie-register-objectFifos,"
+      << "aie-objectFifo-stateful-transform{"
       << "dynamic-objFifos=" << (dynamicObjFifos ? "true" : "false")
       << " packet-sw-objFifos=" << (packetSwObjFifos ? "true" : "false") << "},"
-      << "aie-assign-bd-ids," << "aie-lower-cascade-flows,"
-      << "aie-lower-broadcast-packet," << "aie-lower-multicast,"
+      << "aie-assign-bd-ids,"
+      << "aie-lower-cascade-flows,"
+      << "aie-lower-broadcast-packet,"
+      << "aie-lower-multicast,"
       << "aie-assign-tile-controller-ids,"
       << "aie-generate-column-control-overlay{route-shim-to-tile-ctrl="
       << (ctrlPktOverlay ? "true" : "false") << "},"
       << "aie-assign-buffer-addresses{alloc-scheme=" << allocScheme.getValue()
-      << "}," << "aie-vector-transfer-lowering{max-transfer-rank=1}" << "),"
-      << "convert-scf-to-cf" << ")";
+      << "},"
+      << "aie-vector-transfer-lowering{max-transfer-rank=1}"
+      << "),"
+      << "convert-scf-to-cf"
+      << ")";
   return oss.str();
 }
 
@@ -591,10 +600,17 @@ static std::string buildLLVMLoweringPipeline(StringRef deviceName,
       << "aie-standard-lowering{device=" << deviceName.str() << "},"
       << "aiex-standard-lowering,"
       << "convert-aievec-to-llvm{aie-target=" << aieTarget.lower() << "},"
-      << "canonicalize," << "cse," << "expand-strided-metadata,"
-      << "lower-affine," << "arith-expand," << "finalize-memref-to-llvm,"
+      << "canonicalize,"
+      << "cse,"
+      << "expand-strided-metadata,"
+      << "lower-affine,"
+      << "arith-expand,"
+      << "finalize-memref-to-llvm,"
       << "convert-func-to-llvm{use-bare-ptr-memref-call-conv=true},"
-      << "convert-to-llvm{dynamic=true}," << "canonicalize," << "cse" << ")";
+      << "convert-to-llvm{dynamic=true},"
+      << "canonicalize,"
+      << "cse"
+      << ")";
   return oss.str();
 }
 
