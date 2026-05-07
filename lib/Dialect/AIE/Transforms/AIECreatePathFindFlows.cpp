49261e706d46034739f70756a6ffb6999d20184e
Erika Hunhoff <erika.hunhoff@amd.com>
2026-05-07 07:54:35 -0600

    Update lib/Dialect/AIE/Transforms/AIECreatePathFindFlows.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/Dialect/AIE/Transforms/AIECreatePathFindFlows.cpp
+++ lib/Dialect/AIE/Transforms/AIECreatePathFindFlows.cpp
@@ -303,8 +303,8 @@ AIEPathfinderPass::runOnPacketFlow(DeviceOp device, OpBuilder &builder,
     for (Operation &Op : b.getOperations()) {
       if (auto pktSource = dyn_cast<PacketSourceOp>(Op)) {
         auto srcTile = dyn_cast<TileOp>(pktSource.getTile().getDefiningOp());
-        sources.push_back({{srcTile.colIndex(), srcTile.rowIndex()},
-                           pktSource.port()});
+        sources.push_back(
+            {{srcTile.colIndex(), srcTile.rowIndex()}, pktSource.port()});
       }
     }
     if (sources.empty())
