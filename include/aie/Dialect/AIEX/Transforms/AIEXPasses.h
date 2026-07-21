f231b2477ec3b1fd79650505cbb27bb6a01bedcf
Taimuraz Kaitmazov <atassikay38@gmail.com>
2026-07-21 17:55:46 +0300

    [AIEX] Drop duplicate createAIELowerSetLockPass declaration
    AIEXPasses.h declared createAIELowerSetLockPass() twice. The second is
    redundant; removing it, per review.

--- include/aie/Dialect/AIEX/Transforms/AIEXPasses.h
+++ include/aie/Dialect/AIEX/Transforms/AIEXPasses.h
@@ -56,7 +56,6 @@ std::unique_ptr<mlir::OperationPass<AIE::DeviceOp>>
 createAIELowerCoreResetPass();
 std::unique_ptr<mlir::OperationPass<AIE::DeviceOp>>
 createAIETransformBfpTypesPass();
-std::unique_ptr<mlir::OperationPass<AIE::DeviceOp>> createAIELowerSetLockPass();
 std::unique_ptr<mlir::OperationPass<AIE::DeviceOp>>
 createAIETxnToControlPacketPass();
 std::unique_ptr<mlir::OperationPass<AIE::DeviceOp>>
