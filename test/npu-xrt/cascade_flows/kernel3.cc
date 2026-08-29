8957c95ac10a4dafead5a2b563f74aa95d1b4d20
Taimuraz Kaitmazov <atassikay38@gmail.com>
2026-08-30 00:16:37 +0300

    test: build the cascade and tile-mapped tests with Peano

--- test/npu-xrt/cascade_flows/kernel3.cc
+++ test/npu-xrt/cascade_flows/kernel3.cc
@@ -17,10 +17,10 @@
 extern "C" {
 
 void extern_kernel3(int32_t *restrict buf, int N) {
-  v32int32 v32 = get_scd_v32int32();
+  v16int32 lo = extract_v16int32(get_scd_v32int32(), 0);
   for (int i = 0; i < N; i++) {
     if (i == 5) {
-      buf[i] = ext_elem(v32, 0) + 100;
+      buf[i] = ext_elem(lo, 0) + 100;
     } else {
       buf[i] = 0;
     }
