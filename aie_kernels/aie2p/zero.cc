38afaeecb8fc14d70bd104c93bb56af7ea29d4f3
Taimuraz Kaitmazov <taimuraz@kaitmazov.com>
2026-09-08 00:51:51 +0300

    aie_kernels: give each zero.cc a path-scoped include guard

--- aie_kernels/aie2p/zero.cc
+++ aie_kernels/aie2p/zero.cc
@@ -5,8 +5,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef ZERO_CC
-#define ZERO_CC
+#ifndef AIE_KERNELS_AIE2P_ZERO_CC
+#define AIE_KERNELS_AIE2P_ZERO_CC
 
 #include <aie_api/aie.hpp>
 #include <stdint.h>
