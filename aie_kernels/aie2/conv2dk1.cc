25e252c1750bea92ec0bde886e7730cfa08e0c65
Erika Hunhoff <erika.hunhoff@amd.com>
2026-09-19 12:28:40 -0600

    Update aie_kernels/aie2/conv2dk1.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- aie_kernels/aie2/conv2dk1.cc
+++ aie_kernels/aie2/conv2dk1.cc
@@ -145,7 +145,8 @@ void conv2dk1_ui8_scalar(uint8_t *input, int8_t *kernels, uint8_t *output,
 void conv2dk1_i8_vector(int8_t *input, int8_t *kernels, uint8_t *output,
                         const int32_t runtime_input_width,
                         const int32_t runtime_input_channels,
-                        const int32_t runtime_output_channels, const int scale) {
+                        const int32_t runtime_output_channels,
+                        const int scale) {
   const int32_t input_width = CONV_INPUT_WIDTH;
   const int32_t input_channels = CONV_INPUT_CHANNELS;
   const int32_t output_channels = CONV_OUTPUT_CHANNELS;
